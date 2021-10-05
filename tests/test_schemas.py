e9319280fb9369f32db2f7bc9aa21fd6bb9d881c
Nicolas Vivet <nicolas.vivet@datadoghq.com>
2021-10-05 14:28:13 +0200

    Skip schema validation for .NET and java

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -4,12 +4,20 @@ from utils import BaseTestCase, interfaces
 
 
 class Test_Library(BaseTestCase):
+
+    @skipif(context.library <= "java@0.87.0", reason="missing feature")
+    @skipif(context.library > "java@0.87.0", reason="known bug in the http context")
+    @skipif(context.library == "dotnet", reason="known bug in the http context")
     def test_library_format(self):
         """Libraries's payload are valid regarding schemas"""
         interfaces.library.assert_schemas()
 
 
 class Test_Agent(BaseTestCase):
+
+    @skipif(context.library <= "java@0.87.0", reason="missing feature")
+    @skipif(context.library > "java@0.87.0", reason="known bug in the http context")
+    @skipif(context.library == "dotnet", reason="known bug in the http context")
     def test_agent_format(self):
         """Agents's payload are valid regarding schemas"""
         interfaces.agent.assert_schemas()
