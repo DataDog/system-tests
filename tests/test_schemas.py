eb054652374810862ad857723b8a0327ef494b96
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2021-11-26 18:29:17 +0100

    Tmp

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -4,7 +4,7 @@
 
 """Test format specifications"""
 
-from utils import BaseTestCase, interfaces, bug
+from utils import BaseTestCase, interfaces, bug, context
 
 
 class Test_Library(BaseTestCase):
@@ -13,11 +13,26 @@ class Test_Library(BaseTestCase):
     @bug(library="java")
     @bug(library="dotnet", reason="APPSEC-1698")
     @bug(library="golang")
-    def test_library_format(self):
+    def test_full(self):
         # send some requests to be sure to trigger events
         self.weblog_get("/waf", params={"key": "\n :"})  # rules.http_protocol_violation.crs_921_160
 
         interfaces.library.assert_schemas()
+
+    def test_non_regression(self):
+        """ Non-regression test on shemas """
+
+        # Never skip this test. As a full respect of shemas may be hard, this test ensure that
+        # at least the part that was ok stays ok.
+
+        allowed_errors = None
+        if context.library == "golang":
+            allowed_errors = (
+                r"'actor' is a required property on instance \['events'\]\[\d+\]\['context'\]",
+                r"'protocol_version' is a required property on instance ",
+            )
+
+        interfaces.library.assert_schemas(allowed_errors=allowed_errors)
 
 
 class Test_Agent(BaseTestCase):
@@ -32,3 +47,18 @@ class Test_Agent(BaseTestCase):
         self.weblog_get("/waf", params={"key": "\n :"})  # rules.http_protocol_violation.crs_921_160
 
         interfaces.agent.assert_schemas()
+
+    def test_non_regression(self):
+        """ Non-regression test on shemas """
+
+        # Never skip this test. As a full respect of shemas may be hard, this test ensure that
+        # at least the part that was ok stays ok.
+
+        allowed_errors = None
+        if context.library == "golang":
+            allowed_errors = (
+                r"'actor' is a required property on instance \['events'\]\[\d+\]\['context'\]",
+                r"'protocol_version' is a required property on instance ",
+            )
+
+        interfaces.agent.assert_schemas(allowed_errors=allowed_errors)
