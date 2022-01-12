d97c6cea9d38619b3c8f56b2f0cee9c27bd6703d
Julio Guerra <julio@datadog.com>
2022-01-12 16:31:32 +0100

    go: fix v1.35.0 test constraints

--- tests/appsec/test_versions.py
+++ tests/appsec/test_versions.py
@@ -5,45 +5,13 @@
 from utils import BaseTestCase, context, interfaces, released, rfc, irrelevant, missing_feature
 
 
-@released(golang="1.35.0", java="0.90.0", nodejs="2.0.0rc0", python="?", ruby="?")
+@released(golang="1.34.0", java="0.90.0", nodejs="2.0.0rc0", python="?", ruby="?")
 @irrelevant(library="cpp")
 class Test_Events(BaseTestCase):
     """AppSec events uses version 1.0 (legacy appsec events on dedicated entry point)"""
 
-    @irrelevant(context.library != "golang")
-    def test_0_1(self):
-        """ Check sends event using legacy /appsec/proxy/api/v2/appsecevts 0.1.0"""
-
-        def validator(event):
-            assert event["event_version"] == "0.1.0", f"event version should be 0.1.0, not {event['event_version']}"
-
-            return True
-
-        def new_validator(span, event):
-            raise Exception("You are using AppSec in traces, you can skip this test")
-
-        interfaces.library.add_appsec_validation(legacy_validator=validator, validator=new_validator)
-
-    @irrelevant(library="golang")
-    @irrelevant(library="php")
-    @irrelevant(library="dotnet")
-    @irrelevant(library="nodejs")
-    @irrelevant(context.library >= "java@0.93.0")
-    def test_1_0(self):
-        """ Check sends event using legacy /appsec/proxy/api/v2/appsecevts 1.0.0"""
-
-        def validator(event):
-            assert event["event_version"] == "1.0.0", f"event version should be 1.0.0, not {event['event_version']}"
-
-            return True
-
-        def new_validator(span, event):
-            raise Exception("You are using AppSec in traces, you can skip this test")
-
-        interfaces.library.add_appsec_validation(legacy_validator=validator, validator=new_validator)
-
-    @missing_feature(library="golang")
     @missing_feature(context.library < "java@0.93.0")
+    @missing_feature(context.library < "golang@1.36.0")
     def test_appsec_in_traces(self):
         """ AppSec sends event in traces"""
 
