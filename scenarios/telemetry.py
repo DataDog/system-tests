bd34e4931da58a9f1dbdbaf631cd1eae604e307a
paullegranddc <paul.legranddescloizeaux@datadoghq.com>
2022-01-28 16:36:25 +0100

    Relax the telemetry message ordering test

--- scenarios/telemetry.py
+++ scenarios/telemetry.py
@@ -1,3 +1,5 @@
+from time import time
+
 from utils import context, BaseTestCase, interfaces
 from utils.interfaces._core import BaseValidation
 from utils.warmups import default_warmup
@@ -21,19 +23,60 @@ class TelemetryRequestSuccessValidation(BaseValidation):
             self.set_failure(f"Got response code {repsonse_code} telemetry message {data['log_filename']}")
 
 
-class SeqIdValidation(BaseValidation):
+class SeqIdLatencyValidation(BaseValidation):
+    """Verify that the messages seq_id s are sent somewhat in-order."""
+
+    MAX_OUT_OF_ORDER_LAG = 0.1  # s
     path_filters = TELEMETRY_AGENT_ENDPOINT
     is_success_on_expiry = True
 
     def __init__(self, message=None, request=None):
         super().__init__(message=message, request=request)
-        self.seq_id = 1
+        self.max_seq_id = 0
+        self.received_max_time = None
 
     def check(self, data):
-        actual_seq_id = data["request"]["content"]["seq_id"]
-        if actual_seq_id != self.seq_id:
-            self.set_failure(f"Expected seq_id {self.seq_id} got {actual_seq_id}")
-        self.seq_id += 1
+        seq_id = data["request"]["content"]["seq_id"]
+        now = time()
+        if seq_id > self.max_seq_id:
+            self.max_seq_id = seq_id
+            self.received_max_time = now
+        else:
+            if self.received_max_time is None:
+                return
+            elif (now - self.received_max_time) > self.MAX_OUT_OF_ORDER_LAG:
+                self.set_failure(
+                    f"Received message with seq_id {seq_id} to far more than"
+                    f"100ms after message with seq_id {self.max_seq_id}"
+                )
+
+
+class SeqIdsAreConsecutive(BaseValidation):
+    """Verify that the messages seq_id s are sent somewhat in-order."""
+
+    path_filters = TELEMETRY_AGENT_ENDPOINT
+    is_success_on_expiry = True
+
+    def __init__(self, message=None, request=None):
+        super().__init__(message=message, request=request)
+        self.seq_ids = []
+
+    def check(self, data):
+        seq_id = data["request"]["content"]["seq_id"]
+        self.seq_ids.append((seq_id, data["log_filename"]))
+
+    def final_check(self):
+        self.seq_ids.sort()
+        for i in range(len(self.seq_ids) - 1):
+            diff = self.seq_ids[i + 1][0] - self.seq_ids[i][0]
+            if diff == 0:
+                self.set_failure(
+                    f"Detected 2 telemetry messages with same seq_id {self.seq_ids[i + 1][1]} and {self.seq_ids[i][1]}"
+                )
+            elif diff > 1:
+                self.set_failure(
+                    f"Detected non conscutive seq_ids between {self.seq_ids[i + 1][1]} and {self.seq_ids[i][1]}"
+                )
 
 
 class TelemetryProxyValidation(BaseValidation):
@@ -102,6 +145,21 @@ class AppStartedLibraryValidation(BaseValidation):
             self.set_status(True)
 
 
+class IntegrationChangedValidation(BaseValidation):
+    path_filters = TELEMETRY_AGENT_ENDPOINT
+    is_success_on_expiry = True
+
+    def __init__(self, message=None, request=None):
+        super().__init__(message=message, request=request)
+
+    def check(self, data):
+        content = data["request"]["content"]
+        if content["request_type"] != "app-integrations-change":
+            return
+        if not content["payload"]["integrations"]:
+            self.set_failure(f"Empty integration changes sent in {data['log_filename']}")
+
+
 class Test_Telemetry(BaseTestCase):
     """Test that instrumentation telemetry is sent"""
 
@@ -117,11 +175,16 @@ class Test_Telemetry(BaseTestCase):
 
     def test_seq_id(self):
         """Test that messages are sent sequentially"""
-        interfaces.library.append_validation(SeqIdValidation())
+        interfaces.library.append_validation(SeqIdLatencyValidation())
+        interfaces.library.append_validation(SeqIdsAreConsecutive())
 
     def test_app_started(self):
         """Request type app-started is sent on startup"""
         interfaces.library.append_validation(AppStartedLibraryValidation())
+    
+    def test_integrations_change(self):
+        """Request type integrations-change have non empty list of changes"""
+        interfaces.library.append_validation(IntegrationChangedValidation())
 
     def test_proxy_forwarding(self):
         """Test that the telemetry proxy forwards messages correctly"""
