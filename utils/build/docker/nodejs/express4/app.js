d7a4c4884bbbeb376242304f63a1eaecfdcee978
simon-id <simon.id@datadoghq.com>
2022-02-08 15:01:59 +0100

    add new routes

--- utils/build/docker/nodejs/express4/app.js
+++ utils/build/docker/nodejs/express4/app.js
@@ -21,6 +21,20 @@ app.get('/sample_rate_route/:i', (req, res) => {
   res.send('OK');
 });
 
+app.get('/params/:value', (req, res) => {
+  res.send('OK')
+})
+
+app.get('/headers', (req, res) => {
+  res.set({
+    'content-type': 'text/plain',
+    'content-length': '42',
+    'content-language': 'en-US',
+  })
+
+  res.send('Hello, headers!')
+});
+
 app.listen(7777, '0.0.0.0', () => {
   tracer.trace('init.service', () => {});
   console.log('listening');
