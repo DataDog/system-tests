a2fa4332272e2efc67eb140c179db48e761544e4
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-02-10 01:31:54 +0100

    Update to dd-trace-rb 1.0 tip

--- utils/build/docker/ruby/install_ddtrace.sh
+++ utils/build/docker/ruby/install_ddtrace.sh
@@ -8,8 +8,8 @@ if [ -e "/binaries/dd-trace-rb" ]; then
     echo "gem 'ddtrace', require: 'ddtrace/auto_instrument', path: '/binaries/dd-trace-rb'" >> Gemfile
 elif [ $(ls /binaries/ruby-load-from-bundle-add | wc -l) = 0 ]; then
     echo "Install prod version"
-    gem install --source 'https://s3.amazonaws.com/gems.datadoghq.com/prerelease-v2' ddtrace -v '1.0.0.1.0.202209'
-    echo "gem 'ddtrace', '1.0.0.1.0.203435'" >> Gemfile
+    gem install --source 'https://s3.amazonaws.com/gems.datadoghq.com/prerelease-v2' ddtrace -v '1.0.0.1.0.205324'
+    echo "gem 'ddtrace', '1.0.0.1.0.205324'" >> Gemfile
 else
     options=$(cat /binaries/ruby-load-from-bundle-add)
     echo "Install from $options"
