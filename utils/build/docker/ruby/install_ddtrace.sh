069dee2b71be06de1f1644a44b76c6ce69247071
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-02-16 01:39:35 +0100

    Use actual 1.0.0.beta1 from rubygems.org

--- utils/build/docker/ruby/install_ddtrace.sh
+++ utils/build/docker/ruby/install_ddtrace.sh
@@ -8,8 +8,7 @@ if [ -e "/binaries/dd-trace-rb" ]; then
     echo "gem 'ddtrace', require: 'ddtrace/auto_instrument', path: '/binaries/dd-trace-rb'" >> Gemfile
 elif [ $(ls /binaries/ruby-load-from-bundle-add | wc -l) = 0 ]; then
     echo "Install prod version"
-    gem install --source 'https://s3.amazonaws.com/gems.datadoghq.com/prerelease-v2' ddtrace -v '1.0.0.master.208050'
-    echo "gem 'ddtrace', '1.0.0.master.208050', require: 'ddtrace/auto_instrument'" >> Gemfile
+    echo "gem 'ddtrace', '~> 1.0.0.beta1', require: 'ddtrace/auto_instrument'" >> Gemfile
 else
     options=$(cat /binaries/ruby-load-from-bundle-add)
     echo "Install from $options"
