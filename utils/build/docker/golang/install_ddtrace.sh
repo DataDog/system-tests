6a80d1cbb3f0348760c1710aebcab84913ffe511
Julio Guerra <julio@datadog.com>
2022-03-02 22:54:32 +0100

    go: automated rule file version

--- utils/build/docker/golang/install_ddtrace.sh
+++ utils/build/docker/golang/install_ddtrace.sh
@@ -20,17 +20,18 @@ go mod tidy
 # Read the library version out of the version.go file
 mod_dir=$(go list -f '{{.Dir}}' -m gopkg.in/DataDog/dd-trace-go.v1)
 version=$(sed -nrE 's#.*"v(.*)".*#\1#p' $mod_dir/internal/version/version.go) # Parse the version string content "v.*"
-echo $version > /app/SYSTEM_TESTS_LIBRARY_VERSION
+echo $version > SYSTEM_TESTS_LIBRARY_VERSION
 
 touch SYSTEM_TESTS_LIBDDWAF_VERSION
 
 # Read the rule file version
 if [[ $version =~ v1.36 ]]; then
-    echo "1.2.5" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
+    rules_version="1.2.5"
 else
     # Parse the appsec rules version string out of the inlined rules json
     rules_version=$(sed -nrE 's#.*rules_version\\\\":\\\\"([[:digit:]\.-]+)\\\\".*#\1#p' $mod_dir/internal/appsec/rule.go)
 fi
+echo $rules_version > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
 
 echo "dd-trace-go version: $(cat /app/SYSTEM_TESTS_LIBRARY_VERSION)"
 echo "rules version: $(cat /app/SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION)"
