0f53cb7718edd817e8952f69339a3afe710b15eb
Julio Guerra <julio@datadog.com>
2022-03-02 22:33:25 +0100

    go: automated rule file version

--- utils/build/docker/golang/install_ddtrace.sh
+++ utils/build/docker/golang/install_ddtrace.sh
@@ -7,9 +7,9 @@ if [ -e "/binaries/dd-trace-go" ]; then
     go mod edit -replace gopkg.in/DataDog/dd-trace-go.v1=/binaries/dd-trace-go
     # Read from the version file directly because go list cannot know the local
     # directory version
-    version_file=$(go list -f '{{.Dir}}' -m gopkg.in/DataDog/dd-trace-go.v1)/internal/version/version.go
-    # Parse and print the version string content "v.*"
-    version=$(sed -nrE 's#.*"v(.*)".*#\1#p' $version_file)
+    mod_dir=$(go list -f '{{.Dir}}' -m gopkg.in/DataDog/dd-trace-go.v1)
+    # Parse the version string content "v.*"
+    version=$(sed -nrE 's#.*"v(.*)".*#\1#p' $mod_dir/internal/version/version.go)
 
 elif [ -e "/binaries/golang-load-from-go-get" ]; then
     echo "Install from go get -d $(cat /binaries/golang-load-from-go-get)"
@@ -24,14 +24,14 @@ fi
 
 go mod tidy
 
-echo $version > /app/SYSTEM_TESTS_LIBRARY_VERSION
+#echo $version > /app/SYSTEM_TESTS_LIBRARY_VERSION
 touch SYSTEM_TESTS_LIBDDWAF_VERSION
-if [[ $version >= "1.37.0" ]]; then
+if [[ $version =~ v1.36 ]]; then
+    echo "1.2.5" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
+else
     # Parse the appsec rules version string out of the inlined rules json
     mod_dir=$(go list -f '{{.Dir}}' -m gopkg.in/DataDog/dd-trace-go.v1)
     rules_version=$(sed -nrE 's#.*rules_version\\\\":\\\\"([[:digit:]\.-]+)\\\\".*#\1#p' $mod_dir/internal/appsec/rule.go)
-else
-    echo "1.2.5" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
 fi
 
 echo "dd-trace-go version: $(cat /app/SYSTEM_TESTS_LIBRARY_VERSION)"
