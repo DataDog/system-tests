2884119b56cd923b407a02fe450ca846d3875d82
simon-id <simon.id@datadoghq.com>
2022-03-03 14:07:05 +0100

    Better node install script (#208)

--- utils/build/docker/nodejs/install_ddtrace.sh
+++ utils/build/docker/nodejs/install_ddtrace.sh
@@ -2,26 +2,27 @@
 
 set -eu
 
-cd /binaries
+cd /usr/app
 
-if [ -e "/binaries/dd-trace-js" ]; then
-    echo "Install from local folder /binaries/dd-trace-js"
-    cd /usr/app
-    npm install "$(npm pack /binaries/dd-trace-js)"
+if [ -e /binaries/nodejs-load-from-npm ]; then
+    target=$(</binaries/nodejs-load-from-npm)
+    echo "install from: $target"
 
-elif [ $(ls nodejs-load-from-npm | wc -l) = 0 ]; then
-    echo "install from NPM"
-    cd /usr/app
-    npm install dd-trace
+elif [ -e /binaries/dd-trace-js ]; then
+    target=$(npm pack /binaries/dd-trace-js)
+    echo "install from local folder /binaries/dd-trace-js"
 
 else
-    echo "install from $(cat nodejs-load-from-npm)"
-    cd /usr/app
-    npm install "$(cat /binaries/nodejs-load-from-npm)"
+    target="dd-trace"
+    echo "install from NPM"
 fi
 
-npm list | grep dd-trace | sed 's/.*@//' | sed 's/ .*//'> /usr/app/SYSTEM_TESTS_LIBRARY_VERSION
-touch SYSTEM_TESTS_LIBDDWAF_VERSION
-echo "1.2.5" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
+npm install $target
 
-echo "dd-trace version: $(cat /usr/app/SYSTEM_TESTS_LIBRARY_VERSION)"
+npm list --json | jq -r '.dependencies."dd-trace".version' > SYSTEM_TESTS_LIBRARY_VERSION
+npm explore @datadog/native-appsec -- cat package.json | jq -r '.libddwaf_version' > SYSTEM_TESTS_LIBDDWAF_VERSION
+npm explore dd-trace -- cat packages/dd-trace/src/appsec/recommended.json | jq -r '.metadata.rules_version // "1.2.5"' > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
+
+echo "dd-trace version: $(cat SYSTEM_TESTS_LIBRARY_VERSION)"
+echo "libddwaf version: $(cat SYSTEM_TESTS_LIBDDWAF_VERSION)"
+echo "rules version: $(cat SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION)"
