c9c5b45cd17a905ba28f36a0c39870477edb78e3
Adrian Menendez Valle <adrian.menendezvalle@datadoghq.com>
2022-03-04 17:48:51 +0100

    Add basic test

---
+++ tests/test_miscs.py
@@ -0,0 +1,21 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2022 Datadog, Inc.
+
+from utils import BaseTestCase, interfaces, missing_feature
+
+
+@missing_feature(library="cpp")
+@missing_feature(library="golang")
+@missing_feature(library="nodejs")
+@missing_feature(library="java")
+@missing_feature(library="php")
+@missing_feature(library="python")
+@missing_feature(library="ruby")
+class Test_Basic(BaseTestCase):
+    """ Make sure the spans endpoint is successful """
+
+    def test_spans_generation(self):
+        r = self.weblog_get("/spans")
+        assert r.status_code == 200
+        interfaces.library.assert_trace_exists(r)
