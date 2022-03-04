bf2f4afb318b2098f65001f44643321662310ad9
Adrian Menendez Valle <adrian.menendezvalle@datadoghq.com>
2022-03-04 15:59:50 +0100

    Add endpoint to generate lots of spans

---
+++ utils/build/docker/dotnet/Helper.cs
@@ -0,0 +1,32 @@
+using Datadog.Trace;
+using System;
+using System.Linq;
+using System.Threading;
+
+
+namespace weblog
+{
+    public class Helper
+    {
+        private static Random random = new Random();
+        private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public void GenerateSpan(int garbageTags)
+        {
+            using (var childScope = Tracer.Instance.StartActive("spans.child"))
+            {
+                childScope.Span.ResourceName = "span_" + System.Guid.NewGuid().ToString();
+                for (int i = 0; i < garbageTags; i++)
+                {
+                    childScope.Span.SetTag("garbage" + i, RandomString(50));
+                }
+            }
+        }
+
+        private static string RandomString(int length)
+        {
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
--- utils/build/docker/dotnet/Startup.cs
+++ utils/build/docker/dotnet/Startup.cs
@@ -12,6 +12,8 @@ namespace weblog
 {
     public class Startup
     {
+        private static Helper helper = new Helper();
+
         private static void SetupDatabase()
         {
             string script = @"
@@ -96,6 +98,29 @@ END
                         var value = reader["Value"]?.ToString();
                         await context.Response.WriteAsync(value + Environment.NewLine);
                     }
+                });
+
+                endpoints.MapGet("/spans", async context =>
+                {
+                    int repeats = 1;
+                    var repeatsStr = context.Request.Query["repeats"];
+                    if (!String.IsNullOrEmpty(repeatsStr)) {
+                        repeats = Int32.Parse(repeatsStr);
+                    }
+
+                    int garbageTags = 1;
+                    var garbageStr = context.Request.Query["garbage"];
+                    if (!String.IsNullOrEmpty(garbageStr)) {
+                        garbageTags = Int32.Parse(garbageStr);
+                    }
+
+                    for (int i = 0; i < repeats; i++)
+                    {
+                        helper.GenerateSpan(garbageTags);
+                    }
+                                        
+                    await context.Response.WriteAsync(
+                        String.Format("Generated {0} spans with {1} garbage tags\n", repeats, garbageTags));
                 });
 
 
