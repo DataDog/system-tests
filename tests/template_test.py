8f7dbfecf01bb151d25f912582bcddf54a9fa85c
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-03-10 16:23:48 -0500

    disable black

--- tests/template_test.py
+++ tests/template_test.py
@@ -1,3 +1,6 @@
+# fmt: off
+# Remove the above line from your test, this is to disable linting for this file
+
 # Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
