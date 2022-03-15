ceb55329d474813b144c66106969e663e892344a
François Mazeau <francois.mazeau@datadoghq.com>
2022-03-15 17:49:47 +0100

    appsec: add identify test & enable for go

--- tests/appsec/test_identify.py
+++ tests/appsec/test_identify.py
@@ -10,15 +10,30 @@ if context.library == "cpp":
     pytestmark = pytest.mark.skip("not relevant")
 
 
-@missing_feature(library="golang")
 @missing_feature(library="dotnet")
 @missing_feature(library="nodejs")
 @missing_feature(library="java")
 @missing_feature(library="php")
 @missing_feature(library="python")
 @missing_feature(library="ruby")
+@released(golang="1.37.0")
 class Test_Basic(BaseTestCase):
     """ Basic tests for Identify SDK """
 
-    def test_main(self):
-        interfaces.library.append_not_implemented_validation()
+    def test_indentify_tags(self):
+        def assertTagInSpanMeta(span, tag):
+            if tag not in span["meta"]:
+                raise Exception(f"Can't find {tag} in span's meta")
+
+            val = span["meta"][tag]
+            if val != tag:
+                raise Exception(f"{tag} value is '{val}', should be '{tag}'")
+
+        def validate_response_headers(span):
+            for tag in ["id", "name", "email", "session_id", "role", "scope"]:
+                assertTagInSpanMeta(span, f"usr.{tag}")
+            return True
+
+        # Send a random attack on the identify endpoint
+        r = self.weblog_get("/identify/", headers={"User-Agent": "Arachni/v1"})
+        interfaces.library.add_span_validation(r, validate_response_headers)
