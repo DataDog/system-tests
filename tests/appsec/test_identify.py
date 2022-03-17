21f5a6afd1326cf892135458e2f611e121a9c056
simon-id <simon.id@datadoghq.com>
2022-03-17 11:44:35 +0100

    set released version for identify test

--- tests/appsec/test_identify.py
+++ tests/appsec/test_identify.py
@@ -11,12 +11,12 @@ if context.library == "cpp":
 
 
 @missing_feature(library="dotnet")
-@missing_feature(library="nodejs")
 @missing_feature(library="java")
 @missing_feature(library="php")
 @missing_feature(library="python")
 @missing_feature(library="ruby")
 @released(golang="1.37.0")
+@released(nodejs="2.4.0")
 class Test_Basic(BaseTestCase):
     """ Basic tests for Identify SDK """
 
