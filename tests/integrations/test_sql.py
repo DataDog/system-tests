ba68835cb02220b97f5fe759e90d6a073a2ac041
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-03-30 18:02:54 -0400

    Revert test change

--- tests/integrations/test_sql.py
+++ tests/integrations/test_sql.py
@@ -5,7 +5,7 @@
 from utils import BaseTestCase, interfaces, context, irrelevant
 
 
-@irrelevant(context.library not in ["dotnet", "java"], reason="Need to build endpoint on weblog")
+@irrelevant(context.library != "java", reason="Need to build endpoint on weblog")
 class Test_Misc(BaseTestCase):
     """ Check that traces are reported for some services """
 
