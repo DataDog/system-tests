7bb76501ea7ad43aa921feaf3a6f81fa97606e08
Emile-Hugo SPIR <emile.spir@datadoghq.com>
2022-04-01 15:35:19 +0200

    Java only got the compatible WAF with 0.98

--- tests/appsec/waf/test_rules.py
+++ tests/appsec/waf/test_rules.py
@@ -298,7 +298,7 @@ class Test_NoSqli(BaseTestCase):
         r = self.weblog_get("/waf/", headers={"x-attack": "$nin"})
         interfaces.library.assert_waf_attack(r, rules.nosql_injection.sqr_000_007)
 
-    @missing_feature(context.library != "java", reason="Need to use last WAF version")
+    @missing_feature(context.library != "java", java < "0.96.0", reason="Need to use last WAF version")
     @irrelevant(context.appsec_rules_version < "1.3.0", reason="before 1.3.0, keys was not supported")
     def test_nosqli_keys(self):
         """AppSec catches NoSQLI attacks"""
