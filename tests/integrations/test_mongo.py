920e87bb678be4e2d5333269c619082d612668f1
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-04-04 10:01:17 -0400

    Formatting

--- tests/integrations/test_mongo.py
+++ tests/integrations/test_mongo.py
@@ -4,6 +4,7 @@
 
 from utils import BaseTestCase, interfaces, context, irrelevant
 
+
 @irrelevant(reason="Endpoint is unimplemented on weblog")
 class Test_Mongo(BaseTestCase):
     """ Verify that a mongodb span is created """
