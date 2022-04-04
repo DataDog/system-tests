d6cfd4ac2e048816cdbf6fe0766785bbe684d260
Colin Higgins <colin-higgins@users.noreply.github.com>
2022-04-04 11:09:14 -0400

    Better detail

--- tests/integrations/test_sql.py
+++ tests/integrations/test_sql.py
@@ -2,10 +2,11 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import BaseTestCase, interfaces, context, irrelevant
+from utils import BaseTestCase, interfaces, context, bug, missing_feature
 
 
-@irrelevant(reason="Endpoint is unimplemented on weblog")
+@bug(library="java", reason="Endpoint is probably improperly implemented on weblog")
+@missing_feature(condition=context.library != "java", reason="Endpoint is not implemented on weblog")
 class Test_Sql(BaseTestCase):
     """ Verify that a sql span is created """
 
