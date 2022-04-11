43bb438b480dc5f10c295b1ad11b49eff5a21cad
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2022-04-11 15:00:52 +0100

    Updated PHP AppSec version on actor.ip test

--- tests/appsec/test_reports.py
+++ tests/appsec/test_reports.py
@@ -37,7 +37,7 @@ class Test_StatusCode(BaseTestCase):
 
 
 @released(golang="1.36.0" if context.weblog_variant in ["echo", "chi"] else "1.34.0")
-@released(dotnet="1.30.0", java="0.98.1", nodejs="2.0.0", php_appsec="0.2.2", python="?")
+@released(dotnet="1.30.0", java="0.98.1", nodejs="2.0.0", php_appsec="0.3.0", python="?")
 @missing_feature(context.library == "ruby" and context.libddwaf_version is None)
 @missing_feature(context.library <= "golang@1.36.2" and context.weblog_variant == "gin")
 class Test_ActorIP(BaseTestCase):
