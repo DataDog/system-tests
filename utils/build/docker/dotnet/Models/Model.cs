2456431c07c44b102c12926032a9e61b3b4520fd
Anna <anna.yafi@datadoghq.com>
2022-04-13 14:14:33 +0200

    Change weblog waf endpoint so that it accepts POST request and expects a model

---
+++ utils/build/docker/dotnet/Models/Model.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weblog
+{
+    public class Model
+    {
+        public string Value { get; set; }
+        public string Value2 { get; set; }
+    }
+}
