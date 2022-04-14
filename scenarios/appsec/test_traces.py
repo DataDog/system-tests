906ec97944615962b4895f9e5127a51dbea72905
Julio Guerra <julio@datadog.com>
2022-04-14 16:17:29 +0200

    Add an obfuscator test on the matched parameter value

--- scenarios/appsec/test_traces.py
+++ scenarios/appsec/test_traces.py
@@ -16,12 +16,12 @@ if context.library == "cpp":
 class Test_AppSecObfuscator(BaseTestCase):
     """AppSec obfuscates sensitive data."""
 
-    def test_appsec_obfuscator(self):
+    def test_appsec_obfuscator_key(self):
         """General obfuscation test of several attacks on several rule addresses."""
         # Validate that the AppSec events do not contain the following secret value.
         # Note that this value must contain an attack pattern in order to be part of the security event data
         # that is expected to be obfuscated.
-        SECRET = "this is a very secret value having the .htaccess attack"
+        SECRET = "this is a very secret value having the attack"
 
         def validate_appsec_span_tags(span, appsec_data):
             if SECRET in span["meta"]["_dd.appsec.json"]:
@@ -29,9 +29,10 @@ class Test_AppSecObfuscator(BaseTestCase):
             return True
 
         r = self.weblog_get(
-            "/waf/", headers={"DD_API_TOKEN": SECRET}, cookies={"Bearer": SECRET}, params={"pwd": SECRET},
+            "/waf/", cookies={"Bearer": f"{SECRET} select pg_sleep"}, params={"pwd": f"{SECRET} o:3:\"d\":3:{{}}"},
         )
-        interfaces.library.assert_waf_attack(r)
+        interfaces.library.assert_waf_attack(r, address="server.request.cookies")
+        interfaces.library.assert_waf_attack(r, address="server.request.query")
         interfaces.library.add_appsec_validation(r, validate_appsec_span_tags)
 
     def test_appsec_obfuscator_cookies(self):
@@ -55,5 +56,5 @@ class Test_AppSecObfuscator(BaseTestCase):
         r = self.weblog_get(
             "/waf/", cookies={"Bearer": SECRET_VALUE_WITH_SENSITIVE_KEY, "Good": SECRET_VALUE_WITH_NON_SENSITIVE_KEY}
         )
-        interfaces.library.assert_waf_attack(r)
+        interfaces.library.assert_waf_attack(r, address="server.request.cookies")
         interfaces.library.add_appsec_validation(r, validate_appsec_span_tags)
