7ee5e357efd273a3f6d4c658afda003e45bc31ed
Vladimir de Turckheim <vlad2t@hotmail.com>
2022-04-27 16:56:19 +0200

    update python target version

--- tests/appsec/test_alpha.py
+++ tests/appsec/test_alpha.py
@@ -14,6 +14,7 @@ if context.library == "cpp":
 @released(golang="1.36.0" if context.weblog_variant in ["echo", "chi"] else "1.34.0")
 @released(dotnet="1.28.6", java="0.87.0")
 @released(nodejs="2.0.0", php_appsec="0.2.1")
+@released(python="1.1.0rc2")
 @missing_feature(context.library == "ruby" and context.libddwaf_version is None)
 @missing_feature(context.library <= "golang@1.36.2" and context.weblog_variant == "gin")
 class Test_Basic(BaseTestCase):
