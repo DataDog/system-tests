433821e3fd550a5c4db97b2fb07885fd97cb6201
Anna <anna.yafi@datadoghq.com>
2022-04-29 18:03:34 +0200

    update tests attributes

--- scenarios/appsec/test_traces.py
+++ scenarios/appsec/test_traces.py
@@ -12,7 +12,7 @@ if context.library == "cpp":
 
 
 @rfc("https://datadoghq.atlassian.net/wiki/spaces/APS/pages/2365948382/Sensitive+Data+Obfuscation")
-@released(golang="1.38.0", dotnet="?", java="0.100.0", nodejs="?", php_appsec="0.3.0", python="?", ruby="?")
+@released(golang="1.38.0", dotnet="2.7.0", java="0.100.0", nodejs="?", php_appsec="0.3.0", python="?", ruby="?")
 class Test_AppSecObfuscator(BaseTestCase):
     """AppSec obfuscates sensitive data."""
 
