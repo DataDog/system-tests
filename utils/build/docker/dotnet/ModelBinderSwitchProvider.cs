73210c5df50754115ab981d7c1fc87243b3d35a1
Anna <anna.yafi@datadoghq.com>
2022-05-11 16:13:18 +0200

    make all models deserializable without conflict

--- utils/build/docker/dotnet/ModelBinderSwitchProvider.cs
+++ utils/build/docker/dotnet/ModelBinderSwitchProvider.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using weblog.Models;
 
 namespace weblog.ModelBinders
 {
@@ -18,7 +19,7 @@ namespace weblog.ModelBinders
             }
 
             var subclasses = new[] { typeof(IEnumerable<string>), typeof(Model), typeof(Models.String), typeof(string) };
-
+            var validators = new Dictionary<Type, Func<bool>>();
             var binders = new Dictionary<Type, (ModelMetadata, IModelBinder)>();
             foreach (var type in subclasses)
             {
@@ -51,9 +52,18 @@ namespace weblog.ModelBinders
                 bindingContext.FieldName = string.Empty;
                 try
                 {
+                    bindingContext.HttpContext.Request.Body.Seek(0, SeekOrigin.Begin);
+                    bindingContext.ModelState.Clear();
                     await binder.BindModelAsync(bindingContext).ConfigureAwait(false);
                     if (bindingContext.Result != ModelBindingResult.Failed())
                     {
+                        if (bindingContext.Result.Model is IValidable validable)
+                        {
+                            if (!validable.IsValid())
+                            {
+                                continue;
+                            }
+                        }
                         // Setting the ValidationState ensures properties on derived types are correctly 
                         bindingContext.ValidationState[bindingContext.Result.Model] = new ValidationStateEntry
                         {
@@ -61,8 +71,6 @@ namespace weblog.ModelBinders
                         };
                         return;
                     }
-                    bindingContext.HttpContext.Request.Body.Seek(0, SeekOrigin.Begin);
-                    bindingContext.ModelState.Clear();
                 }
                 catch (Exception)
                 {
--- utils/build/docker/dotnet/Models/String.cs
+++ utils/build/docker/dotnet/Models/String.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -8,10 +9,12 @@ using System.Xml.Serialization;
 namespace weblog.Models
 {
     [XmlType(TypeName = "string")]
-    public class String
+    public class String : IValidable
     {
         [XmlAttribute("attack")]
-        public string Attack { get; set; }
+        public string? Attack { get; set; }
+
+        public bool IsValid() => !string.IsNullOrEmpty(Attack);
 
         public override string ToString() => $"model Models.String with property attack {Attack}";
     }
