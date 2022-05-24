a98bc52fd6b57cee883e27bec1a31a9e7d1cef88
simon-id <simon.id@datadoghq.com>
2022-05-24 15:41:38 +0200

    Update test_rules.py

--- tests/appsec/waf/test_rules.py
+++ tests/appsec/waf/test_rules.py
@@ -303,8 +303,6 @@ class Test_NoSqli(BaseTestCase):
         r = self.weblog_get("/waf/", params={"[$ne]": "value"})
         interfaces.library.assert_waf_attack(r, rules.nosql_injection.crs_942_290)
 
-    @irrelevant(context.appsec_rules_version < "1.3.0", reason="before 1.3.0, keys was not supported")
-    def test_nosqli_keys(self):
         """AppSec catches NoSQLI attacks"""
         r = self.weblog_get("/waf/", params={"$nin": "value"})
         interfaces.library.assert_waf_attack(r, rules.nosql_injection.sqr_000_007)
