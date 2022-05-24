1ed63512a8ccea12bb51e9d798eac3d579be6728
simon-id <simon.id@datadoghq.com>
2022-05-24 16:58:09 +0200

    Update test_rules.py

--- tests/appsec/waf/test_rules.py
+++ tests/appsec/waf/test_rules.py
@@ -295,7 +295,7 @@ class Test_NoSqli(BaseTestCase):
         r = self.weblog_get("/waf/", headers={"x-attack": "$nin"})
         interfaces.library.assert_waf_attack(r, rules.nosql_injection.sqr_000_007)
 
-    @missing_feature(context.library in ["golang", "php", "ruby"], reason="Need to use last WAF version")
+    @missing_feature(context.library in ["golang", "nodejs", "php", "ruby"], reason="Need to use last WAF version")
     @missing_feature(context.library < "java@0.96.0", reason="Was using a too old WAF version")
     @irrelevant(context.appsec_rules_version < "1.3.0", reason="before 1.3.0, keys was not supported")
     def test_nosqli_keys(self):
