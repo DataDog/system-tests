507107e093e14ed8307cfe095f8d61b55cf42969
Anna <anna.yafi@datadoghq.com>
2022-06-20 14:55:20 +0200

    reading from env variable

--- utils/build/docker/dotnet/Endpoints/IdentifyEndpoint.cs
+++ utils/build/docker/dotnet/Endpoints/IdentifyEndpoint.cs
@@ -1,3 +1,4 @@
+#if !DDTRACE_2_7_0_OR_GREATER
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Datadog.Trace;
@@ -5,46 +6,20 @@ using Datadog.Trace;
 
 namespace weblog
 {
-    public class IdentifyEndpoint : ISystemTestEndpoint
+    public partial class IdentifyEndpoint : ISystemTestEndpoint
     {
         public void Register(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder routeBuilder)
         {
             routeBuilder.MapGet("/identify", async context =>
             {
-
-#if DDTRACE_HAS_USERDETAILS
-                var userDetails = new UserDetails()
-                {
-                    Id = "usr.id",
-                    Email = "usr.email",
-                    Name = "usr.name",
-                    SessionId = "usr.session_id",
-                    Role = "usr.role",
-                    Scope = "usr.scope",
-                };
-                var scope = Tracer.Instance.ActiveScope;
-                scope?.Span.SetUser(userDetails);
-#endif
- 
-                await context.Response.WriteAsync("Hello world!\\n");
+                await context.Response.WriteAsync("Not implemented before 2.7.0\\n");
             });
 
             routeBuilder.MapGet("/identify-propagate", async context =>
             {
-
-#if DDTRACE_HAS_USERDETAILS
-                var userDetails = new UserDetails()
-                {
-                    Id = "usr.id",
-                    // TODO Needs new SDK version merging before enabling this next line
-                    // PropagateId = true
-                };
-                var scope = Tracer.Instance.ActiveScope;
-                scope?.Span.SetUser(userDetails);
-#endif
-
-                await context.Response.WriteAsync("Hello world!\\n");
+                await context.Response.WriteAsync("Not implemented yet\\n");
             });
         }
     }
 }
+#endif
