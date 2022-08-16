e2a8253c00ada44eb06c820c60b8f3d8bc0f38c4
Vladimir de Turckheim <vdeturckheim@users.noreply.github.com>
2022-08-16 13:44:31 +0200

    add pylons test app (#433)
    * add pylons test app
    
    * feat: updated pylons weblog project
    
    * feat: missing controller, fix service name
    
    * fix: increase python timeout 10 more seconds
    
    * feat: improve test_root_span_coherence test
    
    * fix: increase python expected_timeout
    
    * feat: update test_traces tests
    
    * feat(pylons): headers controller
    
    * feat: rename constant
    
    Co-authored-by: Alberto Vara <alberto.vara@datadoghq.com>
    Co-authored-by: Federico Mon <federico.mon@datadoghq.com>
    Co-authored-by: Pawel Chojnacki <pawel.chcki@gmail.com>

---
+++ utils/build/docker/python/pylons/app/app/tests/functional/test_waf.py
@@ -0,0 +1,7 @@
+from app.tests import *
+
+class TestWafController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='waf', action='index'))
+        # Test response...
