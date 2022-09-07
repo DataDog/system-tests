05243782fa7a39a3e7c6a7df5dc58ff9ba68a1ec
Alberto Vara <alberto.vara@datadoghq.com>
2022-09-07 11:34:59 +0200

    chore(refactor): set GA 1.1 constant

--- tests/appsec/test_identify.py
+++ tests/appsec/test_identify.py
@@ -2,15 +2,18 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import BaseTestCase, bug, context, interfaces, released, coverage
-import pytest
 import base64
+
+import pytest
+
+from tests.constants import PYTHON_RELEASE_GA_1_1
+from utils import BaseTestCase, bug, context, coverage, interfaces, released
 
 if context.library == "cpp":
     pytestmark = pytest.mark.skip("not relevant")
 
 
-@released(dotnet="2.7.0", golang="1.37.0", java="?", nodejs="2.4.0", php="0.72.0", python="1.5.0.dev", ruby="1.0.0")
+@released(dotnet="2.7.0", golang="1.37.0", java="?", nodejs="2.4.0", php="0.72.0", python=PYTHON_RELEASE_GA_1_1, ruby="1.0.0")
 @coverage.basic
 class Test_Basic(BaseTestCase):
     """Basic tests for Identify SDK for AppSec"""
