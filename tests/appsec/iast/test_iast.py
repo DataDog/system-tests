57627603a9bb3ca07a947f06204e623417b6ebb7
roberto montero <roberto.montero@datadoghq.com>
2022-09-09 12:06:37 +0200

    [IAST] Testing failures

--- tests/appsec/iast/test_iast.py
+++ tests/appsec/iast/test_iast.py
@@ -4,7 +4,7 @@
 
 from utils import BaseTestCase, interfaces, context, missing_feature, coverage, released
 
-# Weblog are ok for nodejs/express4 and java/spring-boot
+# Weblog are ok for nodejs/express4 and java/spring-boot. TESTING PR
 @coverage.basic
 @released(
     dotnet="?",
