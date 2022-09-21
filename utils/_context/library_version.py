54977b78f5cbf8250b80075ab694d3b7f73965f6
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-09-21 17:40:34 +0200

    Clean code again

--- utils/_context/library_version.py
+++ utils/_context/library_version.py
@@ -2,8 +2,8 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from packaging import version as version_module
 import re
+from packaging import version as version_module
 
 # some monkey patching
 def _parse_letter_version(letter, number):
@@ -21,7 +21,7 @@ def _parse_letter_version(letter, number):
     return None
 
 
-version_module._parse_letter_version = _parse_letter_version
+version_module._parse_letter_version = _parse_letter_version  # pylint: disable=protected-access
 
 RUBY_VERSION_PATTERN = r"""
     v?
@@ -97,10 +97,11 @@ class Version(version_module.Version):
     def build(cls, version, component):
         if isinstance(version, str):
             return cls(version, component)
-        elif isinstance(version, cls):
+
+        if isinstance(version, cls):
             return version
-        else:
-            raise TypeError(version)
+
+        raise TypeError(version)
 
     def __init__(self, version, component):
 
@@ -193,9 +194,9 @@ class LibraryVersion:
                 raise ValueError("Weblog does not provide an library version number")
 
             return self.library == library and self.version == version
-        else:
-            library = other
-            return self.library == library
+
+        library = other
+        return self.library == library
 
     def _extract_members(self, other):
         if not isinstance(other, str):
