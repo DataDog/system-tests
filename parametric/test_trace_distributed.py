9e1da67d3cc8f0d4af30fbed0a0a77fb77c53a26
ZStriker19 <zach.groves@datadoghq.com>
2022-10-05 17:26:02 -0400

    runs without exceptions but headers empty

--- parametric/test_trace_distributed.py
+++ parametric/test_trace_distributed.py
@@ -56,24 +56,17 @@ def test_distributed_headers_inject_datadog(test_agent, test_client):
     """Ensure that Datadog distributed tracing headers are injected properly.
     """
     with test_client:
-        # http_headers = DistributedHTTPHeaders()
-        # http_headers.traceparent_key = "traceparent"
-        # http_headers.traceparent_value = "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01"
-
         with test_client.start_span(
             name="name"
         ) as span:
-            # span.set_meta(key="http.status_code", val="200")
             headers = test_client.inject_headers()
-    # I think I should add headers and make span, then while that span is active try to inject its context
-    # Or we could create a span, inject the headers (need to see if we can pull the span id off of the span) and then we can just do the injection
-    # Then from there we can call get_span and then 
+
     print("lala traces")
-    print(test_agent.traces())
     span = get_span(test_agent)
-    print("rawr head")
+    print(span)
+    print("rawr headers")
     print(headers)
-    assert span.get("parent_id") == 123
+    assert span.get("trace_id") == 123
 
 
 
