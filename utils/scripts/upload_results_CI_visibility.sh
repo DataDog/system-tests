4bdada05a12b33284e0ed76a585bb21362a37add
roberto montero <roberto.montero@datadoghq.com>
2022-10-06 10:29:47 +0200

    absolute path for junit xml

--- utils/scripts/upload_results_CI_visibility.sh
+++ utils/scripts/upload_results_CI_visibility.sh
@@ -6,8 +6,7 @@ export DD_ENV='ci-system-tests-draft3'
 export DD_CIVISIBILITY_LOGS_ENABLED='1'
 export DD_CIVISIBILITY_AGENTLESS_ENABLED='1'
 
-
 #Download tool
 curl -L --fail "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_linux-x64" --output "$(pwd)/datadog-ci" && chmod +x $(pwd)/datadog-ci
 
-./datadog-ci junit upload --service ci-system-test-service-draft3 logs/reportJunit.xml 
+./datadog-ci junit upload --service ci-system-test-service-draft3 $(pwd)/logs/reportJunit.xml 
