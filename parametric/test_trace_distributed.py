2593c228d154c6433acd141f31c0f8823ab5196b
ZStriker19 <zach.groves@datadoghq.com>
2022-10-06 14:32:54 -0400

    reorg for merge

--- parametric/test_trace_distributed.py
+++ parametric/test_trace_distributed.py
@@ -1,25 +1,25 @@
 import pytest
 
-from parametric.protos.apm_test_client_pb2 import DistributedHTTPHeaders
+from parametric.protos.apm_test_library_pb2 import DistributedHTTPHeaders
 from parametric.spec.trace import SAMPLING_PRIORITY_KEY
 
 
 @pytest.mark.skip_library("golang", "not implemented")
 @pytest.mark.skip_library("dotnet", "not implemented")
 @pytest.mark.skip_library("nodejs", "not implemented")
-def test_distributed_headers_extract_datadog(test_agent, test_client):
+def test_distributed_headers_extract_datadog(test_agent, test_library):
     """Ensure that Datadog distributed tracing headers are extracted
     and activated properly.
     """
 
-    with test_client:
+    with test_library:
         http_headers = DistributedHTTPHeaders()
         http_headers.x_datadog_trace_id_key = "x-datadog-trace-id"
         http_headers.x_datadog_trace_id_value = "12345"
         http_headers.x_datadog_parent_id_key = "x-datadog-parent-id"
         http_headers.x_datadog_parent_id_value = "123"
 
-        with test_client.start_span(
+        with test_library.start_span(
             name="name", service="service", resource="resource", origin="synthetics", http_headers=http_headers
         ) as span:
             span.set_meta(key="http.status_code", val="200")
@@ -32,12 +32,12 @@ def test_distributed_headers_extract_datadog(test_agent, test_client):
 @pytest.mark.skip_library("golang", "not implemented")
 @pytest.mark.skip_library("dotnet", "not implemented")
 @pytest.mark.skip_library("nodejs", "not implemented")
-def test_distributed_headers_inject_datadog(test_agent, test_client):
+def test_distributed_headers_inject_datadog(test_agent, test_library):
     """Ensure that Datadog distributed tracing headers are injected properly.
     """
-    with test_client:
-        with test_client.start_span(name="name") as span:
-            headers = test_client.inject_headers()
+    with test_library:
+        with test_library.start_span(name="name") as span:
+            headers = test_library.inject_headers()
 
     span = get_span(test_agent)
     assert span.get("trace_id") == int(headers.http_headers.x_datadog_trace_id_value)
@@ -47,17 +47,17 @@ def test_distributed_headers_inject_datadog(test_agent, test_client):
 
 @pytest.mark.skip("needs to be implemented by tracers and test needs to adhere to RFC")
 @pytest.mark.parametrize("apm_test_server_env", [{"DD_TRACE_PROPAGATION_STYLE_EXTRACT": "W3C"}])
-def test_distributed_headers_extract_w3c001(apm_test_server_env, test_agent, test_client):
+def test_distributed_headers_extract_w3c001(apm_test_server_env, test_agent, test_library):
     """Ensure that W3C distributed tracing headers are extracted
     and activated properly.
     """
 
-    with test_client:
+    with test_library:
         http_headers = DistributedHTTPHeaders()
         http_headers.traceparent_key = "traceparent"
         http_headers.traceparent_value = "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01"
 
-        with test_client.start_span(
+        with test_library.start_span(
             name="name", service="service", resource="resource", origin="synthetics", http_headers=http_headers
         ) as span:
             span.set_meta(key="http.status_code", val="200")
