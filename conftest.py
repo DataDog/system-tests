ac63027a1b004bca43743e2e5a1c7d0aa790ade4
roberto montero <roberto.montero@datadoghq.com>
2022-10-11 08:13:22 +0200

    change service name, env name and test suite namne

--- conftest.py
+++ conftest.py
@@ -447,6 +447,9 @@ def _pytest_junit_modifyreport():
         )
 
     for testsuite in junit_report_root.findall("testsuite"):
+        #Test suite name will be the scanario name
+        testsuite.set("name", os.environ.get('SYSTEMTESTS_SCENARIO'))
+        #New properties node to add our custom tags
         ts_props = ET.SubElement(testsuite, "properties")
         _create_junit_testsuite_context(ts_props)
         _create_junit_testsuite_summary(ts_props, json_report["summary"])
@@ -545,7 +548,7 @@ def _create_testcase_results(
 
     testcase = junit_xml_root.find(f"testsuite/testcase[@classname='{testclass_name}'][@name='{testcase_name}']")
     if testcase:
-        # Change name att because CI Visibility use identifier: testsuite+name
+        # Change name att because CI Visibility uses identifier: testsuite+name
         testcase.set("name", testclass_name + "." + testcase_name)
         # Add custom tags
         tc_props = ET.SubElement(testcase, "properties")
--- utils/scripts/upload_results_CI_visibility.sh
+++ utils/scripts/upload_results_CI_visibility.sh
@@ -1,8 +1,15 @@
+if [ "$#" -ne 3 ] ; then
+  echo "Script execution requires 3 parameters: environment name (dev, prod), language (java,node...) and weblog variant name " 
+  exit 1
+fi
+SYS_TEST_ENV=$1
+SYS_TEST_LANG=$2
+SYS_TEST_WEBLOG=$3
+
 if test -f ".env"; then
     source .env
 fi
 
-export DD_ENV='ci-system-tests-draft7'
 export DD_CIVISIBILITY_LOGS_ENABLED='1'
 export DD_CIVISIBILITY_AGENTLESS_ENABLED='1'
 export DD_SITE=datad0g.com
@@ -10,7 +17,7 @@ export DD_SITE=datad0g.com
 #Download tool
 curl -L --fail "https://github.com/DataDog/datadog-ci/releases/latest/download/datadog-ci_linux-x64" --output "$(pwd)/datadog-ci" && chmod +x $(pwd)/datadog-ci
 for folder in $(find . -name "logs*" -type d -maxdepth 1); do 
-    ./datadog-ci junit upload --service ci-system-test-service-draft7 $folder/reportJunit.xml 
+    ./datadog-ci junit upload --service ci-system-test-$SYS_TEST_LANG-$SYS_TEST_WEBLOG --env env-system-test-$SYS_TEST_ENV $folder/reportJunit.xml 
 done
 
 
