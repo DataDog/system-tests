eb944520243c2a1cc3990b71647b88b5fecb5ce9
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-10-20 16:17:35 +0200

    skip Test_RemoteConfigurationUpdateSequenceASMDDNoCache

--- scenarios/remote_config/test_remote_configuration.py
+++ scenarios/remote_config/test_remote_configuration.py
@@ -278,6 +278,7 @@ class Test_RemoteConfigurationUpdateSequenceLiveDebuggingNoCache(BaseTestCase):
 
 @rfc("https://docs.google.com/document/d/1u_G7TOr8wJX0dOM_zUDKuRJgxoJU_hVTd5SeaMucQUs/edit#heading=h.octuyiil30ph")
 @released(cpp="?", dotnet="2.15.0", golang="?", java="?", php="?", python="?", ruby="?", nodejs="?")
+@bug(library="dotnet")
 @coverage.basic
 class Test_RemoteConfigurationUpdateSequenceASMDDNoCache(BaseTestCase):
     """Tests that over a sequence of related updates, tracers follow the RFC for the ASM DD product"""
