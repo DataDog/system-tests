f7c67fd7c24c409f0e7ac7fac9625f52fc6ac4e0
roberto montero <roberto.montero@datadoghq.com>
2022-10-25 21:49:23 +0200

    add repo origin parameter

--- utils/scripts/upload_results_CI_visibility.sh
+++ utils/scripts/upload_results_CI_visibility.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ] ; then
-  echo "Script execution requires 4 parameters: environment name (dev, prod), language (java,node...), weblog variant name, pipeline run-id " 
+set +e
+
+if [ "$#" -ne 3 ] ; then
+  echo "Script execution requires 3 parameters: environment name (dev, prod), repo origin (systest, nightly,tracer_java), pipeline run-id " 
   exit 1
 fi
 SYS_TEST_ENV=$1
 SYS_ORIGIN_REPO=$2
-SYS_TEST_RUN_ID=$4
+SYS_TEST_RUN_ID=$3
 
 echo "Uploading test results for pipelineId-runid:${SYS_TEST_RUN_ID}"
 
@@ -23,7 +25,3 @@ curl -L --fail "https://github.com/DataDog/datadog-ci/releases/latest/download/d
 for folder in $(find . -name "logs*" -type d -maxdepth 1); do 
     ./datadog-ci junit upload --service ci-$SYS_ORIGIN_REPO --env env-system-test-$SYS_TEST_ENV --tags "ci.pipeline.run_id:$SYS_ORIGIN_REPO-$SYS_TEST_RUN_ID" $folder/reportJunit.xml 
 done
-
-
-
-
