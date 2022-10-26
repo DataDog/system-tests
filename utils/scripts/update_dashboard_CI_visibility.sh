ce31c9eb39b419d5fd93e08231385ac05595dc54
roberto montero <roberto.montero@datadoghq.com>
2022-10-26 11:05:24 +0200

    update dashboard using only one variable

--- utils/scripts/update_dashboard_CI_visibility.sh
+++ utils/scripts/update_dashboard_CI_visibility.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ] ; then
-  echo "Script execution requires 2 parameters: Pipeline id and dashboard variable name" 
+  echo "Script execution requires 2 parameter.  Repo Identification ,  Pipeline id + pipeline run attemp " 
+  echo "Repositoriy identification examples: system-tests, night-tracer, night-agent, tracer-java, tracer-nodejs"
   exit 1
 fi
-SYS_PIPELINE_RUN_ID=$1
-DASHBOARD_VARIABLE_NAME=$2
+
+SYS_ORIGIN_REPO=$1
+SYS_PIPELINE_RUN_ID=$2
 
 echo "PIPELINE RUN ID $SYS_PIPELINE_RUN_ID"
+SYS_UNIQUE_IDENTIFIER=$SYS_ORIGIN_REPO-$SYS_PIPELINE_RUN_ID
 
 # Path parameters
 export dashboard_id="zqg-kqn-2mc"
 
+# Get dashboard json
 export dashboard_json=$(
 curl -X GET "https://api.datadoghq.com/api/v1/dashboard/${dashboard_id}" \
 -H "Accept: application/json" \
 -H "DD-API-KEY: ${DD_API_KEY}" \
 -H "DD-APPLICATION-KEY: ${DD_APP_KEY}")
 
-#Search in all template variables (no more than 4) trying to find DASHBOARD_VARIABLE_NAME pass as parameter
-#When I achieve to find the variable, I will change the default value with the last pipelineId + run attemp number 
-for counter in 0 1 2 3  
-do
-    dashboard_json_variable=$(jq -r ".template_variables[$counter].prefix"  <<< "$dashboard_json")
-    echo "-> $dashboard_json_variable"
-    if [ "$dashboard_json_variable" = "$DASHBOARD_VARIABLE_NAME" ]; then
-        echo "Updating dashboard variable: $DASHBOARD_VARIABLE_NAME "
-        export dashboard_json_updated=$(jq -r ".template_variables[${counter}].default = \"$SYS_PIPELINE_RUN_ID\""  <<< "$dashboard_json")
-        break
+# We search for previous dashboard variable using "origin repo"  
+available_value_before=$(
+jq -c '.template_variables[0].available_values[]' <<< "$dashboard_json"| while read available_value; do
+    if [[ "$available_value" == *"$SYS_ORIGIN_REPO"* ]]; then
+      echo $available_value
+      break
     fi
 done
+)
 
+# Json that we will to update
+dashboard_variable_json=$(jq -r ".template_variables[0]"  <<< "$dashboard_json")
+
+# If we have a previous variable with the "origin repo" we will update, if it doesn't we will add
+if [ -z "$available_value_before" ]
+then
+      echo "There is no previous value for the source repo: ${SYS_ORIGIN_REPO}"
+      echo "We will add the NEW value: ${SYS_UNIQUE_IDENTIFIER}"
+      dashboard_variable_json=${dashboard_variable_json//"\"available_values\": ["/"\"available_values\": [\"$SYS_UNIQUE_IDENTIFIER\","}
+else
+      echo "We will update the last variable value: ${available_value_before} to new value: ${SYS_UNIQUE_IDENTIFIER}"
+      dashboard_variable_json=${dashboard_variable_json//$available_value_before/\"$SYS_UNIQUE_IDENTIFIER\"}
+fi
+
+# Updated json
+dashboard_json_updated=$(jq -r ".template_variables[0]=$dashboard_variable_json" <<< "$dashboard_json")
+
+# Commit the updated json
 curl -X PUT "https://api.datadoghq.com/api/v1/dashboard/${dashboard_id}" \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
