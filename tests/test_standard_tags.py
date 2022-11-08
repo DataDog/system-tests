8c9c5e973b8cad3e1fbe3f0d2ba6817c9979b8c4
Christophe Papazian <christophe.papazian@datadoghq.com>
2022-11-08 11:05:06 +0100

    chore(asm): better fix for uwsgi and django

--- tests/test_standard_tags.py
+++ tests/test_standard_tags.py
@@ -165,8 +165,10 @@ class Test_StandardTagsRoute(BaseTestCase):
         if context.library == "dotnet":
             tags["http.route"] = "/sample_rate_route/{i:int}"
         if context.library == "python":
-            if context.weblog_variant == "flask-poc":
+            if context.weblog_variant in ("flask-poc", "uwsgi-poc"):
                 tags["http.route"] = "/sample_rate_route/<i>"
+            elif context.weblog_variant == "django-poc":
+                tags["http.route"] = "/sample_rate_route/<int:i>"
 
         interfaces.library.add_span_tag_validation(request=r, tags=tags)
 
