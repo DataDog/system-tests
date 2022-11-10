bf6d7d12213ef18fde115df1455d38465f89f50f
Pawel Chojnacki <pawelchcki@gmail.com>
2022-11-10 14:16:46 +0100

    Quick and dirty POC - to demonstrate how building Images straight from python could work

--- utils/build/docker/tools/library_proxy.py
+++ utils/build/docker/tools/library_proxy.py
@@ -1,3 +1,65 @@
 import docker
+import os
+from os import path
+import tempfile
+import shutil
+from pathlib import Path, PurePath
+import json
 
-docker.
+current_dir = Path(path.dirname(path.realpath(__file__)))
+root_dir = Path(path.realpath(path.join(current_dir, "../../../../")))
+
+def build_image_directly():
+    client = docker.APIClient()
+    return client.build(path="./", dockerfile=os.path.join(current_dir, "library_proxy.Dockerfile"), tag="ehlo_direct")
+
+def build_image_indirectly():
+    work_dir = tempfile.TemporaryDirectory()
+    target = Path(work_dir.name)
+
+    files = [
+        current_dir / "library_proxy.Dockerfile",
+        current_dir / "library_proxy.entrypoint.sh",
+        root_dir / "utils/proxy/forwarder.py"
+    ]
+
+    dirs_to_create = [ f.relative_to(root_dir).parent for f in files]
+
+    for dir in dirs_to_create:
+        path = target / dir
+        os.makedirs(path, exist_ok=True)
+    
+    for file in files:
+        target_file = target / file.relative_to(root_dir)
+        shutil.copyfile(file, target_file, follow_symlinks = True)
+
+    client = docker.APIClient()
+    return client.build(path=target.as_posix(), dockerfile= (current_dir.relative_to(root_dir) / "library_proxy.Dockerfile").as_posix(), tag="ehlo_indirect")
+
+def parse_output(msg):
+    msg = json.loads(msg)
+    if "stream" in msg:
+        print(msg["stream"])
+    if "aux" in msg:
+        print("Built Image ID: %s" % msg["aux"])
+
+
+if __name__ == '__main__':
+    import timeit
+
+    # warmup the cache and check build output
+    for msg in build_image_directly():
+        parse_output(msg)
+
+    for msg in build_image_indirectly():
+        parse_output(msg)
+
+    print("timing the direct build")
+    iterations = 10
+    time = timeit.timeit(lambda: build_image_directly(), number = iterations ) / iterations
+    print("time per iteration: ", time)
+
+    print("timing the docker build that isolates dependencies")
+    iterations = 100
+    time = timeit.timeit(lambda: build_image_indirectly(), number = iterations ) / iterations
+    print("time per iteration: ", time)
