c0e797a76269e0df1ee74eeecfe66e0cf5330db1
Zach Montoya <zach.montoya@datadoghq.com>
2022-11-11 17:38:52 -0800

    Add test cases for parametric/test_trace_distributed.py and run them against the Python tracer

--- parametric/test_trace_distributed.py
+++ parametric/test_trace_distributed.py
@@ -1,7 +1,7 @@
 import pytest
 
 from parametric.protos.apm_test_client_pb2 import DistributedHTTPHeaders
-from parametric.spec.trace import SAMPLING_PRIORITY_KEY
+from parametric.spec.trace import SAMPLING_PRIORITY_KEY, ORIGIN
 
 
 @pytest.mark.skip_library("dotnet", "not implemented")
@@ -13,17 +13,47 @@ def test_distributed_headers_extract_datadog(test_agent, test_library):
     """
     with test_library:
         distributed_message = DistributedHTTPHeaders()
-        distributed_message.http_headers["x-datadog-trace-id"] = "12345"
-        distributed_message.http_headers["x-datadog-parent-id"] = "123"
+        distributed_message.http_headers["x-datadog-trace-id"] = "123456789"
+        distributed_message.http_headers["x-datadog-parent-id"] = "987654321"
+        distributed_message.http_headers["x-datadog-sampling-priority"] = "1"
+        distributed_message.http_headers["x-datadog-origin"] = "synthetics"
+        distributed_message.http_headers["x-datadog-tags"] = "_dd.p.dm=-1"
 
         with test_library.start_span(
-            name="name", service="service", resource="resource", origin="synthetics", http_headers=distributed_message
+            name="name", service="service", resource="resource", http_headers=distributed_message
+        ) as span:
+            span.set_meta(key="http.status_code", val="200")
+
+    expected_datadog_tags_dict = dict(e.split("=") for e in distributed_message.http_headers["x-datadog-tags"].split(','))
+
+    span = get_span(test_agent)
+    assert span.get("trace_id") == int(distributed_message.http_headers["x-datadog-trace-id"])
+    assert span.get("parent_id") == int(distributed_message.http_headers["x-datadog-parent-id"])
+    assert span["meta"].get(ORIGIN) == distributed_message.http_headers["x-datadog-origin"]
+    assert span["meta"].get("_dd.p.dm") == expected_datadog_tags_dict.get("_dd.p.dm")
+    assert span["metrics"].get(SAMPLING_PRIORITY_KEY) == int(distributed_message.http_headers["x-datadog-sampling-priority"])
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+def test_distributed_headers_extractinvalid_datadog(test_agent, test_library):
+    """Ensure that Datadog distributed tracing headers are extracted
+    and activated properly.
+    """
+    with test_library:
+        distributed_message = DistributedHTTPHeaders()
+        distributed_message.http_headers["x-datadog-trace-id"] = "0"
+        distributed_message.http_headers["x-datadog-parent-id"] = "0"
+        distributed_message.http_headers["x-datadog-sampling-priority"] = "1"
+
+        with test_library.start_span(
+            name="name", service="service", resource="resource", http_headers=distributed_message
         ) as span:
             span.set_meta(key="http.status_code", val="200")
 
     span = get_span(test_agent)
-    assert span.get("trace_id") == 12345
-    assert span.get("parent_id") == 123
+    assert span.get("trace_id") != int(distributed_message.http_headers["x-datadog-trace-id"])
+    assert span.get("parent_id") != int(distributed_message.http_headers["x-datadog-parent-id"])
 
 
 @pytest.mark.skip_library("dotnet", "not impemented")
@@ -38,6 +68,36 @@ def test_distributed_headers_inject_datadog(test_agent, test_library):
     span = get_span(test_agent)
     assert span.get("trace_id") == int(headers["x-datadog-trace-id"])
     assert span.get("span_id") == int(headers["x-datadog-parent-id"])
+    assert span["metrics"].get(SAMPLING_PRIORITY_KEY) == int(headers["x-datadog-sampling-priority"])
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+def test_distributed_headers_extractandinject_datadog(test_agent, test_library):
+    """Ensure that Datadog distributed tracing headers are extracted
+    and activated properly.
+    """
+    with test_library:
+        distributed_message = DistributedHTTPHeaders()
+        distributed_message.http_headers["x-datadog-trace-id"] = "123456789"
+        distributed_message.http_headers["x-datadog-parent-id"] = "987654321"
+        distributed_message.http_headers["x-datadog-sampling-priority"] = "1"
+        distributed_message.http_headers["x-datadog-origin"] = "synthetics"
+        distributed_message.http_headers["x-datadog-tags"] = "_dd.p.dm=-1"
+
+        with test_library.start_span(
+            name="name", service="service", resource="resource", http_headers=distributed_message
+        ) as span:
+            headers = test_library.inject_headers().http_headers.http_headers
+
+    expected_datadog_tags_dict = dict(e.split("=") for e in distributed_message.http_headers["x-datadog-tags"].split(','))
+
+    span = get_span(test_agent)
+    assert span.get("parent_id") == int(distributed_message.http_headers["x-datadog-parent-id"])
+    assert span.get("trace_id") == int(headers["x-datadog-trace-id"])
+    assert span.get("span_id") == int(headers["x-datadog-parent-id"])
+    assert span["meta"].get(ORIGIN) == headers["x-datadog-origin"]
+    assert span["meta"].get("_dd.p.dm") == expected_datadog_tags_dict.get("_dd.p.dm")
     assert span["metrics"].get(SAMPLING_PRIORITY_KEY) == int(headers["x-datadog-sampling-priority"])
 
 
