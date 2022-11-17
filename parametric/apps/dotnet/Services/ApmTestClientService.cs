176c0067f1f6ebfa29b0b53950c64d61ff984a44
Zach Montoya <zach.montoya@datadoghq.com>
2022-11-16 18:20:35 -0800

    tracecontext tests: Add w3c tests. Their license is weird, so I don't know if we want to check this in or pull their code and run the test suite, which other OTEL repos are doing

--- parametric/apps/dotnet/Services/ApmTestClientService.cs
+++ parametric/apps/dotnet/Services/ApmTestClientService.cs
@@ -43,6 +43,11 @@ namespace ApmTestClient.Services
         public ApmTestClientService(ILogger<ApmTestClientService> logger)
         {
             _logger = logger;
+
+            // TODO: Remove when the Tracer sets the correct results in the SpanContextPropagator.Instance getter
+            // This should avoid a bug in the SpanContextPropagator.Instance getter where it is populated WITHOUT consulting the TracerSettings.
+            // By instantiating the Tracer first, that faulty getter code path will not be invoked
+            _ = Tracer.Instance;
         }
 
         public override Task<StartSpanReturn> StartSpan(StartSpanArgs request, ServerCallContext context)
