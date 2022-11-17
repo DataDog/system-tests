35f70850cfe0531442bf2564a4ae1cd707af64c6
Javier Santos <javier.santos@datadoghq.com>
2022-11-17 10:32:52 +0100

    added wildfly system tests

--- utils/build/docker/java/spring-boot/src/main/java/com/datadoghq/system_tests/springboot/App.java
+++ utils/build/docker/java/spring-boot/src/main/java/com/datadoghq/system_tests/springboot/App.java
@@ -287,29 +287,15 @@ public class App {
     @EventListener(ApplicationReadyEvent.class)
     @Trace
     public void init() {
+        new AppReadyHandler(this).start();
+        /*
         cassandra = new CassandraConnector();
         cassandra.setup();
         initMongo();
         System.out.println("Initialized");
+        */
     }
 
-    void initMongo() {
-        mongoClient = new MongoClient("mongodb");
-        MongoCollection<Document> collection = mongoClient.getDatabase("mydb").getCollection("test");
-
-        collection.insertOne(new Document("name", "MongoDB")
-                .append("id", 1)
-                .append("title", "Skydiving is fun")
-                .append("subject", "Have you ever thought about jumping off an airplane?"));
-        collection.insertOne(new Document("name", "MongoDB")
-                .append("id", 2)
-                .append("title", "Mastering skydiving")
-                .append("subject", "So jump in empty air, but many times"));
-        collection.insertOne(new Document("name", "MongoDB")
-                .append("id", 3)
-                .append("title", "Wingsuit")
-                .append("subject", "Flying like a bird made of cloth who just left a perfectly working airplane"));
-    }
 
 
     @Bean
@@ -326,43 +312,4 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-}
-
-class CassandraConnector {
-    CqlSession session;
-
-    public void setup() {
-        boolean successInit = false;
-        int retry = 1000;
-        while (!successInit && retry-- > 0)
-        {
-            try {
-                TimeUnit.MILLISECONDS.sleep(500);
-                session = CqlSession.builder()
-                        .addContactPoint(new InetSocketAddress("cassandra", 9042))
-                        .withLocalDatacenter("datacenter1")
-                        .build();
-                successInit = true;
-            } catch (Exception ignored) {
-            }
-        }
-
-        // Create KeySpace
-        session.execute("CREATE KEYSPACE IF NOT EXISTS \"testDB\" WITH replication = {'class':'SimpleStrategy','replication_factor':1};");
-
-        // Create table
-        session.execute("USE \"testDB\";");
-        session.execute("DROP TABLE IF EXISTS \"table\";");
-        session.execute("CREATE TABLE \"table\" (id int PRIMARY KEY, title text, subject text);");
-
-        // Insert data
-        session.execute("INSERT INTO \"table\"(id, title, subject) VALUES (1, 'book1', 'subject1');");
-        session.execute("INSERT INTO \"table\"(id, title, subject) VALUES (2, 'book2', 'subject2');");
-        session.execute("INSERT INTO \"table\"(id, title, subject) VALUES (3, 'book3', 'subject3');");
-        session.execute("INSERT INTO \"table\"(id, title, subject) VALUES (4, 'book4', 'subject4');");
-    }
-
-    public CqlSession getSession() {
-        return this.session;
-    }
 }
