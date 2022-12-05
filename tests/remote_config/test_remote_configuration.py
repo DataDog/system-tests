fe826c79b568cd86b30a5459469e047902216358
simon-id <simon.id@datadoghq.com>
2022-12-05 11:47:23 +0100

    Update test_remote_configuration.py

--- tests/remote_config/test_remote_configuration.py
+++ tests/remote_config/test_remote_configuration.py
@@ -143,7 +143,7 @@ class Test_RemoteConfigurationUpdateSequenceFeatures(RemoteConfigurationFieldsBa
         if context.library == "nodejs":
             # time out for nodejs is very low (5 seconds)
             # we need a longer timeout for this test
-            interfaces.library.timeout = 60
+            interfaces.library.timeout = 120
 
     @bug(context.weblog_variant == "spring-boot-openliberty", reason="APPSEC-6721")
     @bug(context.library >= "java@1.1.0", reason="?")
