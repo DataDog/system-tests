1e007cc49b83a39d8ac74596d48084d34a93e6fe
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-12-14 17:56:10 +0100

    skip some tests

--- tests/appsec/waf/test_addresses.py
+++ tests/appsec/waf/test_addresses.py
@@ -344,6 +344,7 @@ class Test_BodyJson:
     @irrelevant(reason="unsupported by framework", library="ruby", weblog_variant="sinatra14")
     @irrelevant(reason="unsupported by framework", library="ruby", weblog_variant="sinatra20")
     @irrelevant(reason="unsupported by framework", library="ruby", weblog_variant="sinatra21")
+    @irrelevant(reason="unsupported by framework", library="ruby", weblog_variant="uds-sinatra")
     def test_json_array(self):
         """AppSec detects attacks in JSON body arrays"""
         interfaces.library.assert_waf_attack(self.r_array, value='<vmlframe src="xss">', address="server.request.body")
