b530c134c9204e467d171fa96292d63e455c1e5c
roberto montero <roberto.montero@datadoghq.com>
2022-12-26 11:16:19 +0100

    test

--- utils/build/docker/cpp/install_ddtrace.sh
+++ utils/build/docker/cpp/install_ddtrace.sh
@@ -8,15 +8,13 @@ get_latest_release() {
 
 NGINX_VERSION=1.17.3
 
-until $OPENTRACING_NGINX_VERSION || (( count++ >= 5 )); 
-do 
-
-    OPENTRACING_NGINX_VERSION="$(get_latest_release opentracing-contrib/nginx-opentracing)"
+attempts=0
+while ! OPENTRACING_NGINX_VERSION="$(get_latest_release opentracing-contrib/nginx-opentracing)" || (( attempts++ > 5 )); do
     DD_OPENTRACING_CPP_VERSION="$(get_latest_release DataDog/dd-opentracing-cpp)"
     echo "opentracing-contrib/nginx-opentracing version: $OPENTRACING_NGINX_VERSION"
     echo "DataDog/dd-opentracing-cpp version: $DD_OPENTRACING_CPP_VERSION"
-
 done
+
 echo $DD_OPENTRACING_CPP_VERSION > SYSTEM_TESTS_LIBRARY_VERSION
 touch SYSTEM_TESTS_LIBDDWAF_VERSION
 echo "0.0.0" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
