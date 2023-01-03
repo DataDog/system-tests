42e398eaec94d75e738f5ed9b0571c81ec2f93ff
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2023-01-03 18:03:36 +0100

    Use new test model

--- tests/appsec/iast/test_iast_no_request.py
+++ tests/appsec/iast/test_iast_no_request.py
@@ -2,50 +2,22 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import BaseTestCase, missing_feature, interfaces, coverage, released
-from utils.interfaces._library._utils import get_root_spans
-from utils.interfaces._core import BaseValidation
+from utils import missing_feature, interfaces, coverage, released
 
 
+@released(dotnet="?", golang="?", java="?", nodejs="?")
+@released(php_appsec="?", python="?", ruby="?", cpp="?")
 @coverage.basic
-@released(
-    dotnet="?", golang="?", java="?", nodejs="?", php_appsec="?", python="?", ruby="?", cpp="?",
-)
-class TestIastNoRequest(BaseTestCase):
+class TestIastNoRequest:
     """ IAST deals with vulnerabilities outside of requests """
 
     @missing_feature(reason="Need to implement span creation")
     def test_span_creation(self):
         """ IAST creates a new span when no request is active """
 
-        def is_iast_root_span(span):
-            return span.get("type", "") == "vulnerability"
-
-        def has_root_iast_span(iast_spans):
-            root_spans = list(filter(is_iast_root_span, iast_spans))
-            return len(root_spans) > 0
-
-        interfaces.library.append_validation(_IastSpansValidation(has_root_iast_span))
-
-
-class _IastSpansValidation(BaseValidation):
-    """ Performs a validation on all spans with IAST metadata """
-
-    path_filters = ["/v0.4/traces"]
-
-    def __init__(self, validator):
-        super().__init__()
-        self.validator = validator
-        self.iast_spans = []  # list of all spans with IAST information
-
-    def check(self, data):
-        content = data["request"]["content"]
-        for i, span in enumerate(get_root_spans(content)):
+        for _, span in interfaces.library.get_root_spans():
             if "_dd.iast.json" in span.get("meta", {}):
-                self.iast_spans.append(span)
+                if span.get("type", "") == "vulnerability":
+                    return
 
-    def final_check(self):
-        try:
-            self.set_status(self.validator(self.iast_spans))
-        except Exception as e:
-            self.set_failure(message="Failed to find matching spans", exception=e, data=self.iast_spans)
+        raise Exception("Did not find any span if IAST data and type == vulnerability")
