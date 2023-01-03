87108f32caf5c57cf645012598fc3a60b79cd5d9
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2023-01-03 16:10:07 +0100

    Move proxy inside runner

--- utils/proxy/core.py
+++ utils/proxy/core.py
@@ -1,69 +1,52 @@
-# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
-# This product includes software developed at Datadog (https://www.datadoghq.com/).
-# Copyright 2021 Datadog, Inc.
-
-import os
-import json
-import socket
+import asyncio
 from collections import defaultdict
+import json
+import os
+import threading
 from datetime import datetime
-from http.client import HTTPConnection
-import logging
-from mitmproxy import http  # pylint: disable=import-error
-from mitmproxy.flow import Error as FlowError  # pylint: disable=import-error
 
+from mitmproxy import master, options
+from mitmproxy.addons import errorcheck, keepserving, default_addons
+from mitmproxy.flow import Error as FlowError
+
+from utils import interfaces
+from utils.tools import logger
 
 SIMPLE_TYPES = (bool, int, float, type(None))
 
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-handler.setFormatter(logging.Formatter("%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s", "%H:%M:%S"))
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_live_debugging.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_live_debugging.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_LIVE_DEBUGGING = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_asm_features.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_asm_features.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_ASM_FEATURES = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_asm_activate_only.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_asm_activate_only.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_ASM_ACTIVATE_ONLY = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_asm_dd.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_asm_dd.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_ASM_DD = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_asm_data.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_asm_data.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_ASM_DATA = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_live_debugging_nocache.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_live_debugging_nocache.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_LIVE_DEBUGGING_NO_CACHE = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_asm_features_nocache.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_asm_features_nocache.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_ASM_FEATURES_NO_CACHE = json.load(f)
 
-with open("system-tests/utils/proxy/rc_mocked_responses_asm_dd_nocache.json", encoding="utf-8") as f:
+with open("utils/proxy/rc_mocked_responses_asm_dd_nocache.json", encoding="utf-8") as f:
     RC_MOCKED_RESPONSES_ASM_DD_NO_CACHE = json.load(f)
 
 
-class Forwarder:
-    def __init__(self):
-        self.forward_ip = os.environ.get("FORWARD_TO_HOST", "runner")
-        self.forward_port = os.environ.get("FORWARD_TO_PORT", "8081")
-        self.interface_name = os.environ.get("INTERFACE_NAME", "")
-
+class _RequestLogger:
+    def __init__(self) -> None:
         self.dd_api_key = os.environ["DD_API_KEY"]
-
-        # This is the proxy state. Basically, true or false values that tells the proxy to enable, or not
-        # specific behavior. You can get/modify it using a direct GET/POST /_system_tests_state request to the proxy
-        self.state = json.loads(os.environ.get("INITIAL_PROXY_STATE", "") or "{}")
+        self.state = json.loads(os.environ.get("SYSTEMTESTS_PROXY_STATE", "") or "{}")
 
         # for config backend mock
         self.config_request_count = defaultdict(int)
-
-        logger.info(f"Initial state: {self.state}")
-        logger.info(f"Forward flows to {self.forward_ip}:{self.forward_port}")
 
     def _scrub(self, content):
         if isinstance(content, str):
@@ -81,46 +64,12 @@ class Forwarder:
         logger.error(f"Can't scrub type {type(content)}")
         return content
 
-    @staticmethod
-    def is_direct_command(flow):
-        return flow.request.path == "/_system_tests_state"
-
-    @staticmethod
-    def is_health_request(flow):
-        return flow.request.path == "/_system_tests_health"
-
     def request(self, flow):
-        if self.is_health_request(flow):
-            flow.response = http.Response.make(200, "ok\n")
-
-        if self.is_direct_command(flow):
-            logger.info(f"Direct command to proxy: {flow.request.pretty_url}")
-
-            try:
-                if flow.request.method == "GET":
-                    flow.response = http.Response.make(
-                        200, json.dumps(self.state), {"Content-Type": "application/json"}
-                    )
-
-                elif flow.request.method == "POST":
-                    new_state = json.loads(flow.request.content)
-                    logger.info(f"New state: {new_state}")
-                    self.state.clear()
-                    self.state.update(new_state)
-                    flow.response = http.Response.make(
-                        200, json.dumps(self.state), {"Content-Type": "application/json"}
-                    )
-                else:
-                    flow.response = http.Response.make(405)
-            except Exception as e:
-                flow.response = http.Response.make(500, repr(e))
+        if flow.request.host == "runner":
+            flow.request.host, flow.request.port = "agent", 8126
+            flow.request.scheme = "http"
 
     def response(self, flow):
-        if self.is_direct_command(flow) or self.is_health_request(flow):
-            return
-
-        logger.info(f"Received {flow.request.pretty_url} {flow.response.status_code}")
-
         self._modify_response(flow)
 
         request_content = str(flow.request.content)
@@ -153,27 +102,15 @@ class Forwarder:
         if flow.error and flow.error.msg == FlowError.KILLED_MESSAGE:
             payload["response"] = None
 
-        conn = HTTPConnection(self.forward_ip, self.forward_port)
+        if flow.request.host == "agent":
+            interface = interfaces.library
+        else:
+            interface = interfaces.agent
 
         try:
-            conn.request(
-                "POST",
-                f"/proxy/{self.interface_name}",
-                body=json.dumps(self._scrub(payload)),
-                headers={"Content-type": "application/json"},
-            )
-        except socket.gaierror:
-            logger.error(f"Can't resolve to forward {self.forward_ip}:{self.forward_port}")
-        except ConnectionRefusedError:
-            logger.error("Can't forward, connection refused")
-        except BrokenPipeError:
-            logger.error("Can't forward, broken pipe")
-        except TimeoutError:
-            logger.error("Can't forward, time out")
-        except Exception as e:
-            logger.error(f"Can't forward: {e}")
-        finally:
-            conn.close()
+            interface.append_data(self._scrub(payload))
+        except:
+            logger.exception("Fail to send data to {interface}")
 
     def _modify_response(self, flow):
         if self.state.get("mock_remote_config_backend") == "ASM_FEATURES":
@@ -194,6 +131,9 @@ class Forwarder:
             self._modify_response_rc(flow, RC_MOCKED_RESPONSES_ASM_DD_NO_CACHE)
 
     def _modify_response_rc(self, flow, mocked_responses):
+        if flow.request.host != "agent":
+            return  # modify only tracer/agent flow
+
         if flow.request.path == "/info" and str(flow.response.status_code) == "200":
             logger.info("Overwriting /info response to include /v0.7/config")
             c = json.loads(flow.response.content)
@@ -216,4 +156,30 @@ class Forwarder:
             self.config_request_count[runtime_id] += 1
 
 
-addons = [Forwarder()]
+def _start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+
+def start_proxy() -> None:
+    loop = asyncio.new_event_loop()
+
+    thread = threading.Thread(target=_start_background_loop, args=(loop,), daemon=True)
+    thread.start()
+
+    opts = options.Options(listen_host="0.0.0.0", listen_port=8126, confdir="utils/proxy/.mitmproxy")
+    proxy = master.Master(opts, event_loop=loop)
+    proxy.addons.add(*default_addons())
+    proxy.addons.add(keepserving.KeepServing())
+    proxy.addons.add(errorcheck.ErrorCheck())
+    proxy.addons.add(_RequestLogger())
+
+    asyncio.run_coroutine_threadsafe(proxy.run(), loop)
+
+
+if __name__ == "__main__":
+
+    import time
+
+    start_proxy()
+    time.sleep(1000)
