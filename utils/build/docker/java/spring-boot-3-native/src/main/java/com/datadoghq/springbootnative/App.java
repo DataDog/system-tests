0eca8b2c10f768dbd7c58a937b8989b1dbc0caae
roberto montero <roberto.montero@datadoghq.com>
2023-01-04 11:49:42 +0100

    test cache, remove unused imports

--- utils/build/docker/java/spring-boot-3-native/src/main/java/com/datadoghq/springbootnative/App.java
+++ utils/build/docker/java/spring-boot-3-native/src/main/java/com/datadoghq/springbootnative/App.java
@@ -2,11 +2,7 @@ package com.datadoghq.springbootnative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.datadoghq.springbootnative"})
