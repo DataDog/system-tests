ae038a58c5f12b4e3c6e4cd4aff27dc43b8577f3
roberto montero <roberto.montero@datadoghq.com>
2023-01-09 12:45:35 +0100

    build tag

--- utils/build/docker/java/spring-native_build/build.sh
+++ utils/build/docker/java/spring-native_build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform "linux/amd64" --tag "spring_native_build" --push .
+docker buildx build --platform "linux/amd64" --tag "datadog/system_tests/spring_native_build:latest" --push .
