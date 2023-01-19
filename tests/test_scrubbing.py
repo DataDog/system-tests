aa845fddf560f9ba9e89dbadb9a346bba943fc92
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2023-01-19 15:43:07 +0100

    Test for data scrubbing

---
+++ tests/test_scrubbing.py
@@ -0,0 +1,90 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2022 Datadog, Inc.
+
+import pytest
+from utils import bug, context, coverage, interfaces, released, rfc, weblog
+
+if context.library == "cpp":
+    pytestmark = pytest.mark.skip("not relevant")
+
+
+def validate_no_leak(needle):
+    def crawler(data):
+        if isinstance(data, str):
+            assert needle not in data
+        elif isinstance(data, (list, tuple)):
+            for value in data:
+                crawler(value)
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                assert needle not in key
+                crawler(value)
+        elif isinstance(data, (int, float, bool)) or data is None:
+            pass
+
+    return crawler
+
+
+@released(dotnet="2.13.0", golang="1.40.0", java="0.107.1", nodejs="3.0.0")
+@released(php="0.76.0", python="1.6.0rc1.dev", ruby="1.0.0")
+@rfc("https://datadoghq.atlassian.net/wiki/spaces/APS/pages/2490990623/QueryString+-+Sensitive+Data+Obfuscation")
+@bug(weblog_variant="spring-boot-undertow", reason="APMJAVA-877")
+@coverage.good
+class Test_UrlQuery:
+    """ PII values in query parameter are all removed"""
+
+    def setup_main(self):
+        weblog.get("/", params={"pass": "leak-url-main-v1", "key2": "val2", "key3": "val3"})
+        weblog.get("/", params={"key1": "val1", "public_key": "leak-url-main-v2", "key3": "val3"})
+        weblog.get("/", params={"key1": "val1", "key2": "val2", "token": "leak-url-main-v3"})
+        weblog.get("/", params={"json": '{"sign":"leak-url-main-v4"}'})
+
+    def test_main(self):
+        interfaces.library.validate(validate_no_leak("leak-url-main"), success_by_default=True)
+
+    def setup_multiple_matching_substring(self):
+        weblog.get(
+            "/",
+            params={
+                "token": "leak-url-multiple-v1",
+                "key1": "val1",
+                "key2": "val2",
+                "pass": "leak-url-multiple-v2",
+                "public_key": "leak-url-multiple-v3",
+                "key3": "val3",
+                "json": '{"sign":"leak-url-multiple-v4"}',
+            },
+        )
+
+    @bug(context.library < "dotnet@2.21.0", reason="APPSEC-5773")
+    def test_multiple_matching_substring(self):
+        interfaces.library.validate(validate_no_leak("leak-url-multiple"), success_by_default=True)
+
+
+@coverage.basic
+class Test_UrlField:
+    """ PII in url field are removed"""
+
+    def setup_main(self):
+        r = weblog.get(domain="name:leak-password-url@weblog", params={"hello": "1234567"})
+        assert r.status_code == 200
+
+    def test_main(self):
+        interfaces.library.validate(validate_no_leak("leak-password-url"), success_by_default=True)
+
+
+@coverage.good
+class Test_EnvVar:
+    """ Environnement variables are not leaked"""
+
+    def test_library(self):
+        interfaces.library.validate(validate_no_leak("leaked-env-var"), success_by_default=True)
+
+    def test_agent(self):
+        interfaces.agent.validate(validate_no_leak("leaked-env-var"), success_by_default=True)
+
+    def test_logs(self):
+        interfaces.library_stdout.assert_absence(".*leaked-env-var.*")
+        interfaces.library_dotnet_managed.assert_absence(".*leaked-env-var.*")
+        # interfaces.agent_stdout.assert_absence(".*leaked-env-var.*")  # TODO
