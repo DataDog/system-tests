90f152a38824f3c3086210cd8741469d215e4e6d
Zach Montoya <zach.montoya@datadoghq.com>
2023-01-24 15:09:45 -0800

    Fix additional Python tests that are flaky

--- parametric/test_headers_b3multi.py
+++ parametric/test_headers_b3multi.py
@@ -72,6 +72,7 @@ def test_headers_b3multi_extract_invalid(test_agent, test_library):
 
 @enable_b3multi()
 @pytest.mark.skip_library("dotnet", "Latest release does not implement new configuration")
+@pytest.mark.skip_library("python", "Issue: Python doesn't pad the trace-id to length of 16 or 32 lower-hex characters")
 @pytest.mark.skip_library("golang", "not impemented")
 @pytest.mark.skip_library("nodejs", "TODO: remove when https://github.com/DataDog/dd-trace-js/pull/2477 lands")
 def test_headers_b3multi_inject_valid(test_agent, test_library):
@@ -125,6 +126,7 @@ def test_headers_b3multi_propagate_valid(test_agent, test_library):
 
 @enable_b3multi()
 @pytest.mark.skip_library("dotnet", "Latest release does not implement new configuration")
+@pytest.mark.skip_library("python", "Issue: Python doesn't pad the trace-id to length of 16 or 32 lower-hex characters")
 @pytest.mark.skip_library("golang", "not implemented")
 @pytest.mark.skip_library("nodejs", "TODO: remove when https://github.com/DataDog/dd-trace-js/pull/2477 lands")
 def test_headers_b3multi_propagate_invalid(test_agent, test_library):
