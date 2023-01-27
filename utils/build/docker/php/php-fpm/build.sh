6a7a76e26a1d8a20dd89ccc78bcae36c1edc50b5
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2023-01-27 09:21:27 +0100

    fix php

--- utils/build/docker/php/php-fpm/build.sh
+++ utils/build/docker/php/php-fpm/build.sh
@@ -47,3 +47,6 @@ export TRACER_VERSION=latest
 export APPSEC_VERSION=latest
 cp /tmp/php/common/install_ddtrace.sh /
 /install_ddtrace.sh 0
+
+SYSTEM_TESTS_LIBRARY_VERSION=$(cat /binaries/SYSTEM_TESTS_LIBRARY_VERSION)
+echo "datadog.trace.request_init_hook = /opt/datadog/dd-library/$SYSTEM_TESTS_LIBRARY_VERSION/dd-trace-sources/bridge/dd_wrap_autoloader.php" >> /etc/php/$PHP_VERSION/fpm/php.ini
