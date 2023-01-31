b6f269ce4ccc413ee10bc21807152abd12c33d37
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2023-01-31 13:53:10 +0100

    temporarily exclude more jobs

--- lib-injection/run-auto-lib-injection.sh
+++ lib-injection/run-auto-lib-injection.sh
@@ -9,53 +9,53 @@ SCRIPT_PATH=$(readlink -f "$0")
 export BASE_DIR=$(dirname "${SCRIPT_PATH}")
 
 
-echo "**************************"
-echo "*  Running Test Case #1  *"
-echo "**************************"
+# echo "**************************"
+# echo "*  Running Test Case #1  *"
+# echo "**************************"
 
-# Nominal case:
-#   - deploy app & agent
-#   - apply config
-#   - check for traces
-echo "[run-auto-lib-injection TesCase#1] Deploying deployment"
-${BASE_DIR}/execFunction.sh deploy-app-auto
-echo "[run-auto-lib-injection TesCase#1] Deploying agents"
-${BASE_DIR}/execFunction.sh deploy-agents-auto
-echo "[run-auto-lib-injection TesCase#1] Trigger config"
-${BASE_DIR}/execFunction.sh trigger-config-auto
-echo "[run-auto-lib-injection TesCase#1] Running tests"
-${BASE_DIR}/execFunction.sh test-for-traces-auto
-${BASE_DIR}/execFunction.sh check-for-env-vars
-echo "[run-auto-lib-injection TesCase#1] Cleaning up resources"
-${BASE_DIR}/execFunction.sh cleanup-auto
-echo "[run-auto-lib-injection TesCase#1] Completed successfully"
+# # Nominal case:
+# #   - deploy app & agent
+# #   - apply config
+# #   - check for traces
+# echo "[run-auto-lib-injection TesCase#1] Deploying deployment"
+# ${BASE_DIR}/execFunction.sh deploy-app-auto
+# echo "[run-auto-lib-injection TesCase#1] Deploying agents"
+# ${BASE_DIR}/execFunction.sh deploy-agents-auto
+# echo "[run-auto-lib-injection TesCase#1] Trigger config"
+# ${BASE_DIR}/execFunction.sh trigger-config-auto
+# echo "[run-auto-lib-injection TesCase#1] Running tests"
+# ${BASE_DIR}/execFunction.sh test-for-traces-auto
+# ${BASE_DIR}/execFunction.sh check-for-env-vars
+# echo "[run-auto-lib-injection TesCase#1] Cleaning up resources"
+# ${BASE_DIR}/execFunction.sh cleanup-auto
+# echo "[run-auto-lib-injection TesCase#1] Completed successfully"
 
-echo "**************************"
-echo "*  Running Test Case #2  *"
-echo "**************************"
+# echo "**************************"
+# echo "*  Running Test Case #2  *"
+# echo "**************************"
 
-# Config change:
-#   - deploy app & agent
-#   - apply config
-#   - check for traces
-#   - apply different tracers config
-#   - check for traces
-echo "[run-auto-lib-injection TesCase#2] Deploying deployment"
-${BASE_DIR}/execFunction.sh deploy-app-auto
-echo "[run-auto-lib-injection TesCase#2] Deploying agents"
-${BASE_DIR}/execFunction.sh deploy-agents-auto
-echo "[run-auto-lib-injection TesCase#2] Trigger default config"
-${BASE_DIR}/execFunction.sh trigger-config-auto
-echo "[run-auto-lib-injection TesCase#2] Running tests for default config"
-${BASE_DIR}/execFunction.sh test-for-traces-auto
-echo "[run-auto-lib-injection TesCase#2] Trigger config-1"
-CONFIG_NAME=config-1 ${BASE_DIR}/execFunction.sh trigger-config-auto
-echo "[run-auto-lib-injection TesCase#2] Running tests for config-1"
-${BASE_DIR}/execFunction.sh test-for-traces-auto
-CONFIG_NAME=config-1 ${BASE_DIR}/execFunction.sh check-for-env-vars
-echo "[run-auto-lib-injection TesCase#2] Cleaning up resources"
-${BASE_DIR}/execFunction.sh cleanup-auto
-echo "[run-auto-lib-injection TesCase#2] Completed successfully"
+# # Config change:
+# #   - deploy app & agent
+# #   - apply config
+# #   - check for traces
+# #   - apply different tracers config
+# #   - check for traces
+# echo "[run-auto-lib-injection TesCase#2] Deploying deployment"
+# ${BASE_DIR}/execFunction.sh deploy-app-auto
+# echo "[run-auto-lib-injection TesCase#2] Deploying agents"
+# ${BASE_DIR}/execFunction.sh deploy-agents-auto
+# echo "[run-auto-lib-injection TesCase#2] Trigger default config"
+# ${BASE_DIR}/execFunction.sh trigger-config-auto
+# echo "[run-auto-lib-injection TesCase#2] Running tests for default config"
+# ${BASE_DIR}/execFunction.sh test-for-traces-auto
+# echo "[run-auto-lib-injection TesCase#2] Trigger config-1"
+# CONFIG_NAME=config-1 ${BASE_DIR}/execFunction.sh trigger-config-auto
+# echo "[run-auto-lib-injection TesCase#2] Running tests for config-1"
+# ${BASE_DIR}/execFunction.sh test-for-traces-auto
+# CONFIG_NAME=config-1 ${BASE_DIR}/execFunction.sh check-for-env-vars
+# echo "[run-auto-lib-injection TesCase#2] Cleaning up resources"
+# ${BASE_DIR}/execFunction.sh cleanup-auto
+# echo "[run-auto-lib-injection TesCase#2] Completed successfully"
 
 echo "**************************"
 echo "*  Running Test Case #3  *"
