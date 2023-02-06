a257857e63e2c6a6f4023f047181ef4b25c1a239
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2023-02-06 14:17:17 +0100

    add function docs

--- lib-injection/src/test/shell/functions.sh
+++ lib-injection/src/test/shell/functions.sh
@@ -99,6 +99,9 @@ function use-admission-controller() {
 }
 
 ## FUNCTIONS
+
+# ensure-cluster creates a Kind cluster named "lib-injection-testing" if it doesn't exist.
+# It returns when the all the nodes are ready.
 function ensure-cluster() {
     if ! [[ "$(kind get clusters)" =~ "lib-injection-testing" ]] ;  then
         kind create cluster --image=kindest/node:v1.25.3@sha256:f52781bc0d7a19fb6c405c2af83abfeb311f130707a0e219175677e366cc45d1 --name lib-injection-testing --config "${SRC_DIR}/test/resources/kind-config.yaml"
@@ -109,6 +112,7 @@ function ensure-cluster() {
     kubectl wait --for=condition=Ready nodes --all --timeout=5m
 }
 
+# ensure-buildx creates a builder instance named "lib-injection-testing" or use it if it already exists.
 function ensure-buildx() {
     if ! [[ "$(docker buildx ls)" =~ "lib-injection-testing" ]] ;  then
         docker buildx create --name lib-injection-testing
@@ -116,6 +120,8 @@ function ensure-buildx() {
     docker buildx use lib-injection-testing
 }
 
+# deploy-operator installs the Datadog Cluster Agent via helm for manual library injection testing.
+# It returns when the Cluster Agent pod is ready.
 function deploy-operator() {
     operator_file=${BASE_DIR}/common/operator-helm-values.yaml
     if [ ${USE_UDS} -eq 1 ] ; then
@@ -135,6 +141,8 @@ function deploy-operator() {
     kubectl get pods
 }
 
+# deploy-operator installs the Datadog Cluster Agent via helm for auto library injection testing.
+# It returns when the Cluster Agent pod is ready.
 function deploy-operator-auto() {
     echo "[Deploy operator] Using Patcher"
     operator_file=${BASE_DIR}/common/operator-helm-values-auto.yaml
@@ -156,6 +164,7 @@ function deploy-operator-auto() {
     kubectl get pods
 }
 
+# deploy-test-agent installs the test agent pod.
 function deploy-test-agent() {
     echo "[Deploy] deploy-test-agent"
     kubectl apply -f "${SRC_DIR}/test/resources/dd-apm-test-agent-config.yaml"
@@ -166,6 +175,7 @@ function deploy-test-agent() {
     echo "[Deploy] deploy-test-agent done"
 }
 
+# deploy-agents-manual installs all the required agents for manual library injection testing.
 function deploy-agents-manual() {
     echo "[Deploy] deploy-agents"
     deploy-test-agent
@@ -175,6 +185,7 @@ function deploy-agents-manual() {
     fi
 }
 
+# deploy-agents-auto installs all the required agents for auto library injection testing.
 function deploy-agents-auto() {
     echo "[Deploy] deploy-agents"
     deploy-test-agent
@@ -182,6 +193,8 @@ function deploy-agents-auto() {
     deploy-operator-auto
 }
 
+# apply-config-auto applies an configuration change for auto injection.
+# It returns when the targeted deployment finishes the rollout.
 function apply-config-auto() {
     echo "[Auto Config] Triggering config change"
     config_name="${CONFIG_NAME:-config}"
@@ -195,6 +208,8 @@ function apply-config-auto() {
     echo "[Auto Config] apply-config-auto: done"
 }
 
+# deploy-app-manual installs a target app for manual library injection testing.
+# It returns when the app pod is ready.
 function deploy-app-manual() {
     app_name=my-app
     echo "[Deploy] deploy-app: ${app_name} . Using UDS: ${USE_UDS}. Using adm.controller: ${USE_ADMISSION_CONTROLLER}"
@@ -217,6 +232,8 @@ function deploy-app-manual() {
     echo "[Deploy] deploy-app done"
 }
 
+# deploy-app-auto installs a target app for auto library injection testing.
+# It returns when the deployment is available and the rollout is finished.
 function deploy-app-auto() {
     echo "[Deploy] deploy-app-auto: deploying app for library ${TEST_LIBRARY}"
     deployment_name=test-${TEST_LIBRARY}-deployment
@@ -237,6 +254,8 @@ function deploy-app-auto() {
     echo "[Deploy] deploy-app-auto: done"
 }
 
+# trigger-app-rolling-update starts a rolling update of the target deployment by injecting an environment variable.
+# It returns when the deployment is available and the rollout is finished. 
 function trigger-app-rolling-update() {
     echo "[Deploy] trigger-app-rolling-update: updating deployment app ${TEST_LIBRARY}"
     deployment_name=test-${TEST_LIBRARY}-deployment
@@ -251,6 +270,7 @@ function trigger-app-rolling-update() {
     echo "[Deploy] trigger-app-rolling-update: done"
 }
 
+# check-for-env-vars evaluates whether the expected tracer config is reflected in the env vars of the targeted pod.
 function check-for-env-vars() {
     kubectl get pods
     pod=$(latest-app-pod-auto)
@@ -263,6 +283,7 @@ function check-for-env-vars() {
     kubectl get ${pod} -oyaml | grep -A1 DD_TRACE_SAMPLE_RATE | grep ${trace_sample_rate}
 }
 
+# check-for-pod-metadata evaluates whether the expected admission labels and annotations are applied to the targeted pod.
 function check-for-pod-metadata() {
     kubectl get pods
     pod=$(latest-app-pod-auto)
@@ -278,19 +299,21 @@ function check-for-pod-metadata() {
     kubectl get ${pod} -ojson | jq .metadata.annotations | grep "admission.datadoghq.com/${library}-lib.config.v1"
 }
 
+# check-for-no-pod-metadata ensures the targeted pod doesn't have admission labels.
 function check-for-no-pod-metadata() {
     kubectl get pods
     pod=$(latest-app-pod-auto)
     echo "[Test] test for labels/annotations ${pod}"
     has_enabled_key=$(kubectl get ${pod} -ojson | jq .metadata.labels | jq 'has("admission.datadoghq.com/enabled")')
     if [[ $has_enabled_key != "false" ]]; then
-        echo "[Test] annotation 'admission.datadoghq.com/enabled' was unexpectedly applied to the pod!"
+        echo "[Test] label 'admission.datadoghq.com/enabled' was unexpectedly applied to the pod!"
         print-debug-info || true
         exit 1
     fi
     print-debug-info || true
 }
 
+# check-for-no-pod-metadata ensures that admission is disabled on the targeted pod.
 function check-for-disabled-pod-metadata() {
     kubectl get pods
     pod=$(latest-app-pod-auto)
@@ -302,6 +325,7 @@ function check-for-disabled-pod-metadata() {
     fi
 }
 
+# check-for-deploy-metadata evaluates whether the expected admission annotations are applied to the targeted deployment.
 function check-for-deploy-metadata() {
     deployment_name=test-${TEST_LIBRARY}-deployment
     echo "[Test] test for labels/annotations ${deployment_name}"
@@ -314,6 +338,7 @@ function check-for-deploy-metadata() {
     kubectl get deploy ${deployment_name} -ojson | jq .metadata.annotations | jq -e '."admission.datadoghq.com/rc.rev"' | grep ${rev}
 }
 
+# test-for-traces ensures that the test agent has received traces.
 function test-for-traces() {
     sleep 60 && kubectl get pods
     echo "[Test] test for traces"
@@ -336,10 +361,12 @@ function test-for-traces() {
     echo "[Test] test-for-traces completed successfully"
 }
 
+# print-debug-info prints and zip debug information
 function print-debug-info() {
     if [[ $MODE == "manual" ]]; then print-debug-info-manual; else print-debug-info-auto; fi
 }
 
+# print-debug-info-auto prints and zip debug information for auto library injection tests
 function print-debug-info-auto() {
     log_dir=${BASE_DIR}/../logs_lib-injection
     mkdir -p ${log_dir}/pod
@@ -363,6 +390,7 @@ function print-debug-info-auto() {
     kubectl logs ${pod_cluster_name} > "${log_dir}/${pod_cluster_name}.log"
 }
 
+# print-debug-info-manual prints and zip debug information for manual library injection tests
 function print-debug-info-manual() {
     log_dir=${BASE_DIR}/../logs_lib-injection
     mkdir -p ${log_dir}/pod
@@ -394,6 +422,7 @@ function print-debug-info-manual() {
     fi
 }
 
+# build-and-push-test-app-image prepares the test app image.
 function build-and-push-test-app-image() { 
     current_dir=$(pwd)
     cd lib-injection/build/docker/$TEST_LIBRARY/$WEBLOG_VARIANT
@@ -401,6 +430,7 @@ function build-and-push-test-app-image() {
     cd $current_dir
 }
 
+# build-and-push-init-image prepares the init image.
 # Used only to local testing
 function build-and-push-init-image() {
     ensure-buildx
@@ -418,14 +448,17 @@ function build-and-push-init-image() {
    
 }
 
+# latest-app-pod-auto prints the latest pod name of the target app.
 function latest-app-pod-auto() {
     kubectl get pods --field-selector=status.phase=Running --sort-by=.metadata.creationTimestamp -l app=${TEST_LIBRARY}-app -o name | head -n 1
 }
 
+# remove-terminating-pods force removes any target pods stuck in a terminating state.
 function remove-terminating-pods() {
     for p in $(kubectl get pods | grep Terminating | awk '{print $1}'); do kubectl delete pod $p --grace-period=0 --force; done
 }
 
+# get-library prints the library language.
 function get-library() {
     local library
     [[ $TEST_LIBRARY = nodejs ]] && library=js || library=$TEST_LIBRARY
