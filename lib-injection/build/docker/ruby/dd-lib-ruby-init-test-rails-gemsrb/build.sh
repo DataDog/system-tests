54392f75a110a4f24c4dabc637246b70c33c0a6f
Tony Hsu <tonyc.t.hsu@gmail.com>
2023-02-24 18:41:04 +0100

    Add rails app with gems.rb

---
+++ lib-injection/build/docker/ruby/dd-lib-ruby-init-test-rails-gemsrb/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "${BUILDX_PLATFORMS}" ] ; then
+    BUILDX_PLATFORMS=`docker buildx imagetools inspect --raw ruby:3.1 | jq -r 'reduce (.manifests[] | [ .platform.os, .platform.architecture, .platform.variant ] | join("/") | sub("\\/$"; "")) as $item (""; . + "," + $item)' | sed 's/,//'`
+fi
+
+cp -r ../lib_injection_rails_app .
+
+docker buildx build --platform ${BUILDX_PLATFORMS} --tag ${LIBRARY_INJECTION_TEST_APP_IMAGE} --push .
+
+rm -rf lib_injection_rails_app
