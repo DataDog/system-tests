cc474ae48ee59e59288d10ecf5a7938f4d9b5f6a
roberto montero <roberto.montero@datadoghq.com>
2023-03-08 21:32:42 +0100

    fix conftest

--- onboarding/conftest.py
+++ onboarding/conftest.py
@@ -8,8 +8,9 @@ def pytest_generate_tests(metafunc):
     testing_machines = []
     with open("pulumi.output.json", "r") as f:
         obj = json.load(f)
-        machine_desc = dict()
+
         for key, value in obj.items():
+            machine_desc = dict()
             machine_desc["private_ip"] = value
             machine_desc["name"] = key
             testing_machines.append(machine_desc)
