be6251ca3c355aa64e69488fac5fe4cc350b1c1a
Dino Oliva <dino.oliva@datadoghq.com>
2023-03-12 17:17:17 -0700

    Skip libraries that do not currently support the expected behavior for 128-bit TraceIds.

--- parametric/test_128_bit_traceids.py
+++ parametric/test_128_bit_traceids.py
@@ -5,6 +5,7 @@ from parametric.utils.test_agent import get_span
 
 parametrize = pytest.mark.parametrize
 
+
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -36,6 +37,7 @@ def test_datadog_propagation_D001(test_agent, test_library):
     validate_dd_p_tid(dd_p_tid)
     assert dd_p_tid == "640cfd8d00000000"
 
+
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -44,7 +46,7 @@ def test_datadog_propagation_D001(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,
         }
     ],
-)    
+)
 def test_datadog_generation_64_bit_D002(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in
     datadog headers, and populated in trace data.
@@ -61,6 +63,13 @@ def test_datadog_generation_64_bit_D002(test_agent, test_library):
     assert int(header_trace_id, 10) == span_trace_id
     assert dd_p_tid is None
 
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -69,7 +78,7 @@ def test_datadog_generation_64_bit_D002(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,
         }
     ],
-)    
+)
 def test_datadog_generation_128_bit_D003(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in
     datadog headers, and populated in trace data.
@@ -86,6 +95,12 @@ def test_datadog_generation_128_bit_D003(test_agent, test_library):
     validate_dd_p_tid(dd_p_tid)
 
 
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -102,19 +117,18 @@ def test_b3single_propagation_D004(test_agent, test_library):
     with test_library:
         headers = make_single_request_and_get_inject_headers(
             test_library,
-            [
-                ["b3", "640cfd8d00000000abcdefab12345678-000000003ade68b1-1"],
-            ],
+            [["b3", "640cfd8d00000000abcdefab12345678-000000003ade68b1-1"],],
         )
     span = get_span(test_agent)
     trace_id = span.get("trace_id")
     dd_p_tid = span["meta"].get("_dd.p.tid")
     b3 = headers["b3"]
-    
+
     assert trace_id == int("abcdefab12345678", 16)
     assert dd_p_tid == "640cfd8d00000000"
-    assert (len(b3) > 32 + 16)
+    assert len(b3) > 32 + 16
     check_128_bit_trace_id(b3[0:32], trace_id, dd_p_tid)
+
 
 @pytest.mark.parametrize(
     "library_env",
@@ -124,7 +138,7 @@ def test_b3single_propagation_D004(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,
         }
     ],
-)    
+)
 def test_b3single_generation_64_bit_D005(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in B3
     single-header, and populated in trace data.
@@ -133,11 +147,16 @@ def test_b3single_generation_64_bit_D005(test_agent, test_library):
         headers = make_single_request_and_get_inject_headers(test_library, [])
     span = get_span(test_agent)
     b3 = headers["b3"]
-    assert(len(b3) > 16 + 16)
-    check_64_bit_trace_id(b3[0:16],
-                          span.get("trace_id"),
-                          span["meta"].get("_dd.p.tid"))
+    assert len(b3) > 16 + 16
+    check_64_bit_trace_id(b3[0:16], span.get("trace_id"), span["meta"].get("_dd.p.tid"))
 
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -146,7 +165,7 @@ def test_b3single_generation_64_bit_D005(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,
         }
     ],
-)    
+)
 def test_b3single_generation_128_bit_D006(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in B3
     single-header, and populated in trace data.
@@ -156,10 +175,18 @@ def test_b3single_generation_128_bit_D006(test_agent, test_library):
     span = get_span(test_agent)
     b3 = headers["b3"]
 
-    assert(len(b3) > 32 + 16)
-    check_128_bit_trace_id(b3[0:32],
-                           span.get("trace_id"),
-                           span["meta"].get("_dd.p.tid"))
+    assert len(b3) > 32 + 16
+    check_128_bit_trace_id(
+        b3[0:32], span.get("trace_id"), span["meta"].get("_dd.p.tid")
+    )
+
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -189,6 +216,7 @@ def test_b3multi_propagation_D007(test_agent, test_library):
     assert dd_p_tid == "640cfd8d00000000"
     check_128_bit_trace_id(headers["x-b3-traceid"], trace_id, dd_p_tid)
 
+
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -197,7 +225,7 @@ def test_b3multi_propagation_D007(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,
         }
     ],
-)    
+)
 def test_b3multi_generation_64_bit_D008(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in B3
     multi-headers, and populated in trace data.
@@ -205,10 +233,17 @@ def test_b3multi_generation_64_bit_D008(test_agent, test_library):
     with test_library:
         headers = make_single_request_and_get_inject_headers(test_library, [])
     span = get_span(test_agent)
-    check_64_bit_trace_id(headers["x-b3-traceid"],
-                          span.get("trace_id"),
-                          span["meta"].get("_dd.p.tid"))
+    check_64_bit_trace_id(
+        headers["x-b3-traceid"], span.get("trace_id"), span["meta"].get("_dd.p.tid")
+    )
 
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -217,7 +252,7 @@ def test_b3multi_generation_64_bit_D008(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,
         }
     ],
-)    
+)
 def test_b3multi_generation_128_bit_D009(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in B3
     multi-headers, and populated in trace data.
@@ -225,10 +260,17 @@ def test_b3multi_generation_128_bit_D009(test_agent, test_library):
     with test_library:
         headers = make_single_request_and_get_inject_headers(test_library, [])
     span = get_span(test_agent)
-    check_128_bit_trace_id(headers["x-b3-traceid"],
-                           span.get("trace_id"),
-                           span["meta"].get("_dd.p.tid"))
+    check_128_bit_trace_id(
+        headers["x-b3-traceid"], span.get("trace_id"), span["meta"].get("_dd.p.tid")
+    )
 
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -246,7 +288,10 @@ def test_w3c_propagation_D010(test_agent, test_library):
         headers = make_single_request_and_get_inject_headers(
             test_library,
             [
-                ["traceparent", "00-640cfd8d00000000abcdefab12345678-000000003ade68b1-01"],
+                [
+                    "traceparent",
+                    "00-640cfd8d00000000abcdefab12345678-000000003ade68b1-01",
+                ],
             ],
         )
     span = get_span(test_agent)
@@ -259,6 +304,8 @@ def test_w3c_propagation_D010(test_agent, test_library):
     assert len(traceparent) == 55
     check_128_bit_trace_id(traceparent[3:35], trace_id, dd_p_tid)
 
+
+@pytest.mark.skip_library("java", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -267,7 +314,7 @@ def test_w3c_propagation_D010(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,
         }
     ],
-)    
+)
 def test_w3c_generation_64_bit_D011(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in W3C
     headers, and populated in trace data.
@@ -277,10 +324,17 @@ def test_w3c_generation_64_bit_D011(test_agent, test_library):
     span = get_span(test_agent)
     traceparent = headers["traceparent"]
     assert len(traceparent) == 55
-    check_64_bit_trace_id(traceparent[3:35],
-                          span.get("trace_id"),
-                          span["meta"].get("_dd.p.tid"))
+    check_64_bit_trace_id(
+        traceparent[3:35], span.get("trace_id"), span["meta"].get("_dd.p.tid")
+    )
 
+
+@pytest.mark.skip_library("dotnet", "not implemented")
+@pytest.mark.skip_library("golang", "not implemented")
+@pytest.mark.skip_library("java", "not implemented")
+@pytest.mark.skip_library("nodejs", "not implemented")
+@pytest.mark.skip_library("python", "not implemented")
+@pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -289,7 +343,7 @@ def test_w3c_generation_64_bit_D011(test_agent, test_library):
             "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,
         }
     ],
-)    
+)
 def test_w3c_generation_128_bit_D012(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in W3C
     headers, and populated in trace data.
@@ -299,22 +353,26 @@ def test_w3c_generation_128_bit_D012(test_agent, test_library):
     span = get_span(test_agent)
     traceparent = headers["traceparent"]
     assert len(traceparent) == 55
-    check_128_bit_trace_id(traceparent[3:35],
-                           span.get("trace_id"),
-                           span["meta"].get("_dd.p.tid"))
+    check_128_bit_trace_id(
+        traceparent[3:35], span.get("trace_id"), span["meta"].get("_dd.p.tid")
+    )
 
 
 ZERO8 = "00000000"
 ZERO16 = ZERO8 + ZERO8
 
+
 def check_64_bit_trace_id(header_trace_id, span_trace_id, dd_p_tid):
     """Ensure that 128-bit TraceIds are properly formatted and populated in
     trace data.
     """
-    assert len(header_trace_id) == 16 or (len(header_trace_id) == 32 and header_trace_id[0:16] == ZERO16)
+    assert len(header_trace_id) == 16 or (
+        len(header_trace_id) == 32 and header_trace_id[0:16] == ZERO16
+    )
     assert int(header_trace_id, 16) == span_trace_id
     assert dd_p_tid is None
-    
+
+
 def check_128_bit_trace_id(header_trace_id, span_trace_id, dd_p_tid):
     """Ensure that 128-bit TraceIds are well-formed and populated in trace
     data.
@@ -325,6 +383,7 @@ def check_128_bit_trace_id(header_trace_id, span_trace_id, dd_p_tid):
     validate_dd_p_tid(dd_p_tid)
     assert header_trace_id.startswith(dd_p_tid)
 
+
 def validate_dd_p_tid(dd_p_tid):
     """Validate that dd_p_tid is well-formed.
     """
@@ -333,4 +392,4 @@ def validate_dd_p_tid(dd_p_tid):
     assert dd_p_tid != ZERO16
     assert dd_p_tid[8:16] == ZERO8
     # add check that dd_p_tid[0:8] is a timestamp
-    assert int(dd_p_tid[0:8], 16) > 0x640cfd8c
+    assert int(dd_p_tid[0:8], 16) > 0x640CFD8C
