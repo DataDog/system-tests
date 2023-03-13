600a9c0170a8aafad089f670fe7f4700d00baad7
Dino Oliva <dino.oliva@datadoghq.com>
2023-03-13 14:37:11 -0700

    Rename tests with clearer naming convention. Also change environment variable values to be strings.

--- parametric/test_128_bit_traceids.py
+++ parametric/test_128_bit_traceids.py
@@ -7,9 +7,9 @@ parametrize = pytest.mark.parametrize
 
 
 @pytest.mark.parametrize(
-    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "Datadog", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "Datadog", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_datadog_propagation_D001(test_agent, test_library):
+def test_datadog_128_bit_propagation_D001(test_agent, test_library):
     """ Ensure that external 128-bit TraceIds are properly propagated in Datadog
     headers.
     """
@@ -33,9 +33,9 @@ def test_datadog_propagation_D001(test_agent, test_library):
 
 
 @pytest.mark.parametrize(
-    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "Datadog", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "Datadog", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_datadog_generation_64_bit_D002(test_agent, test_library):
+def test_datadog_128_bit_generation_disabled_D002(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in
     datadog headers, and populated in trace data.
     """
@@ -47,6 +47,7 @@ def test_datadog_generation_64_bit_D002(test_agent, test_library):
     span_trace_id = span.get("trace_id")
     dd_p_tid = span["meta"].get("_dd.p.tid")
 
+    # Note: length of 2^64-1 encoded as decimal is 20.
     assert len(header_trace_id) < 21
     assert int(header_trace_id, 10) == span_trace_id
     assert dd_p_tid is None
@@ -59,9 +60,9 @@ def test_datadog_generation_64_bit_D002(test_agent, test_library):
 @pytest.mark.skip_library("python", "not implemented")
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
-    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "Datadog", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,}],
+    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "Datadog", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "true",}],
 )
-def test_datadog_generation_128_bit_D003(test_agent, test_library):
+def test_datadog128_bit_generation_enabled_D003(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in
     datadog headers, and populated in trace data.
     """
@@ -85,9 +86,9 @@ def test_datadog_generation_128_bit_D003(test_agent, test_library):
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
-    [{"DD_TRACE_PROPAGATION_STYLE": "B3 single header", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    [{"DD_TRACE_PROPAGATION_STYLE": "B3 single header", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_b3single_propagation_D004(test_agent, test_library):
+def test_b3single_128_bit_propagation_D004(test_agent, test_library):
     """Ensure that external 128-bit TraceIds are properly propagated in B3
     single-header.
     """
@@ -110,9 +111,9 @@ def test_b3single_propagation_D004(test_agent, test_library):
 @pytest.mark.skip_library("java", "Issue: Java doesn't pad the trace-id to length of 16 or 32 lower-hex characters")
 @pytest.mark.parametrize(
     "library_env",
-    [{"DD_TRACE_PROPAGATION_STYLE": "B3 single header", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    [{"DD_TRACE_PROPAGATION_STYLE": "B3 single header", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_b3single_generation_64_bit_D005(test_agent, test_library):
+def test_b3single_128_bit_generation_disabled_D005(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in B3
     single-header, and populated in trace data.
     """
@@ -120,6 +121,7 @@ def test_b3single_generation_64_bit_D005(test_agent, test_library):
         headers = make_single_request_and_get_inject_headers(test_library, [])
     span = get_span(test_agent)
     b3 = headers["b3"]
+
     assert len(b3) > 16 + 16
     check_64_bit_trace_id(b3[0:16], span.get("trace_id"), span["meta"].get("_dd.p.tid"))
 
@@ -132,9 +134,9 @@ def test_b3single_generation_64_bit_D005(test_agent, test_library):
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
-    [{"DD_TRACE_PROPAGATION_STYLE": "B3 single header", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,}],
+    [{"DD_TRACE_PROPAGATION_STYLE": "B3 single header", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "true",}],
 )
-def test_b3single_generation_128_bit_D006(test_agent, test_library):
+def test_b3single128_bit_generation_enabled_D006(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in B3
     single-header, and populated in trace data.
     """
@@ -154,9 +156,9 @@ def test_b3single_generation_128_bit_D006(test_agent, test_library):
 @pytest.mark.skip_library("python", "not implemented")
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
-    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "b3multi", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "b3multi", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_b3multi_propagation_D007(test_agent, test_library):
+def test_b3multi_128_bit_propagation_D007(test_agent, test_library):
     """Ensure that external 128-bit TraceIds are properly propagated in B3
     multi-headers.
     """
@@ -176,9 +178,9 @@ def test_b3multi_propagation_D007(test_agent, test_library):
 @pytest.mark.skip_library("python", "Issue: Python doesn't pad the trace-id to length of 16 or 32 lower-hex characters")
 @pytest.mark.skip_library("java", "Issue: Java doesn't pad the trace-id to length of 16 or 32 lower-hex characters")
 @pytest.mark.parametrize(
-    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "b3multi", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "b3multi", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_b3multi_generation_64_bit_D008(test_agent, test_library):
+def test_b3multi_128_bit_generation_disabled_D008(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in B3
     multi-headers, and populated in trace data.
     """
@@ -195,9 +197,9 @@ def test_b3multi_generation_64_bit_D008(test_agent, test_library):
 @pytest.mark.skip_library("python", "not implemented")
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
-    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "b3multi", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,}],
+    "library_env", [{"DD_TRACE_PROPAGATION_STYLE": "b3multi", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "true",}],
 )
-def test_b3multi_generation_128_bit_D009(test_agent, test_library):
+def test_b3multi128_bit_generation_enabled_D009(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in B3
     multi-headers, and populated in trace data.
     """
@@ -215,9 +217,9 @@ def test_b3multi_generation_128_bit_D009(test_agent, test_library):
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
-    [{"DD_TRACE_PROPAGATION_STYLE": "tracecontext", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    [{"DD_TRACE_PROPAGATION_STYLE": "tracecontext", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_w3c_propagation_D010(test_agent, test_library):
+def test_w3c_128_bit_propagation_D010(test_agent, test_library):
     """Ensure that external 128-bit TraceIds are properly propagated in W3C
     headers.
     """
@@ -236,12 +238,13 @@ def test_w3c_propagation_D010(test_agent, test_library):
     check_128_bit_trace_id(traceparent[3:35], trace_id, dd_p_tid)
 
 
+@pytest.mark.skip_library("python", "Issue: Python doesn't pad the trace-id to length of 16 or 32 lower-hex characters")
 @pytest.mark.skip_library("java", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
-    [{"DD_TRACE_PROPAGATION_STYLE": "tracecontext", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": False,}],
+    [{"DD_TRACE_PROPAGATION_STYLE": "tracecontext", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "false",}],
 )
-def test_w3c_generation_64_bit_D011(test_agent, test_library):
+def test_w3c_128_bit_generation_disabled_D011(test_agent, test_library):
     """Ensure that 64-bit TraceIds are properly generated, propagated in W3C
     headers, and populated in trace data.
     """
@@ -249,6 +252,7 @@ def test_w3c_generation_64_bit_D011(test_agent, test_library):
         headers = make_single_request_and_get_inject_headers(test_library, [])
     span = get_span(test_agent)
     traceparent = headers["traceparent"]
+
     assert len(traceparent) == 55
     check_64_bit_trace_id(traceparent[3:35], span.get("trace_id"), span["meta"].get("_dd.p.tid"))
 
@@ -261,9 +265,9 @@ def test_w3c_generation_64_bit_D011(test_agent, test_library):
 @pytest.mark.skip_library("python_http", "not implemented")
 @pytest.mark.parametrize(
     "library_env",
-    [{"DD_TRACE_PROPAGATION_STYLE": "tracecontext", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": True,}],
+    [{"DD_TRACE_PROPAGATION_STYLE": "tracecontext", "DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED": "true",}],
 )
-def test_w3c_generation_128_bit_D012(test_agent, test_library):
+def test_w3c128_bit_generation_enabled_D012(test_agent, test_library):
     """Ensure that 128-bit TraceIds are properly generated, propagated in W3C
     headers, and populated in trace data.
     """
@@ -271,6 +275,7 @@ def test_w3c_generation_128_bit_D012(test_agent, test_library):
         headers = make_single_request_and_get_inject_headers(test_library, [])
     span = get_span(test_agent)
     traceparent = headers["traceparent"]
+
     assert len(traceparent) == 55
     check_128_bit_trace_id(traceparent[3:35], span.get("trace_id"), span["meta"].get("_dd.p.tid"))
 
