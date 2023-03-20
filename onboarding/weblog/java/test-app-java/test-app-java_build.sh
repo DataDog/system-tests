92820ae0e66e4c5219f11b9fe7794699a7c090e9
roberto montero <roberto.montero@datadoghq.com>
2023-03-20 09:10:00 +0100

    change name

--- onboarding/weblog/java/test-app-java/test-app-java_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export APP_DOCKER_IMAGE_REPO="test" 
-export DOCKER_IMAGE_WEBLOG_TAG="test_tests"
-cd ../lib-injection/build/docker/java/dd-lib-java-init-test-app/
-./gradlew build
--- onboarding/weblog/java/test-app-java/test-app-java_run.sh
+++ onboarding/weblog/java/test-app-java/test-app-java_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "START RUN APP"
-
+tar xvf test-app-java.tar
+./gradlew build
+cp build/libs/k8s-lib-injection-app-0.0.1-SNAPSHOT.jar .
 sudo sed -i "s/MY_USER/$(whoami)/g" test-app-java.service 
 sudo cp test-app-java.service /etc/systemd/system/test-app-java.service
 sudo systemctl daemon-reload
