c066c1584badecae3e848dc90fdd95fbc9a3692b
Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>
2023-03-21 17:01:11 +0100

    Update version
    Signed-off-by: Juanjo Alvarez <juanjo.alvarezmartinez@datadoghq.com>

--- tests/appsec/test_event_tracking.py
+++ tests/appsec/test_event_tracking.py
@@ -87,7 +87,7 @@ class Test_UserLoginFailureEvent:
         interfaces.library.validate_spans(self.r, validate_user_login_failure_tags)
 
 
-@released(dotnet="?", golang="1.47.0", java="1.8.0", nodejs="?", php_appsec="0.6.0", python="1.9.0", ruby="1.9.0")
+@released(dotnet="?", golang="1.47.0", java="1.8.0", nodejs="?", php_appsec="0.6.0", python="1.10.0", ruby="1.9.0")
 @irrelevant(_is_spring_native_weblog, reason="GraalVM. Tracing support only")
 @coverage.basic
 class Test_CustomEvent:
