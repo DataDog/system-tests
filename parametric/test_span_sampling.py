191972a789a7b67455788c349926e2a88b8a96cd
Marco Costa <marco.costa@datadoghq.com>
2023-03-23 12:30:34 -0700

    Skip manual drop test

--- parametric/test_span_sampling.py
+++ parametric/test_span_sampling.py
@@ -13,7 +13,7 @@ import json
 
 
 @pytest.mark.skip_library("dotnet", "Not implemented")
-@pytest.mark.skip_library("ruby", "Bug: _dd.span_sampling.max_per_second is always set in Ruby")
+@pytest.mark.skip_library("ruby", "Issue: _dd.span_sampling.max_per_second is always set in Ruby")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -38,7 +38,7 @@ def test_single_rule_match_span_sampling_sss001(test_agent, test_library):
 
 
 @pytest.mark.skip_library("dotnet", "Not implemented")
-@pytest.mark.skip_library("ruby", "Bug: _dd.span_sampling.max_per_second is always set in Ruby")
+@pytest.mark.skip_library("ruby", "Issue: _dd.span_sampling.max_per_second is always set in Ruby")
 @pytest.mark.parametrize(
     "library_env",
     [{"DD_SPAN_SAMPLING_RULES": json.dumps([{"service": "webse*", "name": "web.re?uest"}]), "DD_TRACE_SAMPLE_RATE": 0}],
@@ -81,7 +81,7 @@ def test_single_rule_no_match_span_sampling_sss003(test_agent, test_library):
 
 
 @pytest.mark.skip_library("dotnet", "Not implemented")
-@pytest.mark.skip_library("ruby", "Bug: _dd.span_sampling.max_per_second is always set in Ruby")
+@pytest.mark.skip_library("ruby", "Issue: _dd.span_sampling.max_per_second is always set in Ruby")
 @pytest.mark.parametrize(
     "library_env", [{"DD_SPAN_SAMPLING_RULES": json.dumps([{"service": "webserver"}]), "DD_TRACE_SAMPLE_RATE": 0}],
 )
@@ -118,7 +118,7 @@ def test_single_rule_only_name_pattern_no_match_span_sampling_sss005(test_agent,
 
 
 @pytest.mark.skip_library("dotnet", "Not implemented")
-@pytest.mark.skip_library("ruby", "Bug: _dd.span_sampling.max_per_second is always set in Ruby")
+@pytest.mark.skip_library("ruby", "Issue: _dd.span_sampling.max_per_second is always set in Ruby")
 @pytest.mark.parametrize(
     "library_env",
     [
@@ -309,6 +309,7 @@ def test_keep_span_with_stats_computation_sss010(test_agent, test_library):
 
 @pytest.mark.skip_library("dotnet", "Not implemented")
 @pytest.mark.skip_library("golang", "The Go tracer does not have a way to modulate trace sampling once started")
+@pytest.mark.skip_library("ruby", "Issue: does not respect manual.drop or manual.keep span tags")
 @pytest.mark.parametrize(
     "library_env",
     [
