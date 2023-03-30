815d5e88cbad6de11097968ac6836a1e1b74dcb6
Emile-Hugo SPIR <emile.spir@datadoghq.com>
2023-03-30 17:25:22 +0200

    Disable the test on Node.js until library is fixed

--- tests/appsec/waf/test_miscs.py
+++ tests/appsec/waf/test_miscs.py
@@ -93,7 +93,8 @@ class Test_MultipleAttacks:
 
 
 @released(golang={"gin": "1.37.0", "echo": "1.36.0", "chi": "1.36.0", "*": "1.34.0"})
-@released(dotnet="1.28.6", java="0.87.0", nodejs="2.0.0", php_appsec="0.1.0", python="1.1.0")
+@released(dotnet="1.28.6", java="0.87.0", php_appsec="0.1.0", python="1.1.0")
+@bug(library="nodejs", reason="current releases are buggy")
 @missing_feature(context.weblog_variant == "spring-boot-native", reason="GraalVM. Tracing support only")
 @missing_feature(context.weblog_variant == "spring-boot-3-native", reason="GraalVM. Tracing support only")
 @coverage.good
