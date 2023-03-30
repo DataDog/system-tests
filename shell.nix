162891565b052d27d3a34dddabd71b37b069f1d2
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2023-03-30 13:30:01 +0200

    Add a basic `shell.nix` file
    This is entirely optional to use but for nixpkgs users this is a really
    easy way to get things up and running. With just:
    
    ```
    nix-shell
    ```
    
    All needed dependencies get automatically pulled in, dropping the
    user in a ready-to-use shell.
    
    Then as usual:
    
    ```
    pip install -r requirements.txt
    ```
    
    Puts everything in a `vendor/python` directory, which behaves like a
    virtualenv. Should it need to be cleaned up, e.g to start form scratch,
    a simple `rm -rf vendor/python` is sufficient.
    
    It also pins to a specific commit for the channel, so that tool versions
    are always consistent.

---
+++ shell.nix
@@ -0,0 +1,44 @@
+{
+  # use the environment channel
+  pkgs ? import <nixpkgs> {},
+
+  # use a pinned package state
+  pinned ? import(fetchTarball("https://github.com/NixOS/nixpkgs/archive/14d9b465c71.tar.gz")) {},
+}:
+let
+  # get these python packages from nix
+  python_packages = python-packages: [
+    python-packages.pip
+  ];
+
+  # use this pyton version, and include the abvoe packages
+  python = pinned.python39.withPackages python_packages;
+
+  # control compiler version (e.g for packages built form source)
+  llvm = pinned.llvmPackages_12;
+  clang = pinned.clang_12;
+in llvm.stdenv.mkDerivation {
+  # unique project name for this environment derivation
+  name = "system-tests.devshell";
+
+  buildInputs = [
+    # version to use + default packages are declared above
+    python
+
+    # linters
+    pinned.shellcheck
+
+    # have docker-compose at hand
+    pinned.docker-compose
+  ];
+
+  shellHook = ''
+    export PYTHON_VERSION="$(python -c 'import platform; import re; print(re.sub(r"\.\d+$", "", platform.python_version()))')"
+
+    # replicate virtualenv behaviour
+    export PIP_PREFIX="$PWD/vendor/python/$PYTHON_VERSION/packages"
+    export PYTHONPATH="$PIP_PREFIX/lib/python$PYTHON_VERSION/site-packages:$PYTHONPATH"
+    unset SOURCE_DATE_EPOCH
+    export PATH="$PIP_PREFIX/bin:$PATH"
+  '';
+}
