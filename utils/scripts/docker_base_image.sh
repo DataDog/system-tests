2a3af8e7a3f12e329cd726263bcc44111f4da211
roberto montero <roberto.montero@datadoghq.com>
2023-03-31 11:30:14 +0200

    check

--- utils/scripts/docker_base_image.sh
+++ utils/scripts/docker_base_image.sh
@@ -9,11 +9,13 @@ target_dir="$2"
 mkdir --parent $target_dir
 
 echo "Extracting Docker base image $image"
- 
+echo "Pas1" 
 docker save -o $target_dir/image.tar $image 
+echo "Pas2" 
 tar xvf $target_dir/image.tar -C $target_dir
+echo "Pas3" 
 layers=$(jq -r '.[0].Layers[]' $target_dir/manifest.json)
-
+echo "Pas4" 
 for i in $layers; do
     tar xvf $target_dir/$i -C $target_dir
 done 
