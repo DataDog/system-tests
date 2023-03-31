3a1f0f01c26d653159c5187a97fe5fe9eaf21e1d
roberto montero <roberto.montero@datadoghq.com>
2023-03-31 11:23:44 +0200

    use docker images

---
+++ utils/scripts/docker_base_image.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# https://rmannibucau.metawerx.net/post/docker-extracts-fileystem-with-bash
+# 1.
+set -eu
+
+image="$1"
+target_dir="$2"
+
+mkdir --parent $target_dir
+
+echo "Extracting Docker base image $image"
+ 
+docker save -o $target_dir/image.tar $image 
+tar xvf $target_dir/image.tar -C $target_dir
+layers=$(jq -r '.[0].Layers[]' $target_dir/manifest.json)
+
+for i in $layers; do
+    tar xvf $target_dir/$i -C $target_dir
+done 
+
+#Done! clean
+rm -rf $target_dir/image.tar $target_dir/manifest.json $target_dir/oci-layout  $target_dir/index.json 
+rm -rf $target_dir/blobs/
+
+
