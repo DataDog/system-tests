6f1be99021b196fa42b8e345058f1b441aef0008
Kyle Verhoog <kyle@verhoog.ca>
2023-04-04 16:55:25 -0400

    parametric: ignore context, parallelize tests

--- parametric/run.sh
+++ parametric/run.sh
@@ -5,4 +5,4 @@ ARGS=$*
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 PARENT_DIR=$(dirname $PWD)
-python -m pytest -c $PWD/conftest.py $ARGS
+python -m pytest -n auto -c $PWD/conftest.py $ARGS
