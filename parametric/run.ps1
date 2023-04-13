31ed45f79099fde35c8c8ec502a2f3d8ec981ae0
rochdev <roch.devost@gmail.com>
2023-04-13 17:25:26 -0400

    try to parallelize more than the number of gha cores

--- parametric/run.ps1
+++ parametric/run.ps1
@@ -5,4 +5,4 @@ param
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 $env:PARENT_DIR = [System.IO.Path]::GetDirectoryName($pwd)
-python -m pytest -c $PWD/conftest.py $ARGS
+python -m pytest -n 4 -c $PWD/conftest.py $ARGS
--- parametric/run.sh
+++ parametric/run.sh
@@ -6,4 +6,4 @@ ARGS=$*
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 PARENT_DIR=$(dirname $PWD)
-python -m pytest -n auto -c $PWD/conftest.py $ARGS
+python -m pytest -n 4 -c $PWD/conftest.py $ARGS
