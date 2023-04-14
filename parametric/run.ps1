35d8052dd26d8ca81a1281a8e634f85bcd102fe9
rochdev <roch.devost@gmail.com>
2023-04-14 08:27:06 -0400

    add additional options for splitting up tests

--- parametric/run.ps1
+++ parametric/run.ps1
@@ -5,4 +5,7 @@ param
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 $env:PARENT_DIR = [System.IO.Path]::GetDirectoryName($pwd)
-python -m pytest -n auto -c $PWD/conftest.py $ARGS
+$n = $env:PYTEST_N ?? "auto";
+$splits = $env:PYTEST_SPLITS ?? 1;
+$group = $env:PYTEST_GROUP ?? 1;
+python -m pytest -n $n --splits $splits --group $group -c $PWD/conftest.py $ARGS
--- parametric/run.sh
+++ parametric/run.sh
@@ -6,4 +6,7 @@ ARGS=$*
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 PARENT_DIR=$(dirname $PWD)
-python -m pytest -n auto -c $PWD/conftest.py $ARGS
+PYTEST_N=${PYTEST_N:-auto}
+PYTEST_SPLITS=${PYTEST_SPLITS:-1}
+PYTEST_GROUP=${PYTEST_GROUP:-1}
+python -m pytest -n $PYTEST_N --splits $PYTEST_SPLITS --group $PYTEST_GROUP -c $PWD/conftest.py $ARGS
