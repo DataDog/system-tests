af352f853b4714948ddebd7888c3febe9f74ea35
Santiago Mola <santiago.mola@datadoghq.com>
2023-04-14 12:21:54 +0200

    parametric: use python3.9 binary if available
    This should simplify setting the right python version in various
    scenarios, like having a system-wide Python 3.9 in Ubuntu, or having
    pyenv with multiple Python versions.

--- parametric/run.sh
+++ parametric/run.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
-
-ARGS=$*
+#!/usr/bin/env bash
+set -eu
 
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
-PARENT_DIR=$(dirname $PWD)
-python -m pytest -c $PWD/conftest.py $ARGS
+PARENT_DIR=$(dirname "$PWD")
+
+if python3.9 --version &> /dev/null; then
+  readonly PYTHON=python3.9
+else
+  readonly PYTHON=python
+fi
+
+exec "${PYTHON}" -m pytest -c "$PWD/conftest.py" "$@"
