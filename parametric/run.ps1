cad9f02802756b3a32852b5adc6b60127c5b5bae
rochdev <roch.devost@gmail.com>
2023-04-15 13:22:16 -0400

    move concurrency defaults so they also apply locally

--- parametric/run.ps1
+++ parametric/run.ps1
@@ -5,7 +5,33 @@ param
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 $env:PARENT_DIR = [System.IO.Path]::GetDirectoryName($pwd)
-$n = $env:PYTEST_N ?? "auto";
+$DEFAULT_N = "auto"
 
-# TODO: support splits/group when fixed
-python -m pytest -n $n -c $PWD/conftest.py $ARGS
+# FIXME: all languages should be supported
+if (![string]::IsNullOrEmpty($env:CLIENTS_ENABLED)) {
+    foreach ($client in $env:CLIENTS_ENABLED.split(',')) {
+        # default to "1" for languages with concurrency issues
+        if ($client -eq "dotnet" || $client -eq "python_http") {
+            $DEFAULT_N = "1"
+            break
+        }
+    }
+} else {
+    # default to "1" for all languages since that includes problematic languages
+    $DEFAULT_N = "1"
+}
+
+# TODO: default to "auto" when dotnet is fixed
+$PYTEST_N = $env:PYTEST_N ? $env:PYTEST_N : $DEFAULT_N
+
+$CMD = "python -m pytest -n $PYTEST_N"
+
+# FIXME: dotnet hangs when this plugin is enabled even when both "splits" and
+# "group" are set to "1" which should do effectively nothing.
+if (![string]::IsNullOrEmpty($env:PYTEST_SPLITS) -And ![string]::IsNullOrEmpty($env:PYTEST_GROUP)) {
+    $CMD = "$CMD --splits $env:PYTEST_SPLITS --group $env:PYTEST_GROUP"
+}
+
+$CMD = "$CMD -c $PWD/conftest.py $ARGS"
+
+Invoke-Expression $CMD
--- parametric/run.sh
+++ parametric/run.sh
@@ -6,18 +6,33 @@ ARGS=$*
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 PARENT_DIR=$(dirname $PWD)
+DEFAULT_N=auto
 
-# TODO: default to "auto" when dotnet is fixed
-PYTEST_N=${PYTEST_N:-1}
-
-cmd="python -m pytest -n $PYTEST_N"
-
-# FIXME: dotnet hangs when this plugin is enabled even when both splits and
-# group are set to "1" which should do effectively nothing.
-if [ "$PYTEST_SPLITS" ]; then
-    cmd="${cmd} --splits $PYTEST_SPLITS --group ${PYTEST_GROUP=1}"
+# FIXME: all languages should be supported
+if [ "${CLIENTS_ENABLED}" ]; then
+    for client in $(echo $CLIENTS_ENABLED | sed "s/,/ /g"); do
+        # default to "1" for languages with concurrency issues
+        if [[ "${client}" == "dotnet" || "${client}" == "python_http" ]]; then
+            DEFAULT_N=1
+            break
+        fi
+    done
+else
+    # default to "1" for all languages since that includes problematic languages
+    DEFAULT_N=1
 fi
 
-cmd="$cmd -c $PWD/conftest.py $ARGS"
+# TODO: default to "auto" when dotnet is fixed
+PYTEST_N=${PYTEST_N:-$DEFAULT_N}
 
-eval "$cmd"
+CMD="python -m pytest -n $PYTEST_N"
+
+# FIXME: dotnet hangs when this plugin is enabled even when both "splits" and
+# "group" are set to "1" which should do effectively nothing.
+if [[ "$PYTEST_SPLITS" && "$PYTEST_GROUP" ]]; then
+    CMD="${cmd} --splits $PYTEST_SPLITS --group $PYTEST_GROUP"
+fi
+
+CMD="$CMD -c $PWD/conftest.py $ARGS"
+
+eval "$CMD"
