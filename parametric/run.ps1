4399e57027a3133723709fbc6cbc90d9eb2418f6
rochdev <roch.devost@gmail.com>
2023-04-19 23:35:29 -0400

    rename environment variable for xdist worker count

--- parametric/run.ps1
+++ parametric/run.ps1
@@ -5,26 +5,26 @@ param
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
 $env:PARENT_DIR = [System.IO.Path]::GetDirectoryName($pwd)
-$DEFAULT_N = "auto"
+$DEFAULT_COUNT = "auto"
 
 # FIXME: all languages should be supported
 if (![string]::IsNullOrEmpty($env:CLIENTS_ENABLED)) {
     foreach ($client in $env:CLIENTS_ENABLED.split(',')) {
         # default to "1" for languages with concurrency issues
         if ($client -eq "dotnet" -or $client -eq "go" -or $client -eq "python_http") {
-            $DEFAULT_N = "1"
+            $DEFAULT_COUNT = "1"
             break
         }
     }
 } else {
     # default to "1" for all languages since that includes problematic languages
-    $DEFAULT_N = "1"
+    $DEFAULT_COUNT = "1"
 }
 
 # TODO: default to "auto" when dotnet is fixed
-$PYTEST_N = $env:PYTEST_N ? $env:PYTEST_N : $DEFAULT_N
+$PYTEST_WORKER_COUNT = $env:PYTEST_WORKER_COUNT ? $env:PYTEST_WORKER_COUNT : $DEFAULT_COUNT
 
-$CMD = "python -m pytest -n $PYTEST_N"
+$CMD = "python -m pytest -n $PYTEST_WORKER_COUNT"
 
 # FIXME: dotnet hangs when this plugin is enabled even when both "splits" and
 # "group" are set to "1" which should do effectively nothing.
