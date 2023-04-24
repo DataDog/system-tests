bd9f45d0fbfd01fcfcd95411d69732b4f35059fc
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-04-24 09:51:18 +0200

    php no body multipart

--- tests/appsec/test_blocking_addresses.py
+++ tests/appsec/test_blocking_addresses.py
@@ -2,7 +2,8 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import context, coverage, interfaces, irrelevant, missing_feature, released, rfc, scenarios, weblog
+from utils import (context, coverage, interfaces, irrelevant, missing_feature,
+                   released, rfc, scenarios, weblog)
 
 
 @released(cpp="?", dotnet="2.27.0", php_appsec="0.7.0", python="?", nodejs="?", golang="?", ruby="1.0.0")
@@ -86,6 +87,7 @@ class Test_BlockingAddresses:
         self.rbmp_req = weblog.post("/waf", files={"foo": (None, "bsldhkuqwgervf")})
 
     @missing_feature(context.library == "dotnet", reason="Don't support multipart yet")
+    @missing_feature(context.library == "php", reason="Don't support multipart yet")
     @missing_feature(context.library == "java", reason="Happens on a subsequent WAF run")
     def test_request_body_multipart(self):
         """can block on server.request.body (multipart/form-data variant)"""
