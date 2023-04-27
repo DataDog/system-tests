2ddb254f6b04ebdd412d0949fe7ecc1b2559a55c
Yang Song <yang.song@datadoghq.com>
2023-04-27 13:08:43 -0400

    Increase retries in get trace

--- utils/interfaces/_backend.py
+++ utils/interfaces/_backend.py
@@ -110,7 +110,7 @@ class _BackendInterfaceValidator(InterfaceValidator):
         data = self._wait_for_trace(
             rid=rid,
             trace_id=dd_trace_id,
-            retries=5,
+            retries=8,
             sleep_interval_multiplier=2.0,
             dd_api_key=dd_api_key,
             dd_app_key=dd_app_key,
