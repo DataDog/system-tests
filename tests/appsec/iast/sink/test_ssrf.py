11ec7234c50c9852776d8e6a8db99a81d43faedb
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2023-05-07 15:06:18 +0200

    SSRF vulnerabiilty detection system test implemented in nodejs

---
+++ tests/appsec/iast/sink/test_ssrf.py
@@ -0,0 +1,38 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2021 Datadog, Inc.
+
+import pytest
+from utils import context, coverage, released, missing_feature
+from ..iast_fixtures import SinkFixture
+
+if context.library == "cpp":
+    pytestmark = pytest.mark.skip("not relevant")
+
+
+@coverage.basic
+@released(dotnet="?", java="?", golang="?", php_appsec="?", python="?", ruby="?", nodejs="?")
+class TestCommandInjection:
+    """Test command injection detection."""
+
+    sink_fixture = SinkFixture(
+        vulnerability_type="SSRF",
+        http_method="POST",
+        insecure_endpoint="/iast/ssrf/test_insecure",
+        secure_endpoint="/iast/ssrf/test_secure",
+        data={"url": "https://www.datadoghq.com"},
+        location_map={"nodejs": "iast.js",},
+    )
+
+    def setup_insecure(self):
+        self.sink_fixture.setup_insecure()
+
+    def test_insecure(self):
+        self.sink_fixture.test_insecure()
+
+    def setup_secure(self):
+        self.sink_fixture.setup_secure()
+
+    @missing_feature(reason="Endpoint not implemented")
+    def test_secure(self):
+        self.sink_fixture.test_secure()
