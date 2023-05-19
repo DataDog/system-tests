b914919b3db4d8a02d9a40fdfebad75f89896988
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2023-05-19 10:38:24 +0200

    Improve IAST unvalidated redirect vulnerability

--- tests/appsec/iast/sink/test_unvalidated_redirect_forward.py
+++ tests/appsec/iast/sink/test_unvalidated_redirect_forward.py
@@ -18,8 +18,6 @@ def _expected_location():
             return "com.datadoghq.resteasy.IastSinkResource"
         if context.weblog_variant == "jersey-grizzly2":
             return "com.datadoghq.jersey.IastSinkResource"
-    if context.library.library == "nodejs":
-        return "iast/index.js"
 
 
 @coverage.basic
@@ -37,45 +35,25 @@ def _expected_location():
     }
 )
 class TestUnvalidatedRedirect:
-    """Verify Unvalidated redirect detection."""
+    """Verify Unvalidated redirect forward detection."""
 
-    sink_fixture_header = SinkFixture(
+    sink_fixture_forward = SinkFixture(
         vulnerability_type="UNVALIDATED_REDIRECT",
         http_method="POST",
-        insecure_endpoint="/iast/unvalidated_redirect/test_insecure_header",
-        secure_endpoint="/iast/unvalidated_redirect/test_secure_header",
-        data={"location": "http://dummy.location.com"},
-        location_map=_expected_location,
-    )
-    sink_fixture_redirect = SinkFixture(
-        vulnerability_type="UNVALIDATED_REDIRECT",
-        http_method="POST",
-        insecure_endpoint="/iast/unvalidated_redirect/test_insecure_redirect",
-        secure_endpoint="/iast/unvalidated_redirect/test_secure_redirect",
+        insecure_endpoint="/iast/unvalidated_redirect/test_insecure_forward",
+        secure_endpoint="/iast/unvalidated_redirect/test_secure_forward",
         data={"location": "http://dummy.location.com"},
         location_map=_expected_location,
     )
 
-    def setup_insecure_header(self):
-        self.sink_fixture_header.setup_insecure()
+    def setup_insecure_forward(self):
+        self.sink_fixture_forward.setup_insecure()
 
-    def test_insecure_header(self):
-        self.sink_fixture_header.test_insecure()
+    def test_insecure_forward(self):
+        self.sink_fixture_forward.test_insecure()
 
-    def setup_secure_header(self):
-        self.sink_fixture_header.setup_secure()
+    def setup_secure_forward(self):
+        self.sink_fixture_forward.setup_secure()
 
-    def test_secure_header(self):
-        self.sink_fixture_header.test_secure()
-
-    def setup_insecure_redirect(self):
-        self.sink_fixture_redirect.setup_insecure()
-
-    def test_insecure_redirect(self):
-        self.sink_fixture_redirect.test_insecure()
-
-    def setup_secure_redirect(self):
-        self.sink_fixture_redirect.setup_secure()
-
-    def test_secure_redirect(self):
-        self.sink_fixture_redirect.test_secure()
+    def test_secure_forward(self):
+        self.sink_fixture_forward.test_secure()
