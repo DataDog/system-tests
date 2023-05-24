a8969b18d92b9d06133fb48f0c3a20a281b50bd9
Matthieu Roux <matthieu.roux@datadoghq.com>
2023-05-24 17:40:24 +0200

    remove single user blocking test

--- tests/appsec/test_user_blocking_full_denylist.py
+++ tests/appsec/test_user_blocking_full_denylist.py
@@ -53,6 +53,23 @@ class Test_UserBlocking_FullDenylist:
         interfaces.library.validate_spans(self.r_nonblock, validator=validate_nonblock_user)
         interfaces.library.assert_no_appsec_event(self.r_nonblock)
 
+    def setup_blocking_test_single_usr(self):
+        self.r_block = weblog.get("/users", params={"user": 0})
+
+    def test_blocking_test_single_usr(self):
+        """Test with a denylisted user"""
+
+        def validate_blocking_test_single_usr(span):
+            """Check all fields are present in meta"""
+            assert span["meta"]["appsec.event"] == "true"
+            assert span["meta"]["appsec.blocked"] == "true"
+            assert span["meta"]["http.status_code"] == "403"
+            return True
+
+        assert self.r_block.status_code == 403
+        interfaces.library.assert_waf_attack(self.r_block, rule="block-users", address="usr.id")
+        interfaces.library.validate_spans(self.r_block, validator=validate_blocking_test_single_usr)
+
     def setup_blocking_test(self):
         interfaces.library.wait_for(self._remote_config_asm_payload, timeout=30)
         interfaces.library.wait_for(self._remote_config_is_applied, timeout=30)
