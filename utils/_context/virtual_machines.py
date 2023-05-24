c34f64cb00aab8103866043818c5bd751377bc41
roberto montero <roberto.montero@datadoghq.com>
2023-05-24 09:12:17 -0400

    debug subnets

--- utils/_context/virtual_machines.py
+++ utils/_context/virtual_machines.py
@@ -54,13 +54,14 @@ class TestedVirtualMachine:
         logger.info("start...")
         self.configure()
 
-        # logger.info("SUBNETS ID")
-        # logger.info(aws.ec2.get_subnets())
+        logger.info("SUBNETS ID")
+        subnetresult = aws.ec2.get_subnets()
+        Output.all(subnetresult.ids).apply(lambda args: pulumi_logger("vms_desc").info(f"{args[0]}"))
         server = aws.ec2.Instance(
             self.name,
             instance_type=self.aws_infra_config.instance_type,
             vpc_security_group_ids=self.aws_infra_config.vpc_security_group_ids,
-            # subnet_id=self.aws_infra_config.subnet_id,
+            subnet_id=self.aws_infra_config.subnet_id,
             key_name=self.aws_infra_config.keyPairName,
             ami=self.ec2_data["ami_id"],
             tags={"Name": self.name,},
