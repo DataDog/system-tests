b85eca0ecc755762ddf0b3110550b88128729be9
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-06-01 09:58:09 +0200

    Move all ruby apps inside the system test repo

--- utils/build/docker/ruby/rails32.Dockerfile
+++ utils/build/docker/ruby/rails32.Dockerfile
@@ -1,6 +1,13 @@
-FROM ghcr.io/datadog/system-tests-apps-ruby/rails32:latest
+FROM ghcr.io/datadog/dd-trace-rb/ruby:2.3.8-dd
 
+RUN curl -O https://rubygems.org/downloads/libv8-node-15.14.0.1-$(arch)-linux.gem && gem install libv8-node-15.14.0.1-$(arch)-linux.gem && rm libv8-node-15.14.0.1-$(arch)-linux.gem
+RUN mkdir -p /app
+
+WORKDIR /app
+
+COPY utils/build/docker/ruby/rails32/ .
 COPY utils/build/docker/ruby/install_ddtrace.sh binaries* /binaries/
+
 RUN /binaries/install_ddtrace.sh
 
 ENV DD_TRACE_HEADER_TAGS=user-agent
---
+++ utils/build/docker/ruby/rails32/app/controllers/system_test_controller.rb
@@ -0,0 +1,97 @@
+require 'datadog/kit/appsec/events'
+
+class SystemTestController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def root
+    render text: 'Hello, world!'
+  end
+
+  def waf
+    render text: 'Hello, world!'
+  end
+
+  def handle_path_params
+    render text: 'Hello, world!'
+  end
+
+  def status
+    render text: "Ok", status: params[:code]
+  end
+
+  def generate_spans
+    begin
+      repeats = Integer(request.params['repeats'] || 0)
+      garbage = Integer(request.params['garbage'] || 0)
+    rescue ArgumentError
+      render text: 'bad request', status: 400
+    else
+      repeats.times do |i|
+        Datadog::Tracing.trace('repeat-#{i}') do |span|
+          garbage.times do |j|
+            span.set_tag("garbage-#{j}", "#{j}")
+          end
+        end
+      end
+    end
+
+    render text: 'Generated #{repeats} spans with #{garbage} garbage tags'
+  end
+
+  def test_headers
+    response.headers['Content-Type'] ='text/plain'
+    response.headers['Content-Length'] ='15'
+    response.headers['Content-Language'] ='en-US'
+
+    render text: 'Hello, headers!'
+  end
+
+  def identify
+    trace = Datadog::Tracing.active_trace
+    trace.set_tag('usr.id', 'usr.id')
+    trace.set_tag('usr.name', 'usr.name')
+    trace.set_tag('usr.email', 'usr.email')
+    trace.set_tag('usr.session_id', 'usr.session_id')
+    trace.set_tag('usr.role', 'usr.role')
+    trace.set_tag('usr.scope', 'usr.scope')
+
+    render text: 'Hello, world!'
+  end
+
+  def user_login_success_event
+    Datadog::Kit::AppSec::Events.track_login_success(
+      Datadog::Tracing.active_trace, user: {id: 'system_tests_user'}, metadata0: "value0", metadata1: "value1"
+    )
+
+    render text: 'Hello, world!'
+  end
+
+  def user_login_failure_event
+    Datadog::Kit::AppSec::Events.track_login_failure(
+      Datadog::Tracing.active_trace, user_id: 'system_tests_user', user_exists: true, metadata0: "value0", metadata1: "value1"
+    )
+
+    render text: 'Hello, world!'
+  end
+
+  def custom_event
+    Datadog::Kit::AppSec::Events.track('system_tests_event', Datadog::Tracing.active_trace,  metadata0: "value0", metadata1: "value1")
+
+    render text: 'Hello, world!'
+  end
+
+  def tag_value
+    headers = request.query_string.split('&').map {|e | e.split('=')} || []
+    event_value = params[:key]
+    status_code = params[:status_code]
+
+    trace = Datadog::Tracing.active_trace
+    trace.set_tag("appsec.events.system_tests_appsec_event.value", event_value)
+
+    headers.each do |key, value|
+      response.headers[key] = value
+    end
+
+    render text: 'Value tagged', status: status_code
+  end
+end
