183f6569191ab38f4f68e684d8d62f24f646d27e
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-06-01 09:58:09 +0200

    Move all ruby apps inside the system test repo

---
+++ utils/build/docker/ruby/rails51/config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get  '/' => 'system_test#root'
+  post '/' => 'system_test#root'
+
+  get  '/waf' => 'system_test#waf'
+  post '/waf' => 'system_test#waf'
+  get  '/waf/*other' => 'system_test#waf'
+  post '/waf/*other' => 'system_test#waf'
+
+  get '/params/:value' => 'system_test#handle_path_params'
+  get '/spans' => 'system_test#generate_spans'
+  get '/status' => 'system_test#status'
+  get '/make_distant_call' => 'system_test#make_distant_call'
+
+  get '/headers' => 'system_test#test_headers'
+  get  '/identify' => 'system_test#identify'
+
+  get 'user_login_success_event' => 'system_test#user_login_success_event'
+  get 'user_login_failure_event' => 'system_test#user_login_failure_event'
+  get 'custom_event' => 'system_test#custom_event'
+
+ %i(get post).each do |request_method|
+    send(request_method, '/tag_value/:key/:status_code' => 'system_test#tag_value')
+  end
+  match '/tag_value/:key/:status_code' => 'system_test#tag_value', via: :options
+end
