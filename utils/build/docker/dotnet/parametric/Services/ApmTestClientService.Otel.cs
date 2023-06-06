0171222e0ea7b36c0bf26d74bd1f9cd83704c80a
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2023-06-06 14:47:26 -0400

    implement more OTel endpoints

--- utils/build/docker/dotnet/parametric/Services/ApmTestClientService.Otel.cs
+++ utils/build/docker/dotnet/parametric/Services/ApmTestClientService.Otel.cs
@@ -11,6 +11,16 @@ public partial class ApmTestClientService
     private static readonly ActivitySource ApmTestClientActivitySource = new("ApmTestClient");
     private static readonly Dictionary<ulong, Activity> Activities = new();
 
+    private static Activity FindActivity(ulong spanId)
+    {
+        if (Activities.TryGetValue(spanId, out var activity))
+        {
+            return activity;
+        }
+
+        throw new ApplicationException($"Activity not found with span id {spanId}.");
+    }
+
     public override Task<OtelStartSpanReturn> OtelStartSpan(OtelStartSpanArgs request, ServerCallContext context)
     {
         _logger.LogInformation("OtelStartSpan: {Request}", request);
@@ -51,7 +61,7 @@ public partial class ApmTestClientService
 
         if (activity is null)
         {
-            throw new ApplicationException("Failed to start activity");
+            throw new ApplicationException("Failed to start activity.");
         }
 
         _logger.LogInformation("Started Activity: OperationName={OperationName}", activity.OperationName);
@@ -59,7 +69,10 @@ public partial class ApmTestClientService
         ulong traceId = ulong.Parse(activity.TraceId.ToString().AsSpan(16, 16), NumberStyles.HexNumber);
         ulong spanId = ulong.Parse(activity.SpanId.ToString(), NumberStyles.HexNumber);
 
-        Activities[spanId] = activity;
+        if (!Activities.TryAdd(spanId, activity))
+        {
+            throw new ApplicationException("Failed to add activity to dictionary.");
+        }
 
         var result = new OtelStartSpanReturn
                      {
@@ -75,7 +88,7 @@ public partial class ApmTestClientService
     {
         _logger.LogInformation("OtelEndSpan: {Request}", request);
 
-        var activity = Activities[request.Id];
+        var activity = FindActivity(request.Id);
         activity.Stop();
 
         if (request.HasTimestamp)
@@ -92,7 +105,7 @@ public partial class ApmTestClientService
     {
         _logger.LogInformation("OtelIsRecording: {Request}", request);
 
-        var activity = Activities[request.SpanId];
+        var activity = FindActivity(request.SpanId);
 
         return Task.FromResult(
             new OtelIsRecordingReturn
@@ -105,11 +118,12 @@ public partial class ApmTestClientService
     {
         _logger.LogInformation("OtelSpanContext: {Request}", request);
 
+        // TODO:
         // var w3CTraceContextPropagatorType = Type.GetType("Datadog.Trace.Propagators.W3CTraceContextPropagator, Datadog.Trace", throwOnError: true);
         // FieldInfo? field = w3CTraceContextPropagatorType!.GetField("Instance", BindingFlags.Public | BindingFlags.Static);
         // var w3CTraceContextPropagator = field!.GetValue(null).DuckCast<IDuckW3CTraceContextPropagator>();
 
-        var activity = Activities[request.SpanId];
+        var activity = FindActivity(request.SpanId);
 
         return Task.FromResult(
             new OtelSpanContextReturn
@@ -127,7 +141,7 @@ public partial class ApmTestClientService
     {
         _logger.LogInformation("OtelSetStatus: {Request}", request);
 
-        var activity = Activities[request.SpanId];
+        var activity = FindActivity(request.SpanId);
         activity.SetStatus((ActivityStatusCode)int.Parse(request.Code), request.Description);
 
         return Task.FromResult(new OtelSetStatusReturn());
@@ -137,27 +151,62 @@ public partial class ApmTestClientService
     {
         _logger.LogInformation("OtelSetName: {Request}", request);
 
-        return base.OtelSetName(request, context);
+        var activity = FindActivity(request.SpanId);
+        activity.DisplayName = request.Name;
+
+        return Task.FromResult(new OtelSetNameReturn());
     }
 
     public override Task<OtelSetAttributesReturn> OtelSetAttributes(OtelSetAttributesArgs request, ServerCallContext context)
     {
         _logger.LogInformation("OtelSetAttributes: {Request}", request);
 
-        return base.OtelSetAttributes(request, context);
+        var activity = FindActivity(request.SpanId);
+
+        foreach ((string key, ListVal value) in request.Attributes.KeyVals)
+        {
+            AttrVal? first = value.Val.FirstOrDefault();
+
+            if (first != null)
+            {
+                switch (first.ValCase)
+                {
+                    case AttrVal.ValOneofCase.None:
+                        break;
+                    case AttrVal.ValOneofCase.BoolVal:
+                        activity.SetTag(key, first.BoolVal);
+                        break;
+                    case AttrVal.ValOneofCase.StringVal:
+                        activity.SetTag(key, first.StringVal);
+                        break;
+                    case AttrVal.ValOneofCase.DoubleVal:
+                        activity.SetTag(key, first.DoubleVal);
+                        break;
+                    case AttrVal.ValOneofCase.IntegerVal:
+                        activity.SetTag(key, first.IntegerVal);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException($"Enum value out of range: OtelSetAttributesArgs.Attributes.KeyVals[\"{key}\"][0] = {first.ValCase}.");
+                }
+            }
+        }
+
+        return Task.FromResult(new OtelSetAttributesReturn());
     }
 
-    public override Task<OtelFlushSpansReturn> OtelFlushSpans(OtelFlushSpansArgs request, ServerCallContext context)
+    public override async Task<OtelFlushSpansReturn> OtelFlushSpans(OtelFlushSpansArgs request, ServerCallContext context)
     {
         _logger.LogInformation("OtelFlushSpans: {Request}", request);
 
-        return base.OtelFlushSpans(request, context);
+        await FlushSpans();
+        return new OtelFlushSpansReturn { Success = true };
     }
 
-    public override Task<OtelFlushTraceStatsReturn> OtelFlushTraceStats(OtelFlushTraceStatsArgs request, ServerCallContext context)
+    public override async Task<OtelFlushTraceStatsReturn> OtelFlushTraceStats(OtelFlushTraceStatsArgs request, ServerCallContext context)
     {
         _logger.LogInformation("OtelFlushTraceStats: {Request}", request);
 
-        return base.OtelFlushTraceStats(request, context);
+        await FlushTraceStats();
+        return new OtelFlushTraceStatsReturn();
     }
 }
