5d6cd630b1c87c19956c57602ba74454efdb2865
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-06-06 17:00:32 +0200

    allow concurrency for rails 3-4 versions

--- utils/build/docker/ruby/rails42/config/application.rb
+++ utils/build/docker/ruby/rails42/config/application.rb
@@ -22,5 +22,6 @@ module Rails42
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.delete Rack::Lock
   end
 end
