63d9361357f59125668cf940fe6e63ed237987e2
Santiago Mola <santiago.mola@datadoghq.com>
2023-06-08 14:39:23 +0200

    Simplify RC mocked responses mapping logic

--- utils/_context/_scenarios.py
+++ utils/_context/_scenarios.py
@@ -823,7 +823,7 @@ class scenarios:
     # and it's okay not testing custom rule set for dev mode, as in this scenario, rules are always coming from remote config.
     appsec_blocking_full_denylist = EndToEndScenario(
         "APPSEC_BLOCKING_FULL_DENYLIST",
-        proxy_state={"mock_remote_config_backend": "APPSEC_BLOCKING_FULL_DENYLIST"},
+        proxy_state={"mock_remote_config_backend": "ASM_DATA_FULL_DENYLIST"},
         weblog_env={"DD_APPSEC_RULES": None},
     )
 
@@ -882,21 +882,21 @@ class scenarios:
 
     remote_config_mocked_backend_asm_features_nocache = EndToEndScenario(
         "REMOTE_CONFIG_MOCKED_BACKEND_ASM_FEATURES_NOCACHE",
-        proxy_state={"mock_remote_config_backend": "ASM_FEATURES_NO_CACHE"},
+        proxy_state={"mock_remote_config_backend": "ASM_FEATURES_NOCACHE"},
         weblog_env={"DD_APPSEC_ENABLED": "false", "DD_REMOTE_CONFIGURATION_ENABLED": "true",},
         library_interface_timeout=100,
     )
 
     remote_config_mocked_backend_asm_features_nocache = EndToEndScenario(
         "REMOTE_CONFIG_MOCKED_BACKEND_ASM_FEATURES_NOCACHE",
-        proxy_state={"mock_remote_config_backend": "ASM_FEATURES_NO_CACHE"},
+        proxy_state={"mock_remote_config_backend": "ASM_FEATURES_NOCACHE"},
         weblog_env={"DD_APPSEC_ENABLED": "false", "DD_REMOTE_CONFIGURATION_ENABLED": "true",},
         library_interface_timeout=100,
     )
 
     remote_config_mocked_backend_live_debugging_nocache = EndToEndScenario(
         "REMOTE_CONFIG_MOCKED_BACKEND_LIVE_DEBUGGING_NOCACHE",
-        proxy_state={"mock_remote_config_backend": "LIVE_DEBUGGING_NO_CACHE"},
+        proxy_state={"mock_remote_config_backend": "LIVE_DEBUGGING_NOCACHE"},
         weblog_env={
             "DD_DYNAMIC_INSTRUMENTATION_ENABLED": "1",
             "DD_DEBUGGER_ENABLED": "1",
@@ -907,7 +907,7 @@ class scenarios:
 
     remote_config_mocked_backend_asm_dd_nocache = EndToEndScenario(
         "REMOTE_CONFIG_MOCKED_BACKEND_ASM_DD_NOCACHE",
-        proxy_state={"mock_remote_config_backend": "ASM_DD_NO_CACHE"},
+        proxy_state={"mock_remote_config_backend": "ASM_DD_NOCACHE"},
         library_interface_timeout=100,
     )
 
