1a30bde1926fb38acad372d3f7bb4805ea4e7725
roberto montero <roberto.montero@datadoghq.com>
2023-06-08 11:53:40 +0200

    format code

--- utils/scripts/push_reports_dashboard.sh
+++ utils/scripts/push_reports_dashboard.sh
@@ -2,20 +2,20 @@
 set -eu
 
 base_path=$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd)
-cd $base_path
+cd "$base_path"
 
 mkdir system-tests-dashboard
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/DataDog/system-tests-dashboard.git system-tests-dashboard
+git clone https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/DataDog/system-tests-dashboard.git system-tests-dashboard
 cd system-tests-dashboard
 git checkout robertomonteromiguel/onboarding_tests_reports
 SCENARIO_SUFIX=$(echo "$SCENARIO" | tr '[:upper:]' '[:lower:]')
-mkdir -p reports/onboarding/$TEST_LIBRARY/$SCENARIO_SUFIX
-cp ../logs_${SCENARIO_SUFIX}/report.json reports/onboarding/$TEST_LIBRARY/${SCENARIO_SUFIX}/${SCENARIO_SUFIX}.json
-cp ../logs_${SCENARIO_SUFIX}/pulumi_installed_versions.log reports/onboarding/$TEST_LIBRARY/${SCENARIO_SUFIX}/
-cp ../logs_${SCENARIO_SUFIX}/vms_desc.log reports/onboarding/$TEST_LIBRARY/${SCENARIO_SUFIX}/
-git config user.name ${GITHUB_USER}
-git config user.email ${GITHUB_MAIL}
-git add reports/onboarding/$TEST_LIBRARY/${SCENARIO_SUFIX}/
+mkdir -p reports/onboarding/"$TEST_LIBRARY"/"$SCENARIO_SUFIX"
+cp ../logs_"${SCENARIO_SUFIX}"/report.json reports/onboarding/"$TEST_LIBRARY"/"${SCENARIO_SUFIX}"/"${SCENARIO_SUFIX}".json
+cp ../logs_"${SCENARIO_SUFIX}"/pulumi_installed_versions.log reports/onboarding/"$TEST_LIBRARY"/"${SCENARIO_SUFIX}"/
+cp ../logs_"${SCENARIO_SUFIX}"/vms_desc.log reports/onboarding/"$TEST_LIBRARY"/"${SCENARIO_SUFIX}"/
+git config user.name "${GITHUB_USER}"
+git config user.email "${GITHUB_MAIL}"
+git add reports/onboarding/"$TEST_LIBRARY"/"${SCENARIO_SUFIX}"/
 git commit -m "add onboarding report"  
 git pull #avoid problems with multiple pushes at same time 
 git push
