9db629c90a4e443c7e4472651f5bd1173f2c12ec
roberto montero <roberto.montero@datadoghq.com>
2023-06-09 16:09:39 +0200

    fix python installation

--- tests/onboarding/autoinjection/repositories/autoinstall/execute_install_script_dev.sh
+++ tests/onboarding/autoinjection/repositories/autoinstall/execute_install_script_dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LANG=$1
+
 curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh --output install_script_agent7.sh
 chmod 755 install_script_agent7.sh
 sed  "s/\"7\"/\"apm\"/g" install_script_agent7.sh > install_script_agent7_autoinject.sh
