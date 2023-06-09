090d61e1adc171ea1f175210ea4d3b891924bd7d
Kyle Verhoog <kyle@verhoog.ca>
2023-06-09 02:17:55 -0400

    Update for new schema

--- parametric/test_dynamic_configuration.py
+++ parametric/test_dynamic_configuration.py
@@ -1,6 +1,7 @@
 """
 Test the dynamic configuration via Remote Config (RC) feature of the APM libraries.
 """
+import random
 from typing import Dict
 from typing import Union
 
@@ -12,7 +13,7 @@ parametrize = pytest.mark.parametrize
 
 base_env = {
     "DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1",
-    "DD_REMOTE_CONFIG_POLL_SECONDS": "0.1",
+    "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.1",
 }
 
 def send_and_wait_trace(test_library, test_agent, **span_kwargs):
@@ -26,15 +27,26 @@ def send_and_wait_trace(test_library, test_agent, **span_kwargs):
 def set_and_wait_rc(test_agent, config: Dict[str, Union[str, bool, float, None]]):
     cfg = {
         "runtime_metrics_enabled": None,
-        "trace_debug_enabled": None,
-        "trace_http_header_tags": None,
-        "trace_service_mapping": None,
-        "trace_sample_rate": None,
+        "tracing_debug": None,
+        "tracing_http_header_tags": None,
+        "tracing_service_mapping": None,
+        "tracing_sample_rate": None,
+        "tracing_sampling_rules": None,
+        "span_sampling_rules": None,
+        "data_streams_enabled": None,
     }
     for k, v in config.items():
         cfg[k] = v
 
-    test_agent.set_remote_config(path="datadog/2/APM_TRACING/override_config/config", payload=cfg)
+    cfg_id = "%032x" % random.getrandbits(128)
+    test_agent.set_remote_config(path="datadog/2/APM_TRACING/%s/config" % cfg_id, payload={
+        "action": "enable",
+        "service_target": {
+            "service": "myservice",
+            "env": "dev"
+        },
+        "lib_config": cfg,
+    })
     # TODO: ensure this is the correct config-change event
     test_agent.wait_for_telemetry_event("app-client-configuration-change")
 
@@ -50,7 +62,7 @@ def test_telemetry_app_started(library_env, test_agent, test_library):
 
 @parametrize("library_env", [{
     "DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1",
-    "DD_REMOTE_CONFIG_POLL_SECONDS": "0.1",
+    "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.1",
     "DD_TRACE_SAMPLE_RATE": r,
 } for r in [None, "0.75", "1.0"]])
 def test_trace_sampling_rate(library_env, test_agent, test_library):
@@ -67,38 +79,35 @@ def test_trace_sampling_rate(library_env, test_agent, test_library):
 
     # Create a remote config entry, wait for the configuration change telemetry event to be received
     # and then create a new trace to assert the configuration has been applied.
-    set_and_wait_rc(test_agent, config={"trace_sample_rate": 0.5})
+    set_and_wait_rc(test_agent, config={"tracing_sample_rate": 0.5})
     trace = send_and_wait_trace(test_library, test_agent, name="test")
     assert trace[0]["metrics"]["_dd.rule_psr"] == 0.5
 
     # Unset the sample rate to ensure the previous setting is reapplied.
-    set_and_wait_rc(test_agent, config={"trace_sample_rate": None})
+    set_and_wait_rc(test_agent, config={"tracing_sample_rate": None})
     trace = send_and_wait_trace(test_library, test_agent, name="test")
     assert trace[0]["metrics"]["_dd.agent_psr"] == initial_sample_rate
     # TODO: shouldn't this assertion pass?
     # assert trace[0]["metrics"]["_dd.rule_psr"] == initial_sample_rate
 
 
-@parametrize("library_env", [{"DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1", "DD_REMOTE_CONFIG_POLL_SECONDS": "0.1"}])
+@parametrize("library_env", [{"DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1", "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.1"}])
 def test_trace_service_mapping(library_env, test_agent, test_library):
     with test_library.start_span("test", service="svc1"):
         pass
     traces = test_agent.wait_for_num_traces(num=1, clear=True)
     assert traces[0][0]["service"] == "svc1"
 
-    test_agent.set_remote_config(
-        path="datadog/2/APM_TRACING/override_config/config",
-        payload={
-            "runtime_metrics_enabled": None,
-            "trace_debug_enabled": None,
-            "trace_http_header_tags": None,
-            "trace_service_mapping": None,
-            "trace_sample_rate": 0.5,
-        },
-    )
-    events = test_agent.wait_for_telemetry_event("app-client-configuration-change")
-    with test_library.start_span("test"):
-        pass
-    traces = test_agent.wait_for_num_traces(num=1)
-    assert traces[0][0]["metrics"]["_dd.rule_psr"] == 0.5
-    assert len(events) > 0
+    set_and_wait_rc(test_agent, config={"tracing_service_mapping": [{"from_key": "svc1", "to_key": "svc2"}]})
+    trace = send_and_wait_trace(test_library, test_agent, name="test")
+    assert trace[0][0]["service"] == "svc2"
+
+
+@parametrize("library_env", [{"DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1", "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.1"}])
+def test_trace_sampling_rules(library_env, test_agent, test_library):
+    pass
+
+
+@parametrize("library_env", [{"DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1", "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.1"}])
+def test_span_sampling_rules(library_env, test_agent, test_library):
+    pass
