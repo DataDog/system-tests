4202233aafcae439e64178dba895f3ac5e19a279
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2023-06-20 14:36:34 +0200

    Update parametric/run.sh
    Co-authored-by: Charles de Beauchesne <charles.debeauchesne@datadoghq.com>

--- parametric/run.sh
+++ parametric/run.sh
@@ -5,6 +5,7 @@ ARGS=$*
 
 # FIXME: have to ignore the root conftest as it does a bunch of initialization/teardown
 #        not required for the integration/shared tests.
+echo "Legacy way to run parametric test. Please use ./run.sh PARAMETRIC"
 cd ..
 export TEST_LIBRARY=$CLIENTS_ENABLED 
 echo "OJOOOOOO:::::: $(PWD)"
