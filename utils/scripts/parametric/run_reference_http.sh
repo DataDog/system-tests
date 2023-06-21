ea123afecf8142319955e52b5972368afa86961a
roberto montero <roberto.montero@datadoghq.com>
2023-06-21 14:50:42 +0200

    lint parametric scripts

--- utils/scripts/parametric/run_reference_http.sh
+++ utils/scripts/parametric/run_reference_http.sh
@@ -3,6 +3,6 @@
 
 PORT="${PORT:=8000}"
 
-pushd ../../../utils/build/docker/python_http/parametric
+pushd ../../../utils/build/docker/python_http/parametric || exit
     APM_TEST_CLIENT_SERVER_PORT=$PORT python -m apm_test_client
 popd || exit
