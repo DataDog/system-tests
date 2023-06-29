fb37af6f0bf4266cffba4b38bfa49c1267d98359
Alexander S <alexander.seleznyov@datadoghq.com>
2023-06-29 13:27:47 +0300

    add CI + fix tests

--- tests/debugger/test_debugger.py
+++ tests/debugger/test_debugger.py
@@ -1,7 +1,6 @@
 # Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
-
 import json
 from collections import defaultdict
 
@@ -38,13 +37,27 @@ def check_probe_statuses(expected_data):
     check_info_endpoint()
 
     agent_logs = list(interfaces.agent.get_data(path_filters="/api/v2/logs"))
-    check_logs(agent_logs, expected_data)
+    for expected_probe in expected_data:
+        check_probe_status(expected_probe, agent_logs)
 
-    request_number = 0
+
+def check_probe_status(expected_id, agent_logs):
+    expected_status = expected_id.split("-")[1].upper()
+
     for agent_log in agent_logs:
-        if request_number < len(expected_data):
-            check_probe_status(agent_log, expected_data[request_number])
-            request_number += 1
+        actual_probe_id = agent_log["request"]["content"][0]["debugger"]["diagnostics"]["probeId"]
+
+        if actual_probe_id == expected_id:
+            actual_status = agent_log["request"]["content"][0]["debugger"]["diagnostics"]["status"]
+
+            if actual_status == expected_status:
+                return
+            else:
+                raise ValueError(
+                    "Received probe " + expected_id + " with status " + actual_status + ", but expected for "+ expected_status
+                )
+
+    raise ValueError("Probe " + expected_id + " was not received")
 
 
 def check_info_endpoint():
@@ -57,34 +70,14 @@ def check_info_endpoint():
     raise ValueError("Agent did not provide /v0.7/config endpoint")
 
 
-def check_logs(logs, data):
-    assert logs, "Probe statuses were not recieved"
-
-    if len(data) > len(logs):
-        raise ValueError("Received less probe statuses than expected")
-
-
-def check_probe_status(log, expectedProbeId):
-    actualProbeId = log["request"]["content"][0]["debugger"]["diagnostics"]["probeId"]
-
-    if expectedProbeId != actualProbeId:
-        raise ValueError("Received probe id " + actualProbeId + ", but expected for " + expectedProbeId)
-
-    expectedStatus = expectedProbeId.split("-")[1].upper()
-    actualStatus = log["request"]["content"][0]["debugger"]["diagnostics"]["status"]
-
-    if expectedStatus != actualStatus:
-        raise ValueError("Received probe status " + actualStatus + ", but expected for " + expectedStatus)
-
-
-@released(cpp="?", golang="?", dotnet="2.32.0", java="?", php_appsec="?", python="?", ruby="?", nodejs="?")
+@released(cpp="?", golang="?", dotnet="2.33.0", java="?", php_appsec="?", python="?", ruby="?", nodejs="?")
 @scenarios.debugger_method_probes_status
 class Test_Debugger_Method_Probe_Statuses:
     def test_method_probe_status(self):
         check_probe_statuses(METHOD_PROBES)
 
 
-@released(cpp="?", golang="?", dotnet="2.32.0", java="?", php_appsec="?", python="?", ruby="?", nodejs="?")
+@released(cpp="?", golang="?", dotnet="2.33.0", java="?", php_appsec="?", python="?", ruby="?", nodejs="?")
 @scenarios.debugger_line_probes_status
 class Test_Debugger_Line_Probe_Statuses:
     def test_line_probe_status(self):
