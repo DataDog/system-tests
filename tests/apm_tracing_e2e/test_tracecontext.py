a9111a390ce312bc25b2fbf306da7124357b994e
Zach Montoya <zach.montoya@datadoghq.com>
2023-06-30 14:47:04 -0700

    Looks like I can't pass in a tracecontext in the incoming headers because then the library.get_root_spans() call fails :(

--- tests/apm_tracing_e2e/test_tracecontext.py
+++ tests/apm_tracing_e2e/test_tracecontext.py
@@ -11,15 +11,7 @@ class Test_Tracecontext_Span:
     """
 
     def setup_tracecontext_span(self):
-        id = randint(0, 10000000)
-        self.trace_id = str(id)
-        self.span_id = str(id)
-        self.sampled = 1
-        traceparent = "00-{:032x}-{:016x}-{:02x}".format(id, id, self.sampled)
-
-        self.req = weblog.get(
-            "/make_distant_call", params={"url": "http://weblog:7777"}, headers={"traceparent": traceparent}
-        )
+        self.req = weblog.get("/make_distant_call", params={"url": "http://weblog:7777"})
 
     def test_tracecontext_span(self):
         # Assert the span sent by the agent.
@@ -27,16 +19,16 @@ class Test_Tracecontext_Span:
         assert 1 == len(spans), _assert_msg(1, len(spans), "Agent did not submit the spans we want!")
 
         span = spans[0]
-        assert span.get("traceID") == self.trace_id
-        assert span.get("parentID") == self.span_id
+        trace_id = span.get("traceID")
+        span_id = span.get("parentID")
 
         # Assert the spans received from the backend
         spans = interfaces.backend.assert_library_traces_exist(self.req)
         assert 1 == len(spans), _assert_msg(1, len(spans))
 
         span = spans[0]
-        assert span.get("traceID") == self.trace_id
-        assert span.get("parentID") == self.span_id
+        assert span.get("traceID") == trace_id
+        assert span.get("parentID") == span_id
 
         # Assert the information in the outbound http client request
         interfaces.library.assert_trace_exists(self.req)
@@ -45,3 +37,7 @@ class Test_Tracecontext_Span:
         assert self.req.json() is not None
         data = self.req.json()
         assert "traceparent" in data["request_headers"]
+        assert "x-datadog-parent-id" not in data["request_headers"]
+        assert "x-datadog-sampling-priority" not in data["request_headers"]
+        assert "x-datadog-tags" not in data["request_headers"]
+        assert "x-datadog-trace-id" not in data["request_headers"]
