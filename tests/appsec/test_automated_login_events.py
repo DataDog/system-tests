d89c7fd336e1fcc62294928dbb79c80c097ff282
Julio Gonzalez <107922352+hoolioh@users.noreply.github.com>
2023-07-03 11:54:13 +0200

    Automated user events milestone 1 (#1288)
    * Automated login events.
    
    * Add APPSEC_AUTO_EVENTS_EXTENDED scenario to CI.
    
    ---------
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>
    Co-authored-by: Charles de Beauchesne <charles.debeauchesne@datadoghq.com>

--- tests/appsec/test_automated_login_events.py
+++ tests/appsec/test_automated_login_events.py
@@ -80,6 +80,8 @@ class Test_Login_Events:
             for _, _, span in interfaces.library.get_spans(request=r):
                 meta = span.get("meta", {})
                 if context.library != "nodejs":
+                    # Currently in nodejs there is no way to check if the user exists upon authentication failure so
+                    # this assertion is disabled for this library.
                     assert meta["appsec.events.users.login.failure.usr.exists"] == "false"
 
                 assert meta["appsec.events.users.login.failure.usr.id"] == " "
@@ -99,6 +101,8 @@ class Test_Login_Events:
             for _, _, span in interfaces.library.get_spans(request=r):
                 meta = span.get("meta", {})
                 if context.library != "nodejs":
+                    # Currently in nodejs there is no way to check if the user exists upon authentication failure so
+                    # this assertion is disabled for this library.
                     assert meta["appsec.events.users.login.failure.usr.exists"] == "true"
 
                 assert meta["appsec.events.users.login.failure.usr.id"] == " "
@@ -147,6 +151,128 @@ class Test_Login_Events:
             for _, _, span in interfaces.library.get_spans(request=r):
                 meta = span.get("meta", {})
                 assert meta["_dd.appsec.events.users.login.failure.auto.mode"] == "safe"
+                assert meta["_dd.appsec.events.users.login.failure.sdk"] == "true"
+                assert meta["appsec.events.users.login.failure.track"] == "true"
+                assert meta["appsec.events.users.login.failure.usr.id"] == "sdkUser"
+                assert meta["appsec.events.users.login.failure.usr.exists"] == "true"
+                assert meta["manual.keep"] == "true"
+
+
+@rfc("https://docs.google.com/document/d/1-trUpphvyZY7k5ldjhW-MgqWl0xOm7AMEQDJEAZ63_Q/edit#heading=h.8d3o7vtyu1y1")
+@coverage.good
+@scenarios.appsec_auto_events_extended
+@released(cpp="?", golang="?", java="?", nodejs="?", dotnet="?", php="?", python="?", ruby="?")
+class Test_Login_Events_Extended:
+    "Test login success/failure use cases"
+    USER = "test"
+    UUID_USER = "testuuid"
+    PASSWORD = "1234"
+
+    BASIC_AUTH_USER_HEADER = "Basic dGVzdDoxMjM0"  # base64(test:1234)
+    BASIC_AUTH_USER_UUID_HEADER = "Basic dGVzdHV1aWQ6MTIzNA=="  # base64(testuuid:1234)
+
+    def setup_login_success(self):
+        self.r_success = [
+            weblog.post("/login?auth=local", data={"username": self.USER, "password": self.PASSWORD}),
+            weblog.get("/login?auth=basic", headers={"Authorization": self.BASIC_AUTH_USER_HEADER}),
+        ]
+
+    def test_login_success(self):
+        for r in self.r_success:
+            assert r.status_code == 200
+            for _, _, span in interfaces.library.get_spans(request=r):
+                meta = span.get("meta", {})
+                assert meta["_dd.appsec.events.users.login.success.auto.mode"] == "extended"
+                assert meta["appsec.events.users.login.success.track"] == "true"
+                assert meta["usr.id"] == "1"
+                assert meta["usr.email"] == "testuser@ddog.com"
+                assert meta["usr.username"] == "test"
+                assert meta["usr.login"] == "test"
+                assert meta["manual.keep"] == "true"
+
+    def setup_login_wrong_user_failure(self):
+        self.r_wrong_user_failure = [
+            weblog.post("/login?auth=local", data={"username": "invalidUser", "password": self.PASSWORD}),
+            weblog.get("/login?auth=basic", headers={"Authorization": "Basic aW52YWxpZFVzZXI6MTIzNA=="}),
+        ]
+
+    def test_login_wrong_user_failure(self):
+        for r in self.r_wrong_user_failure:
+            assert r.status_code == 401
+            for _, _, span in interfaces.library.get_spans(request=r):
+                meta = span.get("meta", {})
+                if context.library != "nodejs":
+                    # Currently in nodejs there is no way to check if the user exists upon authentication failure so
+                    # this assertion is disabled for this library.
+                    assert meta["appsec.events.users.login.failure.usr.exists"] == "false"
+
+                assert meta["_dd.appsec.events.users.login.failure.auto.mode"] == "extended"
+                assert meta["appsec.events.users.login.failure.track"] == "true"
+                assert meta["appsec.events.users.login.failure.usr.id"] == "invalidUser"
+                assert meta["manual.keep"] == "true"
+
+    def setup_login_wrong_password_failure(self):
+        self.r_wrong_user_failure = [
+            weblog.post("/login?auth=local", data={"username": self.USER, "password": "12345"}),
+            weblog.get("/login?auth=basic", headers={"Authorization": "Basic dGVzdDoxMjM0NQ=="}),
+        ]
+
+    def test_login_wrong_password_failure(self):
+        for r in self.r_wrong_user_failure:
+            assert r.status_code == 401
+            for _, _, span in interfaces.library.get_spans(request=r):
+                meta = span.get("meta", {})
+                if context.library != "nodejs":
+                    # Currently in nodejs there is no way to check if the user exists upon authentication failure so
+                    # this assertion is disabled for this library.
+                    assert meta["appsec.events.users.login.failure.usr.exists"] == "true"
+
+                assert meta["_dd.appsec.events.users.login.failure.auto.mode"] == "extended"
+                assert meta["appsec.events.users.login.failure.track"] == "true"
+                assert meta["appsec.events.users.login.failure.usr.id"] == "test"
+                assert meta["manual.keep"] == "true"
+
+    def setup_login_sdk_success(self):
+        self.r_sdk_success = [
+            weblog.post(
+                "/login?auth=local&sdk_event=success&sdk_user=sdkUser",
+                data={"username": self.USER, "password": self.PASSWORD},
+            ),
+            weblog.get(
+                "/login?auth=basic&sdk_event=success&sdk_user=sdkUser",
+                headers={"Authorization": self.BASIC_AUTH_USER_HEADER},
+            ),
+        ]
+
+    def test_login_sdk_success(self):
+        for r in self.r_sdk_success:
+            assert r.status_code == 200
+            for _, _, span in interfaces.library.get_spans(request=r):
+                meta = span.get("meta", {})
+                assert meta["_dd.appsec.events.users.login.success.auto.mode"] == "extended"
+                assert meta["_dd.appsec.events.users.login.success.sdk"] == "true"
+                assert meta["appsec.events.users.login.success.track"] == "true"
+                assert meta["usr.id"] == "sdkUser"
+                assert meta["manual.keep"] == "true"
+
+    def setup_login_sdk_failure(self):
+        self.r_sdk_failure = [
+            weblog.post(
+                "/login?auth=local&sdk_event=failure&sdk_user=sdkUser&sdk_user_exists=true",
+                data={"username": "invalidUser", "password": self.PASSWORD},
+            ),
+            weblog.get(
+                "/login?auth=basic&sdk_event=failure&sdk_user=sdkUser&sdk_user_exists=true",
+                headers={"Authorization": "Basic aW52YWxpZFVzZXI6MTIzNA=="},
+            ),
+        ]
+
+    def test_login_sdk_failure(self):
+        for r in self.r_sdk_failure:
+            assert r.status_code == 401
+            for _, _, span in interfaces.library.get_spans(request=r):
+                meta = span.get("meta", {})
+                assert meta["_dd.appsec.events.users.login.failure.auto.mode"] == "extended"
                 assert meta["_dd.appsec.events.users.login.failure.sdk"] == "true"
                 assert meta["appsec.events.users.login.failure.track"] == "true"
                 assert meta["appsec.events.users.login.failure.usr.id"] == "sdkUser"
