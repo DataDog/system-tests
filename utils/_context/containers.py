b256452b4821039acf6e36a960677e7d195cf429
Yang Song <songy23@users.noreply.github.com>
2023-07-12 11:39:11 -0400

    Update utils/_context/containers.py
    Co-authored-by: Charles de Beauchesne <charles.debeauchesne@datadoghq.com>

--- utils/_context/containers.py
+++ utils/_context/containers.py
@@ -516,7 +516,7 @@ class SqlServerContainer(TestedContainer):
 
 class OpenTelemetryCollectorContainer(TestedContainer):
     def __init__(self, host_log_folder) -> None:
-        image = os.environ.get("OTEL_COLLECTOR_IMAGE", "otel/opentelemetry-collector-contrib:latest")
+        image = os.environ.get("SYSTEM_TESTS_OTEL_COLLECTOR_IMAGE", "otel/opentelemetry-collector-contrib:latest")
         self._otel_config_host_path = "./utils/build/docker/otelcol-config.yaml"
         super().__init__(
             image_name=image,
