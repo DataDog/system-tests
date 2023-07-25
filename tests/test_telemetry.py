463da85d99f4efffdade75a65afae4b55cbb97af
“Aditi Maheshwari” <aditi.maheshwari@datadoghq.com>
2023-07-25 16:55:11 -0400

    lint fix

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -45,10 +45,10 @@ class Test_Telemetry:
         for data in telemetry_data:
             content = data["request"]["content"]
             language_name = content["application"]["language_name"]
-            language_enum = ["cpp", "dotnet", "go", "node", "java", "php", "python", "ruby"]            
+            language_enum = ["cpp", "dotnet", "go", "node", "java", "php", "python", "ruby"]
             if language_name not in language_enum:
-                raise Exception(f"Received language_name : {language_name} is not valid")
-
+                 raise Exception(f"Received language_name : {language_name} is not valid")
+ 
     def test_telemetry_message_data_size(self):
         """Test telemetry message data size"""
 
