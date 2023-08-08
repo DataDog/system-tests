c65a1acdb1e062fc57b0d5a8e1f1abf4b4c5a2b6
Julio Gonzalez <107922352+hoolioh@users.noreply.github.com>
2023-08-08 14:49:17 +0200

    [Node.js] Enable tests. (#1446)
    * Enable tests.
    
    * Solve PR comments.

--- tests/appsec/test_automated_login_events.py
+++ tests/appsec/test_automated_login_events.py
@@ -59,7 +59,7 @@ class Test_Login_Events:
             "/login?auth=local", data={self.username_key: self.USER, self.password_key: self.PASSWORD}
         )
 
-    @bug(context.library == "nodejs", reason="usr.id present in meta")
+    @bug(context.library < "nodejs@4.9.0", reason="Reports empty space in usr.id when id is a PII")
     def test_login_pii_success_local(self):
         assert self.r_pii_success.status_code == 200
         for _, _, span in interfaces.library.get_spans(request=self.r_pii_success):
@@ -72,8 +72,8 @@ class Test_Login_Events:
     def setup_login_pii_success_basic(self):
         self.r_pii_success = weblog.get("/login?auth=basic", headers={"Authorization": self.BASIC_AUTH_USER_HEADER})
 
-    @bug(context.library == "nodejs", reason="usr.id present in meta")
     @missing_feature(context.library == "php", reason="Basic auth not implemented")
+    @bug(context.library < "nodejs@4.9.0", reason="Reports empty space in usr.id when id is a PII")
     def test_login_pii_success_basic(self):
         assert self.r_pii_success.status_code == 200
         for _, _, span in interfaces.library.get_spans(request=self.r_pii_success):
@@ -115,7 +115,7 @@ class Test_Login_Events:
             "/login?auth=local", data={self.username_key: self.INVALID_USER, self.password_key: self.PASSWORD}
         )
 
-    @bug(context.library == "nodejs", reason="usr.id present in meta")
+    @bug(context.library < "nodejs@4.9.0", reason="Reports empty space in usr.id when id is a PII")
     def test_login_wrong_user_failure_local(self):
         assert self.r_wrong_user_failure.status_code == 401
         for _, _, span in interfaces.library.get_spans(request=self.r_wrong_user_failure):
@@ -135,8 +135,8 @@ class Test_Login_Events:
             "/login?auth=basic", headers={"Authorization": self.BASIC_AUTH_INVALID_USER_HEADER}
         )
 
-    @bug(context.library == "nodejs", reason="usr.id present in meta")
     @missing_feature(context.library == "php", reason="Basic auth not implemented")
+    @bug(context.library < "nodejs@4.9.0", reason="Reports empty space in usr.id when id is a PII")
     def test_login_wrong_user_failure_basic(self):
         assert self.r_wrong_user_failure.status_code == 401
         for _, _, span in interfaces.library.get_spans(request=self.r_wrong_user_failure):
@@ -156,7 +156,7 @@ class Test_Login_Events:
             "/login?auth=local", data={self.username_key: self.USER, "password": "12345"}
         )
 
-    @bug(context.library == "nodejs", reason="usr.id present in meta")
+    @bug(context.library < "nodejs@4.9.0", reason="Reports empty space in usr.id when id is a PII")
     def test_login_wrong_password_failure_local(self):
         assert self.r_wrong_user_failure.status_code == 401
         for _, _, span in interfaces.library.get_spans(request=self.r_wrong_user_failure):
@@ -176,8 +176,8 @@ class Test_Login_Events:
             "/login?auth=basic", headers={"Authorization": self.BASIC_AUTH_INVALID_PASSWORD_HEADER}
         )
 
-    @bug(context.library == "nodejs", reason="usr.id present in meta")
     @missing_feature(context.library == "php", reason="Basic auth not implemented")
+    @bug(context.library < "nodejs@4.9.0", reason="Reports empty space in usr.id when id is a PII")
     def test_login_wrong_password_failure_basic(self):
         assert self.r_wrong_user_failure.status_code == 401
         for _, _, span in interfaces.library.get_spans(request=self.r_wrong_user_failure):
