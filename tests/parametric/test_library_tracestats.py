8ad0072709639c8fa4fa2237e6c6fe6a3b4f62c7
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2023-08-09 10:37:52 +0200

    tests/parametric: align Go with client-stats spec

--- tests/parametric/test_library_tracestats.py
+++ tests/parametric/test_library_tracestats.py
@@ -26,8 +26,7 @@ def _human_stats(stats: V06StatsAggr) -> str:
 
 def enable_tracestats(sample_rate: Optional[float] = None) -> Any:
     env = {
-        "DD_TRACE_STATS_COMPUTATION_ENABLED": "1",  # reference, dotnet, python
-        "DD_TRACE_FEATURES": "discovery",  # golang
+        "DD_TRACE_STATS_COMPUTATION_ENABLED": "1",  # reference, dotnet, python, golang
         "DD_TRACE_TRACER_METRICS_ENABLED": "true",  # java
     }
     if sample_rate is not None:
--- utils/build/docker/golang/parametric/go.mod
+++ utils/build/docker/golang/parametric/go.mod
@@ -7,7 +7,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.16.0
 	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
-	gopkg.in/DataDog/dd-trace-go.v1 v1.39.0-alpha.1.0.20230801190547-5ebe300ac718
+	gopkg.in/DataDog/dd-trace-go.v1 v1.39.0-alpha.1.0.20230808142237-30bfd5205d6a
 )
 
 require (
