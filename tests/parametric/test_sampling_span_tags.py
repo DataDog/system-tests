b545e5227b3cc45db4804efb7e51f43019cb1354
Emmett Butler <emmett.butler321@gmail.com>
2023-08-11 11:58:42 -0700

    mark some python bugs

--- tests/parametric/test_sampling_span_tags.py
+++ tests/parametric/test_sampling_span_tags.py
@@ -1,7 +1,7 @@
 import json
 
 import pytest
-from utils import flaky, scenarios
+from utils import bug, scenarios
 from utils.parametric.spec.trace import MANUAL_DROP_KEY  # noqa
 from utils.parametric.spec.trace import MANUAL_KEEP_KEY  # noqa
 from utils.parametric.spec.trace import SAMPLING_AGENT_PRIORITY_RATE  # noqa
@@ -38,9 +38,6 @@ def _assert_sampling_tags(parent_span, child_span, child_dm, parent_dm, parent_p
 
 @scenarios.parametric
 class Test_Sampling_Span_Tags:
-    @flaky(True, library="php")
-    @flaky(True, library="cpp")
-    @flaky(True, library="ruby")
     @pytest.mark.parametrize("library_env", [{"DD_TRACE_SAMPLE_RATE": 1}])
     def test_tags_child_dropped_sst001(self, test_agent, test_library):
         with test_library:
@@ -86,6 +83,7 @@ class Test_Sampling_Span_Tags:
         # ruby: child dm None
         # cpp: child dm None
 
+    @bug(library="python", reason="Python sets dm tag on child span")
     def test_tags_defaults_sst002(self, test_agent, test_library):
         parent_span, child_span = _get_parent_and_child_span(test_agent, test_library)
         _assert_sampling_tags(parent_span, child_span, None, "-0", 1, 1)
@@ -98,6 +96,7 @@ class Test_Sampling_Span_Tags:
         # cpp: child dm None
 
     @pytest.mark.parametrize("library_env", [{"DD_TRACE_SAMPLE_RATE": 1}])
+    @bug(library="python", reason="Python sets dm tag on child span")
     def test_tags_defaults_rate_1_sst003(self, test_agent, test_library):
         parent_span, child_span = _get_parent_and_child_span(test_agent, test_library)
         _assert_sampling_tags(parent_span, child_span, None, "-3", 2, 1)
@@ -120,6 +119,7 @@ class Test_Sampling_Span_Tags:
     @pytest.mark.parametrize(
         "library_env", [{"DD_TRACE_SAMPLE_RATE": 1, "DD_TRACE_SAMPLING_RULES": json.dumps([{"sample_rate": 1}])}]
     )
+    @bug(library="python", reason="Python sets dm tag on child span")
     def test_tags_defaults_rate_1_and_rule_1_sst005(self, test_agent, test_library):
         parent_span, child_span = _get_parent_and_child_span(test_agent, test_library)
         _assert_sampling_tags(parent_span, child_span, None, "-3", 2, 1)
