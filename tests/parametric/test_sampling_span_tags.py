b2047584e5b8f6de56550de8365eb1fce1b960ca
Emmett Butler <emmett.butler321@gmail.com>
2023-08-18 09:58:02 -0700

    skip manual drop test for java

--- tests/parametric/test_sampling_span_tags.py
+++ tests/parametric/test_sampling_span_tags.py
@@ -45,6 +45,7 @@ class Test_Sampling_Span_Tags:
     @bug(library="dotnet", reason="dotnet does not set dm tag on first span")
     @bug(library="golang", reason="golang sets priority 2")
     @bug(library="php", reason="php sets priority 2")
+    @bug(library="java", reason="java sets priority 2")
     @bug(library="cpp", reason="c++ does not support magic tags")
     def test_tags_child_dropped_sst001(self, test_agent, test_library):
         with test_library:
