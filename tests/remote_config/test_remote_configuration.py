c9c9f96471384c21a0b06e71ed619b07656cbc40
Robert Pickering <robert.pickering@datadoghq.com>
2023-08-18 14:41:55 +0200

    Add waiting to try and avoid a pass local but not in CI issue

--- tests/remote_config/test_remote_configuration.py
+++ tests/remote_config/test_remote_configuration.py
@@ -288,12 +288,28 @@ class Test_RemoteConfigurationUpdateSequenceFeatures(RemoteConfigurationFieldsBa
 @scenarios.remote_config_mocked_backend_asm_features
 class Test_RemoteConfigurationExtraServices:
     """Tests that extra services are sent in the RC message"""
+    remote_config_is_sent = False
 
     def setup_tracer_extra_services(self):
         self.r_outgoing = weblog.get("/createextraservice/?serviceName=extraVegetables")
 
     def test_tracer_extra_services(self):
         """ test """
+
+        def remote_config_asm_extra_services_available(data):
+            if data["path"] == "/v0.7/config":
+                if "extra_services" in data.get("request", {}).get("content", {}).get("client", {}).get("client_tracer", {}):
+                    client_tracer = data["request"]["content"]["client"]["client_tracer"]
+
+                    if not ("extra_services" in client_tracer):
+                        return False
+
+                    extra_services = client_tracer["extra_services"]
+
+                    if extra_services is not None and len(extra_services) > 0:
+                        return True
+
+                return False
 
         def validate(data):
             """ Helper to validate config request content """
@@ -313,6 +329,7 @@ class Test_RemoteConfigurationExtraServices:
 
             return True
 
+        interfaces.library.wait_for(remote_config_asm_extra_services_available, timeout=30)
         interfaces.library.validate_remote_configuration(validator=validate)
 
 
