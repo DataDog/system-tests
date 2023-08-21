b9663117e7be9643269031b9bea0910d8f917c95
Alexander S <alexander.seleznyov@datadoghq.com>
2023-08-21 14:51:44 +0300

    Support dotnet arm64

--- utils/build/docker/dotnet/install_ddtrace.sh
+++ utils/build/docker/dotnet/install_ddtrace.sh
@@ -18,7 +18,15 @@ else
     else
         echo "Install ddtrace from github releases"
         DDTRACE_VERSION="$(get_latest_release DataDog/dd-trace-dotnet)"
-        curl -L https://github.com/DataDog/dd-trace-dotnet/releases/download/v${DDTRACE_VERSION}/datadog-dotnet-apm-${DDTRACE_VERSION}.tar.gz --output datadog-dotnet-apm-${DDTRACE_VERSION}.tar.gz
+
+        if [ $(uname -m) = "aarch64" ]; then
+            artifact=datadog-dotnet-apm-${DDTRACE_VERSION}.arm64.tar.gz
+        else
+            artifact=datadog-dotnet-apm-${DDTRACE_VERSION}.tar.gz
+        fi      
+
+        echo "Using artifact ${artifact}"
+        curl -L https://github.com/DataDog/dd-trace-dotnet/releases/download/v${DDTRACE_VERSION}/${artifact} --output ${artifact}
     fi
 
     tar xzf $(ls datadog-dotnet-apm*.tar.gz) -C /opt/datadog
