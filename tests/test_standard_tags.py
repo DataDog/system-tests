55b573498c2df43a30fc2c92fe9e0be0bfe7bff1
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-09-01 16:56:37 +0200

    improve guard for dev branch

--- tests/test_standard_tags.py
+++ tests/test_standard_tags.py
@@ -81,7 +81,7 @@ class Test_StandardTagsUrl:
         ]
 
     # when tracer is updated, add (for example)
-    @irrelevant(context.library > "python@1.18", reason="python released the new version at 1.19.0")
+    @irrelevant(context.library >= "python@1.18", reason="python released the new version at 1.19.0")
     def test_url_with_sensitive_query_string_legacy(self):
         for r, tag in self.requests_sensitive_query_string:
             interfaces.library.add_span_tag_validation(
@@ -128,7 +128,7 @@ class Test_StandardTagsUrl:
         )
 
     # when tracer is updated, add (for exemple)
-    @irrelevant(context.library > "python@1.18", reason="python released the new version at 1.19.0")
+    @irrelevant(context.library >= "python@1.18", reason="python released the new version at 1.19.0")
     def test_multiple_matching_substring_legacy(self):
         tag = r"^.*/waf\?<redacted>&key1=val1&key2=val2&<redacted>&<redacted>&key3=val3&json=%7B%20%22<redacted>%7D$"  # pylint: disable=line-too-long
         interfaces.library.add_span_tag_validation(
