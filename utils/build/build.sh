9710cbf77feeef620a4d88c518bca7fd3c8a9bc9
Yang Song <yang.song@datadoghq.com>
2023-09-05 17:34:09 -0400

    Fix unbound var

--- utils/build/build.sh
+++ utils/build/build.sh
@@ -165,7 +165,7 @@ build() {
                 .
 
         elif [[ $IMAGE_NAME == agent ]]; then
-            if [[ -z "${AGENT_BASE_IMAGE}" ]]; then
+            if [[ -z "${AGENT_BASE_IMAGE:-}" ]]; then
                 if [ -f ./binaries/agent-image ]; then
                     AGENT_BASE_IMAGE=$(cat ./binaries/agent-image)
                 else
