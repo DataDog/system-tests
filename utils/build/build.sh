214d77a0819c9721d717bbb62508b9291064066a
roberto montero <roberto.montero@datadoghq.com>
2023-09-07 16:55:17 +0200

    python docker base images. Fixed

--- utils/build/build.sh
+++ utils/build/build.sh
@@ -213,10 +213,13 @@ build() {
             fi
 
             DOCKERFILE=utils/build/docker/${TEST_LIBRARY}/${WEBLOG_VARIANT}.Dockerfile
+            
             IMAGE_PULL_SOURCE="--pull"
-            if [ "$BUILD_FROM_LOCAL" = true ] ; then
+            if [ "${BUILD_FROM_LOCAL:-false}" = true ] ; then
+                echo "Building weblog using previously generated local base images"
                  IMAGE_PULL_SOURCE="--pull=false"
             fi
+
             docker buildx build \
                 --build-arg BUILDKIT_INLINE_CACHE=1 \
                 --load \
