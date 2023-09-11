48c1a20acadf4d76794baa64912fc4ad1d8afc65
Alexander S <alexander.seleznyov@datadoghq.com>
2023-09-11 11:53:21 +0300

    Enable debugger snapshot tests (#1565)
    * enable snapshot tests

--- utils/_context/_scenarios.py
+++ utils/_context/_scenarios.py
@@ -1136,7 +1136,7 @@ class scenarios:
             "DD_DEBUGGER_DIAGNOSTICS_INTERVAL": "1",
         },
         library_interface_timeout=100,
-        doc="",
+        doc="Test scenario for checking if method probe statuses can be successfully 'RECEIVED' and 'INSTALLED'",
     )
 
     debugger_line_probes_status = EndToEndScenario(
@@ -1149,7 +1149,15 @@ class scenarios:
             "DD_DEBUGGER_DIAGNOSTICS_INTERVAL": "1",
         },
         library_interface_timeout=100,
-        doc="",
+        doc="Test scenario for checking if line probe statuses can be successfully 'RECIEVED' and 'INSTALLED'",
+    )
+
+    debugger_method_probes_snapshot = EndToEndScenario(
+        "DEBUGGER_METHOD_PROBES_SNAPSHOT",
+        proxy_state={"mock_remote_config_backend": "DEBUGGER_METHOD_PROBES_SNAPSHOT"},
+        weblog_env={"DD_DYNAMIC_INSTRUMENTATION_ENABLED": "1", "DD_REMOTE_CONFIG_ENABLED": "true",},
+        library_interface_timeout=10,
+        doc="Test scenario for checking if debugger successfully generates snapshots for specific probes",
     )
 
     fuzzer = _DockerScenario("_FUZZER", doc="Fake scenario for fuzzing (launch without pytest)")
