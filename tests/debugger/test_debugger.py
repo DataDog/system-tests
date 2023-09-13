0922aa8884a21e5e159ae249fd50a164ee605610
Alexander Seleznyov <alexander.seleznyov@datadoghq.com>
2023-09-13 12:32:08 +0300

    use base class

--- tests/debugger/test_debugger.py
+++ tests/debugger/test_debugger.py
@@ -123,29 +123,32 @@ class Test_Debugger_Line_Probe_Statuses:
         validate_data(expected_data, [])
 
 
-def wait_for_remote_config(self, data):
-    if data["path"] == "/v0.7/config":
-        if "client_configs" in data.get("response", {}).get("content", {}):
-            self.remote_config_is_sent = True
-            return True
-    return False
+class _Base_Debugger_Snapshot_Test:
+    remote_config_is_sent = False
+    probe_installed = False
+
+    def wait_for_remote_config(self, data):
+        if data["path"] == "/v0.7/config":
+            if "client_configs" in data.get("response", {}).get("content", {}):
+                self.remote_config_is_sent = True
+                return True
+        return False
 
 
-def wait_for_probe(self, data):
-    if data["path"] == "/api/v2/logs":
-        contents = data.get("request", {}).get("content", {})
+    def wait_for_probe(self, data):
+        if data["path"] == "/api/v2/logs":
+            contents = data.get("request", {}).get("content", {})
 
-        if contents is None:
-            return False
+            if contents is None:
+                return False
 
-        for content in contents:
-            debuggerData = content["debugger"]
-            if "diagnostics" in debuggerData:
-                if debuggerData["diagnostics"]["status"] == "INSTALLED":
-                    self.probe_installed = True
-                    return True
-    return False
-
+            for content in contents:
+                debuggerData = content["debugger"]
+                if "diagnostics" in debuggerData:
+                    if debuggerData["diagnostics"]["status"] == "INSTALLED":
+                        self.probe_installed = True
+                        return True
+        return False
 
 @missing_feature(
     context.library == "java" and context.weblog_variant not in ["spring-boot", "uds-spring-boot"],
@@ -158,14 +161,12 @@ def wait_for_probe(self, data):
 @irrelevant(library="nodejs")
 @irrelevant(library="cpp")
 @scenarios.debugger_method_probes_snapshot
-class Test_Debugger_Method_Probe_Snaphots:
-    remote_config_is_sent = False
-    probe_installed = False
+class Test_Debugger_Method_Probe_Snaphots(_Base_Debugger_Snapshot_Test):
     log_probe_response = None
 
     def setup_method_probe_snaphots(self):
-        interfaces.library.wait_for(lambda data: wait_for_remote_config(self, data), timeout=30)
-        interfaces.agent.wait_for(lambda data: wait_for_probe(self, data), timeout=30)
+        interfaces.library.wait_for(self.wait_for_remote_config, timeout=30)
+        interfaces.agent.wait_for(self.wait_for_probe, timeout=30)
         self.log_probe_response = weblog.get("/debugger/log")
 
     def test_method_probe_snaphots(self):
@@ -188,14 +189,12 @@ class Test_Debugger_Method_Probe_Snaphots:
 @irrelevant(library="nodejs")
 @irrelevant(library="cpp")
 @scenarios.debugger_line_probes_snapshot
-class Test_Debugger_Line_Probe_Snaphots:
-    remote_config_is_sent = False
-    probe_installed = False
+class Test_Debugger_Line_Probe_Snaphots(_Base_Debugger_Snapshot_Test):
     log_probe_response = None
 
     def setup_line_probe_snaphots(self):
-        interfaces.library.wait_for(lambda data: wait_for_remote_config(self, data), timeout=30)
-        interfaces.agent.wait_for(lambda data: wait_for_probe(self, data), timeout=30)
+        interfaces.library.wait_for(self.wait_for_remote_config, timeout=30)
+        interfaces.agent.wait_for(self.wait_for_probe, timeout=30)
         self.log_probe_response = weblog.get("/debugger/log")
 
     def test_line_probe_snaphots(self):
