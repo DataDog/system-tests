66ff49b15859c4732ade3af34891ee71e960dd14
roberto montero <roberto.montero@datadoghq.com>
2023-09-13 14:36:29 +0200

    fix lint

--- utils/build/docker/python/parametric/ddtracer_version.sh
+++ utils/build/docker/python/parametric/ddtracer_version.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-if [ -z ${PYTHON_DDTRACE_PACKAGE} ]; then pip install ddtrace; else pip install $PYTHON_DDTRACE_PACKAGE; fi
+if [ -z "${PYTHON_DDTRACE_PACKAGE}" ]; then pip install ddtrace; else pip install $PYTHON_DDTRACE_PACKAGE; fi
 
 python -c "import ddtrace; print(ddtrace.__version__)" > SYSTEM_TESTS_LIBRARY_VERSION
 
