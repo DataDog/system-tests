098a00e134fcc46094021810010f9de9fd49b068
roberto montero <roberto.montero@datadoghq.com>
2023-09-14 08:23:07 +0200

    fix lint

--- utils/build/docker/golang/parametric/ddtracer_version.sh
+++ utils/build/docker/golang/parametric/ddtracer_version.sh
@@ -6,5 +6,5 @@ go mod tidy
 
 # Read the library version out of the version.go file
 mod_dir=$(go list -f '{{.Dir}}' -m gopkg.in/DataDog/dd-trace-go.v1)
-version=$(sed -nrE 's#.*"v(.*)".*#\1#p' $mod_dir/internal/version/version.go) # Parse the version string content "v.*"
+version=$(sed -nrE 's#.*"v(.*)".*#\1#p' "$mod_dir"/internal/version/version.go) # Parse the version string content "v.*"
 echo "$version" > SYSTEM_TESTS_LIBRARY_VERSION
