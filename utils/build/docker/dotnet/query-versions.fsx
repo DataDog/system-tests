c18babff855b21664142f8734d0863eb165c64ef
Anna <anna.yafi@datadoghq.com>
2023-09-29 16:56:05 +0200

    Fix f#

--- utils/build/docker/dotnet/query-versions.fsx
+++ utils/build/docker/dotnet/query-versions.fsx
@@ -37,8 +37,9 @@ module QueryVersions =
    
     let writeRulesVersion () =
         let ruleVersion =
-            let (|?) lhs rhs = (if lhs = null then rhs else lhs)
-            use stream = assem.GetManifestResourceStream("Datadog.Trace.AppSec.Waf.rule-set.json") |? assem.GetManifestResourceStream("Datadog.Trace.AppSec.Waf.ConfigFiles.rule-set.json")
+            let mutable stream = assem.GetManifestResourceStream("Datadog.Trace.AppSec.Waf.rule-set.json")
+            if stream = null then
+                stream <- assem.GetManifestResourceStream("Datadog.Trace.AppSec.Waf.ConfigFiles.rule-set.json")
             use reader = new StreamReader(stream);
             use jsonReader = new JsonTextReader(reader);
             let root = JToken.ReadFrom(jsonReader);
