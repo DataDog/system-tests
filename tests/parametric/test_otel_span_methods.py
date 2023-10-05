ce35871d9c4d1f68aa103c21fafba09f63d70e9d
Steven Bouwkamp <stevenbouwkamp@gmail.com>
2023-10-05 09:11:47 -0400

    Change `message_consumer` to `message_client`
    Co-authored-by: Zach Montoya <zach.montoya@datadoghq.com>

--- tests/parametric/test_otel_span_methods.py
+++ tests/parametric/test_otel_span_methods.py
@@ -414,7 +414,7 @@ class Test_Otel_Span_Methods:
     @irrelevant(context.library == "cpp", reason="library does not implement OpenTelemetry")
     @missing_feature(context.library == "ruby", reason="Not implemented")
     @missing_feature(context.library == "php", reason="Not implemented")
-    def test_otel_span_operation_name_message_consumer_001(self, test_agent, test_library):
+    def test_otel_span_operation_name_message_client(self, test_agent, test_library):
         """
             Tests that the operation name will be set to `messaging.system + "." + messaging.operation` when:
             - Span kind is set to Client
