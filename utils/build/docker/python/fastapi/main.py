933f56af56c96aca68eaee4f88139d1f5d430ab8
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-10-06 16:48:13 +0200

    pin fastapi in fastapi weblog

--- utils/build/docker/python/fastapi/main.py
+++ utils/build/docker/python/fastapi/main.py
@@ -3,20 +3,16 @@ import random
 import subprocess
 import typing
 
+import fastapi
 import psycopg2
 import requests
 from ddtrace import Pin, tracer
 from ddtrace.appsec import trace_utils as appsec_trace_utils
 from fastapi import Cookie, FastAPI, Form, Header, Request
 from fastapi.responses import JSONResponse, PlainTextResponse
-from iast import (
-    weak_cipher,
-    weak_cipher_secure_algorithm,
-    weak_hash,
-    weak_hash_duplicates,
-    weak_hash_multiple,
-    weak_hash_secure_algorithm,
-)
+from iast import (weak_cipher, weak_cipher_secure_algorithm, weak_hash,
+                  weak_hash_duplicates, weak_hash_multiple,
+                  weak_hash_secure_algorithm)
 from pydantic import BaseModel
 
 try:
@@ -459,5 +455,5 @@ def view_cmdi_secure(cmd: typing.Annotated[str, Form()]):
 @app.get("/createextraservice", response_class=PlainTextResponse)
 def create_extra_service(serviceName: str = ""):
     if serviceName:
-        Pin.override(Flask, service=serviceName, tracer=tracer)
+        Pin.override(fastapi, service=serviceName, tracer=tracer)
     return "OK"
