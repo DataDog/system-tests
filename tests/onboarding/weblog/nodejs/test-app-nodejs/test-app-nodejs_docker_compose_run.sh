33a559337d191da34d6bfb79ada492ebcf819f2a
roberto montero <roberto.montero@datadoghq.com>
2023-10-09 14:44:37 +0200

    upload nodejs app files

--- tests/onboarding/weblog/nodejs/test-app-nodejs/test-app-nodejs_docker_compose_run.sh
+++ tests/onboarding/weblog/nodejs/test-app-nodejs/test-app-nodejs_docker_compose_run.sh
@@ -5,9 +5,9 @@ echo "COMANDO 1"
 echo "COMANDO 2"
 #tar xvf test-app-nodejs.tar 
 ls
-#sudo docker build -t system-tests/local .
-#sudo -E docker-compose -f docker-compose-agent-prod.yml up -d --remove-orphans datadog
-#sleep 20
-#sudo -E docker-compose -f docker-compose.yml up -d test-app-nodejs
-#sudo docker-compose logs
+sudo docker build -t system-tests/local .
+sudo -E docker-compose -f docker-compose-agent-prod.yml up -d --remove-orphans datadog
+sleep 20
+sudo -E docker-compose -f docker-compose.yml up -d test-app-nodejs
+sudo docker-compose logs
 echo "RUN DONE!!!!!!!!!!"
