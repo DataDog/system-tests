51e6a1e90cb74371e62648de350e5bbc7bcab437
roberto montero <roberto.montero@datadoghq.com>
2023-10-09 10:32:04 +0200

    debug amazon

--- tests/onboarding/weblog/nodejs/test-app-nodejs/test-app-nodejs_docker_compose_run.sh
+++ tests/onboarding/weblog/nodejs/test-app-nodejs/test-app-nodejs_docker_compose_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tar xvf test-app-nodejs.tar
+#tar xvf test-app-nodejs.tar
 #sudo docker build -t system-tests/local .
 #sudo -E docker-compose -f docker-compose-agent-prod.yml up -d --remove-orphans datadog
 #sleep 20
 #sudo -E docker-compose -f docker-compose.yml up -d test-app-nodejs
 #sudo docker-compose logs
-echo "RUN DONE!!!"
+echo "RUN DONE!!!!"
