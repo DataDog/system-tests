0d04925e1fe7054f38f685e0e1149390897d8fb3
roberto montero <roberto.montero@datadoghq.com>
2023-10-09 13:03:40 +0200

    debug nodejs

--- tests/onboarding/weblog/nodejs/test-app-nodejs/test-app-nodejs_docker_compose_run.sh
+++ tests/onboarding/weblog/nodejs/test-app-nodejs/test-app-nodejs_docker_compose_run.sh
@@ -3,10 +3,10 @@
 echo "COMANDO 1"
 sudo yum install -y tar
 echo "COMANDO 2"
-#tar xf test-app-nodejs.tar &>/dev/null
+tar xvf test-app-nodejs.tar 
 #sudo docker build -t system-tests/local .
 #sudo -E docker-compose -f docker-compose-agent-prod.yml up -d --remove-orphans datadog
 #sleep 20
-#sudo -E docker-compose -f docker-compose.yml up -d test-app-nodejs
-#sudo docker-compose logs
+sudo -E docker-compose -f docker-compose.yml up -d test-app-nodejs
+sudo docker-compose logs
 echo "RUN DONE!!!!!!!!!!"
