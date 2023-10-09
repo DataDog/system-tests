43c06f31cb9578d4afd3dcac30c954ed8f6fd963
roberto montero <roberto.montero@datadoghq.com>
2023-10-09 20:51:53 +0200

    fix parametric test

--- tests/parametric/conftest.py
+++ tests/parametric/conftest.py
@@ -624,7 +624,7 @@ class _TestAgentAPI:
             if resp.status_code != 200:
                 pytest.fail(resp.text.decode("utf-8"), pytrace=False)
 
-    def wait_for_num_traces(self, num: int, clear: bool = False, wait_loops: int = 20) -> List[Trace]:
+    def wait_for_num_traces(self, num: int, clear: bool = False, wait_loops: int = 30) -> List[Trace]:
         """Wait for `num` traces to be received from the test agent.
 
         Returns after the number of traces has been received or raises otherwise after 2 seconds of polling.
