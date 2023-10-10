e52d6c69cfaee2d1ed6e9b6ca82615af159bacd4
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-10-10 10:47:20 +0200

    revert unrelated change

--- tests/test_scrubbing.py
+++ tests/test_scrubbing.py
@@ -3,8 +3,7 @@
 # Copyright 2022 Datadog, Inc.
 
 import re
-
-from utils import bug, context, coverage, interfaces, missing_feature, rfc, weblog
+from utils import bug, context, coverage, interfaces, rfc, weblog, missing_feature
 from utils.tools import logger
 
 
