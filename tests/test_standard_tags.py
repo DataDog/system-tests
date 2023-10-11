4704a1ac5c8cd1943c9efb45728cdb3201178fdd
Anna <anna.yafi@datadoghq.com>
2023-10-11 16:36:31 +0200

    Fix version number

--- tests/test_standard_tags.py
+++ tests/test_standard_tags.py
@@ -79,7 +79,7 @@ class Test_StandardTagsUrl:
     # when tracer is updated, add (for example)
     @irrelevant(context.library >= "java@1.21.0", reason="java released the new version at 1.21.0")
     @irrelevant(context.library >= "python@1.18.0rc1", reason="python released the new version at 1.19.0")
-    @irrelevant(context.library >= "dotnet@2.39", reason="dotnet released the new version at 2.39.0")
+    @irrelevant(context.library >= "dotnet@2.40", reason="dotnet released the new version at 2.39.0")
     def test_url_with_sensitive_query_string_legacy(self):
         for r, tag in self.requests_sensitive_query_string:
             interfaces.library.add_span_tag_validation(
@@ -116,7 +116,7 @@ class Test_StandardTagsUrl:
     @missing_feature(context.library < "java@1.21.0", reason="previous obfuscation regex")
     @irrelevant(context.library < "python@1.19", reason="python released the new version at 1.19.0")
     # remove = of <= when PR https://github.com/DataDog/dd-trace-dotnet/pull/4504 is merged
-    @irrelevant(context.library <= "dotnet@2.39", reason="dotnet released the new version at 2.39.0")
+    @irrelevant(context.library <= "dotnet@2.40", reason="dotnet released the new version at 2.39.0")
     def test_url_with_sensitive_query_string(self):
         for r, tag in self.requests_sensitive_query_string:
             interfaces.library.add_span_tag_validation(
@@ -131,7 +131,7 @@ class Test_StandardTagsUrl:
     # when tracer is updated, add (for example)
     @irrelevant(context.library >= "java@1.21.0", reason="java released the new version at 1.21.0")
     @irrelevant(context.library >= "python@1.18.0rc1", reason="python released the new version at 1.19.0")
-    @irrelevant(context.library >= "dotnet@2.39", reason="dotnet released the new version at 2.39.0")
+    @irrelevant(context.library >= "dotnet@2.40", reason="dotnet released the new version at 2.39.0")
     def test_multiple_matching_substring_legacy(self):
         tag = r"^.*/waf\?<redacted>&key1=val1&key2=val2&<redacted>&<redacted>&key3=val3&json=%7B%20%22<redacted>%7D$"  # pylint: disable=line-too-long
         interfaces.library.add_span_tag_validation(
@@ -150,7 +150,7 @@ class Test_StandardTagsUrl:
     @missing_feature(context.library < "java@1.21.0", reason="previous obfuscation regex")
     @irrelevant(context.library < "python@1.19", reason="python released the new version at 1.19.0")
     # remove = of <= when PR https://github.com/DataDog/dd-trace-dotnet/pull/4504 is merged
-    @irrelevant(context.library <= "dotnet@2.39", reason="dotnet released the new version at 2.39.0")
+    @irrelevant(context.library <= "dotnet@2.40", reason="dotnet released the new version at 2.39.0")
     def test_multiple_matching_substring(self):
         tag = r"^.*/waf\?<redacted>&key1=val1&key2=val2&<redacted>&<redacted>&key3=val3&json=%7B%20<redacted>%7D$"  # pylint: disable=line-too-long
         interfaces.library.add_span_tag_validation(
