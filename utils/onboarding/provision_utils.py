d1c8747214de3d3fe960f50e829c9c7aa4a8fa5c
roberto montero <roberto.montero@datadoghq.com>
2023-10-17 16:42:10 +0200

    split onboarding tests on stages

--- utils/onboarding/provision_utils.py
+++ utils/onboarding/provision_utils.py
@@ -291,4 +291,4 @@ class ProvisionFilter:
         self.env = env
         self.os_distro = os.getenv("ONBOARDING_FILTER_OS_DISTRO")
         self.weblog = weblog
-        self.uninstall = os.getenv("ONBOARDING_UNINSTALL", 'False').lower()
+        self.uninstall = os.getenv("ONBOARDING_UNINSTALL", "False").lower() == "true"
