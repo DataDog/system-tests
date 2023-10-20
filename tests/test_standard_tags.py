8ce922bebf4205bc717277dfc84e6b28413538c6
Anna <anna.yafi@datadoghq.com>
2023-10-20 17:33:14 +0200

    activate new tests for dotnet

--- tests/test_standard_tags.py
+++ tests/test_standard_tags.py
@@ -115,8 +115,7 @@ class Test_StandardTagsUrl:
     )
     @missing_feature(context.library < "java@1.21.0", reason="previous obfuscation regex")
     @irrelevant(context.library < "python@1.19", reason="python released the new version at 1.19.0")
-    # remove = of <= when PR https://github.com/DataDog/dd-trace-dotnet/pull/4504 is merged
-    @irrelevant(context.library <= "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
+    @irrelevant(context.library < "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
     def test_url_with_sensitive_query_string(self):
         for r, tag in self.requests_sensitive_query_string:
             interfaces.library.add_span_tag_validation(
@@ -149,8 +148,7 @@ class Test_StandardTagsUrl:
     )
     @missing_feature(context.library < "java@1.21.0", reason="previous obfuscation regex")
     @irrelevant(context.library < "python@1.19", reason="python released the new version at 1.19.0")
-    # remove = of <= when PR https://github.com/DataDog/dd-trace-dotnet/pull/4504 is merged
-    @irrelevant(context.library <= "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
+    @irrelevant(context.library < "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
     def test_multiple_matching_substring(self):
         tag = r"^.*/waf\?<redacted>&key1=val1&key2=val2&<redacted>&<redacted>&key3=val3&json=%7B%20<redacted>%7D&<redacted>$"  # pylint: disable=line-too-long
         interfaces.library.add_span_tag_validation(
