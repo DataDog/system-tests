b65a61d7bf1c672cf0148a07e79060ec994600b0
Javier Santos <javier.santos@datadoghq.com>
2023-10-23 14:53:48 +0200

    updated test to latest base class

--- tests/appsec/iast/source/test_multipart.py
+++ tests/appsec/iast/source/test_multipart.py
@@ -2,36 +2,15 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import coverage, weblog, bug
-from .._test_iast_fixtures import SourceFixture
-
+from utils import coverage
+from .._test_iast_fixtures import BaseSourceTest
 
 @coverage.basic
-class TestMultipart:
+class TestMultipart(BaseSourceTest):
     """Verify that multipart parameter is tainted"""
 
-    source_fixture = SourceFixture(
-        http_method="POST",
-        endpoint="/iast/source/multipart/test",
-        request_kwargs={"files": {"file1": ("file1", "bsldhkuqwgervf")}},
-        source_type="http.request.multipart.parameter",
-        source_name="Content-Disposition",
-        source_value=None,
-    )
-
-    def test_source_reported(self):
-        self.source_fixture.test()
-
-    def setup_telemetry_metric_instrumented_source(self):
-        self.source_fixture.setup_telemetry_metric_instrumented_source()
-
-    @bug(library="java", reason="Not working as expected")
-    def test_telemetry_metric_instrumented_source(self):
-        self.source_fixture.test_telemetry_metric_instrumented_source()
-
-    def setup_telemetry_metric_executed_source(self):
-        self.source_fixture.setup_telemetry_metric_executed_source()
-
-    @bug(library="java", reason="Not working as expected")
-    def test_telemetry_metric_executed_source(self):
-        self.source_fixture.test_telemetry_metric_executed_source()
+    endpoint="/iast/source/multipart/test"
+    requests_kwargs=[{"method": "POST", "files": {"file1": ("file1", "bsldhkuqwgervf")}}]
+    source_type="http.request.multipart.parameter"
+    source_name="Content-Disposition"
+    source_value=None
