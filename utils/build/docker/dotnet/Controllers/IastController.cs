ee63d7e21d8a382b99ea51bff0e166ada86bd0a8
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2023-10-24 12:23:36 +0200

    Dotnet IAST WeakCipher tests

--- utils/build/docker/dotnet/Controllers/IastController.cs
+++ utils/build/docker/dotnet/Controllers/IastController.cs
@@ -62,6 +62,20 @@ namespace weblog
             return Content(result.ToString());
         }
 
+        [HttpGet("insecure_cipher/test_insecure_algorithm")]
+        public IActionResult test_insecure_weakCipher()
+        {
+            DES.Create();
+            return StatusCode(200);
+        }
+        
+        [HttpGet("insecure_cipher/test_secure_algorithm")]
+        public IActionResult test_secure_weakCipher()
+        {
+            Aes.Create();
+            return StatusCode(200);
+        }
+
         [HttpPost("cmdi/test_insecure")]
         public IActionResult test_insecure_cmdI([FromForm] RequestData data)
         {
