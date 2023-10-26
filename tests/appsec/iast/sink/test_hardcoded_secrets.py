08edb65a35f2a6262a38a8d2b2314d68616bd131
Igor Unanua <igor.unanua@datadoghq.com>
2023-10-26 08:49:03 +0200

    Hardcoded secret vuln test

---
+++ tests/appsec/iast/sink/test_hardcoded_secrets.py
@@ -0,0 +1,31 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2021 Datadog, Inc.
+
+from utils import flaky, context, coverage, missing_feature
+from utils.tools import logging
+from .._test_iast_fixtures import BaseSinkTest
+
+
+@coverage.basic
+class Test_HardcodedSecrets(BaseSinkTest):
+    """Test Hardcoded secrets detection."""
+
+    vulnerability_type = "HARDCODED_SECRET"
+    http_method = "GET"
+    insecure_endpoint = "/iast/hardcoded_secrets/test_insecure"
+    secure_endpoint = "/iast/hardcoded_secrets/test_secure"
+    data = {}
+    location_map = {"nodejs": "iast/index.js"}
+
+    def test_insecure(self):
+        self.insecure_request = None
+        super().test_insecure()
+
+    @missing_feature(library="nodejs", reason="Not implemented yet")
+    def test_telemetry_metric_instrumented_sink(self):
+        super().test_telemetry_metric_instrumented_sink()
+
+    @missing_feature(library="nodejs", reason="Not implemented yet")
+    def test_telemetry_metric_executed_sink(self):
+        super().test_telemetry_metric_executed_sink()
