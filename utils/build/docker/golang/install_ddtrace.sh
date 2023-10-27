8ca8078c2f5b4d367e2c6fc80aa76e4ecde19c02
Dario Castañé <dario.castane@datadoghq.com>
2023-10-27 14:18:43 +0200

    utils/build/docker/golang: add TODO for v2 release

--- utils/build/docker/golang/install_ddtrace.sh
+++ utils/build/docker/golang/install_ddtrace.sh
@@ -12,6 +12,7 @@ elif [ -e "/binaries/golang-load-from-go-get" ]; then
 
 else
     echo "Installing production dd-trace-version"
+    # TODO(darccio): remove @ref on v2 release
     go get -v -d -u github.com/DataDog/dd-trace-go/v2@v2.0.0-20231026141357-d9d8b0ef3849
 fi
 
