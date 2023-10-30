29d2967895be749829ad909e75b03176e444f62a
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-10-30 17:22:55 +0000

    Update test_standard_tags.py

--- tests/test_standard_tags.py
+++ tests/test_standard_tags.py
@@ -80,7 +80,7 @@ class Test_StandardTagsUrl:
     @irrelevant(context.library >= "java@1.21.0", reason="java released the new version at 1.21.0")
     @irrelevant(context.library >= "python@1.18.0rc1", reason="python released the new version at 1.19.0")
     @irrelevant(context.library >= "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
-    @irrelevant(context.library >= "php@0.92.0.dev", reason="php released the new version at 0.93.0")
+    @irrelevant(context.library >= "php@0.93.0", reason="php released the new version at 0.93.0")
     def test_url_with_sensitive_query_string_legacy(self):
         for r, tag in self.requests_sensitive_query_string:
             interfaces.library.add_span_tag_validation(
@@ -117,7 +117,7 @@ class Test_StandardTagsUrl:
     @missing_feature(context.library < "java@1.21.0", reason="previous obfuscation regex")
     @irrelevant(context.library < "python@1.19", reason="python released the new version at 1.19.0")
     @irrelevant(context.library < "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
-    @irrelevant(context.library < "php@0.92.0.dev", reason="php released the new version at 0.93.0")
+    @irrelevant(context.library < "php@0.93.0", reason="php released the new version at 0.93.0")
     def test_url_with_sensitive_query_string(self):
         for r, tag in self.requests_sensitive_query_string:
             interfaces.library.add_span_tag_validation(
@@ -133,7 +133,7 @@ class Test_StandardTagsUrl:
     @irrelevant(context.library >= "java@1.21.0", reason="java released the new version at 1.21.0")
     @irrelevant(context.library >= "python@1.18.0rc1", reason="python released the new version at 1.19.0")
     @irrelevant(context.library >= "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
-    @irrelevant(context.library >= "php@0.92.0.dev", reason="php released the new version at 0.93.0")
+    @irrelevant(context.library >= "php@0.93.0", reason="php released the new version at 0.93.0")
     def test_multiple_matching_substring_legacy(self):
         tag = r"^.*/waf\?<redacted>&key1=val1&key2=val2&<redacted>&<redacted>&key3=val3&json=%7B%20%22<redacted>%7D$"  # pylint: disable=line-too-long
         interfaces.library.add_span_tag_validation(
@@ -152,7 +152,7 @@ class Test_StandardTagsUrl:
     @missing_feature(context.library < "java@1.21.0", reason="previous obfuscation regex")
     @irrelevant(context.library < "python@1.19", reason="python released the new version at 1.19.0")
     @irrelevant(context.library < "dotnet@2.41", reason="dotnet released the new version at 2.41.0")
-    @irrelevant(context.library < "php@0.92.0.dev", reason="php released the new version at 0.93.0")
+    @irrelevant(context.library < "php@0.93.0", reason="php released the new version at 0.93.0")
     def test_multiple_matching_substring(self):
         tag = r"^.*/waf\?<redacted>&key1=val1&key2=val2&<redacted>&<redacted>&key3=val3&json=%7B%20<redacted>%7D&<redacted>$"  # pylint: disable=line-too-long
         interfaces.library.add_span_tag_validation(
