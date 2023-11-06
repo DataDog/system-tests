fb373e21303f09b2f713ca2052df097d6b1fb80a
roberto montero <roberto.montero@datadoghq.com>
2023-11-06 17:25:36 +0100

    Upload to FPD only once per pipeline

--- utils/scripts/push_reports_dashboard.sh
+++ utils/scripts/push_reports_dashboard.sh
@@ -7,16 +7,14 @@ cd "$base_path"
 mkdir system-tests-dashboard
 git clone https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/DataDog/system-tests-dashboard.git system-tests-dashboard
 cd system-tests-dashboard
-SCENARIO_SUFIX=$(echo "$SCENARIO" | tr '[:upper:]' '[:lower:]')
-REPORTS_PATH="reports/$ENV/$TEST_LIBRARY/$WEBLOG"
+#from system-tests/reports to system-tests-dashboard/reports
+cp -R ../reports/ reports/
 
 git config user.name "${GITHUB_USER}"
 git config user.email "${GITHUB_MAIL}"
 
 git pull #avoid problems with multiple pushes at same time 
-mkdir -p "$REPORTS_PATH"
-cp ../logs_"${SCENARIO_SUFIX}"/report.json "$REPORTS_PATH"/"${SCENARIO_SUFIX}".json
-git add "$REPORTS_PATH"
+git add reports/
 git commit -m "add onboarding report"  
 git push
 echo "DONE Reports commited to system-tests-dashboard!"
