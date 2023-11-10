ffbbb948aa8f22690063a7076db9a9ef41a424c3
Yury Gribkov <yury.gribkov@gmail.com>
2023-11-09 21:28:10 -0800

    Reorganize trace sampling tests by features: service+name, globs, resource, tags

--- tests/parametric/test_trace_sampling.py
+++ tests/parametric/test_trace_sampling.py
@@ -2,101 +2,47 @@ import pytest
 from utils.parametric.spec.trace import Span
 from utils.parametric.spec.trace import find_span_in_traces
 import json
-from utils import scenarios
+from utils import coverage, rfc, scenarios
 
 
 @scenarios.parametric
-class Test_Trace_Sampling:
+class Test_Trace_Sampling_Basic:
     @pytest.mark.parametrize(
         "library_env",
         [
             {
                 "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"service": "webserver", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"name": "web.request", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"resource": "/bar", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
                 "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [{"service": "webserver", "name": "web.request", "sample_rate": 1},]
-                ),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"name": "web.request", "resource": "/bar", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [{"service": "webserver", "resource": "/bar", "sample_rate": 1},]
+                    [
+                        {"service": "webserver.non-matching", "sample_rate": 0},
+                        {"service": "webserver", "sample_rate": 1},
+                    ]
                 ),
             },
             {
                 "DD_TRACE_SAMPLE_RATE": 0,
                 "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [{"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 1},]
+                    [{"name": "web.request.non-matching", "sample_rate": 0}, {"name": "web.request", "sample_rate": 1}]
+                ),
+            },
+            {
+                "DD_TRACE_SAMPLE_RATE": 0,
+                "DD_TRACE_SAMPLING_RULES": json.dumps(
+                    [
+                        {"service": "webserver.non-matching", "name": "web.request", "sample_rate": 0},
+                        {"service": "webserver", "name": "web.request.non-matching", "sample_rate": 0},
+                        {"service": "webserver", "name": "web.request", "sample_rate": 1},
+                    ]
                 ),
             },
         ],
     )
-    def test_trace_sampled_by_defined_trace_sampling_rules_exact_match(self, test_agent, test_library):
-        """Test that a trace is sampled by the matching defined trace sampling rules (exact match)"""
+    def test_trace_sampled_by_trace_sampling_rule_exact_match(self, test_agent, test_library):
+        """Test that a trace is sampled by the exact matching trace sampling rule"""
         with test_library:
-            with test_library.start_span(name="web.request", service="webserver", resource="/bar"):
+            with test_library.start_span(name="web.request", service="webserver"):
                 pass
-        span = find_span_in_traces(
-            test_agent.wait_for_num_traces(1), Span(name="web.request", service="webserver", resource="/bar")
-        )
-
-        assert span["metrics"].get("_sampling_priority_v1") == 2
-
-    @pytest.mark.parametrize(
-        "library_env",
-        [
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"service": "web*", "sample_rate": 1},]),
-            },
-            {"DD_TRACE_SAMPLE_RATE": 0, "DD_TRACE_SAMPLING_RULES": json.dumps([{"name": "web.*", "sample_rate": 1},])},
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"resource": "/b*", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"service": "*", "name": "web.req*", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"name": "web*", "resource": "/b?r", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps([{"service": "webser???", "resource": "/b*", "sample_rate": 1},]),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 0,
-                "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [{"service": "web*", "name": "web.???????", "resource": "*", "sample_rate": 1},]
-                ),
-            },
-        ],
-    )
-    def test_trace_sampled_by_defined_trace_sampling_rules_glob_match(self, test_agent, test_library):
-        """Test that a trace is sampled by the matching defined trace sampling rules (glob match)"""
-        with test_library:
-            with test_library.start_span(name="web.request", service="webserver", resource="/bar"):
-                pass
-        span = find_span_in_traces(
-            test_agent.wait_for_num_traces(1), Span(name="web.request", service="webserver", resource="/bar")
-        )
+        span = find_span_in_traces(test_agent.wait_for_num_traces(1), Span(name="web.request", service="webserver"))
 
         assert span["metrics"].get("_sampling_priority_v1") == 2
 
@@ -106,12 +52,12 @@ class Test_Trace_Sampling:
             {
                 "DD_TRACE_SAMPLE_RATE": 1,
                 "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [{"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 0},]
+                    [{"service": "webserver", "name": "web.request", "sample_rate": 0}]
                 ),
             }
         ],
     )
-    def test_trace_dropped_by_defined_trace_sampling_rule(self, test_agent, test_library):
+    def test_trace_dropped_by_trace_sampling_rule(self, test_agent, test_library):
         """Test that a trace is dropped by the matching defined trace sampling rule"""
         with test_library:
             with test_library.start_span(name="web.request", service="webserver", resource="/bar"):
@@ -122,15 +68,86 @@ class Test_Trace_Sampling:
 
         assert span["metrics"].get("_sampling_priority_v1") == -1
 
+
+@scenarios.parametric
+@rfc("https://docs.google.com/document/d/1S9pufnJjrsxH6pRbpigdYFwA5JjSdZ6iLZ-9E7PoAic/")
+class Test_Trace_Sampling_Globs:
     @pytest.mark.parametrize(
         "library_env",
         [
             {
                 "DD_TRACE_SAMPLE_RATE": 0,
                 "DD_TRACE_SAMPLING_RULES": json.dumps(
+                    [{"service": "web.non-matching*", "sample_rate": 0}, {"service": "web*", "sample_rate": 1},]
+                ),
+            },
+            {
+                "DD_TRACE_SAMPLE_RATE": 0,
+                "DD_TRACE_SAMPLING_RULES": json.dumps(
+                    [{"name": "web.non-matching*", "sample_rate": 0}, {"name": "web.*", "sample_rate": 1}]
+                ),
+            },
+            {
+                "DD_TRACE_SAMPLE_RATE": 0,
+                "DD_TRACE_SAMPLING_RULES": json.dumps(
                     [
-                        {"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 1},
-                        {"service": "webserver", "name": "web.request", "sample_rate": 0},
+                        {"service": "webserv?r.non-matching", "name": "web.req*", "sample_rate": 0},
+                        {"service": "webserv?r", "name": "web.req*.non-matching", "sample_rate": 0},
+                        {"service": "webserv?r", "name": "web.req*", "sample_rate": 1},
+                    ]
+                ),
+            },
+        ],
+    )
+    def test_trace_sampled_by_trace_sampling_rule_glob_match(self, test_agent, test_library):
+        """Test that a trace is sampled by the glob matching trace sampling rule"""
+        with test_library:
+            with test_library.start_span(name="web.request", service="webserver"):
+                pass
+        span = find_span_in_traces(test_agent.wait_for_num_traces(1), Span(name="web.request", service="webserver"))
+
+        assert span["metrics"].get("_sampling_priority_v1") == 2
+
+    @pytest.mark.parametrize(
+        "library_env",
+        [
+            {
+                "DD_TRACE_SAMPLE_RATE": 1,
+                "DD_TRACE_SAMPLING_RULES": json.dumps([{"service": "w?bs?rv?r", "name": "web.*", "sample_rate": 0}]),
+            }
+        ],
+    )
+    def test_trace_dropped_by_trace_sampling_rule(self, test_agent, test_library):
+        """Test that a trace is dropped by the matching defined trace sampling rule"""
+        with test_library:
+            with test_library.start_span(name="web.request", service="webserver", resource="/bar"):
+                pass
+        span = find_span_in_traces(
+            test_agent.wait_for_num_traces(1), Span(name="web.request", service="webserver", resource="/bar")
+        )
+
+        assert span["metrics"].get("_sampling_priority_v1") == -1
+
+
+@scenarios.parametric
+@rfc("https://docs.google.com/document/d/1S9pufnJjrsxH6pRbpigdYFwA5JjSdZ6iLZ-9E7PoAic/")
+class Test_Trace_Sampling_Resource:
+    @pytest.mark.parametrize(
+        "library_env",
+        [
+            {
+                "DD_TRACE_SAMPLE_RATE": 0,
+                "DD_TRACE_SAMPLING_RULES": json.dumps(
+                    [{"resource": "/bar.non-matching", "sample_rate": 0}, {"resource": "/?ar", "sample_rate": 1},]
+                ),
+            },
+            {
+                "DD_TRACE_SAMPLE_RATE": 0,
+                "DD_TRACE_SAMPLING_RULES": json.dumps(
+                    [
+                        {"name": "web.request.non-matching", "resource": "/bar", "sample_rate": 0},
+                        {"name": "web.request", "resource": "/bar.non-matching", "sample_rate": 0},
+                        {"name": "web.request", "resource": "/b*", "sample_rate": 1},
                     ]
                 ),
             },
@@ -138,15 +155,42 @@ class Test_Trace_Sampling:
                 "DD_TRACE_SAMPLE_RATE": 0,
                 "DD_TRACE_SAMPLING_RULES": json.dumps(
                     [
-                        {"service": "webserver", "name": "web.request", "sample_rate": 1},
-                        {"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 0},
+                        {"service": "webserver.non-matching", "resource": "/bar", "sample_rate": 0},
+                        {"service": "webserver", "resource": "/bar.non-matching", "sample_rate": 0},
+                        {"service": "webserver", "resource": "/bar", "sample_rate": 1},
+                    ]
+                ),
+            },
+            {
+                "DD_TRACE_SAMPLE_RATE": 0,
+                "DD_TRACE_SAMPLING_RULES": json.dumps(
+                    [
+                        {
+                            "service": "webserver.non-matching",
+                            "name": "web.request",
+                            "resource": "/bar",
+                            "sample_rate": 0,
+                        },
+                        {
+                            "service": "webserver",
+                            "name": "web.request.non-matching",
+                            "resource": "/bar",
+                            "sample_rate": 0,
+                        },
+                        {
+                            "service": "webserver",
+                            "name": "web.request",
+                            "resource": "/bar.non-matching",
+                            "sample_rate": 0,
+                        },
+                        {"service": "webserver", "name": "web.request", "resource": "/b?r", "sample_rate": 1},
                     ]
                 ),
             },
         ],
     )
-    def test_trace_sampled_by_first_matching_trace_sampling_rule(self, test_agent, test_library):
-        """Test that a trace is sampled by the first matching trace sampling rules"""
+    def test_trace_sampled_by_trace_sampling_rule_exact_match(self, test_agent, test_library):
+        """Test that a trace is sampled by the exact matching trace sampling rule"""
         with test_library:
             with test_library.start_span(name="web.request", service="webserver", resource="/bar"):
                 pass
@@ -162,25 +206,13 @@ class Test_Trace_Sampling:
             {
                 "DD_TRACE_SAMPLE_RATE": 1,
                 "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [
-                        {"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 0},
-                        {"service": "webserver", "name": "web.request", "sample_rate": 1},
-                    ]
+                    [{"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 0}]
                 ),
-            },
-            {
-                "DD_TRACE_SAMPLE_RATE": 1,
-                "DD_TRACE_SAMPLING_RULES": json.dumps(
-                    [
-                        {"service": "webserver", "name": "web.request", "sample_rate": 0},
-                        {"service": "webserver", "name": "web.request", "resource": "/bar", "sample_rate": 1},
-                    ]
-                ),
-            },
+            }
         ],
     )
-    def test_trace_dropped_by_first_matching_trace_sampling_rule(self, test_agent, test_library):
-        """Test that a trace is dropped by the first matching trace sampling rules"""
+    def test_trace_dropped_by_trace_sampling_rule(self, test_agent, test_library):
+        """Test that a trace is dropped by the matching defined trace sampling rule"""
         with test_library:
             with test_library.start_span(name="web.request", service="webserver", resource="/bar"):
                 pass
@@ -189,3 +221,9 @@ class Test_Trace_Sampling:
         )
 
         assert span["metrics"].get("_sampling_priority_v1") == -1
+
+
+@coverage.not_implemented
+@rfc("https://docs.google.com/document/d/1S9pufnJjrsxH6pRbpigdYFwA5JjSdZ6iLZ-9E7PoAic/")
+class Test_Trace_Sampling_Tags:
+    pass
