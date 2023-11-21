6b20bff154a241f462711d26efcd1437f2587a0e
Marco Costa <marco.costa@datadoghq.com>
2023-11-20 16:16:47 -0800

    wip

--- tests/parametric/test_otel_span_methods.py
+++ tests/parametric/test_otel_span_methods.py
@@ -128,13 +128,6 @@ class Test_Otel_Span_Methods:
             assert root_span["meta"]["d_bool_val"] == "false"
             assert root_span["meta"]["array_val_int"] == "[10,20]"
             assert root_span["meta"]["array_val_double"] == "[10.1,20.2]"
-        elif root_span["meta"]["language"] == "ruby":
-            assert root_span["meta"]["bool_val"] == "true"
-            assert root_span["meta"]["array_val_bool"] == "[true, false]"
-            assert root_span["meta"]["array_val_str"] == '["val1", "val2"]'
-            assert root_span["meta"]["d_bool_val"] == "false"
-            assert root_span["meta"]["array_val_int"] == "[10, 20]"
-            assert root_span["meta"]["array_val_double"] == "[10.1, 20.2]"
         else:
             assert root_span["meta"]["bool_val"] == "True"
             assert root_span["meta"]["array_val_bool"] == "[True, False]"
