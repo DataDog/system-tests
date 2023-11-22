80f3401b0e8620504ddcfbef743b5d23ee25db99
roberto montero <roberto.montero@datadoghq.com>
2023-11-22 09:52:25 +0100

    test dotnet

--- tests/onboarding/weblog/dotnet/test-app-dotnet/test-app-dotnet_run.sh
+++ tests/onboarding/weblog/dotnet/test-app-dotnet/test-app-dotnet_run.sh
@@ -3,7 +3,8 @@ set -e
 sudo chmod -R 755 *
 
 echo "START RUN APP"
-
+#If we are trying to inject the library on the "restore" or "build" command we should show the traces
+export DD_APM_INSTRUMENTATION_DEBUG=TRUE
 sudo sed -i "s/net7.0/net6.0/g" MinimalWebApp.csproj 
 dotnet restore
 dotnet build -c Release
