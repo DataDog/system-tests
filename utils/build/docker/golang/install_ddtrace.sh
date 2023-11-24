5ee8065011c043b27c17db6e86e6b5b4ff78b229
Dario Castañé <dario.castane@datadoghq.com>
2023-11-24 14:45:23 +0100

    Revert "utils/build/docker/golang/app: use branch ref instead of commit"
    This reverts commit d74dcf6200d704ecf1651bd249201d4c64df4577.

--- utils/build/docker/golang/install_ddtrace.sh
+++ utils/build/docker/golang/install_ddtrace.sh
@@ -14,7 +14,8 @@ else
     echo "Installing production dd-trace-version"
     # TODO(darccio): remove @$ref on v2 release
     # Temporary fix to pass the tests isolating gin
-    go get -v -d -u github.com/DataDog/dd-trace-go/v2@dario.castane/AIT-8717/isolate-contribs-batch-2
+    ref=$(curl --silent https://api.github.com/repos/DataDog/dd-trace-go/branches/dario.castane/AIT-8717/isolate-contribs-batch-2 | jq --raw-output '.commit.sha')
+    go get -v -d -u github.com/DataDog/dd-trace-go/v2@$ref
 fi
 
 # Downloading a newer version of the tracer may require to resolve again all dependencies
