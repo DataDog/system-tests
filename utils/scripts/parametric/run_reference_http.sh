ccb57fac4bfa06d2b27638dfeb51bca07453e8bc
ZStriker19 <zach.groves@datadoghq.com>
2023-12-04 12:56:17 -0500

    add quotes for linting

--- utils/scripts/parametric/run_reference_http.sh
+++ utils/scripts/parametric/run_reference_http.sh
@@ -3,8 +3,8 @@
 
 PORT="${PORT:=8000}"
 
-source $PWD/venv/bin/activate
+source "$PWD/venv/bin/activate"
 
-pushd $PWD/utils/build/docker/python_http/parametric || exit
+pushd "$PWD/utils/build/docker/python_http/parametric" || exit
     APM_TEST_CLIENT_SERVER_PORT=$PORT python -m apm_test_client
 popd || exit
