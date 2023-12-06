29098b5ae32d990866a0dee99d25ef4baaf70cb6
Steven Bouwkamp <steven.bouwkamp@datadoghq.com>
2023-12-06 10:29:03 -0500

    Attempt to fix linting errors

--- utils/build/docker/dotnet/parametric/install_ddtrace.sh
+++ utils/build/docker/dotnet/parametric/install_ddtrace.sh
@@ -9,17 +9,17 @@ get_latest_release() {
 
 mkdir -p /opt/datadog
 
-if [ $(ls /binaries/Datadog.Trace.ClrProfiler.Native.so | wc -l) = 1 ]; then
+if [ "$(ls /binaries/Datadog.Trace.ClrProfiler.Native.so | wc -l)" = 1 ]; then
     echo "Install from local folder"
     cp -r /binaries/* /opt/datadog/
 else
-    if [ $(ls datadog-dotnet-apm*.tar.gz | wc -l) = 1 ]; then
+    if [ "$(ls datadog-dotnet-apm*.tar.gz | wc -l)" = 1 ]; then
         echo "Install ddtrace from $(ls datadog-dotnet-apm*.tar.gz)"
     else
         echo "Install ddtrace from github releases"
         DDTRACE_VERSION="$(get_latest_release DataDog/dd-trace-dotnet)"
 
-        if [ $(uname -m) = "aarch64" ]; then
+        if [ "$(uname -m)" = "aarch64" ]; then
             artifact=datadog-dotnet-apm-${DDTRACE_VERSION}.arm64.tar.gz
         else
             artifact=datadog-dotnet-apm-${DDTRACE_VERSION}.tar.gz
