527830fa7ecb4e66b689a884d044d133a35e5e36
Ida Liu <119438987+ida613@users.noreply.github.com>
2023-12-13 09:11:15 -0500

    Update test_telemetry.py

--- tests/appsec/waf/test_telemetry.py
+++ tests/appsec/waf/test_telemetry.py
@@ -223,7 +223,7 @@ def _validate_headers(headers, request_type):
 
     if context.library == "python":
         # APM Python migrates Telemetry to V2
-        # test
+        # test again
         expected_headers["DD-Telemetry-API-Version"] = "v2"
     elif context.library >= "java@1.23.0":
         expected_headers["DD-Telemetry-API-Version"] = "v2"
