c95b95173c517c84067dc37648bf887081dc8e3e
Christophe Papazian <christophe.papazian@datadoghq.com>
2023-12-20 17:31:42 +0100

    revert install script change

--- utils/build/docker/python/install_ddtrace.sh
+++ utils/build/docker/python/install_ddtrace.sh
@@ -15,9 +15,8 @@ elif [ $(ls python-load-from-pip | wc -l) = 1 ]; then
     echo "Install ddtrace from $(cat python-load-from-pip)"
     pip install "$(cat python-load-from-pip)"
 elif [ $(ls *.whl | wc -l) = 0 ]; then
-    echo "Install ddtrace from pypi@"
-    # pip install ddtrace
-    pip install git+https://github.com/Datadog/dd-trace-py.git@main
+    echo "Install ddtrace from pypi"
+    pip install ddtrace
 else
     echo "ERROR: Found several wheel files in binaries/, abort."
     exit 1
