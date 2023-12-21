c7f9548941ea9531184bca2ac0850e6731799e5d
Damien Mehala <damien.mehala@datadoghq.com>
2023-12-21 16:51:35 +0100

    Use dd-trace-cpp

--- utils/build/docker/cpp/nginx/install_ddtrace.sh
+++ utils/build/docker/cpp/nginx/install_ddtrace.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
-
 set -eu
 
 get_latest_release() {
     wget -qO- "https://api.github.com/repos/$1/releases/latest" | jq -r '.tag_name'
 }
 
+get_architecture() {
+  case "$(uname -m)" in
+    aarch64)
+      echo "arm64"
+      ;;
+    arm64)
+      echo "arm64"
+      ;;
+    x86_64)
+      echo "amd64"
+      ;;
+    amd64)
+      echo "amd64"
+      ;;
+    *)
+      echo ""
+      ;;
+  esac
+}
+
+ARCH=$(get_architecture)
+
+if [ -z "$ARCH" ]; then
+    echo 1>&2 "ERROR: Architecture $(uname -m) is not supported."
+    exit 1
+fi
+
 NGINX_VERSION=1.17.3
 
-OPENTRACING_NGINX_VERSION="$(get_latest_release opentracing-contrib/nginx-opentracing)"
-DD_OPENTRACING_CPP_VERSION="$(get_latest_release DataDog/dd-opentracing-cpp)"
+RELEASE_TAG="$(get_latest_release DataDog/nginx-datadog)"
 
-echo "opentracing-contrib/nginx-opentracing version: $OPENTRACING_NGINX_VERSION"
-echo "DataDog/dd-opentracing-cpp version: $DD_OPENTRACING_CPP_VERSION"
-echo $DD_OPENTRACING_CPP_VERSION > SYSTEM_TESTS_LIBRARY_VERSION
+echo "DataDog/nginx-datadog version: $RELEASE_TAG"
+echo $RELEASE_TAG > SYSTEM_TESTS_LIBRARY_VERSION
 touch SYSTEM_TESTS_LIBDDWAF_VERSION
 echo "0.0.0" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
 
-# Install NGINX plugin for OpenTracing
-wget https://github.com/opentracing-contrib/nginx-opentracing/releases/download/${OPENTRACING_NGINX_VERSION}/linux-amd64-nginx-${NGINX_VERSION}-ot16-ngx_http_module.so.tgz
-tar zxf linux-amd64-nginx-${NGINX_VERSION}-ot16-ngx_http_module.so.tgz -C /usr/lib/nginx/modules
-# Install Datadog Opentracing C++ Plugin
-wget https://github.com/DataDog/dd-opentracing-cpp/releases/download/${DD_OPENTRACING_CPP_VERSION}/linux-amd64-libdd_opentracing_plugin.so.gz
-gunzip linux-amd64-libdd_opentracing_plugin.so.gz -c > /usr/local/lib/libdd_opentracing_plugin.so
+# Install Datadog NGINX plugin
+tarball="nginx_${NGINX_VERSION}-${ARCH}-ngx_http_datadog_module.so.tgz"
+wget "https://github.com/DataDog/nginx-datadog/releases/download/$RELEASE_TAG/$tarball"
+tar -xzf "$tarball" -C /usr/lib/nginx/modules
+rm "$tarball"
 
 echo "Library version : $(cat SYSTEM_TESTS_LIBRARY_VERSION)"
