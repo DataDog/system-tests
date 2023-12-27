71de6a7fda05ec7b0b536a99cc6aab6f01a7db15
William Conti <william.conti@datadoghq.com>
2023-12-27 14:32:41 -0500

    fix access issue

--- utils/build/build_tracer_buddies.sh
+++ utils/build/build_tracer_buddies.sh
@@ -4,8 +4,8 @@
 # they are used in the CROSSED_TRACING_LIBRARIES scenario, where we can tests data propagation between different languages 
 
 docker buildx build --load --progress=plain -f utils/build/docker/python/flask-poc.Dockerfile -t datadog/system-tests:python_buddy-v0 .
-docker buildx build --load --progress=plain -f utils/build/docker/nodejs/express4.Dockerfile -t datadog/system-tests:nodejs_buddy-v0 .
+docker buildx build --load --progress=plain -f utils/build/docker/nodejs/express4.Dockerfile -t williamconti549/system-tests:nodejs_buddy-v0 .
 
 # Leave this line commented, it's only used when we need to push a new version of the buddy
 # docker push datadog/system-tests:python_buddy-v0
-docker push datadog/system-tests:nodejs_buddy-v0
+# docker push williamconti549/system-tests:nodejs_buddy-v0
