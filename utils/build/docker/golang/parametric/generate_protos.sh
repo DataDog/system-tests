a097b9fe96863458679b519a6487070a42a78155
Dario Castañé <dario.castane@datadoghq.com>
2024-01-10 15:51:46 +0100

    utils/build/docker/golang/parametric: use string instead of uint64 for trace IDs

--- utils/build/docker/golang/parametric/generate_protos.sh
+++ utils/build/docker/golang/parametric/generate_protos.sh
@@ -2,6 +2,6 @@
 
 # Generate the protobuf source files for the grpc client
 # Requires protoc to be installed on the host.
-protoc -I=../.. --go_out=. --go_opt=Mprotos/apm_test_client.proto=. --go-grpc_out=. --go-grpc_opt=Mprotos/apm_test_client.proto=. ../../protos/apm_test_client.proto
+protoc -I=../../../../parametric --go_out=. --go_opt=Mprotos/apm_test_client.proto=. --go-grpc_out=. --go-grpc_opt=Mprotos/apm_test_client.proto=. ../../../../parametric/protos/apm_test_client.proto
 # FIXME: the generated *.pb.go are given a package of __, so rewrite this to the main package
 sed -i '' -e 's/package __/package main/g' ./*.pb.go
