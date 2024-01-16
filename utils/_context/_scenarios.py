8feea3910a78dd5ae8ba9a515a0d8fe00bf91983
Anna <anna.yafi@datadoghq.com>
2024-01-16 10:21:44 +0100

    lint

--- utils/_context/_scenarios.py
+++ utils/_context/_scenarios.py
@@ -1266,7 +1266,7 @@ class scenarios:
             "DD_EXPERIMENTAL_API_SECURITY_ENABLED": "true",
             "DD_TRACE_DEBUG": "false",
             "DD_API_SECURITY_REQUEST_SAMPLE_RATE": "1.0",
-            "DD_API_SECURITY_MAX_CONCURRENT_REQUESTS": "50"
+            "DD_API_SECURITY_MAX_CONCURRENT_REQUESTS": "50",
         },
         doc="""
         Scenario for API Security feature, testing schema types sent into span tags if
