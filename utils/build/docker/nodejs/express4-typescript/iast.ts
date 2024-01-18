857d78e9ece5d2661d8153d67ed83797493d88f5
Carles Capell <107924659+CarlesDD@users.noreply.github.com>
2024-01-18 10:42:01 +0100

    Lint fix
    Co-authored-by: simon-id <simon.id@datadoghq.com>

--- utils/build/docker/nodejs/express4-typescript/iast.ts
+++ utils/build/docker/nodejs/express4-typescript/iast.ts
@@ -5,13 +5,13 @@ import type { Express, Request, Response } from 'express'
 const crypto = require('crypto')
 
 module.exports = function (app: Express) {
-    app.get('/iast/weak_randomness/test_insecure', (req: Request, res: Response) => {
-        const randomNumber = Math.random()
-        res.send(`OK:${randomNumber}`)
-    })
+  app.get('/iast/weak_randomness/test_insecure', (req: Request, res: Response) => {
+    const randomNumber = Math.random()
+    res.send(`OK:${randomNumber}`)
+  })
 
-    app.get('/iast/weak_randomness/test_secure', (req: Request, res: Response) => {
-        const randomBytes = crypto.randomBytes(256).toString('hex')
-        res.send(`OK:${randomBytes}`)
-    })
+  app.get('/iast/weak_randomness/test_secure', (req: Request, res: Response) => {
+    const randomBytes = crypto.randomBytes(256).toString('hex')
+    res.send(`OK:${randomBytes}`)
+  })
 }
