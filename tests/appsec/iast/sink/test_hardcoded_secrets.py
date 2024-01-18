1d06ef7bc2a12fa79aeb916a5940e2d78bb6a099
Alejandro Gonzalez <alejandro.gonzalez@datadoghq.com>
2024-01-18 12:34:43 +0100

    modify hardcode secrets for java

--- tests/appsec/iast/sink/test_hardcoded_secrets.py
+++ tests/appsec/iast/sink/test_hardcoded_secrets.py
@@ -2,27 +2,50 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import coverage, missing_feature, features
-from .._test_iast_fixtures import BaseSinkTest, DetectionStage
+from utils import coverage, interfaces, weblog, features, context
 
 
 @features.iast_sink_hardcoded_secrets
 @coverage.basic
-class Test_HardcodedSecrets(BaseSinkTest):
+class Test_HardcodedSecrets:
     """Test Hardcoded secrets detection."""
 
-    vulnerability_type = "HARDCODED_SECRET"
-    http_method = "GET"
-    insecure_endpoint = "/iast/hardcoded_secrets/test_insecure"
-    secure_endpoint = "/iast/hardcoded_secrets/test_secure"
-    data = {}
-    location_map = {"nodejs": "iast/index.js"}
-    detection_stage = DetectionStage.STARTUP
+    location_map = {"nodejs": "iast/index.js", "java": "com.datadoghq.system_tests.springboot.AppSecIast"}
 
-    @missing_feature(library="nodejs", reason="Not implemented yet")
-    def test_telemetry_metric_instrumented_sink(self):
-        super().test_telemetry_metric_instrumented_sink()
+    def setup_hardcoded_secrets_exec(self):
+        self.r_hardcoded_secrets_exec = weblog.get("/iast/hardcoded_secrets/test_insecure")
 
-    @missing_feature(library="nodejs", reason="Not implemented yet")
-    def test_telemetry_metric_executed_sink(self):
-        super().test_telemetry_metric_executed_sink()
+    def test_hardcoded_secrets_exec(self):
+        assert self.r_hardcoded_secrets_exec.status_code == 200
+        hardcode_secrets = self.get_hardcoded_secret_vulnerabilities()
+        evidence_condition = lambda x: x["evidence"]["value"] == "aws-access-token"
+        location_condition = lambda x: x["location"]["path"] == self._get_expectation(self.location_map)
+        assert any([evidence_condition(x) and location_condition(x) for x in hardcode_secrets]), (
+            "Hardcoded secrets not found %s" % hardcode_secrets
+        )
+
+    def get_hardcoded_secret_vulnerabilities(self):
+        spans = [span[2] for span in interfaces.library.get_spans()]
+        assert spans, "No spans found"
+        spans_meta = [span.get("meta") for span in spans]
+        assert spans_meta, "No spans meta found"
+        iast_events = [meta.get("_dd.iast.json") for meta in spans_meta if meta.get("_dd.iast.json")]
+        assert iast_events, "No iast events found"
+        vulnerabilities = [event.get("vulnerabilities") for event in iast_events if event.get("vulnerabilities")]
+        assert vulnerabilities, "No vulnerabilities found"
+        vulnerabilities = sum(vulnerabilities, [])  # set all the vulnerabilities in a single list
+        hadcoded_secrets = [vuln for vuln in vulnerabilities if vuln.get("type") == "HARDCODED_SECRET"]
+        assert hadcoded_secrets, "No hardcoded secrets found"
+        return hadcoded_secrets
+
+    def _get_expectation(self, d):
+        if d is None or isinstance(d, str):
+            return d
+
+        if isinstance(d, dict):
+            expected = d.get(context.library.library)
+            if isinstance(expected, dict):
+                expected = expected.get(context.weblog_variant)
+            return expected
+
+        raise TypeError(f"Unsupported expectation type: {d}")
---
+++ utils/build/docker/java/iast-common/src/main/java/com/datadoghq/system_tests/iast/utils/HardcodedSecretExamples.java
@@ -0,0 +1,8 @@
+package com.datadoghq.system_tests.iast.utils;
+
+import java.io.PrintWriter;
+
+public class HardcodedSecretExamples {
+
+    public static final String SECRET = "A3TMAWZUKIWR6O0OGR7B";
+}
