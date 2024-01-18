f2a9681af86a5d2ed7f66361ee0b1d92a5041226
Christophe Papazian <christophe.papazian@datadoghq.com>
2024-01-18 14:27:46 +0100

    use logs instead of stdout in the tests. Add new feature for the parity dashboard

--- tests/appsec/api_security/test_apisec_sampling.py
+++ tests/appsec/api_security/test_apisec_sampling.py
@@ -7,18 +7,9 @@ from utils import (
     weblog,
 )
 
+from utils.tools import logger
 import random
 import string
-import pytest
-
-
-@pytest.fixture(name="printer")
-def printer(request):
-    """Pytest plugin to print test progress steps in verbose mode."""
-    terminal_reporter = request.config.pluginmanager.getplugin("terminalreporter")
-    if terminal_reporter is not None:  # pragma: no branch
-        return terminal_reporter.write_line
-    return lambda msg: None
 
 
 def get_schema(request, address):
@@ -34,7 +25,7 @@ def get_schema(request, address):
 @rfc("https://docs.google.com/document/d/1OCHPBCAErOL2FhLl64YAHB8woDyq66y5t-JGolxdf1Q/edit#heading=h.bth088vsbjrz")
 @coverage.basic
 @scenarios.appsec_api_security_with_sampling
-@features.api_security_schemas
+@features.api_security_configuration
 class Test_API_Security_sampling:
     """Test API Security - Default 0.1 Sampling on Request Headers Schema"""
 
@@ -45,25 +36,20 @@ class Test_API_Security_sampling:
             weblog.get(
                 f"/tag_value/api_match_AS001/200?{''.join(random.choices(string.ascii_letters, k=16))}={random.randint(1<<31, (1<<32)-1)}"
             )
-            for _ in range(self.N ** 2)
+            for _ in range(self.N**2)
         ]
 
-    def test_sampling_rate(self, printer):
+    def test_sampling_rate(self):
         """can provide request header schema"""
         N = self.N
         assert all(r.status_code == 200 for r in self.all_requests)
-        self.s = sum(get_schema(r, "req.headers") is not None for r in self.all_requests)
+        s = sum(get_schema(r, "req.headers") is not None for r in self.all_requests)
         # check result is in at most 4 standard deviations from expected
         # (assuming 99.98% confidence interval)
         # standard deviation is N * 0.3 for 0.1 sampling rate
-        self.diff = abs(self.s / N - N * 0.1) / 0.3
-        self.log_fun = printer
-        assert (N ** 2) * 0.1 - 1.2 * N <= self.s <= (N ** 2) * 0.1 + 1.2 * N, "sampling rate is not 0.1"
-
-    def teardown_method(self):
-        """add result visibility to pytest report"""
-        self.log_fun(
-            f"  got {self.s} requests with api sec schemas out of {self.N**2} requests, expecting {int(self.N**2 * 0.1)}"
+        diff = abs(s / N - N * 0.1) / 0.3
+        logger.info(
+            f"API SECURITY SAMPLING RESULT: got {s} requests with api sec schemas out of {N**2} requests, expecting {int(N**2 * 0.1)}"
         )
-        if self.diff:
-            self.log_fun(f"  diff is {self.diff:.2f} standard deviations")
+        logger.info(f"API SECURITY SAMPLING RESULT: diff is {diff:.2f} standard deviations")
+        assert diff <= 4.0, "sampling rate is not 0.1"
