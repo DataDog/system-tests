89e92be04b407f040d3e4b80fab6f7ff84f06226
Maximo Bautista <max.bautista97@gmail.com>
2024-01-26 15:19:22 -0400

    Removing debugging logging

--- utils/parametric/_library_client.py
+++ utils/parametric/_library_client.py
@@ -15,7 +15,6 @@ from utils.parametric.protos import apm_test_client_pb2 as pb
 from utils.parametric.protos import apm_test_client_pb2_grpc
 from utils.parametric.spec.otel_trace import OtelSpanContext
 from utils.parametric.spec.otel_trace import convert_to_proto
-from utils.tools import logger
 
 class StartSpanResponse(TypedDict):
     span_id: int
@@ -149,9 +148,7 @@ class APMLibraryClientHTTP(APMLibraryClient):
                 "links": links,
             },
         )
-        logger.error(f"RESP RESP RESP: {resp}")
         resp_json = resp.json()
-        logger.error(f"resp_json resp_json resp_json: {resp_json}")
         return StartSpanResponse(span_id=resp_json["span_id"], trace_id=resp_json["trace_id"],)
 
     def finish_span(self, span_id: int) -> None:
