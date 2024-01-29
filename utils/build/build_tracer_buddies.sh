46b7254c7febc2ea0cf264953970f35d668bb8b5
roberto montero <roberto.montero@datadoghq.com>
2024-01-29 15:05:22 +0100

    This is my commit #2079 and other things

--- utils/build/build_tracer_buddies.sh
+++ utils/build/build_tracer_buddies.sh
@@ -4,10 +4,10 @@
 # they are used in the CROSSED_TRACING_LIBRARIES scenario, where we can tests data propagation between different languages 
 
 docker buildx build --load --progress=plain -f utils/build/docker/python/flask-poc.Dockerfile -t datadog/system-tests:python_buddy-v0 .
-docker buildx build --load --progress=plain -f utils/build/docker/nodejs/express4.Dockerfile -t datadog/system-tests:nodejs_buddy-v0 .
-docker buildx build --load --progress=plain -f utils/build/docker/java/spring-boot.Dockerfile -t datadog/system-tests:java_buddy-v0 .
-docker buildx build --load --progress=plain -f utils/build/docker/ruby/rails70.Dockerfile -t datadog/system-tests:ruby_buddy-v0 .
-docker buildx build --load --progress=plain -f utils/build/docker/golang/net-http.Dockerfile -t datadog/system-tests:golang_buddy-v0 .
+#docker buildx build --load --progress=plain -f utils/build/docker/nodejs/express4.Dockerfile -t datadog/system-tests:nodejs_buddy-v0 .
+#docker buildx build --load --progress=plain -f utils/build/docker/java/spring-boot.Dockerfile -t datadog/system-tests:java_buddy-v0 .
+#docker buildx build --load --progress=plain -f utils/build/docker/ruby/rails70.Dockerfile -t datadog/system-tests:ruby_buddy-v0 .
+#docker buildx build --load --progress=plain -f utils/build/docker/golang/net-http.Dockerfile -t datadog/system-tests:golang_buddy-v0 .
 
 
 if [ -z "$PUSH_IMAGES" ]
@@ -15,9 +15,9 @@ then
       echo "Skip pushing buddies images"
 else
       docker push datadog/system-tests:python_buddy-v0
-      docker push datadog/system-tests:nodejs_buddy-v0
-      docker push datadog/system-tests:java_buddy-v0
-      docker push datadog/system-tests:ruby_buddy-v0
-      docker push datadog/system-tests:golang_buddy-v0
+    #  docker push datadog/system-tests:nodejs_buddy-v0
+    #  docker push datadog/system-tests:java_buddy-v0
+    #  docker push datadog/system-tests:ruby_buddy-v0
+    #  docker push datadog/system-tests:golang_buddy-v0
 fi
 
