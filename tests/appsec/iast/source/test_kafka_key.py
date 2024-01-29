209149ee0385e87ac7082050b367739ea87309fa
Igor Unanua <igor.unanua@datadoghq.com>
2024-01-29 17:01:53 +0100

    Remove not needed overrides

--- tests/appsec/iast/source/test_kafka_key.py
+++ tests/appsec/iast/source/test_kafka_key.py
@@ -8,6 +8,7 @@ from .._test_iast_fixtures import BaseSourceTest, get_all_iast_events, get_iast_
 
 @coverage.basic
 @features.iast_source_kafka_key
+@scenarios.integrations
 class TestKafkaKey(BaseSourceTest):
     """Verify that kafka message key is tainted"""
 
@@ -17,18 +18,6 @@ class TestKafkaKey(BaseSourceTest):
     source_names = ["key"]
     source_value = "hello key!"
 
-    @scenarios.integrations
-    def test_source_reported(self):
-        super().test_source_reported()
-
     def get_sources(self, request):
         iast_event = get_all_iast_events()
         return get_iast_sources(iast_event)
-
-    @scenarios.integrations
-    def test_telemetry_metric_instrumented_source(self):
-        return super().test_telemetry_metric_instrumented_source()
-
-    @scenarios.integrations
-    def test_telemetry_metric_executed_source(self):
-        return super().test_telemetry_metric_executed_source()
