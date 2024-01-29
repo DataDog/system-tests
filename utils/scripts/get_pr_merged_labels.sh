2db201250e66b93419b2f03064c4eb3ab79a99af
roberto montero <roberto.montero@datadoghq.com>
2024-01-29 16:12:19 +0100

    This is my commit #2079 and other things

--- utils/scripts/get_pr_merged_labels.sh
+++ utils/scripts/get_pr_merged_labels.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+#Check the commit message to figure out if we are merging a PR.
+#We extract the PR number and using GitHub API we check the PR labes.
+#If the PR contains the label "build-buddies-images" we launch the build and push process
+
 PR_PATTERN='#[0-9]+'
+BUILD_BUDDIES_LABEL="build-buddies-images"
+
 if [[ $CI_COMMIT_MESSAGE =~ ($PR_PATTERN) ]]; then
     PR_NUMBER=${BASH_REMATCH[1]:1}
     echo "Merged the PR number: [$PR_NUMBER]"; 
@@ -12,13 +19,13 @@ if [[ $CI_COMMIT_MESSAGE =~ ($PR_PATTERN) ]]; then
 
     echo "We found PR labels: $PR_DATA"    
 
-    is_build_buddies=$(echo "$PR_DATA" | jq -c '.[] | select(.name | contains("build-buddies-images"))');
+    is_build_buddies=$(echo "$PR_DATA" | jq -c '.[] | select(.name | contains("$BUILD_BUDDIES_LABEL"))');
 
     if [ -z "$is_build_buddies" ]
     then
-        echo "The PR $PR_NUMBER doesn't contain the 'build-buddies-images' label "
+        echo "The PR $PR_NUMBER doesn't contain the '$BUILD_BUDDIES_LABEL' label "
     else
-        echo "The PR $PR_NUMBER contains the 'build-buddies-images' label. Launching the images generation process "
+        echo "The PR $PR_NUMBER contains the '$$BUILD_BUDDIES_LABEL' label. Launching the images generation process "
         echo $DOCKER_LOGIN_PASS | docker login --username $DOCKER_LOGIN --password-stdin
         PUSH_IMAGES=true ./utils/build/build_tracer_buddies.sh
     fi
