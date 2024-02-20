0618b455fff8615dae2e8b0218891e8439eaec60
Michael N <michael.nikitochkin@gmx.net>
2024-02-20 11:31:05 +0100

    Use Nginx Opentracing 0.34

--- utils/build/docker/cpp/nginx/install_ddtrace.sh
+++ utils/build/docker/cpp/nginx/install_ddtrace.sh
@@ -11,7 +11,7 @@ NGINX_VERSION=1.17.3
 # https://github.com/opentracing-contrib/nginx-opentracing/issues/586
 # ot16 file are missing from the latest release of nginx-opentracing
 # OPENTRACING_NGINX_VERSION="$(get_latest_release opentracing-contrib/nginx-opentracing)"
-OPENTRACING_NGINX_VERSION="v0.33.0"
+OPENTRACING_NGINX_VERSION="v0.34.0"
 
 DD_OPENTRACING_CPP_VERSION="$(get_latest_release DataDog/dd-opentracing-cpp)"
 
@@ -23,7 +23,7 @@ echo "0.0.0" > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
 
 # Install NGINX plugin for OpenTracing
 wget https://github.com/opentracing-contrib/nginx-opentracing/releases/download/${OPENTRACING_NGINX_VERSION}/linux-amd64-nginx-${NGINX_VERSION}-ot16-ngx_http_module.so.tgz
-tar zxf linux-amd64-nginx-${NGINX_VERSION}-ot16-ngx_http_module.so.tgz -C /usr/lib/nginx/modules
+tar zxf linux-amd64-nginx-${NGINX_VERSION}-ngx_http_module.so.tgz -C /usr/lib/nginx/modules
 # Install Datadog Opentracing C++ Plugin
 wget https://github.com/DataDog/dd-opentracing-cpp/releases/download/${DD_OPENTRACING_CPP_VERSION}/linux-amd64-libdd_opentracing_plugin.so.gz
 gunzip linux-amd64-libdd_opentracing_plugin.so.gz -c > /usr/local/lib/libdd_opentracing_plugin.so
