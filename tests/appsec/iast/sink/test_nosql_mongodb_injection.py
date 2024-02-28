66f3d1357b175eb0937372fc4ad9780a888e04c0
Igor Unanua <igor.unanua@datadoghq.com>
2024-02-28 12:34:07 +0100

    Enable sql and nosql metrics tests

--- tests/appsec/iast/sink/test_nosql_mongodb_injection.py
+++ tests/appsec/iast/sink/test_nosql_mongodb_injection.py
@@ -26,14 +26,12 @@ class TestNoSqlMongodbInjection(BaseSinkTest):
         super().test_secure()
 
     @missing_feature(context.library < "java@1.13.0", reason="Not implemented yet")
-    @missing_feature(library="nodejs", reason="Not implemented yet")
     @missing_feature(library="python", reason="Not implemented yet")
     @missing_feature(library="dotnet", reason="Not implemented yet")
     def test_telemetry_metric_instrumented_sink(self):
         super().test_telemetry_metric_instrumented_sink()
 
     @missing_feature(context.library < "java@1.13.0", reason="Not implemented yet")
-    @missing_feature(library="nodejs", reason="Not implemented yet")
     @missing_feature(library="python", reason="Not implemented yet")
     def test_telemetry_metric_executed_sink(self):
         super().test_telemetry_metric_executed_sink()
