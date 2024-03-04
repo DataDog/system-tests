7456c5192d12652d3c9e55d870b24bee9b4faf1e
tanguy.lichau <tanguy.lichau@datadoghq.com>
2024-03-04 17:15:55 +0100

    Updated test for external wafs identification through headers collection

--- tests/appsec/test_traces.py
+++ tests/appsec/test_traces.py
@@ -119,33 +119,6 @@ class Test_AppSecEventSpanTags:
         interfaces.library.validate_spans(self.r, validate_request_headers)
         interfaces.library.validate_spans(self.r, validate_response_headers)
 
-    def setup_x_amzn_trace_id_header_collection(self):
-        self.r2 = weblog.get("/headers", headers={"X-Amzn-Trace-Id": "Root=1-65ae48bc-04fb551979979b6c57973027",})
-
-    @missing_feature(library="golang", reason="Not implemented yet")
-    @missing_feature(library="java", reason="Not implemented yet")
-    @missing_feature(library="dotnet", reason="Not implemented yet")
-    @missing_feature(library="python", reason="Not implemented yet")
-    @missing_feature(library="ruby", reason="Not implemented yet")
-    @missing_feature(library="php", reason="Not implemented yet")
-    @missing_feature(library="cpp", reason="Not implemented yet")
-    @missing_feature(context.library < "nodejs@5.1.0")
-    def test_x_amzn_trace_id_header_collection(self):
-        """
-        Collect X-Amzn-Trace-Id when appsec is enabled.
-        """
-
-        def assertHeaderInSpanMeta(span, header):
-            if header not in span["meta"]:
-                raise Exception(f"Can't find {header} in span's meta")
-
-        def validate_request_headers(span):
-            for header in ["x-amzn-trace-id"]:
-                assertHeaderInSpanMeta(span, f"http.request.headers.{header}")
-            return True
-
-        interfaces.library.validate_spans(self.r2, validate_request_headers)
-
     @bug(context.library < "java@0.93.0")
     def test_root_span_coherence(self):
         """Appsec tags are not on span where type is not web"""
@@ -361,3 +334,33 @@ class Test_DistributedTraceInfo:
 
     def test_main(self):
         assert False, "Test not implemented"
+
+@rfc("https://docs.google.com/document/d/1xf-s6PtSr6heZxmO_QLUtcFzY_X_rT94lRXNq6-Ghws/edit?pli=1")
+class Test_ExternalWafRequestsIdentification:
+    def setup_external_wafs_header_collection(self):
+        self.r = weblog.get("/headers", headers={"X-Amzn-Trace-Id": "Root=1-65ae48bc-04fb551979979b6c57973027","CloudFront-Viewer-Ja3-Fingerprint":"e7d705a3286e19ea42f587b344ee6865" ,
+                                                  "Cf-Ray":"230b030023ae2822-SJC","X-Cloud-Trace-Context":"105445aa7843bc8bf206b12000100000/1","X-Appgw-Trace-id":"ac882cd65a2712a0fe1289ec2bb6aee7",
+                                                  "X-SigSci-RequestID":"55c24b96ca84c02201000001","X-SigSci-Tags":"SQLI, XSS", "Akamai-User-Risk": "uuid=913c4545-757b-4d8d-859d-e1361a828361;status=0"})
+    
+    @missing_feature(library="golang", reason="Not implemented yet")
+    @missing_feature(library="java", reason="Not implemented yet")
+    @missing_feature(library="dotnet", reason="Not implemented yet")
+    @missing_feature(library="python", reason="Not implemented yet")
+    @missing_feature(library="ruby", reason="Not implemented yet")
+    @missing_feature(library="php", reason="Not implemented yet")
+    @missing_feature(library="nodejs", reason="Not supported yet")
+    def test_external_wafs_header_collection(self):
+        """
+        Collect external wafs request identifier and other security info when appsec is enabled.
+        """
+
+        def assertHeaderInSpanMeta(span, header):
+            if header not in span["meta"]:
+                raise Exception(f"Can't find {header} in span's meta")
+
+        def validate_request_headers(span):
+            for header in ["x-amzn-trace-id","cloudfront-viewer-ja3-fingerprint","cf-ray", "x-cloud-trace-context","x-appgw-trace-id","x-sigsci-request-id","x-sigsci-tags", "akamai-user-risk"]:
+                assertHeaderInSpanMeta(span, f"http.request.headers.{header}")
+            return True
+
+        interfaces.library.validate_spans(self.r, validate_request_headers)
