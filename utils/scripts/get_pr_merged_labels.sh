8c343583cabaf47a3b3c8f490f67814bcf3e30bb
roberto montero <roberto.montero@datadoghq.com>
2024-03-07 14:48:23 +0100

    script tests

--- utils/scripts/get_pr_merged_labels.sh
+++ utils/scripts/get_pr_merged_labels.sh
@@ -4,6 +4,33 @@
 #We extract the PR number and using GitHub API we check the PR labels.
 #If the PR contains the label "build-buddies-images" we launch the build and push process
 
+##################################################
+        echo "TESTTTTTTTTTTTTT"
+        echo "$DOCKER_LOGIN_PASS" | docker login --username "$DOCKER_LOGIN" --password-stdin
+        export DOCKER_IMAGE_WEBLOG_TAG="latest"
+
+        echo "Building and pushing the lib-injection apps for java"
+        export APP_DOCKER_IMAGE_REPO=ghcr.io/datadog/system-tests/dd-lib-java-init-test-app
+        cd lib-injection/build/docker/java/dd-lib-java-init-test-app
+        LIBRARY_INJECTION_TEST_APP_IMAGE=$APP_DOCKER_IMAGE_REPO:latest ./build.sh
+        cd ../../../../..
+
+        echo "Building and pushing the lib-injection apps for nodejs"
+        export APP_DOCKER_IMAGE_REPO=ghcr.io/datadog/system-tests/sample-app
+        cd lib-injection/build/docker/nodejs/sample-app
+        LIBRARY_INJECTION_TEST_APP_IMAGE=$APP_DOCKER_IMAGE_REPO:latest ./build.sh
+        cd ../../../../..
+
+        echo "Building and pushing the lib-injection apps for python"
+        export APP_DOCKER_IMAGE_REPO=ghcr.io/datadog/system-tests/dd-lib-python-init-test-django
+        cd lib-injection/build/docker/python/dd-lib-python-init-test-django
+        LIBRARY_INJECTION_TEST_APP_IMAGE=$APP_DOCKER_IMAGE_REPO:latest ./build.sh
+        cd ../../../../..
+
+##################################################
+
+
+
 PR_PATTERN='#[0-9]+'
 
 if [[ $CI_COMMIT_MESSAGE =~ ($PR_PATTERN) ]]; then
@@ -20,7 +47,7 @@ if [[ $CI_COMMIT_MESSAGE =~ ($PR_PATTERN) ]]; then
 
     is_build_buddies=$(echo "$PR_DATA" | jq -c '.[] | select(.name | contains("build-buddies-images"))');
     is_build_lib_injections_apps=$(echo "$PR_DATA" | jq -c '.[] | select(.name | contains("build-lib-injection-app-images"))');
-    is_build_lib_injections_apps="true"
+
     if [ -z "$is_build_buddies" ]
     then
         echo "The PR $PR_NUMBER doesn't contain the 'build-buddies-images' label "
