7efe8bf246fdb7f7d125476952b7758cd217d63e
roberto montero <roberto.montero@datadoghq.com>
2024-03-12 08:04:57 +0100

    Bug parametric python tracer tracestate

--- tests/parametric/test_headers_tracestate_dd.py
+++ tests/parametric/test_headers_tracestate_dd.py
@@ -328,6 +328,7 @@ class Test_Headers_Tracestate_DD:
         context.library == "golang",
         reason="False Bug: header[3,6]: can't guarantee the order of strings in the tracestate since they came from the map. BUG: header[4,5]: w3cTraceID shouldn't be present",
     )
+    @bug(library="python@2.7.2", reason="AIT-9945")
     def test_headers_tracestate_dd_propagate_propagatedtags(self, test_agent, test_library):
         """
         harness sends a request with both tracestate and traceparent
