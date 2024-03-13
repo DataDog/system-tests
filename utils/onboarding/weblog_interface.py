e346379138531468100e9839668d6c94265888dd
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2024-03-13 10:09:17 +0100

    OnBoarding: Use 128 bits trace_id and check dates on the backend resp… (#2225)
    * OnBoarding: Use 128 bits trace_id and check dates on the backend response

--- utils/onboarding/weblog_interface.py
+++ utils/onboarding/weblog_interface.py
@@ -4,7 +4,7 @@ import requests
 
 
 def make_get_request(app_url):
-    generated_uuid = str(randint(0, 10000000))
+    generated_uuid = str(randint(0, 100000000000000000))
     requests.get(
         app_url, headers={"x-datadog-trace-id": generated_uuid, "x-datadog-parent-id": generated_uuid}, timeout=10
     )
