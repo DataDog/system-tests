d8e4661f33cde2e6a5dbdee1def48aa6f83db6e5
Tony Hsu <tony.hsu@datadoghq.com>
2024-03-19 00:40:18 +0100

    Derive gem name

--- utils/build/docker/ruby/install_ddtrace.sh
+++ utils/build/docker/ruby/install_ddtrace.sh
@@ -2,14 +2,21 @@
 
 set -eu
 
+# Remove gem reference from Gemfile
+sed -i -e '/gem .ddtrace./d' Gemfile
 sed -i -e '/gem .datadog./d' Gemfile
+
 if [ -e "/binaries/dd-trace-rb" ]; then
     #
     # Install the gem from this local directory
     # Being triggered by upstream: https://github.com/DataDog/dd-trace-rb
     #
     echo "Install from folder /binaries/dd-trace-rb"
-    echo "gem 'datadog', require: 'datadog/auto_instrument', path: '/binaries/dd-trace-rb'" >> Gemfile
+
+    # Read the gem name from the gemspec file
+    export GEM_NAME=$(find /binaries/dd-trace-rb -name *.gemspec | ruby -ne 'puts Gem::Specification.load($_.chomp).name')
+
+    echo "gem '$GEM_NAME', require: '$GEM_NAME/auto_instrument', path: '/binaries/dd-trace-rb'" >> Gemfile
 elif [ $(ls /binaries/ruby-load-from-bundle-add | wc -l) = 0 ]; then
     #
     # Install the gem from https://rubygems.org/
@@ -17,7 +24,7 @@ elif [ $(ls /binaries/ruby-load-from-bundle-add | wc -l) = 0 ]; then
     #
     echo "Install prod version"
     # Support multiple versions of the gem
-    echo "gem 'datadog', require: 'datadog/auto_instrument'" >> Gemfile
+    echo "gem 'datadog'" >> Gemfile
 else
     #
     # Append the content of the file `/binaries/ruby-load-from-bundle-add``
@@ -29,16 +36,19 @@ else
     options=$(cat /binaries/ruby-load-from-bundle-add)
     echo "Install from $options"
     echo $options >> Gemfile
+
+    # Read the gem name from the Gemfile
+    export GEM_NAME=$(ruby -rbundler -e 'puts Bundler::Definition.build(ENV["BUNDLE_GEMFILE"], nil, nil).dependencies.find {|x| x.name == "ddtrace" || x.name == "datadog"}.name')
 fi
 
 bundle config set --local without test development
-bundle remove ddtrace
-bundle update datadog
 
-bundle info datadog | grep -m 1 datadog > SYSTEM_TESTS_LIBRARY_VERSION
+bundle install
+
+bundle info $GEM_NAME | grep -m 1 $GEM_NAME > SYSTEM_TESTS_LIBRARY_VERSION
 bundle list | grep libddwaf > SYSTEM_TESTS_LIBDDWAF_VERSION || true
 
-cat "$(bundle info datadog | grep 'Path:' | awk '{ print $2 }')"/lib/datadog/appsec/assets/waf_rules/recommended.json | ruby -rjson -e 'puts JSON.parse(STDIN.read).fetch("metadata", {}).fetch("rules_version", "1.2.5")' > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
+cat "$(bundle info $GEM_NAME | grep 'Path:' | awk '{ print $2 }')"/lib/datadog/appsec/assets/waf_rules/recommended.json | ruby -rjson -e 'puts JSON.parse(STDIN.read).fetch("metadata", {}).fetch("rules_version", "1.2.5")' > SYSTEM_TESTS_APPSEC_EVENT_RULES_VERSION
 
 echo "dd-trace version: $(cat SYSTEM_TESTS_LIBRARY_VERSION)"
 echo "libddwaf version: $(cat SYSTEM_TESTS_LIBDDWAF_VERSION)"
