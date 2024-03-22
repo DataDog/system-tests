58f4f524eb1ed1101ceba7eed504c97cfc4d673b
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2024-03-22 11:12:04 +0100

    Bump nixpkgs

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
   pkgs ? import <nixpkgs> {},
 
   # use a pinned package state
-  pinned ? import(fetchTarball("https://github.com/NixOS/nixpkgs/archive/88f63d51109.tar.gz")) {},
+  pinned ? import(fetchTarball("https://github.com/NixOS/nixpkgs/archive/c75037bbf909.tar.gz")) {},
 }:
 let
   # get these python packages from nix
