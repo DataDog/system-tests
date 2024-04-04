1fc86b491b238308953f154e372b7f545154f30c
roberto montero <roberto.montero@datadoghq.com>
2024-04-04 10:50:37 +0200

    debug script

--- utils/scripts/replay_scenarios.sh
+++ utils/scripts/replay_scenarios.sh
@@ -2,6 +2,9 @@
 
 NOT_SUPPPORTED=("K8S_LIB_INJECTION_BASIC" "K8S_LIB_INJECTION_FULL" "TRACE_PROPAGATION_STYLE_W3C" "APM_TRACING_E2E_OTEL")
 
+echo "LS BEFORE"
+ls -la
+echo "LS DONE"
 if test -f "logs/tests.log"; then
     sh run.sh DEFAULT --replay
 fi
@@ -9,6 +12,7 @@ fi
 log_folder_prefix="logs_"
 for dir in "$log_folder_prefix"*
 do
+    echo "-----> $dir <-----"
     scenario=${dir#"$log_folder_prefix"}
     scenario=$(echo "$scenario" | tr '[:lower:]' '[:upper:]')
     if [[ ${NOT_SUPPPORTED[*]} =~ $scenario ]]
