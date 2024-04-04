c221a8de311fef0858709884ef08c9c770064db5
roberto montero <roberto.montero@datadoghq.com>
2024-04-04 11:09:36 +0200

    fix replay script

--- utils/scripts/replay_scenarios.sh
+++ utils/scripts/replay_scenarios.sh
@@ -2,24 +2,27 @@
 
 NOT_SUPPPORTED=("K8S_LIB_INJECTION_BASIC" "K8S_LIB_INJECTION_FULL" "TRACE_PROPAGATION_STYLE_W3C" "APM_TRACING_E2E_OTEL")
 
-echo "LS BEFORE"
-ls -la
-echo "LS DONE"
-if test -f "logs/tests.log"; then
-    sh run.sh DEFAULT --replay
+#if test -f "logs/tests.log"; then
+#    sh run.sh DEFAULT --replay
+#fi
+
+if [ -d "logs/" ]; then
+   sh run.sh DEFAULT --replay
 fi
 
 log_folder_prefix="logs_"
-for dir in "$log_folder_prefix"*
-do
-    echo "-----> $dir <-----"
-    scenario=${dir#"$log_folder_prefix"}
-    scenario=$(echo "$scenario" | tr '[:lower:]' '[:upper:]')
-    if [[ ${NOT_SUPPPORTED[*]} =~ $scenario ]]
-    then
-        echo "[$scenario] Replay mode not supported "
-    else
-        echo "[$scenario] Running replay mode"
-        ./run.sh "$scenario" --replay
-    fi
-done
+
+if dirs=( "$log_folder_prefix"*/ ) && [[ -d ${dirs[0]} ]]; then
+  for dir in "$log_folder_prefix"*
+    do
+        scenario=${dir#"$log_folder_prefix"}
+        scenario=$(echo "$scenario" | tr '[:lower:]' '[:upper:]')
+        if [[ ${NOT_SUPPPORTED[*]} =~ $scenario ]]
+        then
+            echo "[$scenario] Replay mode not supported "
+        else
+            echo "[$scenario] Running replay mode"
+            ./run.sh "$scenario" --replay
+        fi
+    done
+fi
