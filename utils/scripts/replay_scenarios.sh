2ac75a968b2753c3c4dc9e07f6cfb1fc5d4e80f1
roberto montero <roberto.montero@datadoghq.com>
2024-04-04 09:23:23 +0200

    Always run replay mode

---
+++ utils/scripts/replay_scenarios.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NOT_SUPPPORTED=("K8S_LIB_INJECTION_BASIC" "K8S_LIB_INJECTION_FULL", "TRACE_PROPAGATION_STYLE_W3C","APM_TRACING_E2E_OTEL")
+
+if test -f "logs/tests.log"; then
+    sh run.sh DEFAULT --replay
+fi
+
+log_folder_prefix="logs_"
+for dir in "$log_folder_prefix"*
+do
+    scenario=${dir#"$log_folder_prefix"}
+    scenario=$(echo "$scenario" | tr '[:lower:]' '[:upper:]')
+    if [[ ${NOT_SUPPPORTED[@]} =~ $scenario ]]
+    then
+        echo "[$scenario] Replay mode not supported "
+    else
+        echo "[$scenario] Running replay mode"
+        ./run.sh $scenario --replay
+    fi
+done
