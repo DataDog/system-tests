a58cadfa771b6ab73073b096348abe750c253a76
roberto montero <roberto.montero@datadoghq.com>
2024-04-10 14:59:53 +0200

    build ruby images

--- lib-injection/build/build_lib_injection_images.sh
+++ lib-injection/build/build_lib_injection_images.sh
@@ -10,7 +10,12 @@ variants=(["dd-lib-dotnet-init-test-app"]="dotnet"
           ["dd-lib-python-init-test-django"]="python"
           ["dd-lib-python-init-test-django-gunicorn"]="python"
           ["dd-lib-python-init-test-django-uvicorn"]="python"
-          ["dd-lib-python-init-test-django-uvicorn"]="python"
+          ["dd-lib-ruby-init-test-rails"]="ruby"
+          ["dd-lib-ruby-init-test-rails-bundle-deploy"]="ruby"
+          ["dd-lib-ruby-init-test-rails-conflict"]="ruby"
+          ["dd-lib-ruby-init-test-rails-explicit"]="ruby"
+          ["dd-lib-ruby-init-test-rails-gemsrb"]="ruby"
+          ["lib_injection_rails_app"]="ruby"          
           )
 docker buildx create --name multiarch --driver docker-container --use
 
