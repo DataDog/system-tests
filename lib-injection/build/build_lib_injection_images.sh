ffc65e80f53e0e181438251eff2e3d5e3f1853d6
roberto montero <roberto.montero@datadoghq.com>
2024-04-10 14:50:29 +0200

    add python variants

--- lib-injection/build/build_lib_injection_images.sh
+++ lib-injection/build/build_lib_injection_images.sh
@@ -5,7 +5,13 @@ export DOCKER_IMAGE_WEBLOG_TAG=latest
 export BUILDX_PLATFORMS=linux/arm64/v8,linux/amd64
 declare -A variants
 #variants=(["dd-lib-dotnet-init-test-app"]="dotnet" ["dd-lib-java-init-test-app"]="java")
-variants=(["dd-lib-dotnet-init-test-app"]="dotnet" ["sample-app"]="nodejs")
+variants=(["dd-lib-dotnet-init-test-app"]="dotnet" 
+          ["sample-app"]="nodejs" 
+          ["dd-lib-python-init-test-django"]="python"
+          ["dd-lib-python-init-test-django-gunicorn"]="python"
+          ["dd-lib-python-init-test-django-uvicorn"]="python"
+          ["dd-lib-python-init-test-django-uvicorn"]="python"
+          )
 docker buildx create --name multiarch --driver docker-container --use
 
 for variant in "${!variants[@]}"; do 
