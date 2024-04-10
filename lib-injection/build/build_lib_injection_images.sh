3f7d84799a07276aee6488835ef0cde21e8cea11
roberto montero <roberto.montero@datadoghq.com>
2024-04-10 10:28:00 +0200

    Build lib injeciton images

--- lib-injection/build/build_lib_injection_images.sh
+++ lib-injection/build/build_lib_injection_images.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
 export DOCKER_IMAGE_WEBLOG_TAG=latest
 
-APP_DOCKER_IMAGE_REPO=ghcr.io/datadog/system-tests/dd-lib-dotnet-init-test-app LIBRARY_INJECTION_TEST_APP_IMAGE=ghcr.io/datadog/system-tests/dd-lib-dotnet-init-test-app:$DOCKER_IMAGE_WEBLOG_TAG ./lib-injection/build/docker/dotnet/dd-lib-dotnet-init-test-app/build.sh
+declare -A variants
+variants=(["dd-lib-dotnet-init-test-app"]="dotnet" ["dd-lib-java-init-test-app"]="java")
+
+for variant in "${!variants[@]}"; do 
+    language="${variants[$variant]}"
+    echo "Building $variant - $language"; 
+    echo "$(pwd)"
+    cd ./lib-injection/build/docker/$language/$variant/ && APP_DOCKER_IMAGE_REPO=ghcr.io/datadog/system-tests/$variant LIBRARY_INJECTION_TEST_APP_IMAGE=ghcr.io/datadog/system-tests/$variant:$DOCKER_IMAGE_WEBLOG_TAG ./build.sh && cd ../../../../../
+done
