272dfa7e3386b1da28c363bea6d96a2318b698f3
simon-id <simon.id@datadoghq.com>
2024-04-24 14:09:46 +0200

    Update tests/test_the_test/test_manifest.py

--- tests/test_the_test/test_manifest.py
+++ tests/test_the_test/test_manifest.py
@@ -67,7 +67,9 @@ def test_content():
             try:
                 semver.NpmSpec(declaration)
             except Exception as e:
-                raise ValueError(f"{declaration} is neither a version, a version range or a test state (bug, flaky ...)") from e
+                raise ValueError(
+                    f"{declaration} is neither a version, a version range or a test state (bug, flaky ...)"
+                ) from e
 
     manifest = load()
 
