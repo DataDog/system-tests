c9cc69d5121be163b2f08fc7c5184c407fff9237
Federico Mon <federico.mon@datadoghq.com>
2024-04-25 12:34:43 +0200

    [python] activate test_library_schema_telemetry_job_object

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -27,7 +27,6 @@ class Test_library:
     def test_library_schema_telemetry_conf_value(self):
         interfaces.library.assert_schema_point("/telemetry/proxy/api/v2/apmtelemetry", "$.payload.configuration[]")
 
-    @bug(library="python", reason="APPSEC-52845")
     def test_library_schema_telemetry_job_object(self):
         interfaces.library.assert_schema_point("/telemetry/proxy/api/v2/apmtelemetry", "$.payload")
 
