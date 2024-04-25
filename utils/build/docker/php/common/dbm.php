04ce6590fb782b90fd8eb641c29e45ba083122fa
William Conti <william.conti@datadoghq.com>
2024-04-25 14:37:10 +0200

    fix php tests

--- utils/build/docker/php/common/dbm.php
+++ utils/build/docker/php/common/dbm.php
@@ -12,7 +12,7 @@ if ($integration == "pdo-mysql") {
     $connection = new PDO("pgsql:dbname=system_tests_dbname;host=postgres;port=5433", "system_tests_user", "system_tests");
     $connection->query($query);
 } elseif ($integration == "mysqli") {
-    $connection = new mysqli("mysqldb", "mysqldb", "mysqldb", "world");
+    $connection = new mysqli("mysqldb", "mysqldb", "mysqldb", "mysql_dbname");
     $connection->query($query);
 }
 
