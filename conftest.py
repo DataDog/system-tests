8a100938d07078783c0a452fcc6c17585d2ba4e0
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-04-29 12:14:49 +0200

    Add --scenario-report option

--- conftest.py
+++ conftest.py
@@ -32,6 +32,8 @@ def pytest_addoption(parser):
     parser.addoption(
         "--force-execute", "-F", action="append", default=[], help="Item to execute, even if they are skipped"
     )
+    parser.addoption("--scenario-report", action="store_true", help="Produce a report on nodeids and their scenario")
+
     # Onboarding scenarios mandatory parameters
     parser.addoption("--vm-weblog", type=str, action="store", help="Set virtual machine weblog")
     parser.addoption("--vm-library", type=str, action="store", help="Set virtual machine library to test")
@@ -196,9 +198,13 @@ def pytest_collection_modifyitems(session, config, items):
     selected = []
     deselected = []
 
+    declared_scenarios = {}
+
     for item in items:
         scenario_markers = list(item.iter_markers("scenario"))
         declared_scenario = scenario_markers[0].args[0] if len(scenario_markers) != 0 else "DEFAULT"
+
+        declared_scenarios[item.nodeid] = declared_scenario
 
         # If we are running scenario with the option sleep, we deselect all
         if session.config.option.sleep:
@@ -220,6 +226,9 @@ def pytest_collection_modifyitems(session, config, items):
     items[:] = selected
     config.hook.pytest_deselected(items=deselected)
 
+    if config.option.scenario_report:
+        with open(f"{context.scenario.host_log_folder}/scenarios.json", "w", encoding="utf-8") as f:
+            json.dump(declared_scenarios, f, indent=2)
 
 def pytest_deselected(items):
     _deselected_items.extend(items)
