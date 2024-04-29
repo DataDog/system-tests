a21518f204611bc58a6e2e5a8e4d242bddd7fc23
Christophe Papazian <christophe.papazian@datadoghq.com>
2024-04-29 15:00:33 +0200

    fix gunicorn for python3.12 too

--- utils/build/docker/python/django/app.sh
+++ utils/build/docker/python/django/app.sh
@@ -4,4 +4,4 @@ if [ ${UDS_WEBLOG:-} = "1" ]; then
     ./set-uds-transport.sh
 fi
 
-ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
+ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
