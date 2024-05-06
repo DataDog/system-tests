1f77dd1c8119f08f655bcfb4c706cb8a997d18f8
Igor Unanua <igor.unanua@datadoghq.com>
2024-05-06 11:55:54 +0200

    include a note for guidance on the permission error

--- utils/build/docker/postgres-init-db.sh
+++ utils/build/docker/postgres-init-db.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# NOTE: if you are getting a permission error when starting postgres instance try doing chmod 777 on this file
+
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
