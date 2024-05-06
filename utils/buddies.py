7110c5f2db35ab3ac7730d26bc6e2b3c870650b1
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-05-06 10:21:53 +0200

    Move buddies in utils/

---
+++ utils/buddies.py
@@ -0,0 +1,21 @@
+from utils._weblog import _Weblog
+
+
+class _Buddy(_Weblog):
+    def __init__(self, port, language, domain="localhost"):
+        from collections import defaultdict
+
+        self.port = port
+        self.domain = domain
+
+        self.responses = defaultdict(list)
+        self.current_nodeid = "not used"
+        self.replay = False
+        self.language = language
+
+
+python_buddy = _Buddy(9001, "python")
+nodejs_buddy = _Buddy(9002, "nodejs")
+java_buddy = _Buddy(9003, "java")
+ruby_buddy = _Buddy(9004, "ruby")
+golang_buddy = _Buddy(9005, "golang")
