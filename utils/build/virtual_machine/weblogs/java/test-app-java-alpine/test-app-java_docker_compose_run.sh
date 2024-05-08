0c2f212ceaecd0555d6aa39aa566fcb039acc792
roberto montero <roberto.montero@datadoghq.com>
2024-05-08 08:41:07 +0200

    multiple arch

--- utils/build/virtual_machine/weblogs/java/test-app-java-alpine/test-app-java_docker_compose_run.sh
+++ utils/build/virtual_machine/weblogs/java/test-app-java-alpine/test-app-java_docker_compose_run.sh
@@ -2,12 +2,18 @@
 # shellcheck disable=SC2015
 set -e
 
+architecture=""
+case $(uname -m) in
+    x86_64) architecture="amd64" ;;
+    arm)    architecture="arm64" ;;
+esac
+
 # shellcheck disable=SC2035
 sudo chmod -R 755 *
 [  -z "$DD_DOCKER_LOGIN_PASS" ] && echo "Skipping docker loging. Consider set the variable DOCKER_LOGIN and DOCKER_LOGIN_PASS" || echo "$DD_DOCKER_LOGIN_PASS" | sudo docker login --username "$DD_DOCKER_LOGIN" --password-stdin 
 
 ./gradlew build
-sudo docker build --no-cache -t system-tests/local .
+sudo docker build --build-arg ARCH=${architecture} --no-cache -t system-tests/local .
 if [ -f docker-compose-agent-prod.yml ]; then
     # Agent may be installed in a different way
     sudo -E docker-compose -f docker-compose-agent-prod.yml up -d --remove-orphans datadog
