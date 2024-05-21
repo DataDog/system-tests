296b9fe1d5fc1e726f52ed3e5efc408694baed8a
Christophe Papazian <christophe.papazian@datadoghq.com>
2024-05-21 14:11:57 +0200

    easy wins for python

--- tests/appsec/iast/sink/test_command_injection.py
+++ tests/appsec/iast/sink/test_command_injection.py
@@ -28,11 +28,9 @@ class TestCommandInjection(BaseSinkTest):
 
     @missing_feature(context.library < "java@1.9.0", reason="Metrics not implemented")
     @missing_feature(library="dotnet", reason="Not implemented yet")
-    @missing_feature(library="python", reason="Not implemented yet")
     def test_telemetry_metric_instrumented_sink(self):
         super().test_telemetry_metric_instrumented_sink()
 
     @missing_feature(context.library < "java@1.9.0", reason="Metrics not implemented")
-    @missing_feature(library="python", reason="Not implemented yet")
     def test_telemetry_metric_executed_sink(self):
         super().test_telemetry_metric_executed_sink()
