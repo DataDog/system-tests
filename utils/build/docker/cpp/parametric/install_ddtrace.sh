d6616179cd53f40eca1587bb1f7169b3a5c965ec
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-05-23 18:47:21 +0200

    Fix version parser for dd-trace-cpp

--- utils/build/docker/cpp/parametric/install_ddtrace.sh
+++ utils/build/docker/cpp/parametric/install_ddtrace.sh
@@ -20,7 +20,7 @@ git_clone_latest_release (){
 
 get_version_from_binaries() {
     # shellcheck disable=SC2002
-    version_line=$(cat /binaries/dd-trace-cpp/src/datadog/version.cpp | grep '^#define VERSION *')
+    version_line=$(cat /binaries/dd-trace-cpp/src/datadog/version.cpp | grep '^#define .*VERSION *')
     current_version=$(echo "$version_line" |  awk -F'VERSION' '{ print $2 }')
     echo "$current_version" | tr -d '"'> SYSTEM_TESTS_LIBRARY_VERSION
 }
