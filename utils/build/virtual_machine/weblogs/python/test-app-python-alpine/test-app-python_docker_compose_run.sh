9f4b097ac4957cf0d5d55d78119ef266ede383b1
roberto montero <roberto.montero@datadoghq.com>
2024-05-27 21:30:39 +0200

    remove docker login

--- utils/build/virtual_machine/weblogs/python/test-app-python-alpine/test-app-python_docker_compose_run.sh
+++ utils/build/virtual_machine/weblogs/python/test-app-python-alpine/test-app-python_docker_compose_run.sh
@@ -3,8 +3,6 @@
 
 set -e
 
-[  -z "$DD_DOCKER_LOGIN_PASS" ] && echo "Skipping docker loging. Consider set the variable DOCKER_LOGIN and DOCKER_LOGIN_PASS" || echo "$DD_DOCKER_LOGIN_PASS" | sudo docker login --username "$DD_DOCKER_LOGIN" --password-stdin 
-
 # shellcheck disable=SC2035
 sudo chmod -R 755 *
 
