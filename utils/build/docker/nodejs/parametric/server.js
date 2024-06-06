a3404f0fca9fb02b4cc614165489b53efc227e35
Ayan Khan <khanayan_123@hotmail.com>
2024-06-06 12:22:19 -0400

    add system tests for otel env var mapping (#2509)
    * add system tests for otel env var mapping

--- utils/build/docker/nodejs/parametric/server.js
+++ utils/build/docker/nodejs/parametric/server.js
@@ -285,6 +285,28 @@ app.post('/trace/otel/set_attributes', (req, res) => {
   res.json({});
 });
 
+app.get('/trace/config', (req, res) => {
+  const dummyTracer = require('dd-trace').init()
+  const config = dummyTracer._tracer._config
+  const config_string = JSON.stringify({
+    'dd_service': config?.service !== undefined ? `${config.service}`.toLowerCase() : 'null',
+    'dd_log_level': config?.logLevel !== undefined ? `${config.logLevel}`.toLowerCase() : 'null',
+    'dd_trace_debug': config?.debug !== undefined ? `${config.debug}`.toLowerCase() : 'null',
+    'dd_trace_sample_rate': config?.sampleRate !== undefined ? `${config.sampleRate}` : 'null',
+    'dd_trace_enabled': config ? 'true' : 'false', // in node if dd_trace_enabled is true the tracer won't have a config object
+    'dd_runtime_metrics_enabled': config?.runtimeMetrics !== undefined ? `${config.runtimeMetrics}`.toLowerCase() : 'null',
+    'dd_tags': config?.tags !== undefined ? Object.entries(config.tags).map(([key, val]) => `${key}:${val}`).join(',') : 'null',
+    'dd_trace_propagation_style': config?.tracePropagationStyle?.inject.join(',') ?? 'null',
+    'dd_trace_sample_ignore_parent': 'null', // not implemented in node
+    'dd_trace_otel_enabled': 'null', // not exposed in config object in node
+    'dd_env': config?.tags?.env !== undefined ? `${config.tags.env}` : 'null',
+    'dd_version': config?.tags?.version !== undefined ? `${config.tags.version}` : 'null'
+  })
+  res.json( { 
+    config: config_string
+  });
+});
+
 // TODO: implement this endpoint correctly, current blockers:
 // 1. Fails on invalid url
 // 2. does not generate span, because http instrumentation turned off
