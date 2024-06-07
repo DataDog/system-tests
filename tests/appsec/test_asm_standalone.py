ee273a3934bdea03f97ba0d33969138e7c07cab9
Federico Mon <federico.mon@datadoghq.com>
2024-06-07 12:32:39 +0200

    extend coverage, fix tests

--- tests/appsec/test_asm_standalone.py
+++ tests/appsec/test_asm_standalone.py
@@ -1,41 +1,43 @@
-from utils import weblog, interfaces, scenarios, features
+from utils import weblog, interfaces, scenarios, features, rfc
 from utils._context.header_tag_vars import *
-import time
 
 
+@rfc("https://docs.google.com/document/d/12NBx-nD-IoQEMiCRnJXneq4Be7cbtSc6pJLOFUWTpNE/edit")
+@features.appsec_standalone
 @scenarios.appsec_standalone
-class Test_AppSecPropagation:
+class Test_AppSecStandalone_UpstreamPropagation:
     """APM correctly propagates AppSec events in distributing tracing."""
 
-    def setup_trace_no_upstream_no_attack_is_kept_by_rate_limiter_minimum(self):
+    # TODO downstream propagation
+
+    def setup_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_minus_1(self):
         trace_id = 1212121212121212121
         parent_id = 34343434
-        # Ensure this is the first span received in 60 seconds
-        time.sleep(60)
         self.r = weblog.get(
             "/waf/",
             headers={
                 "x-datadog-trace-id": str(trace_id),
                 "x-datadog-parent-id": str(parent_id),
+                "x-datadog-sampling-priority": "-1",
                 "x-datadog-origin": "rum",
                 "x-datadog-tags": "_dd.p.other=1",
             },
         )
 
-    def test_trace_no_upstream_no_attack_is_kept_by_rate_limiter_minimum(self):
+    def test_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_minus_1(self):
         for data, _, span in interfaces.library.get_spans(request=self.r):
-            assert span["metrics"]["_sampling_priority_v1"] >= 1
+            assert span["metrics"]["_sampling_priority_v1"] == 1
             assert span["metrics"]["_dd.apm.enabled"] == 0
             assert "_dd.p.appsec" not in span["meta"]
-            assert "_dd.p.other" not in span["meta"]
-            assert span["trace_id"] != 1212121212121212121
+            assert "_dd.p.other" in span["meta"]
+            assert span["trace_id"] == 1212121212121212121
 
             # Some tracers use true while others use yes
             assert any(
                 ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
             )
 
-    def setup_trace_no_upstream_appsec_propagation_no_attack_is_dropped(self):
+    def setup_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_0(self):
         trace_id = 1212121212121212121
         parent_id = 34343434
         self.r = weblog.get(
@@ -43,24 +45,80 @@ class Test_AppSecPropagation:
             headers={
                 "x-datadog-trace-id": str(trace_id),
                 "x-datadog-parent-id": str(parent_id),
+                "x-datadog-sampling-priority": "0",
                 "x-datadog-origin": "rum",
                 "x-datadog-tags": "_dd.p.other=1",
             },
         )
 
-    def test_trace_no_upstream_appsec_propagation_no_attack_is_dropped(self):
+    def test_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_0(self):
         for data, _, span in interfaces.library.get_spans(request=self.r):
-            assert span["metrics"]["_sampling_priority_v1"] == -1
+            assert span["metrics"]["_sampling_priority_v1"] == 1
             assert span["metrics"]["_dd.apm.enabled"] == 0
-            assert span["trace_id"] != 1212121212121212121
             assert "_dd.p.appsec" not in span["meta"]
+            assert "_dd.p.other" in span["meta"]
+            assert span["trace_id"] == 1212121212121212121
 
             # Some tracers use true while others use yes
             assert any(
                 ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
             )
 
-    def setup_trace_no_upstream_appsec_propagation_with_attack_is_kept(self):
+    def setup_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_1(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-sampling-priority": "1",
+                "x-datadog-origin": "rum",
+                "x-datadog-tags": "_dd.p.other=1",
+            },
+        )
+
+    def test_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_1(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 1
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert "_dd.p.appsec" not in span["meta"]
+            assert "_dd.p.other" in span["meta"]
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_2(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-sampling-priority": "2",
+                "x-datadog-origin": "rum",
+                "x-datadog-tags": "_dd.p.other=1",
+            },
+        )
+
+    def test_no_appsec_upstream__no_attack__is_kept_with_priority_1__from_2(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 1
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert "_dd.p.appsec" not in span["meta"]
+            assert "_dd.p.other" in span["meta"]
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_no_upstream_appsec_propagation__with_attack__is_kept_with_priority_2__from_minus_1(self):
         trace_id = 1212121212121212121
         parent_id = 34343434
         self.r = weblog.get(
@@ -69,25 +127,104 @@ class Test_AppSecPropagation:
                 "x-datadog-trace-id": str(trace_id),
                 "x-datadog-parent-id": str(parent_id),
                 "x-datadog-origin": "rum",
+                "x-datadog-sampling-priority": "-1",
                 "x-datadog-tags": "_dd.p.other=1",
                 "User-Agent": "Arachni/v1",
             },
         )
 
-    def test_trace_no_upstream_appsec_propagation_with_attack_is_kept(self):
+    def test_no_upstream_appsec_propagation__with_attack__is_kept_with_priority_2__from_minus_1(self):
         for data, _, span in interfaces.library.get_spans(request=self.r):
             assert span["metrics"]["_sampling_priority_v1"] == 2
             assert span["metrics"]["_dd.apm.enabled"] == 0
             assert span["meta"]["_dd.p.appsec"] == "1"
-            # A new distributed trace is created because of no appsec upstream
-            assert span["trace_id"] != 1212121212121212121
+            assert span["trace_id"] == 1212121212121212121
 
             # Some tracers use true while others use yes
             assert any(
                 ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
             )
 
-    def setup_trace_upstream_appsec_propagation_no_attack_is_propagated(self):
+    def setup_no_upstream_appsec_propagation__with_attack__is_kept_with_priority_2__from_0(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-origin": "rum",
+                "x-datadog-sampling-priority": "0",
+                "x-datadog-tags": "_dd.p.other=1",
+                "User-Agent": "Arachni/v1",
+            },
+        )
+
+    def test_no_upstream_appsec_propagation__with_attack__is_kept_with_priority_2__from_0(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 2
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert span["meta"]["_dd.p.appsec"] == "1"
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_upstream_appsec_propagation__no_attack__is_propagated_as_is__being_0(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-origin": "rum",
+                "x-datadog-sampling-priority": "0",
+                "x-datadog-tags": "_dd.p.appsec=1",
+            },
+        )
+
+    def test_upstream_appsec_propagation__no_attack__is_propagated_as_is__being_0(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 0
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert span["meta"]["_dd.p.appsec"] == "1"
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_upstream_appsec_propagation__no_attack__is_propagated_as_is__being_1(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-origin": "rum",
+                "x-datadog-sampling-priority": "1",
+                "x-datadog-tags": "_dd.p.appsec=1",
+            },
+        )
+
+    def test_upstream_appsec_propagation__no_attack__is_propagated_as_is__being_1(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 1
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert span["meta"]["_dd.p.appsec"] == "1"
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_upstream_appsec_propagation__no_attack__is_propagated_as_is__being_2(self):
         trace_id = 1212121212121212121
         parent_id = 34343434
         self.r = weblog.get(
@@ -101,7 +238,7 @@ class Test_AppSecPropagation:
             },
         )
 
-    def test_trace_upstream_appsec_propagation_no_attack_is_propagated(self):
+    def test_upstream_appsec_propagation__no_attack__is_propagated_as_is__being_2(self):
         for data, _, span in interfaces.library.get_spans(request=self.r):
             assert span["metrics"]["_sampling_priority_v1"] == 2
             assert span["metrics"]["_dd.apm.enabled"] == 0
@@ -113,34 +250,7 @@ class Test_AppSecPropagation:
                 ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
             )
 
-    def setup_trace_any_upstream_propagation_with_attack_raises_priority(self):
-        trace_id = 1212121212121212121
-        parent_id = 34343434
-        self.r = weblog.get(
-            "/waf/",
-            headers={
-                "x-datadog-trace-id": str(trace_id),
-                "x-datadog-parent-id": str(parent_id),
-                "x-datadog-origin": "rum",
-                "x-datadog-sampling-priority": "1",
-                "User-Agent": "Arachni/v1",
-            },
-        )
-
-    def test_trace_any_upstream_propagation_with_attack_raises_priority(self):
-        for data, _, span in interfaces.library.get_spans(request=self.r):
-            assert span["metrics"]["_sampling_priority_v1"] == 2
-            assert span["metrics"]["_dd.apm.enabled"] == 0
-            assert span["meta"]["_dd.p.appsec"] == "1"
-            # A new distributed trace is created because of no appsec upstream
-            assert span["trace_id"] != 1212121212121212121
-
-            # Some tracers use true while others use yes
-            assert any(
-                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
-            )
-
-    def setup_trace_drop_upstream_propagation_with_attack_is_not_honored(self):
+    def setup_any_upstream_propagation__with_attack__raises_priority_to_2__from_minus_1(self):
         trace_id = 1212121212121212121
         parent_id = 34343434
         self.r = weblog.get(
@@ -154,13 +264,64 @@ class Test_AppSecPropagation:
             },
         )
 
-    def test_trace_drop_upstream_propagation_with_attack_is_not_honored(self):
+    def test_any_upstream_propagation__with_attack__raises_priority_to_2__from_minus_1(self):
         for data, _, span in interfaces.library.get_spans(request=self.r):
             assert span["metrics"]["_sampling_priority_v1"] == 2
             assert span["metrics"]["_dd.apm.enabled"] == 0
             assert span["meta"]["_dd.p.appsec"] == "1"
-            # A new distributed trace is created because of no appsec upstream
-            assert span["trace_id"] != 1212121212121212121
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_any_upstream_propagation__with_attack__raises_priority_to_2__from_0(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-origin": "rum",
+                "x-datadog-sampling-priority": "0",
+                "User-Agent": "Arachni/v1",
+            },
+        )
+
+    def test_any_upstream_propagation__with_attack__raises_priority_to_2__from_0(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 2
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert span["meta"]["_dd.p.appsec"] == "1"
+            assert span["trace_id"] == 1212121212121212121
+
+            # Some tracers use true while others use yes
+            assert any(
+                ["Datadog-Client-Computed-Stats", trueish,] in data["request"]["headers"] for trueish in ["yes", "true"]
+            )
+
+    def setup_any_upstream_propagation__with_attack__raises_priority_to_2__from_1(self):
+        trace_id = 1212121212121212121
+        parent_id = 34343434
+        self.r = weblog.get(
+            "/waf/",
+            headers={
+                "x-datadog-trace-id": str(trace_id),
+                "x-datadog-parent-id": str(parent_id),
+                "x-datadog-origin": "rum",
+                "x-datadog-sampling-priority": "0",
+                "User-Agent": "Arachni/v1",
+            },
+        )
+
+    def test_any_upstream_propagation__with_attack__raises_priority_to_2__from_1(self):
+        for data, _, span in interfaces.library.get_spans(request=self.r):
+            assert span["metrics"]["_sampling_priority_v1"] == 2
+            assert span["metrics"]["_dd.apm.enabled"] == 0
+            assert span["meta"]["_dd.p.appsec"] == "1"
+            assert span["trace_id"] == 1212121212121212121
 
             # Some tracers use true while others use yes
             assert any(
