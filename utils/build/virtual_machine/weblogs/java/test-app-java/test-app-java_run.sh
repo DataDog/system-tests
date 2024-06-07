f73edecde9aad9fcd83f65aff69e58f01cd8d601
roberto montero <roberto.montero@datadoghq.com>
2024-06-07 16:52:32 +0200

    java ready

--- utils/build/virtual_machine/weblogs/java/test-app-java/test-app-java_run.sh
+++ utils/build/virtual_machine/weblogs/java/test-app-java/test-app-java_run.sh
@@ -7,21 +7,17 @@ sudo chmod -R 755 * || chmod -R 755 *
 echo "START RUN APP"
 ./gradlew build
 
-if ! command -v systemctl &> /dev/null
-then
-    cp build/libs/k8s-lib-injection-app-0.0.1-SNAPSHOT.jar /home/datadog
-    chmod 755 test-app-java_daemon.sh
-    chmod 777 /shared_volume/std.out
-    ./test-app-java_daemon.sh start
- else
-    sudo cp build/libs/k8s-lib-injection-app-0.0.1-SNAPSHOT.jar /home/datadog
-    sudo cp test-app.service /etc/systemd/system/test-app.service
-    sudo systemctl daemon-reload
-    sudo systemctl enable test-app.service
-    sudo systemctl start test-app.service
-    sudo systemctl status test-app.service
-fi
+#This is the output for the app service
+sudo touch /home/datadog/app-std.out
+sudo chmod 777 /home/datadog/app-std.out
 
-
+sudo cp build/libs/k8s-lib-injection-app-0.0.1-SNAPSHOT.jar /home/datadog
+sudo cp test-app.service /etc/systemd/system/test-app.service
+sudo systemctl daemon-reload
+sudo systemctl enable test-app.service
+sudo systemctl start test-app.service
+sudo systemctl status test-app.service
+sleep 5
+cat /home/datadog/app-std.out
 
 echo "RUN DONE"
