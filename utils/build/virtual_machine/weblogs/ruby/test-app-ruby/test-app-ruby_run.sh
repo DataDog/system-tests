8978e2dc4e85525a2b975eff7854f51ec4d09785
roberto montero <roberto.montero@datadoghq.com>
2024-06-11 18:16:16 +0200

    test ruby

--- utils/build/virtual_machine/weblogs/ruby/test-app-ruby/test-app-ruby_run.sh
+++ utils/build/virtual_machine/weblogs/ruby/test-app-ruby/test-app-ruby_run.sh
@@ -4,11 +4,12 @@ echo "START RUN APP"
 # shellcheck disable=SC2035
 sudo chmod -R 755 *
 
-sed -i "s/3.1.3/>= 3.0.0\", \"< 3.3.0/g" Gemfile
-rm -rf Gemfile.lock
+#sed -i "s/3.1.3/>= 3.0.0\", \"< 3.3.0/g" Gemfile
+#DD_INSTRUMENT_SERVICE_WITH_APM=false bundle lock
+#rm -rf Gemfile.lock && bundle lock
 DD_INSTRUMENT_SERVICE_WITH_APM=false bundle install
 #Fix amazon linux 2023
-cp Gemfile.lock datadog-Gemfile.lock
+#cp Gemfile.lock datadog-Gemfile.lock
 # shellcheck disable=SC2035
 sudo cp -R * /home/datadog
 
@@ -16,6 +17,7 @@ sudo cp -R * /home/datadog
 sudo chmod -R 755 /home/datadog
 
 sudo chown -R datadog:datadog /home/datadog
+sudo su - datadog -c 'DD_INSTRUMENT_SERVICE_WITH_APM=false bundle install'
 sudo cp test-app.service /etc/systemd/system/test-app.service
 sudo systemctl daemon-reload
 sudo systemctl enable test-app.service
