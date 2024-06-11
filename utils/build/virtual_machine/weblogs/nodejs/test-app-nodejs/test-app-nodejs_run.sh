7cd9b52e11b135820130f2075658def4287d96f2
roberto montero <roberto.montero@datadoghq.com>
2024-06-11 08:01:05 +0200

    full ruby restore

--- utils/build/virtual_machine/weblogs/nodejs/test-app-nodejs/test-app-nodejs_run.sh
+++ utils/build/virtual_machine/weblogs/nodejs/test-app-nodejs/test-app-nodejs_run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 echo "START RUN APP"
 
+
 sudo sed -i "s/18080/5985/g" index.js 
 sudo cp index.js /home/datadog
-
-#This is the output for the app service
-sudo touch /home/datadog/app-std.out
-sudo chmod 777 /home/datadog/app-std.out
-
 sudo cp test-app.service /etc/systemd/system/test-app.service
 sudo systemctl daemon-reload
 sudo systemctl enable test-app.service
--- utils/build/virtual_machine/weblogs/ruby/test-app-ruby/test-app-ruby_run.sh
+++ utils/build/virtual_machine/weblogs/ruby/test-app-ruby/test-app-ruby_run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 echo "START RUN APP"
 
- 
-#This is the output for the app service
-sudo touch /home/datadog/app-std.out
-sudo chmod 777 /home/datadog/app-std.out
-#sudo rm Gemfile.lock
-sed -i "s/3.1.3/>= 3.0.0\", \"< 3.3.0/g" Gemfile
-export DD_INSTRUMENT_SERVICE_WITH_APM=false 
-bundle install
-export DD_INSTRUMENT_SERVICE_WITH_APM=true
+sudo sed -i "s/3.1.3/>= 3.0.0\", \"< 3.3.0/g" Gemfile
+
+sudo DD_INSTRUMENT_SERVICE_WITH_APM=false bundle install
+
 # shellcheck disable=SC2035
 sudo cp -R * /home/datadog
+
+# shellcheck disable=SC2035
 sudo chmod -R 755 /home/datadog
+
 sudo chown -R datadog:datadog /home/datadog
 sudo cp test-app.service /etc/systemd/system/test-app.service
 sudo systemctl daemon-reload
