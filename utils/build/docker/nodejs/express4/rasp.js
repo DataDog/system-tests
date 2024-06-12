d82033fd7876ff0aeac2e1398216c0dd75dd626f
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-06-12 10:06:16 +0200

    [nodejs] Enable rasp ssrf metrics system tests in nodejs (#2534)
    ---------
    
    Co-authored-by: simon-id <simon.id@datadoghq.com>

---
+++ utils/build/docker/nodejs/express4/rasp.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const http = require('http')
+function initRaspEndpoints (app) {
+  app.get('/rasp/ssrf', (req, res) => {
+    const clientRequest = http.get(`http://${req.query.domain}`, () => {
+      res.end('end')
+    })
+    clientRequest.on('error', (e) => {
+      // TODO when blocking is supported, throw e when is aborted
+      //  to check that we are blocking as expected
+      res.writeHead(500).end(e.message)
+    })
+  })
+
+  app.post('/rasp/ssrf', (req, res) => {
+    const clientRequest = http.get(`http://${req.body.domain}`, () => {
+      res.end('end')
+    })
+    clientRequest.on('error', (e) => {
+      // TODO when blocking is supported, throw e when is aborted
+      //  to check that we are blocking as expected
+      res.writeHead(500).end(e.message)
+    })
+  })
+}
+module.exports = initRaspEndpoints
