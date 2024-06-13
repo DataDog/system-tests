10ab4a5f461611a1ebc6cbd2fac978e4ddbf18fe
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2024-06-13 10:23:29 +0200

    OnBoarding: KrunVm provider (#2491)
    * OnBoarding: KrunVm provider

--- utils/build/virtual_machine/weblogs/python/test-app-python/python_install.sh
+++ utils/build/virtual_machine/weblogs/python/test-app-python/python_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 DISTRO=$1
 PY_VERSION=$2
 
 #Workaround: if python install pyenv
 if [ "$DISTRO" = "deb" ]; then
     #Install pyenv
-    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    packages_install="make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev git curl llvm libncursesw5-dev xz-utils tk-dev tzdata libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
+    sudo apt-get install -y "$packages_install" || ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends "$packages_install"
     export PYENV_ROOT="/home/datadog/.pyenv"
     sudo curl https://pyenv.run | bash
     export PATH="$PYENV_ROOT/bin:$PATH"
