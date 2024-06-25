2654b81eb061f767e3c3a089e4ed76fd8b7b107d
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-06-25 16:03:36 -0400

    clean up docker builds

--- utils/build/docker/dotnet/weblog/app.sh
+++ utils/build/docker/dotnet/weblog/app.sh
@@ -2,25 +2,28 @@
 
 set -eu
 
-echo starting app
+echo 'starting app'
 
-#Setup Datadog APM
-export ASPNETCORE_hostBuilder__reloadConfigOnChange=false
+# Enable Datadog .NET SDK
 export CORECLR_ENABLE_PROFILING=1
 export CORECLR_PROFILER='{846F5F1C-F9AE-4B07-969E-05C26BC060D8}'
-export CORECLR_PROFILER_PATH=/opt/datadog/Datadog.Trace.ClrProfiler.Native.so
-export DD_INTEGRATIONS=/opt/datadog/integrations.json
+export CORECLR_PROFILER_PATH=/opt/datadog/linux-x64/Datadog.Trace.ClrProfiler.Native.so
 export DD_DOTNET_TRACER_HOME=/opt/datadog
+
+# Datadog .NET SDK config
 export DD_IAST_REQUEST_SAMPLING=100
 export DD_IAST_VULNERABILITIES_PER_REQUEST=100
+export DD_TRACE_HEADER_TAGS='user-agent:http.request.headers.user-agent'
+export DD_DATA_STREAMS_ENABLED=true
+export DD_INTERNAL_TELEMETRY_V2_ENABLED=true
 
-
-# Dump on crash
-export COMPlus_DbgEnableMiniDump=1
-# MiniDumpWithPrivateReadWriteMemory is 2
-export COMPlus_DbgMiniDumpType=2
+# .NET runtime config
+export ASPNETCORE_hostBuilder__reloadConfigOnChange=false
+export COMPlus_DbgEnableMiniDump=1 # Enable dump on crash
+export COMPlus_DbgMiniDumpType=2   # MiniDumpWithPrivateReadWriteMemory is 2
 
 if [ "${UDS_WEBLOG:-0}" = "1" ]; then
+    export DD_APM_RECEIVER_SOCKET=/var/run/datadog/apm.socket
     ./set-uds-transport.sh
 fi
 
