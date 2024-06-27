1db7fb6d2ca0fdf515e5ea4b645eb11592fc314d
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2024-06-27 18:29:01 -0400

    quote curly braces in bash script

--- utils/build/docker/dotnet/app.sh
+++ utils/build/docker/dotnet/app.sh
@@ -7,7 +7,7 @@ echo starting app
 #Setup Datadog APM
 export ASPNETCORE_hostBuilder__reloadConfigOnChange=false
 export CORECLR_ENABLE_PROFILING=1
-export CORECLR_PROFILER={846F5F1C-F9AE-4B07-969E-05C26BC060D8}
+export CORECLR_PROFILER='{846F5F1C-F9AE-4B07-969E-05C26BC060D8}'
 export CORECLR_PROFILER_PATH=/opt/datadog/Datadog.Trace.ClrProfiler.Native.so
 export DD_INTEGRATIONS=/opt/datadog/integrations.json
 export DD_DOTNET_TRACER_HOME=/opt/datadog
