94627457115c97dadbb8e5413cac68b1b1059d9e
Alexander Seleznyov <alexander.seleznyov@datadoghq.com>
2024-07-02 11:56:52 +0300

    check for state errors

--- tests/debugger/test_debugger_expression_language.py
+++ tests/debugger/test_debugger_expression_language.py
@@ -18,33 +18,17 @@ from utils import (
 class Test_Debugger_Expression_Language(base._Base_Debugger_Test):
     version = 0
 
-    def _validate_expression_language_messages(self, expected_message_map):
-        agent_logs_endpoint_requests = list(interfaces.agent.get_data(path_filters="/api/v2/logs"))
+    def _setup(self, probes_name: str, request_path: str):
+        Test_Debugger_Expression_Language.version += 1
 
-        for request in agent_logs_endpoint_requests:
-            content = request["request"]["content"]
-
-            if content is not None:
-                for content in content:
-                    probe_id = content["debugger"]["snapshot"]["probe"]["id"]
-
-                    if probe_id in expected_message_map:
-                        message = content["message"]
-                        assert re.search(expected_message_map[probe_id], message), "Message not found"
-
-    def _setup(self, probes_name: str):
-        probes = base.read_probes(probes_name)
-
-        self.version += 1
-        rc.send_debugger_command(probes=probes, version=self.version)
+        self.rc_state = rc.send_debugger_command(
+            probes=base.read_probes(probes_name), version=Test_Debugger_Expression_Language.version
+        )
 
         interfaces.agent.wait_for(self.wait_for_all_probes_installed, timeout=30)
+        self.weblog_responses = [weblog.get(request_path)]
 
     def setup_expression_language_access_variables(self):
-        self._setup("expression_language_access_variables")
-        self.weblog_responses = [weblog.get("/debugger/expression?inputValue=asd")]
-
-    def test_expression_language_access_variables(self):
         self.expected_probe_ids = [
             "log170aa-expr-lang-0001-1478a6method",
             "log170aa-expr-lang-0002-1478a6method",
@@ -57,7 +41,11 @@ class Test_Debugger_Expression_Language(base._Base_Debugger_Test):
             "log170aa-expr-lang-0009-1478a6method",
             "log170aa-expr-lang-0010-1478a6method",
         ]
+                
+        self._setup("expression_language_access_variables", "/debugger/expression?inputValue=asd")
 
+    def test_expression_language_access_variables(self):
+        self.assert_all_states_not_error()
         self.assert_all_probes_are_installed()
         self.assert_all_weblog_responses_ok()
 
@@ -77,14 +65,14 @@ class Test_Debugger_Expression_Language(base._Base_Debugger_Test):
         self._validate_expression_language_messages(expected_messages)
 
     def setup_expression_language_access_exception(self):
-        self._setup("expression_language_access_exception")
-        self.weblog_responses = [weblog.get("/debugger/expression/exception")]
-
-    def test_expression_language_access_exception(self):
         self.expected_probe_ids = [
             "log170aa-expr-lang-0011-1478a6method",
         ]
+                
+        self._setup("expression_language_access_exception", "/debugger/expression/exception")
 
+    def test_expression_language_access_exception(self):
+        self.assert_all_states_not_error()
         self.assert_all_probes_are_installed()
 
         expected_messages = {
@@ -92,3 +80,17 @@ class Test_Debugger_Expression_Language(base._Base_Debugger_Test):
         }
 
         self._validate_expression_language_messages(expected_messages)
+
+    def _validate_expression_language_messages(self, expected_message_map):
+        agent_logs_endpoint_requests = list(interfaces.agent.get_data(path_filters="/api/v2/logs"))
+
+        for request in agent_logs_endpoint_requests:
+            content = request["request"]["content"]
+
+            if content is not None:
+                for content in content:
+                    probe_id = content["debugger"]["snapshot"]["probe"]["id"]
+
+                    if probe_id in expected_message_map:
+                        message = content["message"]
+                        assert re.search(expected_message_map[probe_id], message), "Message not found"
