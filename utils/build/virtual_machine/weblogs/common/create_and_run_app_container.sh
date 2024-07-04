0e65167c7ef41b27454d62aafe5005e16da455dd
roberto montero <roberto.montero@datadoghq.com>
2024-07-04 22:40:49 +0200

    Onboarding: Common script to run container weblog

--- utils/build/virtual_machine/weblogs/common/create_and_run_app_container.sh
+++ utils/build/virtual_machine/weblogs/common/create_and_run_app_container.sh
@@ -9,20 +9,22 @@ sudo chmod -R 755 *
 rm -rf Dockerfile || true
 cp Dockerfile.template Dockerfile || true
 
-sudo docker build --no-cache -t system-tests/local .
+#The parameter RUNTIME is used only for dotnet
+sudo docker build --no-cache -t --build-arg RUNTIME="bullseye-slim" -t system-tests/local .
+
 if [ -f docker-compose-agent-prod.yml ]; then
     # Agent may be installed in a different way
     sudo -E docker-compose -f docker-compose-agent-prod.yml up -d --remove-orphans datadog
     sleep 30
 fi
-sudo -E docker-compose -f docker-compose.yml up -d test-app-ruby
+sudo -E docker-compose -f docker-compose.yml up -d test-app
 
-echo "**************** RUNNING DOCKER SERVICES *****************" 
+echo "..:: RUNNING DOCKER SERVICES ::.." 
 sudo docker-compose ps
 if [ -f docker-compose-agent-prod.yml ]; then
-    echo "**************** DATADOG AGENT OUTPUT ********************"
+    echo "..:: DATADOG AGENT OUTPUT ::.."
     sudo docker-compose -f docker-compose-agent-prod.yml logs datadog
 fi
-echo "**************** WEBLOG APP OUTPUT********************"
+echo "..:: WEBLOG APP OUTPUT ::..
 sudo docker-compose logs
 echo "RUN DONE"
