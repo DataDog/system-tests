3abf66c3d257885548993cf6191680dd7c08128a
Flavien Darche <flavien.darche@datadoghq.com>
2024-07-12 17:58:05 +0200

    Tests for appsec event in metastruct

---
+++ tests/appsec/test_appsec_metastruct.py
@@ -0,0 +1,28 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2021 Datadog, Inc.
+
+from utils import weblog, context, interfaces, bug, missing_feature, scenarios, features
+
+@features.security_events_metastruct
+class Test_SecurityEvent_Metastruct:
+    """Tests to verify that the meta struct appsec event feature is used by the tracer"""
+
+    def setup_security_event_use_metastruct(self):
+        self.r = weblog.get(
+            "/", headers={"User-Agent": "Arachni/v1"},
+        )
+
+    def test_security_event_use_metastruct(self):
+        for _, span in interfaces.library.get_root_spans(request=self.r):
+            meta = span.get("meta", {})
+            meta_struct = span.get("meta_struct", {})
+            assert meta["appsec.event"] == "true"
+            assert ("_dd.appsec.json" not in meta)
+            assert "appsec" in meta_struct
+
+            # The event is not null
+            assert meta_struct.get("appsec", {}) not in [None, {}]
+            
+            # There is at least one rule triggered
+            assert len(meta_struct["appsec"].get("triggers", [])) > 0
--- utils/_features.py
+++ utils/_features.py
@@ -2216,6 +2216,16 @@ class features:
         """
         pytest.mark.features(feature_id=305)(test_object)
         return test_object
+    
+    @staticmethod
+    def security_events_metastruct(test_object):
+        """
+        Appsec meta struct
+
+        https://feature-parity.us1.prod.dog/#/?feature=314
+        """
+        pytest.mark.features(feature_id=314)(test_object)
+        return test_object
 
     @staticmethod
     def host_auto_installation_script_profiling(test_object):
