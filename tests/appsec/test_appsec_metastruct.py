44456ffd05cc703f92a6e31d4c3ee22f16ed178a
Flavien Darche <flavien.darche@datadoghq.com>
2024-07-16 18:03:28 +0200

    Add an iast event tests

--- tests/appsec/test_appsec_metastruct.py
+++ tests/appsec/test_appsec_metastruct.py
@@ -4,15 +4,16 @@
 
 from utils import weblog, context, interfaces, bug, missing_feature, scenarios, features
 
+
 @features.security_events_metastruct
 class Test_SecurityEvent_Metastruct:
     """Tests to verify that the meta struct appsec event feature is used by the tracer"""
 
-    def setup_security_event_use_metastruct(self):
+    def setup_appsec_event_use_metastruct(self):
         self.r = weblog.get("/", headers={"User-Agent": "Arachni/v1"})
 
-    def test_security_event_use_metastruct(self):
-        spans = [s for _, s in interfaces.library.get_root_spans(request=self.r)] 
+    def test_appsec_event_use_metastruct(self):
+        spans = [s for _, s in interfaces.library.get_root_spans(request=self.r)]
         assert spans
 
         for span in spans:
@@ -27,3 +28,24 @@ class Test_SecurityEvent_Metastruct:
 
             # There is at least one rule triggered
             assert len(meta_struct["appsec"].get("triggers", [])) > 0
+
+    def setup_iast_event_use_metastruct(self):
+        # Using this vulnerability because that's one that is implemented in all tracers
+        self.r = weblog.get("/iast/header_injection/test_insecure", data={"test": "dummyvalue"})
+
+    def test_iast_event_use_metastruct(self):
+        spans = [s for _, s in interfaces.library.get_root_spans(request=self.r)]
+        assert spans
+
+        for span in spans:
+            meta = span.get("meta", {})
+            meta_struct = span.get("meta_struct", {})
+            assert meta["_dd.iast.enabled"] == "1"
+            assert "_dd.iast.json" not in meta
+            assert "iast" in meta_struct
+
+            # The event is not null
+            assert meta_struct.get("appsec", {}) not in [None, {}]
+
+            # There is at least one vulnerability detected
+            assert len(meta_struct["appsec"].get("vulnerabilities", [])) > 0
