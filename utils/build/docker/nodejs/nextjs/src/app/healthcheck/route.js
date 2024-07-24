8597de6670867acba980bfa14ab3d628ffc06d5d
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2024-07-24 12:57:52 +0200

    Apply suggestions from code review
    Co-authored-by: simon-id <simon.id@datadoghq.com>

--- utils/build/docker/nodejs/nextjs/src/app/healthcheck/route.js
+++ utils/build/docker/nodejs/nextjs/src/app/healthcheck/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { version } from 'dd-trace/package.json'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { libddwaf_version as libddwafVersion } from '@datadog/native-appsec/package.json'
 
 export async function GET (request) {
