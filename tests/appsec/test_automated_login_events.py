b2c4759342fde18c67234d2c7224af4b25d7fafb
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-07-24 16:18:44 +0200

    Skip Test_Login_Events_Extended APPSEC-54242

--- tests/appsec/test_automated_login_events.py
+++ tests/appsec/test_automated_login_events.py
@@ -277,6 +277,7 @@ class Test_Login_Events:
 @rfc("https://docs.google.com/document/d/1-trUpphvyZY7k5ldjhW-MgqWl0xOm7AMEQDJEAZ63_Q/edit#heading=h.8d3o7vtyu1y1")
 @scenarios.appsec_auto_events_extended
 @features.user_monitoring
+@bug(context.library >= "java@1.38.0", reason="APPSEC-54242")
 class Test_Login_Events_Extended:
     "Test login success/failure use cases"
 
