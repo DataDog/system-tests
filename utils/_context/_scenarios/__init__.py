bbfd593551274c97e7471348a9e815702d14a21e
rochdev <roch.devost@gmail.com>
2024-07-26 11:30:24 -0400

    switch back to 1s polling interval

--- utils/_context/_scenarios/__init__.py
+++ utils/_context/_scenarios/__init__.py
@@ -578,7 +578,8 @@ class scenarios:
         "REMOTE_CONFIG_MOCKED_BACKEND_ASM_FEATURES",
         rc_api_enabled=True,
         appsec_enabled=False,
-        weblog_env={"DD_REMOTE_CONFIGURATION_ENABLED": "true", "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",},
+        weblog_env={"DD_REMOTE_CONFIGURATION_ENABLED": "true", "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",},
+        library_interface_timeout=20,
         doc="",
         scenario_groups=[ScenarioGroup.APPSEC],
     )
@@ -590,16 +591,18 @@ class scenarios:
             "DD_DYNAMIC_INSTRUMENTATION_ENABLED": "1",
             "DD_DEBUGGER_ENABLED": "1",
             "DD_REMOTE_CONFIG_ENABLED": "true",
-            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",
+            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",
             "DD_INTERNAL_RCM_POLL_INTERVAL": "1000",
         },
+        library_interface_timeout=20,
         doc="",
     )
 
     remote_config_mocked_backend_asm_dd = EndToEndScenario(
         "REMOTE_CONFIG_MOCKED_BACKEND_ASM_DD",
         rc_api_enabled=True,
-        weblog_env={"DD_APPSEC_RULES": None, "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",},
+        weblog_env={"DD_APPSEC_RULES": None, "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",},
+        library_interface_timeout=20,
         doc="""
             The spec says that if DD_APPSEC_RULES is defined, then rules won't be loaded from remote config.
             In this scenario, we use remote config. By the spec, whem remote config is available, rules file
@@ -617,8 +620,9 @@ class scenarios:
         weblog_env={
             "DD_APPSEC_ENABLED": "false",
             "DD_REMOTE_CONFIGURATION_ENABLED": "true",
-            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",
+            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",
         },
+        library_interface_timeout=20,
         doc="",
         scenario_groups=[ScenarioGroup.APPSEC],
     )
@@ -630,15 +634,17 @@ class scenarios:
             "DD_DYNAMIC_INSTRUMENTATION_ENABLED": "1",
             "DD_DEBUGGER_ENABLED": "1",
             "DD_REMOTE_CONFIG_ENABLED": "true",
-            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",
+            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",
         },
+        library_interface_timeout=20,
         doc="",
     )
 
     remote_config_mocked_backend_asm_dd_nocache = EndToEndScenario(
         "REMOTE_CONFIG_MOCKED_BACKEND_ASM_DD_NOCACHE",
         rc_api_enabled=True,
-        weblog_env={"DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",},
+        weblog_env={"DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",},
+        library_interface_timeout=20,
         doc="",
         scenario_groups=[ScenarioGroup.APPSEC],
     )
@@ -684,8 +690,9 @@ class scenarios:
             "DD_REMOTE_CONFIG_ENABLED": "true",
             "DD_INTERNAL_RCM_POLL_INTERVAL": "2000",
             "DD_DEBUGGER_DIAGNOSTICS_INTERVAL": "1",
-            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "0.25",
+            "DD_REMOTE_CONFIG_POLL_INTERVAL_SECONDS": "1",
         },
+        library_interface_timeout=20,
         doc="Test scenario for checking if method probe statuses can be successfully 'RECEIVED' and 'INSTALLED'",
         scenario_groups=[ScenarioGroup.DEBUGGER],
     )
