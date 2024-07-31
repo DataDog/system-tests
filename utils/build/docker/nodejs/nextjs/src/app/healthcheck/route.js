b9f5cf368d7bf4ed0367ffdcce9d4e773b3c220c
rochdev <roch.devost@gmail.com>
2024-07-31 15:21:20 -0400

    wip

--- utils/build/docker/nodejs/nextjs/src/app/healthcheck/route.js
+++ utils/build/docker/nodejs/nextjs/src/app/healthcheck/route.js
@@ -1,9 +1,14 @@
 import { NextResponse } from 'next/server'
 import { version } from 'dd-trace/package.json'
 import { libddwaf_version as libddwafVersion } from 'dd-trace/node_modules/@datadog/native-appsec/package.json'
-import { wafManager } from 'dd-trace/packages/dd-trace/src/appsec/waf'
 
 export async function GET (request) {
+  const { wafManager } = await import('dd-trace/packages/dd-trace/src/appsec/waf')
+  const tracer = import('dd-trace')
+
+  console.log('wafManager', wafManager)
+  console.log('tracer', tracer.tracer)
+
   return NextResponse.json({
     status: 'ok',
     library: {
