c8c89bc215e93520a5e070e9cf9fdff79efb28a9
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-08-08 11:29:09 +0200

    Add and enable rasp sqli tests in nodejs

--- tests/appsec/rasp/test_span_tags.py
+++ tests/appsec/rasp/test_span_tags.py
@@ -2,7 +2,7 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import features, weblog, interfaces, scenarios, rfc, missing_feature
+from utils import features, weblog, interfaces, scenarios, rfc, missing_feature, context
 
 
 def validate_span_tags(request, expected_meta=[], expected_metrics=[]):
@@ -43,7 +43,7 @@ class Test_Mandatory_SpanTags:
         self.r = weblog.get("/rasp/sqli", params={"user_id": "' OR 1 = 1 --"})
 
     @missing_feature(library="dotnet")
-    @missing_feature(library="nodejs", reason="Not supported yet")
+    @missing_feature(context.library < "nodejs@5.21.0")
     def test_sqli_span_tags(self):
         validate_span_tags(self.r, expected_metrics=["_dd.appsec.rasp.duration"])
 
@@ -71,6 +71,6 @@ class Test_Optional_SpanTags:
         self.r = weblog.get("/rasp/sqli", params={"user_id": "' OR 1 = 1 --"})
 
     @missing_feature(library="dotnet")
-    @missing_feature(library="nodejs", reason="Not supported yet")
+    @missing_feature(context.library < "nodejs@5.21.0")
     def test_sqli_span_tags(self):
         validate_span_tags(self.r, expected_metrics=["_dd.appsec.rasp.duration_ext", "_dd.appsec.rasp.rule.eval"])
