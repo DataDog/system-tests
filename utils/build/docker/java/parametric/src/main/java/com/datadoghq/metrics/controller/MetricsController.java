096563b4ab6d3b45a53145f6dbc61343ae4b287c
Bruce Bujon <bruce.bujon@datadoghq.com>
2024-08-16 16:51:22 +0200

    fix: Fix metrics controller mapping

--- utils/build/docker/java/parametric/src/main/java/com/datadoghq/metrics/controller/MetricsController.java
+++ utils/build/docker/java/parametric/src/main/java/com/datadoghq/metrics/controller/MetricsController.java
@@ -4,12 +4,14 @@ import static com.datadoghq.ApmTestClient.LOGGER;
 
 import datadog.trace.api.GlobalTracer;
 import datadog.trace.api.internal.InternalTracer;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/trace/stats")
 public class MetricsController {
+  @PostMapping("flush")
   public void flush() {
     LOGGER.info("Flushing metrics");
     try {
