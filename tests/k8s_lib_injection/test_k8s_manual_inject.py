6b65c2dc08896f877e5e1f3580f4da86f4dd66e3
roberto montero <roberto.montero@datadoghq.com>
2024-08-22 23:31:52 +0200

    debug local

--- tests/k8s_lib_injection/test_k8s_manual_inject.py
+++ tests/k8s_lib_injection/test_k8s_manual_inject.py
@@ -20,7 +20,7 @@ class _TestAdmisionController:
         return []
 
     @flaky(library="python", reason="APMRP-359")
-    def _test_inject_admission_controller(self, test_k8s_instance):
+    def test_inject_admission_controller(self, test_k8s_instance):
         logger.info(
             f"Launching test _test_inject_admission_controller: Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
         )
@@ -63,7 +63,7 @@ class _TestAdmisionController:
         assert len(traces_json) > 0, "No traces found"
         logger.info(f"Test test_inject_uds_without_admission_controller finished")
 
-    def test_deploy_app(self, test_k8s_instance):
+    def _test_deploy_app(self, test_k8s_instance):
         logger.info(
             f"Launching test test_deploy_app: Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
         )
