09a0c1b1946cb3b590a7ae51ed6ff5299ca97780
roberto montero <roberto.montero@datadoghq.com>
2024-08-23 09:55:06 +0200

    test manual inject

--- tests/k8s_lib_injection/test_k8s_manual_inject.py
+++ tests/k8s_lib_injection/test_k8s_manual_inject.py
@@ -35,7 +35,7 @@ class _TestAdmisionController:
         logger.info(f"Test _test_inject_admission_controller finished")
 
     @flaky(library="python", reason="APMRP-359")
-    def _test_inject_uds_admission_controller(self, test_k8s_instance):
+    def test_inject_uds_admission_controller(self, test_k8s_instance):
         logger.info(
             f"Launching test test_inject_uds_admission_controller: Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
         )
@@ -65,14 +65,6 @@ class _TestAdmisionController:
         traces_json = self._get_dev_agent_traces(test_k8s_instance.k8s_kind_cluster)
         assert len(traces_json) > 0, "No traces found"
         logger.info(f"Test test_inject_uds_without_admission_controller finished")
-
-    def _test_deploy_app(self, test_k8s_instance):
-        logger.info(
-            f"Launching test test_deploy_app: Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
-        )
-        test_k8s_instance.deploy_weblog_as_pod(with_admission_controller=False, use_uds=False)
-
-        logger.info(f"Test test_deploy_app finished")
 
 
 @features.k8s_admission_controller
