a523e38ecdf463be3e4ed9305269558046952d12
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-08-26 12:03:15 +0200

    Fix python3.12 profiling

--- utils/_context/containers.py
+++ utils/_context/containers.py
@@ -685,10 +685,6 @@ class WeblogContainer(TestedContainer):
         else:
             self.appsec_rules_file = (self.image.env | self.environment).get("DD_APPSEC_RULES", None)
 
-        if self.weblog_variant == "python3.12":
-            if self.library < "python@2.1.0.dev":  # profiling causes a seg fault on 2.0.0
-                self.environment["DD_PROFILING_ENABLED"] = "false"
-
     def post_start(self):
         from utils import weblog
 
