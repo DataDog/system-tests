2de7f7c473fdac6d34c3dc9c7dc16c9824bf707c
Tony Hsu <tony.hsu@datadoghq.com>
2024-08-26 16:52:13 +0200

    Implement crashtracking with grpc protocol

--- tests/parametric/test_crashtracking.py
+++ tests/parametric/test_crashtracking.py
@@ -14,7 +14,7 @@ from utils import bug, context, features, irrelevant, missing_feature, rfc, scen
 class Test_Crashtracking:
     @missing_feature(context.library == "golang", reason="Not implemented")
     @missing_feature(context.library == "nodejs", reason="Not implemented")
-    @missing_feature(context.library == "ruby", reason="Not implemented")
+    @missing_feature(context.library < "ruby@2.3.0", reason="Release from 2.3.0")
     @missing_feature(context.library == "cpp", reason="Not implemented")
     def test_report_crash(self, test_agent, test_library):
         test_library.crash()
@@ -24,7 +24,7 @@ class Test_Crashtracking:
 
     @missing_feature(context.library == "golang", reason="Not implemented")
     @missing_feature(context.library == "nodejs", reason="Not implemented")
-    @missing_feature(context.library == "ruby", reason="Not implemented")
+    @missing_feature(context.library < "ruby@2.3.0", reason="Release from 2.3.0")
     @missing_feature(context.library == "php", reason="Not implemented")
     @missing_feature(context.library == "cpp", reason="Not implemented")
     @pytest.mark.parametrize("library_env", [{"DD_CRASHTRACKING_ENABLED": "false"}])
