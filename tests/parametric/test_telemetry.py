9a7f99979054054debe319fbc7b558cf61123e35
Mikayla Toffler <mikayla.toffler@datadoghq.com>
2024-09-04 16:24:24 -0400

    Update test comment for better instructions to pass test

--- tests/parametric/test_telemetry.py
+++ tests/parametric/test_telemetry.py
@@ -91,6 +91,7 @@ class Test_Defaults:
 @rfc("https://docs.google.com/document/d/1kI-gTAKghfcwI7YzKhqRv2ExUstcHqADIWA4-TZ387o")
 @features.telemetry_app_started_event
 # To pass this test, ensure the lang you are testing has the necessary mapping in its config_rules.json file: https://github.com/DataDog/dd-go/tree/prod/trace/apps/tracer-telemetry-intake/telemetry-payload/static
+# And replace the `missing_feature` marker under the lang's manifest file, for Test_Consistent_Configs
 class Test_Consistent_Configs:
     """Clients should report modifications to features."""
     @pytest.mark.parametrize(
