d2ff42873f7f6e3ae998f33dbce030796004bece
Matthew Li <matthew.li@datadoghq.com>
2024-09-10 14:46:22 -0400

    adding tests for DD_VERSION, minor fix to python handler rebasing to include other commits

--- tests/parametric/test_config_consistency.py
+++ tests/parametric/test_config_consistency.py
@@ -59,3 +59,31 @@ class Test_Config_TraceLogDirectory:
         success, message = test_library.container_exec_run("ls /parametric-tracer-logs")
         assert success, message
         assert len(message.splitlines()) > 0, "No tracer logs detected"
+def set_service_version_tags():
+    env1 = {}
+    env2 = {"DD_SERVICE": "test_service", "DD_VERSION": "5.2.0"}
+    return parametrize("library_env", [env1,env2])
+
+@scenarios.parametric
+@features.tracing_configuration_consistency
+class Test_Config_UnifiedServiceTagging:
+    @set_service_version_tags()
+    def test_version_tag(self, library_env, test_agent, test_library):
+        assert library_env.get("DD_SERVICE", "test_service") == "test_service"
+        assert library_env.get("DD_VERSION", "5.2.0") == "5.2.0"
+        
+        with test_library:
+            with test_library.start_span(name="s1"):
+                pass
+            with test_library.start_span(name="s2", service="no dd_service"):
+                pass
+
+        traces = test_agent.wait_for_num_traces(2)
+        assert len(traces) == 2
+        
+        for trace in traces:
+            for span in trace:
+                if span['service'] == "test_service":
+                    assert span['meta']['version'] == "5.2.0"
+                else:
+                    assert "version" not in span['meta']
