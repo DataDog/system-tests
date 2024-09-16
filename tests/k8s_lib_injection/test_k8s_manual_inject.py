b8aaa46c05b72c75472109be4eb3f1c47197ae02
roberto montero <roberto.montero@datadoghq.com>
2024-09-16 07:50:41 +0200

    K8s Profiling

--- tests/k8s_lib_injection/test_k8s_manual_inject.py
+++ tests/k8s_lib_injection/test_k8s_manual_inject.py
@@ -12,17 +12,6 @@ from utils import scenarios, features
 
 
 class _TestAdmisionController:
-    def _get_dev_agent_traces(self, agent_port, retry=10):
-        for _ in range(retry):
-            logger.info(f"[Check traces] Checking traces:")
-            response = requests.get(f"http://localhost:{agent_port}/test/traces")
-            traces_json = response.json()
-            if len(traces_json) > 0:
-                logger.debug(f"Test traces response: {traces_json}")
-                return traces_json
-            time.sleep(2)
-        return []
-
     def test_inject_admission_controller(self, test_k8s_instance):
         logger.info(
             f"Launching test _test_inject_admission_controller: Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
@@ -65,6 +54,17 @@ class _TestAdmisionController:
         assert len(traces_json) > 0, "No traces found"
         logger.info(f"Test test_inject_uds_without_admission_controller finished")
 
+    def _get_dev_agent_traces(self, agent_port, retry=10):
+        for _ in range(retry):
+            logger.info(f"[Check traces] Checking traces:")
+            response = requests.get(f"http://localhost:{agent_port}/test/traces")
+            traces_json = response.json()
+            if len(traces_json) > 0:
+                logger.debug(f"Test traces response: {traces_json}")
+                return traces_json
+            time.sleep(2)
+        return []
+
 
 @features.k8s_admission_controller
 @scenarios.k8s_library_injection_asm
@@ -101,7 +101,61 @@ class TestAdmisionControllerAsm:
 class TestAdmisionControllerProfiling:
     """Test profiling activation with the admission controller."""
 
-    def test_inject_asm_admission_controller(self, test_k8s_instance):
+    def _check_profiling_request_sent(self, agent_port):
+        """ Use test agent profiling endpoint to check 
+        if the profiling data has been sent by the injectect library. 
+        Checks the request made to the profiling endpoint (/profiling/v1/input)."""
+        response = requests.get(f"http://localhost:{agent_port}/test/session/requests")
+        for request in response.json():
+            if request["url"].endswith("/profiling/v1/input"):
+                return True
+        return False
+
+    def test_profiling_disabled_by_default(self, test_k8s_instance):
+        logger.info(f"Launching test test_profiling_disabled_by_default")
+        logger.info(
+            f": Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
+        )
+        test_k8s_instance.deploy_test_agent()
+        test_k8s_instance.deploy_datadog_cluster_agent()
+        # if profiling is enabled force some profiling data to be sent
+        test_k8s_instance.deploy_weblog_as_pod(
+            env={"DD_PROFILING_UPLOAD_PERIOD": "10", "DD_INTERNAL_PROFILING_LONG_LIVED_THRESHOLD": "1500"}
+        )
+        profiling_request_found = self._check_profiling_request_sent(test_k8s_instance.k8s_kind_cluster.agent_port)
+        assert not profiling_request_found, "Profiling should be disabled by default, but a profiling request was found"
+
+    def test_profiling_admission_controller(self, test_k8s_instance):
+        logger.info(f"Launching test test_profiling_admission_controller")
+        logger.info(
+            f": Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
+        )
+        test_k8s_instance.deploy_test_agent()
+        test_k8s_instance.deploy_datadog_cluster_agent(features={"datadog.profiling.enabled": "auto"})
+        test_k8s_instance.deploy_weblog_as_pod(
+            env={"DD_PROFILING_UPLOAD_PERIOD": "10", "DD_INTERNAL_PROFILING_LONG_LIVED_THRESHOLD": "1500"}
+        )
+        profiling_request_found = self._check_profiling_request_sent(test_k8s_instance.k8s_kind_cluster.agent_port)
+        assert profiling_request_found, "No profiling request found"
+
+    def test_profiling_override_cluster_env(self, test_k8s_instance):
+        logger.info(f"Launching test test_profiling_override_cluster_env")
+        logger.info(
+            f": Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
+        )
+        cluster_agent_config = {
+            "clusterAgent.env[0].name": "DD_ADMISSION_CONTROLLER_AUTO_INSTRUMENTATION_PROFILING_ENABLED",
+            "clusterAgent.env[0].value": "auto",
+        }
+        test_k8s_instance.deploy_test_agent()
+        test_k8s_instance.deploy_datadog_cluster_agent(features=cluster_agent_config)
+        test_k8s_instance.deploy_weblog_as_pod(
+            env={"DD_PROFILING_UPLOAD_PERIOD": "10", "DD_INTERNAL_PROFILING_LONG_LIVED_THRESHOLD": "1500"}
+        )
+        profiling_request_found = self._check_profiling_request_sent(test_k8s_instance.k8s_kind_cluster.agent_port)
+        assert profiling_request_found, "No profiling request found"
+
+    def _test_inject_profiling_admission_controller_real(self, test_k8s_instance):
         logger.info(
             f"Launching test test_inject_profiling_admission_controller: Weblog: [{test_k8s_instance.k8s_kind_cluster.weblog_port}] Agent: [{test_k8s_instance.k8s_kind_cluster.agent_port}]"
         )
@@ -111,7 +165,6 @@ class TestAdmisionControllerProfiling:
         test_k8s_instance.deploy_weblog_as_pod(
             env={"DD_PROFILING_UPLOAD_PERIOD": "10", "DD_INTERNAL_PROFILING_LONG_LIVED_THRESHOLD": "1500"}
         )
-
         weblog_port = test_k8s_instance.k8s_kind_cluster.weblog_port
         logger.info(f"Waiting for weblog available [localhost:{weblog_port}]")
         wait_for_port(weblog_port, "localhost", 80.0)
