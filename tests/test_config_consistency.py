64da26c22adba9e05ceec3e568b6aa36a898ce0f
Matthew Li <matthew.li@datadoghq.com>
2024-09-17 14:45:48 -0400

    adding comment for default scenario

--- tests/test_config_consistency.py
+++ tests/test_config_consistency.py
@@ -137,4 +137,4 @@ class Test_Config_UnifiedServiceTagging_Default:
         interfaces.library.assert_trace_exists(self.r)
         spans = interfaces.agent.get_spans_list(self.r)
         assert len(spans) == 1, "Agent received the incorrect amount of spans"
-        assert spans[0]["service"] != "service_test"
+        assert spans[0]["service"] != "service_test" #in default scenario, DD_SERVICE is set to "weblog" in the dockerfile; this is a temp fix to test that it is not the value we manually set in the specific scenario
