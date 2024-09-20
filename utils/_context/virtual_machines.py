b00121e7ebec39b2a48c6540fcc63f089e95dab8
roberto montero <roberto.montero@datadoghq.com>
2024-09-20 15:25:59 +0200

    testing ruby

--- utils/_context/virtual_machines.py
+++ utils/_context/virtual_machines.py
@@ -372,7 +372,7 @@ class OracleLinux92arm64(_VirtualMachine):
             os_distro="rpm",
             os_branch="oracle_linux",
             os_cpu="arm64",
-            default_vm=False,
+            default_vm=True,
             **kwargs,
         )
 
@@ -389,7 +389,7 @@ class OracleLinux88amd64(_VirtualMachine):
             os_distro="rpm",
             os_branch="oracle_linux",
             os_cpu="amd64",
-            default_vm=False,
+            default_vm=True,
             **kwargs,
         )
 
@@ -405,7 +405,7 @@ class OracleLinux88arm64(_VirtualMachine):
             os_distro="rpm",
             os_branch="oracle_linux",
             os_cpu="arm64",
-            default_vm=False,
+            default_vm=True,
             **kwargs,
         )
 
@@ -422,6 +422,6 @@ class OracleLinux79amd64(_VirtualMachine):
             os_distro="rpm",
             os_branch="oracle_linux",
             os_cpu="amd64",
-            default_vm=False,
+            default_vm=True,
             **kwargs,
         )
