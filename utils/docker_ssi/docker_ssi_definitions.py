b202beb4ffcae7a734c73d07b271a5057689f2d4
roberto montero <roberto.montero@datadoghq.com>
2024-09-23 15:48:30 +0200

    Docker SSI: oracle linux

--- utils/docker_ssi/docker_ssi_definitions.py
+++ utils/docker_ssi/docker_ssi_definitions.py
@@ -17,6 +17,13 @@ class SupportedImages:
         self.UBUNTU_16_AMD64 = DockerImage("ubuntu:16.04", LINUX_AMD64)
         self.UBUNTU_16_ARM64 = DockerImage("ubuntu:16.04", LINUX_ARM64)
         self.CENTOS_7_AMD64 = DockerImage("centos:7", LINUX_AMD64)
+        self.ORACLELINUX_9_ARM64 = DockerImage("oraclelinux:9", LINUX_ARM64)
+        self.ORACLELINUX_9_AMD64 = DockerImage("oraclelinux:9", LINUX_AMD64)
+        self.ORACLELINUX_8_ARM64 = DockerImage("oraclelinux:8.10", LINUX_ARM64)
+        self.ORACLELINUX_8_AMD64 = DockerImage("oraclelinux:8.10", LINUX_AMD64)
+        self.ORACLELINUX_7_ARM64 = DockerImage("oraclelinux:7.9", LINUX_ARM64)
+        self.ORACLELINUX_7_AMD64 = DockerImage("oraclelinux:7.9", LINUX_AMD64)
+
         # Currently bugged
         # DockerImage("centos:7", LINUX_ARM64, short_name="centos_7")
         # DockerImage("alpine:3", LINUX_AMD64, short_name="alpine_3"),
@@ -70,12 +77,31 @@ JETTY_APP = WeblogDescriptor(
         SupportedImages().UBUNTU_16_ARM64.with_allowed_runtime_versions(
             JavaRuntimeInstallableVersions.get_all_versions()
         ),
+        SupportedImages().ORACLELINUX_9_AMD64.with_allowed_runtime_versions(
+            JavaRuntimeInstallableVersions.get_all_versions()
+        ),
+        SupportedImages().ORACLELINUX_9_ARM64.with_allowed_runtime_versions(
+            JavaRuntimeInstallableVersions.get_all_versions()
+        ),
+        SupportedImages().ORACLELINUX_8_AMD64.with_allowed_runtime_versions(
+            JavaRuntimeInstallableVersions.get_all_versions()
+        ),
+        SupportedImages().ORACLELINUX_8_ARM64.with_allowed_runtime_versions(
+            JavaRuntimeInstallableVersions.get_all_versions()
+        ),
+        SupportedImages().ORACLELINUX_7_AMD64.with_allowed_runtime_versions(
+            JavaRuntimeInstallableVersions.get_all_versions()
+        ),
+        SupportedImages().ORACLELINUX_7_ARM64.with_allowed_runtime_versions(
+            JavaRuntimeInstallableVersions.get_all_versions()
+        ),
         # Commented due to APMON-1491
         # SupportedImages().CENTOS_7_AMD64.with_allowed_runtime_versions(
         #    JavaRuntimeInstallableVersions.get_all_versions()
         # ),
     ],
 )
+
 TOMCAT_APP = WeblogDescriptor("tomcat-app", "java", [SupportedImages().TOMCAT_9_ARM64])
 JAVA7_APP = WeblogDescriptor("java7-app", "java", [SupportedImages().UBUNTU_22_ARM64])
 WEBSPHERE_APP = WeblogDescriptor("websphere-app", "java", [SupportedImages().WEBSPHERE_AMD64])
