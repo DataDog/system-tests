40a7c46f1a9dc9b91f19df0732ecbe29f5874360
Mikayla Toffler <mikayla.toffler@datadoghq.com>
2024-09-26 13:17:21 -0400

    ran formatter

--- utils/_context/_scenarios/__init__.py
+++ utils/_context/_scenarios/__init__.py
@@ -464,7 +464,11 @@ class scenarios:
 
     tracing_config_nondefault_2 = EndToEndScenario(
         "TRACING_CONFIG_NONDEFAULT_2",
-        weblog_env={"DD_TRACE_OBFUSCATION_QUERY_STRING_REGEXP": "", "DD_TRACE_KAFKA_ENABLED": "true", "DD_TRACE_CLIENT_IP_HEADER": "custom-ip-header",},
+        weblog_env={
+            "DD_TRACE_OBFUSCATION_QUERY_STRING_REGEXP": "",
+            "DD_TRACE_KAFKA_ENABLED": "true",
+            "DD_TRACE_CLIENT_IP_HEADER": "custom-ip-header",
+        },
         include_kafka=True,
         doc="Test tracer configuration when a collection of non-default settings are applied",
     )
