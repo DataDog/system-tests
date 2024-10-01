dca504a4116067d47b98212f683a8823a404e41d
Flavien Darche <flavien.darche@datadoghq.com>
2024-10-01 17:31:54 +0100

    add envoy config file + basic http mock python server + update comment doc

--- utils/_context/_scenarios/external_processing.py
+++ utils/_context/_scenarios/external_processing.py
@@ -4,12 +4,19 @@ class ExternalProcessingScenario(EndToEndScenario):
     def __init__(self, name, description, ):
         super().__init__(name, description)
 
-        # start envoyproxy/emvoy
+        # start envoyproxy/envoy:v1.31-latest⁠
+        # -> envoy.yaml configuration in tests/external_processing/envoy.yaml
+
         # start dummy http app on weblog port
+        # -> server.py in tests/external_processing/server.py
+
         # start system-tests proxy
         # start agent
         # start service extension
         #    with agent url threw system-tests proxy
 
-        # need 
-        #   ./build.sh golang -i weblog -w service-extension
+        # service extension image:
+        # https://github.com/DataDog/dd-trace-go/pkgs/container/dd-trace-go%2Fservice-extensions-callout 
+        # Version:
+        # tag: dev
+        # base: latest/v*.*.*
