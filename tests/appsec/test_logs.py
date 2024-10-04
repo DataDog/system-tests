52cd1a934732b6ed290650e5d4b29d0a6f23378d
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-10-04 15:16:11 +0200

    Removes tests on level=DEBUG

--- tests/appsec/test_logs.py
+++ tests/appsec/test_logs.py
@@ -2,7 +2,7 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import weblog, context, interfaces, irrelevant, missing_feature, bug, features
+from utils import weblog, context, interfaces, missing_feature, features
 
 # get the default log output
 stdout = interfaces.library_stdout if context.library != "dotnet" else interfaces.library_dotnet_managed
@@ -12,54 +12,9 @@ stdout = interfaces.library_stdout if context.library != "dotnet" else interface
 class Test_Standardization:
     """AppSec logs should be standardized"""
 
-    @irrelevant(library="java", reason="Cannot be implemented with cooperation from libddwaf")
-    @missing_feature(library="php")
-    def test_d01(self):
-        """Log D1: names and adresses AppSec listen to"""
-        stdout.assert_presence(r"Loaded rule:", level="DEBUG")  # TODO: should be more precise
-
-    @missing_feature(context.library < "dotnet@2.1.0")
-    @irrelevant(library="java", reason="IG doesn't push addresses in Java.")
-    def test_d02(self):
-        """Log D2: Address pushed to Instrumentation Gateway"""
-        stdout.assert_presence(r"Pushing address .* to the Instrumentation Gateway", level="DEBUG")
-
-    @missing_feature(library="dotnet", reason="APPSEC-983, being discussed")
-    @missing_feature(library="java")
-    @missing_feature(library="php", reason="Happens inside the WAF")
-    def test_d03(self):
-        """Log D3: When an address matches a rule needs"""
-        stdout.assert_presence(r"Available addresses .* match needs for rules", level="DEBUG")
-
-    @missing_feature(context.library < "dotnet@2.1.0")
-    @missing_feature(library="java")
-    def test_d04(self):
-        """Log D4: When calling the WAF, logs parameters"""
-        stdout.assert_presence(r"Executing AppSec In-App WAF with parameters:", level="DEBUG")
-
     def setup_d05(self):
         weblog.get("/waf", params={"key": "\n :"})  # rules.http_protocol_violation.crs_921_160
         weblog.get("/waf", headers={"random-key": "acunetix-user-agreement"})  # rules.security_scanner.crs_913_110
-
-    @bug(context.library == "java@0.90.0", reason="APPSEC-2190")
-    @bug(context.library == "java@0.91.0", reason="APPSEC-2190")
-    @missing_feature(context.library < "dotnet@2.1.0")
-    def test_d05(self):
-        """Log D5: WAF outputs"""
-        stdout.assert_presence(r'AppSec In-App WAF returned:.*crs-921-160"', level="DEBUG")
-        stdout.assert_presence(r'AppSec In-App WAF returned:.*crs-913-110"', level="DEBUG")
-
-    @missing_feature(library="php", reason="Would require parsing the WAF result")
-    @missing_feature(library="dotnet", reason="APPSEC-983")
-    def test_d06(self):
-        """Log D6: WAF rule detected an attack with details"""
-        stdout.assert_presence(r"Detecting an attack from rule crs-921-160:.*", level="DEBUG")
-        stdout.assert_presence(r"Detecting an attack from rule crs-913-110:.*", level="DEBUG")
-
-    @missing_feature(True, reason="not testable as now")
-    def test_d07(self):
-        """Log D7: Exception in rule"""
-        stdout.assert_presence(r"Rule .* failed. Error details: ", level="DEBUG")
 
     @missing_feature(library="php")
     @missing_feature(library="dotnet", reason="APPSEC-983, being discussed")
