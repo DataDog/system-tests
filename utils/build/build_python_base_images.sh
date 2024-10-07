3e9295edfe4d454c7d44cabcd2c2209d92cee0b7
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2024-10-07 11:13:39 -0400

    nore base image updates

--- utils/build/build_python_base_images.sh
+++ utils/build/build_python_base_images.sh
@@ -3,14 +3,14 @@
 # build and push python base images
 
 
-docker buildx build --load --progress=plain -f utils/build/docker/python/fastapi.base.Dockerfile -t datadog/system-tests:fastapi.base-v2 .
+docker buildx build --load --progress=plain -f utils/build/docker/python/fastapi.base.Dockerfile -t datadog/system-tests:fastapi.base-v3 .
 docker buildx build --load --progress=plain -f utils/build/docker/python/python3.12.base.Dockerfile -t datadog/system-tests:python3.12.base-v3 .
 docker buildx build --load --progress=plain -f utils/build/docker/python/django-poc.base.Dockerfile -t datadog/system-tests:django-poc.base-v3 .
 docker buildx build --load --progress=plain -f utils/build/docker/python/flask-poc.base.Dockerfile -t datadog/system-tests:flask-poc.base-v7 .
 docker buildx build --load --progress=plain -f utils/build/docker/python/uwsgi-poc.base.Dockerfile -t datadog/system-tests:uwsgi-poc.base-v4 .
 
 if [ "$1" = "--push" ]; then
-      docker push datadog/system-tests:fastapi.base-v2
+      docker push datadog/system-tests:fastapi.base-v3
       docker push datadog/system-tests:python3.12.base-v3
       docker push datadog/system-tests:django-poc.base-v3
       docker push datadog/system-tests:flask-poc.base-v7
