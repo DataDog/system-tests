ea00899daed288fd5247b9b4803ab0a15af400f0
ishabi <ilyasshabi94@gmail.com>
2024-10-14 17:25:16 +0200

    format files

--- utils/build/docker/nodejs/express4/app.js
+++ utils/build/docker/nodejs/express4/app.js
@@ -56,6 +56,11 @@ app.get('/sample_rate_route/:i', (req, res) => {
   res.send('OK')
 })
 
+app.get('/api_security/sampling/:status', (req, res) => {
+  res.status(req.params.status || 200)
+  res.send('Hello!')
+})
+
 app.get('/params/:value', (req, res) => {
   res.send('OK')
 })
