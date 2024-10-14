0d8e2b0a1b53d5fee67b2fc5b8dbcb2c58294dba
roberto montero <roberto.montero@datadoghq.com>
2024-10-14 21:26:47 +0200

    check jira ticket

--- utils/virtual_machine/utils.py
+++ utils/virtual_machine/utils.py
@@ -13,6 +13,8 @@ def parametrize_virtual_machines(bugs: list[dict] = None):
     *     @parametrize_virtual_machines(bugs=[{"vm_cpu":"amd64", "weblog_variant":"app1", "reason": "APMON-1576"}])
     - Marks as bug for all vm with cpu type "amd64" and all weblogs
     *     @parametrize_virtual_machines(bugs=[{"vm_cpu":"amd64","reason": "APMON-1576"}])
+    - Marks as bug for all vms that belong to "debian" and all weblogs
+    *     @parametrize_virtual_machines(bugs=[{"vm_branch":"debian","reason": "APMON-1576"}])
     Reason is mandatory for each bug and it MUST reference to a JIRA ticket.
     """
 
@@ -26,6 +28,7 @@ def parametrize_virtual_machines(bugs: list[dict] = None):
                 for bug in bugs:
                     if (
                         (not "vm_name" in bug or vm.name == bug["vm_name"])
+                        and (not "vm_branch" in bug or vm.os_branch == bug["vm_branch"])
                         and (not "vm_cpu" in bug or vm.os_cpu == bug["vm_cpu"])
                         and (not "weblog_variant" in bug or context.weblog_variant == bug["weblog_variant"])
                     ):
@@ -39,6 +42,8 @@ def parametrize_virtual_machines(bugs: list[dict] = None):
             if bug_found == False:
                 parameters.append(vm)
 
-        return pytest.mark.parametrize("virtual_machine", parameters)(func)
+        return pytest.mark.parametrize(
+            "virtual_machine", parameters, ids=getattr(context.scenario, "required_vm_names", [])
+        )(func)
 
     return decorator
