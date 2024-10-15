a83cce96d98e56b876d87e80b224e47af5c0ef9d
Munir Abdinur <munir_abdinur@hotmail.com>
2024-10-15 11:55:44 -0400

    fmt

--- tests/parametric/test_telemetry.py
+++ tests/parametric/test_telemetry.py
@@ -542,8 +542,7 @@ class Test_TelemetrySCAEnvVar:
         ],
     )
     @missing_feature(
-        context.library <= "python@2.16.0",
-        reason="Converts boolean values to strings",
+        context.library <= "python@2.16.0", reason="Converts boolean values to strings",
     )
     def test_telemetry_sca_enabled_propagated(
         self, library_env, specific_libraries_support, outcome_value, test_agent, test_library
@@ -561,7 +560,10 @@ class Test_TelemetrySCAEnvVar:
         assert cfg_appsec_enabled.get("value") == outcome_value
 
     @pytest.mark.parametrize("library_env", [{**DEFAULT_ENVVARS}])
-    @missing_feature(context.library <= "python@2.16.0", reason="Does not report DD_APPSEC_SCA_ENABLED configuration if the default value is used")
+    @missing_feature(
+        context.library <= "python@2.16.0",
+        reason="Does not report DD_APPSEC_SCA_ENABLED configuration if the default value is used",
+    )
     def test_telemetry_sca_enabled_not_propagated(self, library_env, test_agent, test_library):
         configuration_by_name = self.get_app_started_configuration_by_name(test_agent, test_library)
 
