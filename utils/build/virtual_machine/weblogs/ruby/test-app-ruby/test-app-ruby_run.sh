5dac2fece23010a25c3d11ffc0bfcec6fb29528c
roberto montero <roberto.montero@datadoghq.com>
2024-10-16 15:52:45 +0200

    add all machines

--- utils/build/virtual_machine/weblogs/ruby/test-app-ruby/test-app-ruby_run.sh
+++ utils/build/virtual_machine/weblogs/ruby/test-app-ruby/test-app-ruby_run.sh
@@ -6,7 +6,8 @@ sudo chmod -R 755 *
 export PATH=~/.rbenv/bin/:~/.rbenv/shims:$PATH
 rbenv local 3.0.2
 DD_INSTRUMENT_SERVICE_WITH_APM=false bundle install
-
-./create_and_run_app_service.sh "/home/ubuntu/.rbenv/shims/rails server -b 0.0.0.0 -p 5985"
+current_user=$(whoami)
+sed -i "s/SSI_USER/$current_user/g" test-app.service
+./create_and_run_app_service.sh "/home/$current_user/.rbenv/shims/rails server -b 0.0.0.0 -p 5985"
 
 echo "RUN ruby DONE"
