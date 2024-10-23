c9acb0b15da8a0a885186114482f03c389805dfb
William Conti <william.conti@datadoghq.com>
2024-10-23 10:04:52 -0400

    fix formatting

--- tests/integrations/test_dsm.py
+++ tests/integrations/test_dsm.py
@@ -49,7 +49,9 @@ class Test_DsmKafka:
     def setup_dsm_kafka(self):
         self.r = weblog.get(f"/dsm?integration=kafka&queue={DSM_QUEUE}&group={DSM_CONSUMER_GROUP}")
 
-    @irrelevant(context.library in ["python", "java", "nodejs", "dotnet"], reason="New behavior with cluster id not merged yet.")
+    @irrelevant(
+        context.library in ["python", "java", "nodejs", "dotnet"], reason="New behavior with cluster id not merged yet."
+    )
     def test_dsm_kafka(self):
         assert self.r.text == "ok"
 
@@ -58,10 +60,7 @@ class Test_DsmKafka:
         # There is currently no FNV-1 library availble for node.js
         # So we are using a different algorithm for node.js for now
         language_hashes = {
-            "nodejs": {
-                "producer": 7021878731777772655,
-                "consumer": 4591800307942911915,
-            },
+            "nodejs": {"producer": 7021878731777772655, "consumer": 4591800307942911915,},
             # we are not using a group consumer for testing go as setup is complex, so no group edge_tag is included in hashing
             "golang": {
                 "producer": 4463699290244539355,
