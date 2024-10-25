eb137aaf2f83ca49b417118778a62869a4facab8
William Conti <william.conti@datadoghq.com>
2024-10-25 09:24:02 -0400

    disable aws tests for now

--- tests/integrations/crossed_integrations/test_kinesis.py
+++ tests/integrations/crossed_integrations/test_kinesis.py
@@ -215,6 +215,7 @@ class _Test_Kinesis:
 
 
 @scenarios.crossed_tracing_libraries
+@irrelevant(True, reason="AWS Tests are not currently stable.")
 @features.aws_kinesis_span_creationcontext_propagation_via_message_attributes_with_dd_trace
 class Test_Kinesis_PROPAGATION_VIA_MESSAGE_ATTRIBUTES(_Test_Kinesis):
     buddy_interface = interfaces.python_buddy
