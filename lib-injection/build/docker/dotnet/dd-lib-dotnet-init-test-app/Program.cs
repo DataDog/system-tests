9f2a528f2f2fea54848f5ea9c887c288eb0c2dc5
Emmett Butler <emmett.butler321@gmail.com>
2024-10-31 09:36:12 -0700

    dotnet crash endpoint

--- lib-injection/build/docker/dotnet/dd-lib-dotnet-init-test-app/Program.cs
+++ lib-injection/build/docker/dotnet/dd-lib-dotnet-init-test-app/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+private static string Crash(HttpRequest request)
+{
+    var thread = new Thread(() =>
+    {
+        throw new BadImageFormatException("Expected");
+    });
+
+    thread.Start();
+    thread.Join();
+
+    return "Failed to crash";
+}
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/crashme", Crash);
 
 app.Run();
