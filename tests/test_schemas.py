b2c206adac202d5aec29508de6e405a185a3a471
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2024-11-01 12:15:32 +0000

    resolve schema bug

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -44,7 +44,6 @@ class Test_library:
     def test_library_diagnostics_content(self):
         interfaces.library.assert_schema_point("/debugger/v1/diagnostics", "$[].content")
 
-    @bug(context.library == "python", reason="DEBUG-2743")
     def test_library_schema_debugger(self):
         interfaces.library.assert_schema_point("/debugger/v1/input", "$[].dd.span_id")
         interfaces.library.assert_schema_point("/debugger/v1/input", "$[].dd.trace_id")
