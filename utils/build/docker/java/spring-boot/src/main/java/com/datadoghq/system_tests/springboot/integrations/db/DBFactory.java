b86aa790698cac1ea3a8489a4699c15765284dea
Stuart McCulloch <stuart.mcculloch@datadoghq.com>
2024-11-03 22:30:46 +0000

    Add reactive (R2DBC) database operations

--- utils/build/docker/java/spring-boot/src/main/java/com/datadoghq/system_tests/springboot/integrations/db/DBFactory.java
+++ utils/build/docker/java/spring-boot/src/main/java/com/datadoghq/system_tests/springboot/integrations/db/DBFactory.java
@@ -1,15 +1,21 @@
 package com.datadoghq.system_tests.springboot.integrations.db;
 
 import com.datadoghq.system_tests.springboot.integrations.db.mssql.MssqlCRUDOperation;
+import com.datadoghq.system_tests.springboot.integrations.db.mssql.ReactiveMssqlCRUDOperation;
 import com.datadoghq.system_tests.springboot.integrations.db.mysql.MysqlCRUDOperation;
+import com.datadoghq.system_tests.springboot.integrations.db.mysql.ReactiveMysqlCRUDOperation;
 import com.datadoghq.system_tests.springboot.integrations.db.postgres.PostgresCRUDOperation;
+import com.datadoghq.system_tests.springboot.integrations.db.postgres.ReactivePostgresCRUDOperation;
 
 public class DBFactory {
 
     enum SupportedDB {
         mysql(new MysqlCRUDOperation()),
         postgresql(new PostgresCRUDOperation()),
-        mssql(new MssqlCRUDOperation());
+        mssql(new MssqlCRUDOperation()),
+        reactive_mysql(new ReactiveMysqlCRUDOperation()),
+        reactive_postgresql(new ReactivePostgresCRUDOperation()),
+        reactive_mssql(new ReactiveMssqlCRUDOperation());
 
         ICRUDOperation currentCrud;
 
