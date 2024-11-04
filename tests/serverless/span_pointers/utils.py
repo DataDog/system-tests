278f73ae5111a4ec4f1313e66250d86c073714a2
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2024-11-04 15:52:25 -0500

    chore: more span pointer tests (#3330)

--- tests/serverless/span_pointers/utils.py
+++ tests/serverless/span_pointers/utils.py
@@ -26,7 +26,7 @@ def standard_hashing_function(elements: list[bytes]) -> PointerHash:
 
 
 def make_single_span_link_validator(
-    pointer_kind: str, pointer_direction: str, pointer_hash: PointerHash,
+    resource: str, pointer_kind: str, pointer_direction: str, pointer_hash: PointerHash,
 ):
     """
     Make a validator function for use with interfaces.library.validate_spans.
@@ -37,6 +37,12 @@ def make_single_span_link_validator(
 
     def validator(span):
         if "span_links" not in span:
+            return
+
+        if "resource" not in span:
+            return
+
+        if span["resource"] != resource:
             return
 
         found_matching = False
