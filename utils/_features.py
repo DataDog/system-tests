18fe3da736a5d9bb47d7b63af0ac5505edb10db9
Munir Abdinur <munir_abdinur@hotmail.com>
2024-11-04 12:43:30 -0500

    add feature

--- utils/_features.py
+++ utils/_features.py
@@ -2426,3 +2426,13 @@ class features:
         """
         pytest.mark.features(feature_id=328)(test_object)
         return test_object
+
+    @staticmethod
+    def parametric_endpoint_parity(test_object):
+        """
+        Enforces standardized behaviors for configurations across the tracing libraries.
+
+        https://feature-parity.us1.prod.dog/#/?feature=329
+        """
+        pytest.mark.features(feature_id=329)(test_object)
+        return test_object
