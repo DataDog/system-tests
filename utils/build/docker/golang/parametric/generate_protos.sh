44281005e9d2ddec680f31b2813eb90af831c0fc
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2024-11-05 10:51:41 +0100

    Remove dead gRPC code in parametric tests (#3375)

--- utils/build/docker/golang/parametric/generate_protos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate the protobuf source files for the grpc client
-# Requires protoc to be installed on the host.
-protoc -I=../../../../../utils/parametric/protos --go_out=. --go_opt=Mprotos/apm_test_client.proto=. --go-grpc_out=. --go-grpc_opt=Mprotos/apm_test_client.proto=. ../../../../../utils/parametric/protos/apm_test_client.proto
-# FIXME: the generated *.pb.go are given a package of __, so rewrite this to the main package
-sed -i '' -e 's/package __/package main/g' ./*.pb.go
