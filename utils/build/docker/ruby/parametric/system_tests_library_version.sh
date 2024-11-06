d9f741555ded3753e09245a5e4c310278235b8d4
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2024-11-06 11:59:10 +0100

    Add -dev on dev version for ruby in parametric scenario (#3387)

--- utils/build/docker/ruby/parametric/system_tests_library_version.sh
+++ utils/build/docker/ruby/parametric/system_tests_library_version.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ruby -rbundler/setup -e 'puts Gem::Specification.find_all.find {|x| x.name == "ddtrace" || x.name == "datadog"}.version'
+ruby -rbundler/setup <<EOF
+    gemspec = Gem.loaded_specs['datadog'] || Gem.loaded_specs['ddtrace']
+    version = gemspec.version.to_s
+    version = "#{version}-dev" unless gemspec.source.is_a?(Bundler::Source::Rubygems)
+    puts version
+EOF
