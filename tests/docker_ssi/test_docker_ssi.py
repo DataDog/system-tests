e7ee5eb34d8592c513caf58102fe561038c37491
iamluc <luc.vieillescazes@datadoghq.com>
2024-11-07 17:05:18 +0100

    Enable docker_ssi telemetry test for Python, Ruby and Java

--- tests/docker_ssi/test_docker_ssi.py
+++ tests/docker_ssi/test_docker_ssi.py
@@ -65,9 +65,6 @@ class TestDockerSSIFeatures:
         condition="centos-7" in context.scenario.weblog_variant and context.scenario.library.library == "java",
         reason="APMON-1490",
     )
-    @missing_feature(library="java", reason="INPLAT-11")
-    @missing_feature(library="python", reason="INPLAT-11")
-    @missing_feature(library="ruby", reason="INPLAT-11")
     def test_telemetry(self):
         # There is telemetry data about the auto instrumentation injector. We only validate there is data
         telemetry_autoinject_data = interfaces.test_agent.get_telemetry_for_autoinject()
