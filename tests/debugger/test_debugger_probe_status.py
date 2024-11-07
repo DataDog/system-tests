2cf2c618c2a5e52e68ae204a43ae1de679de6a8e
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2024-11-07 14:04:49 +0000

    Revert "[python] Skip bugs for 2.16.0 release (#3385)"
    This reverts commit ba85dfebdab3ad346cdd4889f765040f71e067e8.

--- tests/debugger/test_debugger_probe_status.py
+++ tests/debugger/test_debugger_probe_status.py
@@ -4,7 +4,7 @@
 
 import tests.debugger.utils as base
 
-from utils import scenarios, features, remote_config as rc, bug, context
+from utils import scenarios, features, remote_config as rc
 
 
 @features.debugger
@@ -27,7 +27,6 @@ class Test_Debugger_Probe_Statuses(base._Base_Debugger_Test):
 
         self._setup(probes)
 
-    @bug(context.library == "python@2.16.0", reason="APMRP-360")
     def test_probe_status_log(self):
         self._assert()
 
@@ -37,7 +36,6 @@ class Test_Debugger_Probe_Statuses(base._Base_Debugger_Test):
 
         self._setup(probes)
 
-    @bug(context.library == "python@2.16.0", reason="APMRP-360")
     def test_probe_status_metric(self):
         self._assert()
 
@@ -56,7 +54,6 @@ class Test_Debugger_Probe_Statuses(base._Base_Debugger_Test):
 
         self._setup(probes)
 
-    @bug(context.library == "python@2.16.0", reason="APMRP-360")
     def test_probe_status_spandecoration(self):
         self._assert()
 
