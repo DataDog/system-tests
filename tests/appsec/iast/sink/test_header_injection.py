0be2d3b2e3d39b09760547b4c46dc5076b2120f9
CarlesDD <carles.capell@datadoghq.com>
2024-11-11 08:34:46 +0100

    Move reflected header exclusion base test to header injection file

--- tests/appsec/iast/sink/test_header_injection.py
+++ tests/appsec/iast/sink/test_header_injection.py
@@ -2,8 +2,48 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import context, features, missing_feature
-from ..utils import BaseSinkTest, BaseTestHeaderInjectionReflectedExclusion
+from utils import context, features, missing_feature, weblog
+from ..utils import BaseSinkTest, assert_iast_vulnerability
+
+
+class BaseTestHeaderInjectionReflectedExclusion:
+    origin_header: None
+    reflected_header: None
+    headers: None
+
+    exclusion_request: None
+    no_exclusion_request: None
+
+    def setup_no_exclusion(self):
+        assert self.origin_header is not None, f"Please set {self}.origin_header"
+        assert isinstance(self.origin_header, str), f"Please set {self}.origin_header"
+        assert self.reflected_header is not None, f"Please set {self}.reflected_header"
+        assert isinstance(self.reflected_header, str), f"Please set {self}.reflected_header"
+
+        self.no_exclusion_request = weblog.get(
+            path="/iast/header_injection/reflected/no-exclusion",
+            params={"origin": self.origin_header, "reflected": self.reflected_header},
+        )
+
+    def test_no_exclusion(self):
+        assert_iast_vulnerability(
+            request=self.no_exclusion_request, vulnerability_count=1, vulnerability_type="HEADER_INJECTION",
+        )
+
+    def setup_exclusion(self):
+        assert self.origin_header is not None, f"Please set {self}.origin_header"
+        assert isinstance(self.origin_header, str), f"Please set {self}.origin_header"
+        assert self.reflected_header is not None, f"Please set {self}.reflected_header"
+        assert isinstance(self.reflected_header, str), f"Please set {self}.reflected_header"
+
+        self.exclusion_request = weblog.get(
+            path="/iast/header_injection/reflected/exclusion",
+            params={"origin": self.origin_header, "reflected": self.reflected_header},
+            headers=self.headers,
+        )
+
+    def test_exclusion(self):
+        BaseSinkTest.assert_no_iast_event(self.exclusion_request)
 
 
 @features.iast_sink_header_injection
