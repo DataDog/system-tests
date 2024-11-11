1ce62ff339ee6ae37daea7f84c866d69ef329a4c
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2024-11-11 12:05:40 -0800

    Update utils/interfaces/_test_agent.py
    Co-authored-by: Brett Langdon <brett.langdon@datadoghq.com>

--- utils/interfaces/_test_agent.py
+++ utils/interfaces/_test_agent.py
@@ -59,13 +59,7 @@ class _TestAgentInterfaceValidator(InterfaceValidator):
     def get_crashlog_for_runtime(self, runtime_id):
         logger.debug(f"Try to find a crashlog related to runtime-id {runtime_id}")
         assert runtime_id is not None, "Runtime ID not found"
-        telemetry_msgs = []
-        for data_received in self._data_telemetry_list:
-            print(data_received)
-            if data_received["request_type"] == "logs" and data_received["runtime_id"] == runtime_id:
-                telemetry_msgs.append(data_received)
-
-        return telemetry_msgs
+        return [l for l in self.get_telemetry_logs() if l["runtime_id"] == runtime_id]
 
     def get_telemetry_for_autoinject(self):
         logger.debug("Try to find telemetry data related to autoinject")
