6ceb6352f198bfb32cf9d70b1b5fff3d805f25db
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-11-13 17:06:54 +0100

    Try not dying immediately

--- lib-injection/build/docker/nodejs/sample-app-node13/child.js
+++ lib-injection/build/docker/nodejs/sample-app-node13/child.js
@@ -1,1 +1,9 @@
-process.kill(process.pid, 'SIGSEGV');
+console.log('Child process started');
+
+setTimeout(() => {
+  console.log('Child process exiting after 5 seconds');
+  // process.exit(0); // Exit the child process gracefully
+  process.kill(process.pid, 'SIGSEGV');
+}, 5000);
+
+// process.kill(process.pid, 'SIGSEGV');
