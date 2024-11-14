34ac1556d187a6d9641ec0f4522b505c0fcd0c58
Emmett Butler <emmett.butler321@gmail.com>
2024-11-14 05:47:58 -0800

    set pythonpath

--- utils/build/virtual_machine/weblogs/python/test-app-python-27/test-app-python_run.sh
+++ utils/build/virtual_machine/weblogs/python/test-app-python-27/test-app-python_run.sh
@@ -10,7 +10,7 @@ sudo cp django_app.py /home/datadog/
 sudo /home/datadog/.pyenv/shims/pip install django
 echo "Testing weblog with python version:"
 sudo /home/datadog/.pyenv/shims/python --version
-./create_and_run_app_service.sh "/home/datadog/.pyenv/shims/python -m django runserver 0.0.0.0:5985" "PYTHONUNBUFFERED=1 DJANGO_SETTINGS_MODULE=django_app"
+./create_and_run_app_service.sh "/home/datadog/.pyenv/shims/python -m django runserver 0.0.0.0:5985" "PYTHONPATH=/home/datadog/:$PYTHONPATH PYTHONUNBUFFERED=1 DJANGO_SETTINGS_MODULE=django_app"
 echo "RUN AFTER THE SERVICE"
 cat test-app.service
 echo "RUN python DONE"
