d3758816092b61cebbdb8cad99b749440d9b2df7
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-11-15 15:58:58 +0100

    Bigger timeout?

--- utils/onboarding/weblog_interface.py
+++ utils/onboarding/weblog_interface.py
@@ -95,4 +95,4 @@ def fork_and_crash(virtual_machine) -> str:
     vm_port = virtual_machine.deffault_open_port
     url = f"http://{vm_ip}:{vm_port}/fork_and_crash"
     # The timeout is high because coredump is triggered in some configs and takes a long time to complete
-    return requests.get(url, timeout=300).text
+    return requests.get(url, timeout=600).text
