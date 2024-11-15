e82b67e3caa5f918888b235b65572a75400e9515
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-11-15 14:55:26 -0500

    install java but leave PHP process

--- utils/build/ssi/base/php_install_runtimes.sh
+++ utils/build/ssi/base/php_install_runtimes.sh
@@ -40,3 +40,14 @@ else
     echo "Unknown OS"
     exit 1
 fi
+
+curl -s "https://get.sdkman.io" | bash
+# shellcheck source=/dev/null
+source "/root/.sdkman/bin/sdkman-init.sh"
+sed -i -e 's/sdkman_auto_answer=false/sdkman_auto_answer=true/g' /root/.sdkman/etc/config
+
+sdk install java "11.0.24-zulu"
+
+
+ln -s "${SDKMAN_DIR}/candidates/java/current/bin/java" /usr/bin/java
+ln -s "${SDKMAN_DIR}/candidates/java/current/bin/javac" /usr/bin/javac
