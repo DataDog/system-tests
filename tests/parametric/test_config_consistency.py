cec165b3b90ed9f4fb0b8ac30c330c81d9c6a31f
Munir Abdinur <munir_abdinur@hotmail.com>
2024-11-19 10:18:22 -0500

    test against 2.8 (not 2.7)

--- tests/parametric/test_config_consistency.py
+++ tests/parametric/test_config_consistency.py
@@ -89,7 +89,7 @@ class Test_Config_UnifiedServiceTagging:
 
     # Assert that iff a span has service name set by DD_SERVICE, it also gets the version specified in DD_VERSION
     @parametrize("library_env", [{"DD_SERVICE": "version_test", "DD_VERSION": "5.2.0"}])
-    @missing_feature(context.library < "ruby@2.7.0.dev")
+    @missing_feature(context.library < "ruby@2.8.0.dev")
     def test_specific_version(self, library_env, test_agent, test_library):
         with test_library:
             with test_library.start_span(name="s1") as s1:
