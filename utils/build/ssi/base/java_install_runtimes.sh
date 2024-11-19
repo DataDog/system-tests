d86c23101c95ca600bdb5afd507a3f1687e99283
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-11-19 12:09:58 -0500

    Lint

--- utils/build/ssi/base/java_install_runtimes.sh
+++ utils/build/ssi/base/java_install_runtimes.sh
@@ -4,7 +4,7 @@ set -e
 
 declare -r RUNTIME_VERSIONS="$1"
 
-sleep $((1 + $RANDOM % 20)) # Sleep a random 1-20 seconds to avoid sdkman rate limits
+sleep $((1 + RANDOM % 20)) # Sleep a random 1-20 seconds to avoid sdkman rate limits
 
 curl -s "https://get.sdkman.io" | bash
 # shellcheck source=/dev/null
@@ -12,7 +12,7 @@ source "/root/.sdkman/bin/sdkman-init.sh"
 sed -i -e 's/sdkman_auto_answer=false/sdkman_auto_answer=true/g' /root/.sdkman/etc/config
 
 for VERSION in $(echo "$RUNTIME_VERSIONS" | tr ',' ' '); do
-    sleep $((1 + $RANDOM % 10)) # Sleep a random 1-10 seconds to avoid sdkman rate limits
+    sleep $((1 + RANDOM % 10)) # Sleep a random 1-10 seconds to avoid sdkman rate limits
     sdk install java "$VERSION"
 done
 
