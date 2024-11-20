002687fdf79e31bc3415a07f2dba41ef1f883929
Andrew Lock <andrew.lock@datadoghq.com>
2024-11-20 15:59:16 +0000

    Switch to 2.1

--- lib-injection/build/docker/dotnet/dd-lib-dotnet-init-test-app-unsupported/Program.cs
+++ lib-injection/build/docker/dotnet/dd-lib-dotnet-init-test-app-unsupported/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MinimalWebApp
 {
@@ -15,24 +9,13 @@ namespace MinimalWebApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            WebHost.CreateDefaultBuilder(args)
+                .Configure(app =>
                 {
-                    webBuilder.Configure((IApplicationBuilder app) =>
-                    {
-                        app.UseRouting();
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapGet("/", async context =>
-                            {
-                                await context.Response.WriteAsync("Hello World!");
-                            });
-                        });
-                    });
-                });
+                    app.Run(async context => await context.Response.WriteAsync("Hello World!"));
+                })
+                .Build()
+                .Run();
+        }
     }
 }
--- utils/build/virtual_machine/weblogs/dotnet/test-app-dotnet-unsupported/test-app-dotnet-unsupported_run.sh
+++ utils/build/virtual_machine/weblogs/dotnet/test-app-dotnet-unsupported/test-app-dotnet-unsupported_run.sh
@@ -12,9 +12,6 @@ export DOTNET_DbgMiniDumpType=4
 export DOTNET_CreateDumpDiagnostics=1
 export DOTNET_DbgMiniDumpName=/var/log/datadog/dotnet/coredump.txt
 
-#We are running the app for dotnet 6.0
-sudo sed -i "s/net7.0/net6.0/g" MinimalWebApp.csproj 
-
 #workaround. Remove the system-tests cloned folder. The sources are copied to current home folder
 #if we don't remove it, the dotnet restore will try to restore the system-tests folder
 sudo rm -rf system-tests
