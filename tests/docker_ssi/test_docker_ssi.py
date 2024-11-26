d61afab3b730be2ef942fe57a75999ea82e1094e
Brett Langdon <brett.langdon@datadoghq.com>
2024-11-25 19:57:33 -0500

    [nodejs] add nodejs docker_ssi test app (#3530)
    Co-authored-by: Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>

--- tests/docker_ssi/test_docker_ssi.py
+++ tests/docker_ssi/test_docker_ssi.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
 
-from utils import scenarios, features, context, irrelevant, bug, interfaces, missing_feature
+from utils import scenarios, features, context, irrelevant, bug, interfaces
 from utils import weblog
 from utils.tools import logger, get_rid_from_request
 
@@ -34,6 +34,7 @@ class TestDockerSSIFeatures:
     @bug(condition=context.library == "python", reason="INPLAT-11")
     @irrelevant(context.library == "java" and context.installed_language_runtime < "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime < "7.0")
+    @irrelevant(context.library == "nodejs" and context.installed_language_runtime < "17.0")
     def test_install_supported_runtime(self):
         logger.info(f"Testing Docker SSI installation on supported lang runtime: {context.scenario.library.library}")
         assert self.r.status_code == 200, f"Failed to get response from {context.scenario.weblog_url}"
@@ -69,6 +70,7 @@ class TestDockerSSIFeatures:
     @irrelevant(context.library == "java" and context.installed_language_runtime < "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime < "7.0")
     @irrelevant(context.library == "python" and context.installed_language_runtime < "3.7.0")
+    @irrelevant(context.library == "nodejs" and context.installed_language_runtime < "17.0")
     def test_telemetry(self):
         # There is telemetry data about the auto instrumentation injector. We only validate there is data
         telemetry_autoinject_data = interfaces.test_agent.get_telemetry_for_autoinject()
@@ -94,6 +96,7 @@ class TestDockerSSIFeatures:
     @irrelevant(context.library == "java" and context.installed_language_runtime >= "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime >= "7.0")
     @irrelevant(context.library == "python" and context.installed_language_runtime >= "3.7.0")
+    @irrelevant(context.library == "nodejs" and context.installed_language_runtime >= "17.0")
     def test_telemetry_abort(self):
         # There is telemetry data about the auto instrumentation injector. We only validate there is data
         telemetry_autoinject_data = interfaces.test_agent.get_telemetry_for_autoinject()
