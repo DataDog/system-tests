01d3a8af4cae87a28b9473acfbe18fdf8a60054d
Igor Unanua <igor.unanua@datadoghq.com>
2024-11-26 10:24:04 +0100

    [nodejs] Enable test_telemetry_sca_enabled_propagated for nextjs (#3541)

--- tests/appsec/test_asm_standalone.py
+++ tests/appsec/test_asm_standalone.py
@@ -3,7 +3,7 @@ import json
 from requests.structures import CaseInsensitiveDict
 
 from utils.telemetry_utils import TelemetryUtils
-from utils import context, weblog, interfaces, scenarios, features, rfc, bug, flaky
+from utils import context, weblog, interfaces, scenarios, features, rfc, bug, flaky, missing_feature
 
 
 class AsmStandalone_UpstreamPropagation_Base:
@@ -711,6 +711,7 @@ class Test_SCAStandalone_Telemetry:
     def setup_app_dependencies_loaded(self):
         self.r = weblog.get("/load_dependency")
 
+    @missing_feature(context.library == "nodejs" and context.weblog_variant == "nextjs")
     def test_app_dependencies_loaded(self):
         self.assert_standalone_is_enabled(self.r)
 
