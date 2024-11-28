e4f2ec60c1db5c7f8aea30280fdf8b6620b5316b
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2024-11-28 10:43:35 +0100

    security control tests for java springboot

--- utils/build/docker/java/spring-boot/src/main/java/com/datadoghq/system_tests/springboot/AppSecIast.java
+++ utils/build/docker/java/spring-boot/src/main/java/com/datadoghq/system_tests/springboot/AppSecIast.java
@@ -380,6 +380,78 @@ public class AppSecIast {
         return "ok";
     }
 
+    @PostMapping("/sc/s/xss")
+    void scSanitizeXSS(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String sanitized = SecurityControlUtil.sanitize(request.getParameter("param"));
+        xssExamples.insecureXSS(response.getWriter(), sanitized);
+    }
+
+    @PostMapping("/sc/s/sqli")
+    Object scSanitizeSqli(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String sanitized = SecurityControlUtil.sanitize(request.getParameter("param"));
+        return sqlExamples.insecureSql(sanitized, "password");
+    }
+
+    @PostMapping("/sc/s/all")
+    Object scSanitizeForAllVulns(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String sanitized = SecurityControlUtil.sanitizeForAllVulns(request.getParameter("param"));
+        return sqlExamples.insecureSql(sanitized, "password");
+    }
+
+    @PostMapping("/sc/iv/xss")
+    void scValidateXSS(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String param = request.getParameter("param");
+        if (SecurityControlUtil.validate(param)) {
+            xssExamples.insecureXSS(response.getWriter(), param);
+        }
+    }
+
+    @PostMapping("/sc/iv/sqli")
+    void scValidateSqli(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String param = request.getParameter("param");
+        if(SecurityControlUtil.validate(param)) {
+            sqlExamples.insecureSql(param, "password");
+        }
+    }
+
+    @PostMapping("/sc/iv/all")
+    void scValidateForAllVulns(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String param = request.getParameter("param");
+        if(SecurityControlUtil.validateForAllVulns(param)) {
+            sqlExamples.insecureSql(param, "password");
+        }
+    }
+
+    @PostMapping("/sc/iv/overloaded/secure")
+    void scIVOverloadedSecure(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("password");
+        if(SecurityControlUtil.overloadedValidation(null, user, pass)) {
+            sqlExamples.insecureSql(user, pass);
+        }
+    }
+
+    @PostMapping("/sc/iv/overloaded/insecure")
+    void scIVOverloadedInsecure(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("password");
+        if(SecurityControlUtil.overloadedValidation(user, pass)) {
+            sqlExamples.insecureSql(user, pass);
+        }
+    }
+
+    @PostMapping("/sc/s/overloaded/secure")
+    void scSOverloadedSecure(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String sanitized = SecurityControlUtil.overloadedSanitize(request.getParameter("param"));
+        xssExamples.insecureXSS(response.getWriter(), sanitized);
+    }
+
+    @PostMapping("/sc/s/overloaded/insecure")
+    void scSOverloadedInsecure(final ServletRequest request,  final ServletResponse response) throws IOException {
+        String sanitized = SecurityControlUtil.overloadedSanitize(request.getParameter("param"), null);
+        xssExamples.insecureXSS(response.getWriter(), sanitized);
+    }
+
 
     /**
      * TODO: Ldap is failing to startup in native image this method ensures it's started lazily
