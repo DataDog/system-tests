2f67f9356e947e46100041dd5be8a63f86389c42
Marco Costa <marco.costa@datadoghq.com>
2024-12-03 11:06:34 -0800

    Refactor as per feedback

--- tests/test_span_events.py
+++ tests/test_span_events.py
@@ -2,7 +2,7 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import context, interfaces, irrelevant, weblog, scenarios, features, rfc
+from utils import context, interfaces, irrelevant, weblog, scenarios, features, rfc, missing_feature
 
 
 @rfc("https://docs.google.com/document/d/1cVod_VI7Yruq8U9dfMRFJd7npDu-uBpste2IB04GyaQ")
@@ -10,18 +10,19 @@ from utils import context, interfaces, irrelevant, weblog, scenarios, features, 
 @scenarios.agent_supporting_span_events
 class Test_SpanEvents_WithAgentSupport:
     """
-    Test that tracers send natively serialized span events if the agent support it.
+    Test that tracers send natively serialized span events if the agent support and serialization format support it.
+    Tracers only need to test for their default serialization format (v0.4, v0.5, v0.7).
 
     Request the Weblog endpoint `/add_event`, which adds a span event (with any name and attributes values)
     to the request root span.
     """
 
-    def setup_v07(self):
+    def setup_v04_v07_default_format(self):
         self.r = weblog.get("/add_event")
 
-    @irrelevant(context.library in ["ruby"], reason="Does not support v0.7")
-    def test_v07(self):
-        """The v0.7 format send events as a top-level `span_events` when the agent supports native serialization"""
+    @missing_feature(context.library in ["ruby"], reason="Native serialization not implemented")
+    def setup_v04_v07_default_format(self):
+        """For traces that default to the v0.4 or v0.7 format, send events as a top-level `span_events` field"""
         interfaces.library.assert_trace_exists(self.r)
 
         span = self._get_span(self.r)
@@ -30,26 +31,15 @@ class Test_SpanEvents_WithAgentSupport:
         assert "span_events" in span
         assert "events" not in meta
 
-    def setup_v04(self):
+    def setup_v05_default_format(self):
         self.r = weblog.get("/add_event")
 
-    @irrelevant(context.library in ["ruby"], reason="Native serialization not supported")
-    def test_v04(self):
-        """The v0.4 format send events as a top-level `span_events` when the agent supports native serialization"""
-        interfaces.library.assert_trace_exists(self.r)
-
-        span = self._get_span(self.r)
-        meta = self._get_root_span_meta(self.r)
-
-        assert "span_events" in span
-        assert "events" not in meta
-
-    def setup_v05(self):
-        self.r = weblog.get("/add_event")
-
-    @irrelevant(context.library in ["ruby"], reason="Does not support v0.5")
-    def test_v05(self):
-        """The v0.5 format continues to send events as tags, as it does not support native serialization"""
+    @irrelevant(context.library in ["ruby"], reason="v0.5 is not the default format")
+    def test_v05_default_format(self):
+        """
+        For traces that default to the v0.5 format, send events as the span tag `events`
+        given this format does not support native serialization.
+        """
         interfaces.library.assert_trace_exists(self.r)
 
         span = self._get_span(self.r)
@@ -77,39 +67,11 @@ class Test_SpanEvents_WithoutAgentSupport:
     to the request root span.
     """
 
-    def setup_v07(self):
+    def setup_send_as_a_tag(self):
         self.r = weblog.get("/add_event")
 
-    @irrelevant(context.library in ["ruby"], reason="Does not support v0.7")
-    def test_v07(self):
-        """The v0.7 format send events as tags when the agent does not support native serialization"""
-        interfaces.library.assert_trace_exists(self.r)
-
-        span = self._get_span(self.r)
-        meta = self._get_root_span_meta(self.r)
-
-        assert "span_events" not in span
-        assert "events" in meta
-
-    def setup_v04(self):
-        self.r = weblog.get("/add_event")
-
-    def test_v04(self):
-        """The v0.4 format send events as tags when the agent does not support native serialization"""
-        interfaces.library.assert_trace_exists(self.r)
-
-        span = self._get_span(self.r)
-        meta = self._get_root_span_meta(self.r)
-
-        assert "span_events" not in span
-        assert "events" in meta
-
-    def setup_v05(self):
-        self.r = weblog.get("/add_event")
-
-    @irrelevant(context.library in ["ruby"], reason="Does not support v0.5")
-    def test_v05(self):
-        """The v0.5 format continues to send events as tags"""
+    def test_send_as_a_tag(self):
+        """Send span events as the tag `events` when the agent does not support native serialization"""
         interfaces.library.assert_trace_exists(self.r)
 
         span = self._get_span(self.r)
