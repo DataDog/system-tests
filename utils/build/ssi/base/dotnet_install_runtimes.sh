1b13b615b9d6f82395c476ac655731447f334e78
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-12-04 15:08:45 +0100

    Ok maybe this one is on me

--- utils/build/ssi/base/dotnet_install_runtimes.sh
+++ utils/build/ssi/base/dotnet_install_runtimes.sh
@@ -8,11 +8,3 @@ curl -sSL https://dot.net/v1/dotnet-install.sh --output dotnet-install.sh  \
     && ./dotnet-install.sh --version $DOTNET_VERSION --install-dir /usr/share/dotnet \
     && rm ./dotnet-install.sh \
     && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
-
-for VERSION in $(echo "$RUNTIME_VERSIONS" | tr ',' ' '); do
-    sleep $((1 + RANDOM % 10)) # Sleep a random 1-10 seconds to avoid sdkman rate limits
-    sdk install java "$VERSION"
-done
-
-ln -s "${SDKMAN_DIR}/candidates/java/current/bin/java" /usr/bin/java
-ln -s "${SDKMAN_DIR}/candidates/java/current/bin/javac" /usr/bin/javac
