71cb900050f8d737f0461b0eff81c0038f593adc
Zach Montoya <zach.montoya@datadoghq.com>
2024-12-04 09:01:53 -0800

    [weblog] Add test for Datadog distributed tracing headers where x-datadog parent-id is set to 0 (#3564)

--- tests/test_distributed.py
+++ tests/test_distributed.py
@@ -3,6 +3,7 @@
 # Copyright 2022 Datadog, Inc.
 
 import json
+from utils.parametric.spec.trace import SAMPLING_PRIORITY_KEY, ORIGIN
 from utils import weblog, interfaces, scenarios, features
 
 
@@ -25,3 +26,51 @@ class Test_DistributedHttp:
         assert "x-datadog-sampling-priority" not in data["request_headers"]
         assert "x-datadog-tags" not in data["request_headers"]
         assert "x-datadog-trace-id" not in data["request_headers"]
+
+
+@scenarios.default
+@features.datadog_headers_propagation
+class Test_Synthetics_APM_Datadog:
+    def setup_synthetics(self):
+        self.r = weblog.get(
+            "/",
+            headers={
+                "x-datadog-trace-id": "1234567890",
+                "x-datadog-parent-id": "0",
+                "x-datadog-sampling-priority": "1",
+                "x-datadog-origin": "synthetics",
+            },
+        )
+
+    def test_synthetics(self):
+        interfaces.library.assert_trace_exists(self.r)
+        spans = interfaces.agent.get_spans_list(self.r)
+        assert len(spans) == 1, "Agent received the incorrect amount of spans"
+
+        span = spans[0]
+        assert span.get("traceID") == "1234567890"
+        assert "parentID" not in span or span.get("parentID") == 0 or span.get("parentID") is None
+        assert span.get("meta")[ORIGIN] == "synthetics"
+        assert span.get("metrics")[SAMPLING_PRIORITY_KEY] == 1
+
+    def setup_synthetics_browser(self):
+        self.r = weblog.get(
+            "/",
+            headers={
+                "x-datadog-trace-id": "1234567891",
+                "x-datadog-parent-id": "0",
+                "x-datadog-sampling-priority": "1",
+                "x-datadog-origin": "synthetics-browser",
+            },
+        )
+
+    def test_synthetics_browser(self):
+        interfaces.library.assert_trace_exists(self.r)
+        spans = interfaces.agent.get_spans_list(self.r)
+        assert len(spans) == 1, "Agent received the incorrect amount of spans"
+
+        span = spans[0]
+        assert span.get("traceID") == "1234567891"
+        assert "parentID" not in span or span.get("parentID") == 0 or span.get("parentID") is None
+        assert span.get("meta")[ORIGIN] == "synthetics-browser"
+        assert span.get("metrics")[SAMPLING_PRIORITY_KEY] == 1
