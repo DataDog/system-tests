9044afa397077aeb04b2814df01131f9956c18c5
Kevin Gosse <kevin.gosse@datadoghq.com>
2024-12-05 14:58:23 +0100

    more pleasing the linter

--- utils/build/ssi/base/dotnet_install_runtimes.sh
+++ utils/build/ssi/base/dotnet_install_runtimes.sh
@@ -5,6 +5,6 @@ set -e
 
 curl -sSL https://dot.net/v1/dotnet-install.sh --output dotnet-install.sh  \
     && chmod +x ./dotnet-install.sh \
-    && ./dotnet-install.sh --version $DOTNET_VERSION --install-dir /usr/share/dotnet \
+    && ./dotnet-install.sh --version "$DOTNET_VERSION" --install-dir /usr/share/dotnet \
     && rm ./dotnet-install.sh \
     && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
