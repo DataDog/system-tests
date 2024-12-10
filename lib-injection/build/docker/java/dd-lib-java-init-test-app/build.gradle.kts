589f2d328ea5e7eee8d7fab0d96e42e0dff07c87
Laplie Anderson <laplie.anderson@datadoghq.com>
2024-12-10 14:28:28 -0500

    use authentication

--- lib-injection/build/docker/java/dd-lib-java-init-test-app/build.gradle.kts
+++ lib-injection/build/docker/java/dd-lib-java-init-test-app/build.gradle.kts
@@ -1,4 +1,18 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+import software.amazon.awssdk.services.ecr.EcrClient
+import software.amazon.awssdk.services.ecr.model.AuthorizationData
+import java.util.Base64
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(platform("software.amazon.awssdk:bom:2.20.121"))
+        classpath("software.amazon.awssdk:ecr")
+        classpath("software.amazon.awssdk:sts") // sts is required to use roleArn in aws profiles
+    }
+}
 
 plugins {
     id("org.springframework.boot") version "2.7.2"
@@ -34,6 +48,32 @@ val dockerImageTag: String by project
 tasks.named<BootBuildImage>("bootBuildImage") {
     imageName = "${resolvedDockerImageRepo}:${dockerImageTag}"
 
-    builder = "669783387624.dkr.ecr.us-east-1.amazonaws.com/dockerhub/paketobuildpacks/builder-jammy-java-tiny:0.0.11"
-    runImage = "669783387624.dkr.ecr.us-east-1.amazonaws.com/dockerhub/paketobuildpacks/run-jammy-tiny:0.2.55"
+    if (System.getenv("DD_VM_NAME") == null) {
+        builder = "paketobuildpacks/builder-jammy-java-tiny:0.0.11"
+        runImage = "paketobuildpacks/run-jammy-tiny:0.2.55"
+    } else {
+        // Use dockerhub mirror
+        builder = "669783387624.dkr.ecr.us-east-1.amazonaws.com/dockerhub/paketobuildpacks/builder-jammy-java-tiny:0.0.11"
+        runImage = "669783387624.dkr.ecr.us-east-1.amazonaws.com/dockerhub/paketobuildpacks/run-jammy-tiny:0.2.55"
+
+        // Setup authentication
+        val authData: Provider<AuthorizationData> = providers.provider {
+            val ecrClient = EcrClient.builder().build()
+            val authorizationData = ecrClient
+                    .getAuthorizationToken()
+                    .authorizationData()[0]
+            return@provider authorizationData
+        }
+
+        val decodedEcrToken: Provider<String> = authData.map { String(Base64.getDecoder().decode(it.authorizationToken())) }
+        val registryUsername: Provider<String> = decodedEcrToken.map { it.split(":")[0] }
+        val registryPassword: Provider<String> = decodedEcrToken.map { it.split(":")[1] }
+
+        docker {
+            builderRegistry {
+                username.set(registryUsername)
+                password.set(registryPassword)
+            }
+        }
+    }
 }
