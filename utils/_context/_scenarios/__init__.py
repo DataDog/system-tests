e4bcb220c485f2572e2fe0ffd739aed4be603f9f
Alejandro González García <alejandro.gonzalez@datadoghq.com>
2024-12-16 20:41:48 +0100

    Disable IAST in appsec_runtime_activation scenario (#3692)
    Motivation
    Having IAST enabled can lead to appsec_runtime_activation scenario behaving incorrectly, masking actual issues.
    Example:
    When running tests/appsec/rasp/test_lfi.py and IAST enabled, the test passes incorrectly.
    This happens due to a limitation in the current Java tracer implementation, which doesn't support hot instrumentation for LFI exploit prevention.
    With IAST enabled, instrumentation for LFI prevention occurs at startup (shared with RASP), creating a false impression that the functionality works as expected when it does not.
    Jira ticket: APPSEC-55649

--- utils/_context/_scenarios/__init__.py
+++ utils/_context/_scenarios/__init__.py
@@ -242,6 +242,7 @@ class scenarios:
         "APPSEC_RUNTIME_ACTIVATION",
         rc_api_enabled=True,
         appsec_enabled=False,
+        iast_enabled=False,
         weblog_env={"DD_APPSEC_WAF_TIMEOUT": "10000000", "DD_APPSEC_TRACE_RATE_LIMIT": "10000"},  # 10 seconds
         doc="",
         scenario_groups=[ScenarioGroup.APPSEC, ScenarioGroup.APPSEC_RASP],
