8ceff1516bee096114334c2736d68b749641e801
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-12-18 10:58:38 +0100

    Lint

--- utils/build/docker/php/common/returnheaders.php
+++ utils/build/docker/php/common/returnheaders.php
@@ -1,11 +1,11 @@
 <?php
 
-function getHeadersFormatted() 
+function getHeadersFormatted()
 {
 	$headers = [];
-	foreach ($_SERVER as $name => $value) 
+	foreach ($_SERVER as $name => $value)
 	{
-		if (substr($name, 0, 5) == 'HTTP_') 
+		if (substr($name, 0, 5) == 'HTTP_')
 		{
 			$key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
 			$headers[$key] = $value;
