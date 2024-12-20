d67511b492046faea2c9f582ea2c992b5c717457
Flavien Darche <flavien.darche@datadoghq.com>
2024-12-20 15:49:45 +0100

    Duplication of base test classes + New scenario for only blocking (with json rule set)

--- tests/external_processing/test_apm.py
+++ tests/external_processing/test_apm.py
@@ -1,9 +1,8 @@
+from tests import test_scrubbing as base_scrubbing
+from tests import test_config_consistency as base_config_consistency
+from tests import test_standard_tags as base_standard_tags
+from tests import test_semantic_conventions as base_semantic_conventions
 from utils import weblog, interfaces, scenarios, features
-
-from tests.test_config_consistency import *
-from tests.test_standard_tags import *
-from tests.test_scrubbing import *
-from tests.test_semantic_conventions import *
 
 
 @features.not_reported  # it's just a POC. We'll need to figure out how we want to see results in FPD
@@ -23,33 +22,58 @@ class Test_ExternalProcessing_Tracing:
             assert span["meta"]["http.host"] == "localhost:7777"
 
 
-CLASSES = [
-    (Test_UrlQuery, []),
-    (Test_EnvVar, []),
-    (Test_Config_UnifiedServiceTagging_CustomService, []),
-    (Test_StandardTagsMethod, []),
-    (Test_StandardTagsUrl, []),
-    (Test_StandardTagsUserAgent, []),
-    (Test_StandardTagsClientIp, []),
-    (
-        Test_Meta,
-        ["test_meta_component_tag"],
-    ),  # The meta component tag would always be http.request and not external-processing
-    (Test_MetricsStandardTags, []),
-]
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_UrlQuery(base_scrubbing.Test_UrlQuery):
+    pass
 
-for cls, methods in CLASSES:
 
-    @features.not_reported
-    @scenarios.external_processing
-    class Test_ExternalProcessing_APM_(cls):
-        pytestmark = []
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_EnvVar(base_scrubbing.Test_EnvVar):
+    pass
 
-    for method in methods:
-        setattr(Test_ExternalProcessing_APM_, method, lambda self: None)
-        getattr(Test_ExternalProcessing_APM_, method).__test__ = False
 
-    globals()[f"Test_ExternalProcessing_APM_{cls.__name__}"] = Test_ExternalProcessing_APM_
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_Config_UnifiedServiceTagging_CustomService(
+    base_config_consistency.Test_Config_UnifiedServiceTagging_CustomService
+):
+    pass
 
-if len(CLASSES) > 0:
-    del Test_ExternalProcessing_APM_
+
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_StandardTagsMethod(base_standard_tags.Test_StandardTagsMethod):
+    pass
+
+
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_StandardTagsUrl(base_standard_tags.Test_StandardTagsUrl):
+    pass
+
+
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_StandardTagsUserAgent(base_standard_tags.Test_StandardTagsUserAgent):
+    pass
+
+
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_StandardTagsClientIp(base_standard_tags.Test_StandardTagsClientIp):
+    pass
+
+
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_Meta(base_semantic_conventions.Test_Meta):
+    def test_meta_component_tag(self):
+        pass
+
+
+@features.not_reported
+@scenarios.external_processing
+class Test_ExternalProcessing_APM_MetricsStandardTags(base_semantic_conventions.Test_MetricsStandardTags):
+    pass
