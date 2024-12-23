8e1dd2b11aacee85d0e073090b8614dcfebcb189
Flavien Darche <flavien.darche@datadoghq.com>
2024-12-23 12:52:28 +0100

    Apply comments

--- tests/external_processing/test_asm_blocking.py
+++ tests/external_processing/test_asm_blocking.py
@@ -1,4 +1,4 @@
-from utils import scenarios, features, irrelevant
+from utils import scenarios, features, missing_feature
 from tests.appsec import test_request_blocking as base_request_blocking
 from tests.appsec import test_blocking_addresses as base_blocking_addresses
 
@@ -18,7 +18,7 @@ class Test_ExternalProcessing_ASM_Blocking_client_ip(base_blocking_addresses.Tes
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_request_method(base_blocking_addresses.Test_Blocking_request_method):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking_before(self):
         pass
 
@@ -26,7 +26,7 @@ class Test_ExternalProcessing_ASM_Blocking_request_method(base_blocking_addresse
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_request_uri(base_blocking_addresses.Test_Blocking_request_uri):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking_before(self):
         pass
 
@@ -36,11 +36,11 @@ class Test_ExternalProcessing_ASM_Blocking_request_uri(base_blocking_addresses.T
 class Test_ExternalProcessing_ASM_Blocking_request_path_params(
     base_blocking_addresses.Test_Blocking_request_path_params
 ):
-    @irrelevant(True, reason="The endpoint /param is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /param is not implemented in the weblog")
     def test_blocking(self):
         pass
 
-    @irrelevant(True, reason="The endpoint /param is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /param is not implemented in the weblog")
     def test_blocking_before(self):
         pass
 
@@ -48,7 +48,7 @@ class Test_ExternalProcessing_ASM_Blocking_request_path_params(
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_request_query(base_blocking_addresses.Test_Blocking_request_query):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking_before(self):
         pass
 
@@ -56,7 +56,7 @@ class Test_ExternalProcessing_ASM_Blocking_request_query(base_blocking_addresses
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_request_headers(base_blocking_addresses.Test_Blocking_request_headers):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking_before(self):
         pass
 
@@ -64,7 +64,7 @@ class Test_ExternalProcessing_ASM_Blocking_request_headers(base_blocking_address
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_request_cookies(base_blocking_addresses.Test_Blocking_request_cookies):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking_before(self):
         pass
 
@@ -72,15 +72,15 @@ class Test_ExternalProcessing_ASM_Blocking_request_cookies(base_blocking_address
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_response_status(base_blocking_addresses.Test_Blocking_response_status):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking(self):
         pass
 
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_non_blocking(self):
         pass
 
-    @irrelevant(True, reason="The endpoint /finger_print is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /finger_print is not implemented in the weblog")
     def test_not_found(self):
         pass
 
@@ -88,10 +88,10 @@ class Test_ExternalProcessing_ASM_Blocking_response_status(base_blocking_address
 @features.not_reported
 @scenarios.external_processing_blocking
 class Test_ExternalProcessing_ASM_Blocking_response_headers(base_blocking_addresses.Test_Blocking_response_headers):
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_blocking(self):
         pass
 
-    @irrelevant(True, reason="The endpoint /tag_value is not implemented in the weblog")
+    @missing_feature(True, reason="The endpoint /tag_value is not implemented in the weblog")
     def test_non_blocking(self):
         pass
