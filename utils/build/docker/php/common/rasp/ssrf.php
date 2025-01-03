f74db527d55312c6c7066ff4d9bb0e29459f99aa
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-01-03 18:03:37 +0100

    Enable SSRF tests for PHP

--- utils/build/docker/php/common/rasp/ssrf.php
+++ utils/build/docker/php/common/rasp/ssrf.php
@@ -1,1 +1,18 @@
-<?php echo "Hello, SSRF!";
+<?php
+
+$domain = null;
+$contentType = $_SERVER['CONTENT_TYPE'] ?? "";
+switch ($contentType) {
+	case 'application/json':
+		$body = file_get_contents("php://input");
+		$decoded = json_decode($body, 1);
+		$domain = $decoded["domain"];
+		break;
+	default:
+		$domain = urldecode($_REQUEST["domain"]);
+		break;
+}
+
+file_get_contents($domain);
+
+echo "Hello, SSRF!";
