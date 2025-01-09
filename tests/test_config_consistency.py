a7d1d9213dbfa28a878bcba24f2364fae5ef07a5
Ayan Khan <khanayan_123@hotmail.com>
2025-01-09 15:19:45 -0500

    run lint

--- tests/test_config_consistency.py
+++ tests/test_config_consistency.py
@@ -476,6 +476,7 @@ class Test_Config_LogInjection_128Bit_TradeId_Default:
         trace_id = dd.get("trace_id")
         assert re.match(r"^[0-9a-f]{32}$", trace_id), f"Invalid 128-bit trace_id: {trace_id}"
 
+
 @rfc("https://docs.google.com/document/d/1kI-gTAKghfcwI7YzKhqRv2ExUstcHqADIWA4-TZ387o/edit#heading=h.8v16cioi7qxp")
 @scenarios.tracing_config_nondefault_3
 @features.tracing_configuration_consistency
@@ -518,6 +519,7 @@ class Test_Config_RuntimeMetrics_Default:
         data = list(interfaces.library.get_data("/dogstatsd/v2/proxy"))
         assert len(data) == 0
 
+
 def parse_log_injection_message(log_message):
     for data in stdout.get_data():
         json_string = json.dumps(data)
@@ -527,9 +529,6 @@ def parse_log_injection_message(log_message):
             message = json.loads(parsed_data.get("message"))
         except json.JSONDecodeError:
             continue
-        if (
-            message.get("dd")
-            and message.get(log_injection_fields[context.library.library]["message"]) == log_message
-        ):
+        if message.get("dd") and message.get(log_injection_fields[context.library.library]["message"]) == log_message:
             dd = message.get("dd")
             return dd
