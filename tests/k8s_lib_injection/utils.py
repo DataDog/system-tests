e0312abaf141e2f8ec99a11a0215c2ac13749c2b
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2025-01-10 08:12:21 +0100

    K8s tests: revamp (#3725)

--- tests/k8s_lib_injection/utils.py
+++ tests/k8s_lib_injection/utils.py
@@ -4,12 +4,12 @@ import requests
 from utils.tools import logger
 
 
-def get_dev_agent_traces(k8s_kind_cluster, retry=10):
+def get_dev_agent_traces(k8s_cluster_info, retry=10):
     """get_dev_agent_traces fetches traces from the dev agent running in the k8s cluster."""
     for _ in range(retry):
         logger.info(f"[Check traces] Checking traces:")
         response = requests.get(
-            f"http://{k8s_kind_cluster.cluster_host_name}:{k8s_kind_cluster.get_agent_port()}/test/traces"
+            f"http://{k8s_cluster_info.cluster_host_name}:{k8s_cluster_info.get_agent_port()}/test/traces"
         )
         traces_json = response.json()
         if len(traces_json) > 0:
