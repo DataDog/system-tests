e34a272e61b07fbe361794e29afb9b3ccfcbaa66
Thomas Watson <w@tson.dk>
2025-01-13 10:18:22 +0100

    [nodejs] Enable DI PII and probe status tests

--- tests/debugger/test_debugger_pii.py
+++ tests/debugger/test_debugger_pii.py
@@ -211,6 +211,7 @@ class Test_Debugger_PII_Redaction(debugger._Base_Debugger_Test):
     @bug(context.library == "python@2.16.0", reason="DEBUG-3127")
     @bug(context.library == "python@2.16.1", reason="DEBUG-3127")
     @missing_feature(context.library == "ruby", reason="Local variable capture not implemented for method probes")
+    @missing_feature(context.library == "nodejs", reason="Not yet implemented")
     def test_pii_redaction_method_full(self):
         self._assert(REDACTED_KEYS, REDACTED_TYPES)
 
