278cd0ef169368bd1ed3c260310642bb81601a9c
Federico Mon <federico.mon@datadoghq.com>
2025-01-16 13:54:02 +0100

    add second flaky marker

--- tests/appsec/iast/source/test_parameter_name.py
+++ tests/appsec/iast/source/test_parameter_name.py
@@ -27,6 +27,7 @@ class TestParameterName(BaseSourceTest):
     )
     @bug(weblog_variant="resteasy-netty3", reason="APPSEC-55687")
     @missing_feature(library="dotnet", reason="Tainted as request body")
+    @flaky(context.weblog_variant == "python3.12", reason="APPSEC-56375")
     def test_source_post_reported(self):
         """for use case where only one is reported, we want to keep a test on the one reported"""
         self.validate_request_reported(self.requests["POST"])
