8dc09b102831317b3cc0f14af2fedc632af80e15
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2025-01-23 13:11:16 +0100

    Linter

--- tests/appsec/test_asm_standalone.py
+++ tests/appsec/test_asm_standalone.py
@@ -6,6 +6,7 @@ from requests.structures import CaseInsensitiveDict
 from utils.telemetry_utils import TelemetryUtils
 from utils import context, weblog, interfaces, scenarios, features, rfc, bug, flaky, missing_feature
 
+
 class AsmStandalone_UpstreamPropagation_Base(ABC):
     """APM correctly propagates AppSec events in distributing tracing."""
 
