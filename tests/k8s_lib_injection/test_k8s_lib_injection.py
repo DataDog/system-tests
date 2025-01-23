10a1630e3a0d8dee2fda42a40f9c5711744de69b
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2025-01-23 17:45:57 +0100

    SSI: Add bug markers (#3883)

--- tests/k8s_lib_injection/test_k8s_lib_injection.py
+++ tests/k8s_lib_injection/test_k8s_lib_injection.py
@@ -1,4 +1,4 @@
-from utils import scenarios, features, context
+from utils import scenarios, features, context, bug
 from tests.k8s_lib_injection.utils import get_dev_agent_traces
 from utils.tools import logger
 from utils.onboarding.weblog_interface import make_get_request, warmup_weblog
@@ -6,51 +6,26 @@ from utils.onboarding.backend_interface import wait_backend_trace_id
 from utils.onboarding.wait_for_tcp_port import wait_for_port
 
 
-class _TestK8sLibInjection:
+@features.k8s_admission_controller
+@scenarios.k8s_lib_injection
+@scenarios.k8s_lib_injection_uds
+@scenarios.k8s_lib_injection_no_ac
+@scenarios.k8s_lib_injection_no_ac_uds
+class TestK8sLibInjection:
     """Test K8s lib injection"""
 
+    @bug(context.library > "python@2.21.0-dev", reason="APMSP-1750")
     def test_k8s_lib_injection(self):
         traces_json = get_dev_agent_traces(context.scenario.k8s_cluster_provider.get_cluster_info())
         assert len(traces_json) > 0, "No traces found"
 
 
 @features.k8s_admission_controller
-@scenarios.k8s_lib_injection
-class TestK8sLibInjection(_TestK8sLibInjection):
-    """Test K8s lib injection with admission controller"""
-
-    pass
-
-
-@features.k8s_admission_controller
-@scenarios.k8s_lib_injection_uds
-class TestK8sLibInjectionUDS(_TestK8sLibInjection):
-    """Test K8s lib injection with admission controller and UDS"""
-
-    pass
-
-
-@features.k8s_admission_controller
-@scenarios.k8s_lib_injection_no_ac
-class TestK8sLibInjectionNoAC(_TestK8sLibInjection):
-    """Test K8s lib injection without admission controller"""
-
-    pass
-
-
-@features.k8s_admission_controller
-@scenarios.k8s_lib_injection_no_ac_uds
-class TestK8sLibInjectionNoAC_UDS(_TestK8sLibInjection):
-    """Test K8s lib injection without admission controller and UDS"""
-
-    pass
-
-
-@features.k8s_admission_controller
 @scenarios.k8s_lib_injection_operator
-class TestK8sLibInjection_operator(_TestK8sLibInjection):
+class TestK8sLibInjection_operator:
     """Test K8s lib injection using the operator"""
 
+    @bug(context.library > "python@2.21.0", reason="APMSP-1750")
     def test_k8s_lib_injection(self):
         cluster_info = context.scenario.k8s_cluster_provider.get_cluster_info()
         context_url = f"http://{cluster_info.cluster_host_name}:{cluster_info.get_weblog_port()}/"
