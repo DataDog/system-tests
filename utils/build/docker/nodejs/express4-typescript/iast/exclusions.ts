a5d7baa9b1ed3cac7085a45cf2e9d0b0f4bb417f
simon-id <simon.id@datadoghq.com>
2025-01-26 23:38:13 +0100

    another typescript fix

--- utils/build/docker/nodejs/express4-typescript/iast/exclusions.ts
+++ utils/build/docker/nodejs/express4-typescript/iast/exclusions.ts
@@ -1,7 +1,5 @@
 'use strict'
 
-// this file is in javascript as it's not part of the weblog app, it's a backport of a fix
-
 const semver = require('semver')
 const version = require('dd-trace/package.json').version
 
@@ -9,7 +7,7 @@ if (semver.satisfies(version, '<5.33.0')) {
   const WeakHashAnalyzer = require('dd-trace/packages/dd-trace/src/appsec/iast/analyzers/weak-hash-analyzer.js')
   const original = WeakHashAnalyzer._isExcluded
 
-  WeakHashAnalyzer._isExcluded = function wrappedIsExcluded (location) {
+  WeakHashAnalyzer._isExcluded = function wrappedIsExcluded (location: any) {
     if (location.path.includes('express-session/index.js')) return true
     else return original.apply(this, arguments)
   }
