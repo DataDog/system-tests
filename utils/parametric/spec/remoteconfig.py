3cf466b912d34793b06622e2df67e3495456552e
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2025-01-27 12:34:14 +0100

    Improve RC code for parametric scenario, clarify APMAPI-1003 (#3892)

--- utils/parametric/spec/remoteconfig.py
+++ utils/parametric/spec/remoteconfig.py
@@ -1,15 +1,5 @@
-from typing import Any, Literal
+from typing import Any
 from utils.dd_constants import Capabilities
-
-
-# Remote Configuration apply status is used by clients to report the application status of a Remote Configuration
-# record.
-# UNKNOWN = 0
-# UNACKNOWLEDGED = 1
-# ACKNOWLEDGED = 2
-# ERROR = 3
-# RFC: https://docs.google.com/document/d/1bUVtEpXNTkIGvLxzkNYCxQzP2X9EK9HMBLHWXr_5KLM/
-APPLY_STATUS = Literal[0, 1, 2, 3]
 
 
 def human_readable_capabilities(caps: int) -> tuple[Any, ...]:
