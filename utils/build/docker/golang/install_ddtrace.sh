60a1b5748c8a620cff51d73b2f5e7180727eac41
Eliott Bouhana <47679741+eliottness@users.noreply.github.com>
2025-01-30 10:34:18 +0000

    [golang] new orchestrion go weblog (#3555)
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>
    Co-authored-by: Romain Marcadier <romain@datadoghq.com>

--- utils/build/docker/golang/install_ddtrace.sh
+++ utils/build/docker/golang/install_ddtrace.sh
@@ -9,6 +9,8 @@ if [ -e "/binaries/dd-trace-go" ]; then
 elif [ -e "/binaries/golang-load-from-go-get" ]; then
     echo "Install from go get -d $(cat /binaries/golang-load-from-go-get)"
     go get -v -d "$(cat /binaries/golang-load-from-go-get)"
+    # Pin that version with a `replace` directive so nothing else can override it.
+    go mod edit -replace "gopkg.in/DataDog/dd-trace-go.v1=$(cat /binaries/golang-load-from-go-get)"
 
 else
     echo "Installing production dd-trace-version"
@@ -20,10 +22,9 @@ go mod tidy
 
 # Read the library version out of the version.go file
 lib_mod_dir=$(go list -f '{{.Dir}}' -m gopkg.in/DataDog/dd-trace-go.v1)
-version=$(sed -nrE 's#.*"v(.*)".*#\1#p' $lib_mod_dir/internal/version/version.go) # Parse the version string content "v.*"
-echo $version > SYSTEM_TESTS_LIBRARY_VERSION
+version=$(sed -nrE 's#.*"v(.*)".*#\1#p' "${lib_mod_dir}/internal/version/version.go") # Parse the version string content "v.*"
+echo "${version}" > SYSTEM_TESTS_LIBRARY_VERSION
 
-rules_mod_dir=$(go list -f '{{.Dir}}' -m github.com/DataDog/appsec-internal-go)
-
-
-echo "dd-trace-go version: $(cat /app/SYSTEM_TESTS_LIBRARY_VERSION)"
+# Output the version of dd-trace-go (per go.mod, as well as the built-in tag).
+echo "dd-trace-go go.mod version: $(go list -f '{{ .Version }}' -m gopkg.in/DataDog/dd-trace-go.v1)"
+echo "dd-trace-go tag:            ${version}"
