f6940a2fa7d2eda690e2c9cad98c804d1ed80bfd
quinna-h <quinna.halim@datadoghq.com>
2025-02-03 17:47:05 -0500

    wip

--- utils/build/docker/python/flask/app.py
+++ utils/build/docker/python/flask/app.py
@@ -68,8 +68,8 @@ if os.environ.get("INCLUDE_RABBITMQ", "true") == "true":
     from integrations.messaging.rabbitmq import rabbitmq_produce
 
 import ddtrace
-from ddtrace.trace import Pin
-from ddtrace.trace import tracer
+from ddtrace import Pin
+from ddtrace import tracer
 from ddtrace.appsec import trace_utils as appsec_trace_utils
 from ddtrace.appsec.iast import ddtrace_iast_flask_patch
 from ddtrace.internal.datastreams import data_streams_processor
@@ -435,6 +435,36 @@ def rasp_cmdi(*args, **kwargs):
 
 
 ### END EXPLOIT PREVENTION
+
+@app.route("/graphql", methods=["GET", "POST"])
+def graphql_error_spans(*args, **kwargs):
+    from integrations.graphql import schema
+    data = request.get_json()
+    if not data or "query" not in data:
+        return jsonify({"error": "Missing GraphQL query"}), 400
+
+    result = schema.execute(
+        data["query"],
+        variables=data.get("variables"),
+        operation_name=data.get("operationName"),
+    )
+
+    if result.errors:
+        return jsonify(format_error(result.errors[0])), 400
+
+    return jsonify(result.to_dict())
+
+def format_error(error):
+    return {
+        "message": error.message,
+        "extensions": [
+            {"key": "int-1", "value": "1"},
+            {"key": "str-1", "value": "1"},
+            {"key": "array-1-2", "value": [1, "2"]},
+            {"key": "empty", "value": "empty string"},
+            {"key": "comma", "value": "comma"},
+        ],
+    }
 
 
 @app.route("/read_file", methods=["GET"])
