ee620406a39851011e999d6b0efda5a2134b771e
Federico Mon <federico.mon@datadoghq.com>
2025-02-05 10:40:05 +0000

    remove unused import

--- tests/appsec/iast/source/test_parameter_name.py
+++ tests/appsec/iast/source/test_parameter_name.py
@@ -2,7 +2,7 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2021 Datadog, Inc.
 
-from utils import context, missing_feature, bug, features, flaky
+from utils import context, missing_feature, bug, features
 from ..utils import BaseSourceTest
 
 
