b308fae23a3e35c6fc561cf6a7c222e702ba5847
William Conti <william.conti@datadoghq.com>
2025-02-12 12:57:24 -0500

    fix lint

--- tests/integrations/test_inferred_proxy.py
+++ tests/integrations/test_inferred_proxy.py
@@ -172,8 +172,8 @@ def assert_api_gateway_span(testCase, span, path, status_code, is_distributed=Fa
     ), f"Inferred AWS API Gateway span meta expected HTTP Status Code of '{status_code}'"
 
     if not interfaces.library.replay:
-        assert (
-            int(span["start"]) == int(testCase.start_time_ns)
+        assert int(span["start"]) == int(
+            testCase.start_time_ns
         ), f"Inferred AWS API Gateway span startTime should equal expected '{testCase.start_time_ns!s}''"
 
     if is_distributed:
