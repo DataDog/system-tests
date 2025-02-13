2ad8318ccfc90597c2ad3ecee836889cca6bf3b9
roberto montero <roberto.montero@datadoghq.com>
2025-02-13 07:44:34 +0100

    fix app

--- utils/build/virtual_machine/weblogs/nodejs/demo-app/demo_app_run.sh
+++ utils/build/virtual_machine/weblogs/nodejs/demo-app/demo_app_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# shellcheck disable=SC2028
+
 sudo chmod +x create_and_run_app_service.sh 
 echo 'while true; do { echo -e "HTTP/1.1 200 OK\nContent-Length: 5\n\nHola"; } | nc -l -p 5985; done' > socket_listener.sh
 sudo chmod +x socket_listener.sh
