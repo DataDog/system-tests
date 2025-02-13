7d69514b59bd4890e6ba54973b5d616022628b8c
roberto montero <roberto.montero@datadoghq.com>
2025-02-13 12:08:23 +0100

    demo scenario

--- tests/auto_inject/test_demo.py
+++ tests/auto_inject/test_demo.py
@@ -9,7 +9,7 @@ class TestDemoAws:
     """Demo test for AWS scenario"""
 
     def test_demo_provision_weblog(self):
-        """Simple demo test"""
+        """Simple demo test to check if the weblog is running"""
         virtual_machine = context.scenario.virtual_machine
         # http request configuration
         vm_ip = virtual_machine.get_ip()
@@ -22,3 +22,15 @@ class TestDemoAws:
             vm_port, vm_ip, weblog_request_timeout
         ), "Weblog port not reachable. Is the weblog running?"
         assert make_get_request(weblog_url) is not None, "Wrong response from weblog"
+
+    def test_run_simple_command(self):
+        """Simple demo test to run a command on the virtual machine"""
+        virtual_machine = context.scenario.virtual_machine
+        ssh_client = virtual_machine.ssh_config.get_ssh_connection()
+        check_folder_command = "ls -la /"
+        _, stdout, stderr = ssh_client.exec_command(check_folder_command)
+
+        full_output = stdout.read().decode()
+        error_output = stderr.read().decode()
+        assert error_output == "", f"Error while running the command. The error output is not empty [{error_output}]"
+        assert "home" in full_output, f"The command output is not as expected [{full_output}]"
