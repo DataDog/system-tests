7a815a99df0b7c2a45afba54278be773f8f3f2b5
Zarir Hamza <zarir.hamza@datadoghq.com>
2025-02-13 11:54:16 -0500

    linter fix

--- tests/integrations/test_inferred_proxy.py
+++ tests/integrations/test_inferred_proxy.py
@@ -154,9 +154,8 @@ def assert_api_gateway_span(testCase, span, path, status_code, is_distributed=Fa
     assert "stage" in span["meta"], "Inferred AWS API Gateway span meta should contain 'stage'"
     assert span["meta"]["stage"] == "staging", "Inferred AWS API Gateway span meta expected stage to be 'staging'"
     assert "start" in span, "Inferred AWS API Gateway span should have 'startTime'"
-    assert (
-        span["metrics"]["_dd.inferred_span"] == 1
-    ), f"Inferred AWS API Gateway span meta expected _dd.inferred_span = 1"
+    assert span["metrics"]["_dd.inferred_span"] == 1, "Inferred AWS API Gateway span meta expected _dd.inferred_span = 1"
+
 
     # assert on HTTP tags
     assert "http.method" in span["meta"], "Inferred AWS API Gateway span meta should contain 'http.method'"
