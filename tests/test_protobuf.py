365487c82125fabf29c43103a58935b8c2da3181
Raphaël Vandon <raphael.vandon@datadoghq.com>
2025-02-18 18:04:30 +0100

    write assertions for the test

--- tests/test_protobuf.py
+++ tests/test_protobuf.py
@@ -1,3 +1,5 @@
+import json
+
 from utils import weblog, interfaces, scenarios, irrelevant, context, bug, features, missing_feature, flaky
 from utils.tools import logger
 
@@ -13,11 +15,35 @@ class Test_Protobuf:
         assert self.serialization_response.status_code == 200, self.serialization_response.text
         assert self.deserialization_response.status_code == 200, self.deserialization_response.text
 
-        def validator(span):
+        def validator(trace: list):
+            if len(trace) == 1:
+                span = trace[0]
+            else:
+                # find root span
+                span = next(s for s in trace if s["parent_id"] == 0)
+                # then find child-most span
+                while True:
+                    child = next((s for s in trace if s["parent_id"] == span["span_id"]), None)
+                    if child:
+                        span = child
+                    else:
+                        break
+
             meta = span.get("meta", {})
-           # TODO
+            assert "schema.id" in meta
+            assert "schema.type" in meta
+            assert "schema.definition" in meta
+            assert "schema.name" in meta
+            assert "schema.operation" in meta
+
+            assert meta["schema.id"] == "14603317962659197404", \
+                meta["schema.hash_data"]
+            assert meta["schema.type"] == "protobuf"
+            json.loads(meta["schema.definition"])  # will throw if malformed
+            assert "x-protobuf-number" in meta["schema.definition"]  # rough check that we register the protobuf numbers
+            assert meta["schema.name"] == "proto_message.AddressBook"
 
             return True
 
-        interfaces.library.validate_spans(request=self.serialization_response, validator=validator)
-        interfaces.library.validate_spans(request=self.deserialization_response, validator=validator)
+        interfaces.library.validate_traces(request=self.serialization_response, validator=validator)
+        interfaces.library.validate_traces(request=self.deserialization_response, validator=validator)
