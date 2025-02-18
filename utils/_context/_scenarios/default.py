689596dfd82c9a45f3fd5e65a0a1029e7ca6f452
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-02-18 15:08:36 +0100

    revert

--- utils/_context/_scenarios/default.py
+++ utils/_context/_scenarios/default.py
@@ -51,7 +51,6 @@ class DefaultScenario(EndToEndScenario):
                 "DD_TRACE_COMPUTE_STATS": "true",
                 "SOME_SECRET_ENV": "leaked-env-var",  # used for test that env var are not leaked
                 "DD_EXTERNAL_ENV": "it-false,cn-weblog,pu-75a2b6d5-3949-4afb-ad0d-92ff0674e759",
-                "DD_TESTING_RAISE": "true",
             },
             agent_env={"SOME_SECRET_ENV": "leaked-env-var"},
             include_postgres_db=True,
