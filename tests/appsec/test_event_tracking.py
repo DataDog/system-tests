7e0ef23db4514ce7211c38e7d6bd282bdcc0a428
Romain Marcadier <romain@datadoghq.com>
2025-02-19 17:26:52 +0100

    appsec/event_tracking: assert presence of the `<event>.sdk` key
    This commit updates the user monitoring SDK test to verify that the
    `appsec.events.users.<event>.sdk` tag is correctly set to `true`.
    
    # Conflicts:
    #	utils/_context/_scenarios/parametric.py
    #	utils/build/docker/golang/chi.Dockerfile
    #	utils/build/docker/golang/echo.Dockerfile
    #	utils/build/docker/golang/gin.Dockerfile
    #	utils/build/docker/golang/gqlgen.Dockerfile
    #	utils/build/docker/golang/graph-gophers.Dockerfile
    #	utils/build/docker/golang/graphql-go.Dockerfile
    #	utils/build/docker/golang/net-http-orchestrion.Dockerfile
    #	utils/build/docker/golang/net-http.Dockerfile
    #	utils/build/docker/golang/uds-echo.Dockerfile

--- tests/appsec/test_event_tracking.py
+++ tests/appsec/test_event_tracking.py
@@ -44,6 +44,7 @@ class Test_UserLoginSuccessEvent:
                 "http.client_ip": "1.2.3.4",
                 "usr.id": "system_tests_user",
                 "appsec.events.users.login.success.track": "true",
+                "appsec.events.users.login.success.sdk": "true",
                 "appsec.events.users.login.success.metadata0": "value0",
                 "appsec.events.users.login.success.metadata1": "value1",
             }
@@ -97,6 +98,7 @@ class Test_UserLoginFailureEvent:
                 "http.client_ip": "1.2.3.4",
                 "appsec.events.users.login.failure.usr.id": "system_tests_user",
                 "appsec.events.users.login.failure.track": "true",
+                "appsec.events.users.login.failure.sdk": "true",
                 "appsec.events.users.login.failure.usr.exists": "true",
                 "appsec.events.users.login.failure.metadata0": "value0",
                 "appsec.events.users.login.failure.metadata1": "value1",
@@ -150,6 +152,7 @@ class Test_CustomEvent:
             expected_tags = {
                 "http.client_ip": "1.2.3.4",
                 "appsec.events.system_tests_event.track": "true",
+                "appsec.events.system_tests_event.sdk": "true",
                 "appsec.events.system_tests_event.metadata0": "value0",
                 "appsec.events.system_tests_event.metadata1": "value1",
             }
