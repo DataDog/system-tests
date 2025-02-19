9516b9837ddaa9400f7e56b51fc132b09875a511
roberto montero <roberto.montero@datadoghq.com>
2025-02-19 10:50:22 +0100

    exporter

--- utils/scripts/ci_orchestrators/gitlab_exporter.py
+++ utils/scripts/ci_orchestrators/gitlab_exporter.py
@@ -15,6 +15,50 @@ def print_aws_gitlab_pipeline(language, aws_matrix, ci_environment) -> None:
     result_pipeline["stages"] = ["CONFIG"]
     pipeline_file = ".gitlab/aws_gitlab-ci.yml"
     pipeline_data = None
+
+    with open(pipeline_file, encoding="utf-8") as f:
+        pipeline_data = yaml.load(f, Loader=yaml.FullLoader)  # noqa: S506
+
+    result_pipeline["include"] = pipeline_data["include"]
+    # Copy the base job and default job
+    result_pipeline[".base_job_onboarding_system_tests"] = pipeline_data[".base_job_onboarding_system_tests"]
+    result_pipeline["configure_run_aws"] = pipeline_data["configure_run_aws"]
+
+    # Create the jobs by scenario. Each job (vm) will have a parallel matrix with the weblogs
+    for scenario, weblogs in aws_matrix.items():
+        result_pipeline["stages"].append(scenario)
+
+        # Collect all unique VMs for this scenario
+        vm_set = set()
+        for vms in weblogs.values():
+            vm_set.update(vms)
+
+        for vm in vm_set:
+            result_pipeline[vm] = {}
+            result_pipeline[vm]["stage"] = scenario
+            result_pipeline[vm]["extends"] = ".base_job_onboarding_system_tests"
+            result_pipeline[vm]["variables"] = {}
+            result_pipeline[vm]["variables"]["TEST_LIBRARY"] = language
+            result_pipeline[vm]["variables"]["SCENARIO"] = scenario
+            result_pipeline[vm]["variables"]["VIRTUAL_MACHINE"] = vm
+            result_pipeline[vm]["variables"]["ONBOARDING_FILTER_ENV"] = ci_environment
+            result_pipeline[vm]["parallel"] = {"matrix": []}
+
+            for weblog in weblogs.keys():
+                if vm in weblogs[weblog]:
+                    result_pipeline[vm]["parallel"]["matrix"].append({"WEBLOG": weblog})
+
+    # TODO: write the gitlab pipeline
+    pipeline_yml = yaml.dump(result_pipeline, sort_keys=False, default_flow_style=False)
+    print(pipeline_yml)
+
+
+def print_aws_gitlab_pipeline2(language, aws_matrix, ci_environment) -> None:
+    result_pipeline = {}  # type: dict
+    result_pipeline["include"] = []
+    result_pipeline["stages"] = ["CONFIG"]
+    pipeline_file = ".gitlab/aws_gitlab-ci.yml"
+    pipeline_data = None
     with open(pipeline_file, encoding="utf-8") as f:
         pipeline_data = yaml.load(f, Loader=yaml.FullLoader)  # noqa: S506
 
