d083fad6ea62b8ae122e149e957ef7228ad1a1fa
ishabi <ilyasshabi94@gmail.com>
2025-03-04 12:51:42 +0100

    add telemetry tests

--- tests/appsec/waf/test_truncation.py
+++ tests/appsec/waf/test_truncation.py
@@ -12,6 +12,19 @@ def create_nested_object(n, obj):
 class Test_Truncation:
     """Test WAF truncation"""
 
+    def _find_series(self, request_type, namespace, metrics):
+        series = []
+        for data in interfaces.library.get_telemetry_data():
+            content = data["request"]["content"]
+            if content.get("request_type") != request_type:
+                continue
+            fallback_namespace = content["payload"].get("namespace")
+            for serie in content["payload"]["series"]:
+                computed_namespace = serie.get("namespace", fallback_namespace)
+                if computed_namespace == namespace and serie["metric"] in metrics:
+                    series.append(serie)
+        return series
+    
     def setup_truncation(self):
       # Create complex data
       long_value = "testattack" * 500
@@ -40,3 +53,24 @@ class Test_Truncation:
         assert int(metrics["_dd.appsec.truncated.string_length"]) == 5000
         assert int(metrics["_dd.appsec.truncated.container_size"]) == 300
         assert int(metrics["_dd.appsec.truncated.container_depth"]) == 20
+
+        count_series = self._find_series("generate-metrics", "appsec", ["waf.input_truncated"])
+        input_truncated = count_series[0] if count_series else None
+
+        assert input_truncated is not None, f"No telemetry data received for metric appsec.waf.input_truncated"
+
+        assert input_truncated["type"] == "count"
+        assert 'truncation_reason:7' in input_truncated["tags"]
+
+        distribution_series = self._find_series("distributions", "appsec", ["waf.truncated_value_size"])
+
+        assert len(distribution_series) == 3, f"Not all telemetry data received for metric appsec.waf.truncated_value_size"
+
+        long_string = [d for d in distribution_series if 'truncation_reason:1' in d['tags']]
+        assert long_string, "No distribution for long string truncation"
+
+        large_container = [d for d in distribution_series if 'truncation_reason:2' in d['tags']]
+        assert large_container, "No distribution for large container truncation"
+
+        deep_container = [d for d in distribution_series if 'truncation_reason:4' in d['tags']]
+        assert deep_container, "No distribution for deep container truncation"
