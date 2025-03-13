5b63a65426cbaeb6aed620c2284cfc680d925962
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2025-03-13 11:07:11 +0100

    SSI gitlab pipeline: delay for parallel (#4293)

--- utils/scripts/ci_orchestrators/external_gitlab_pipeline.py
+++ utils/scripts/ci_orchestrators/external_gitlab_pipeline.py
@@ -11,6 +11,8 @@ ALLOWED_VARIABLES = [
     "DD_INSTALLER_INJECTOR_VERSION",
 ]
 
+LANG_STAGES = ["java", "python", "nodejs", "dotnet", "ruby", "php"]
+
 
 def main(language=None) -> None:
     """Main function to generate the gitlab system-tests pipeline
@@ -27,10 +29,12 @@ def main(language=None) -> None:
     # Ensure 'variables' section exists and update with new values
     data.setdefault("variables", {}).update(new_variables)
 
-    if language in ["java", "python", "nodejs", "dotnet", "ruby", "php"]:
+    if language in LANG_STAGES:
         data = filter_yaml(data, language)
+
+    handle_parallelism(data)
+
     # Print the modified YAML
-    update_needs(data)
     print(yaml.dump(data, default_flow_style=False, sort_keys=False))
 
 
@@ -62,18 +66,42 @@ def filter_yaml(yaml_data, language) -> dict:
     return filtered_data
 
 
-def update_needs(yaml_data) -> None:
+def handle_parallelism(yaml_data) -> None:
     """Update jobs that have 'needs:' containing 'compute_pipeline' and another value, keeping only ['compute_pipeline']
     We will launch all the languges in parallel when we run system-tests in external repositories.
     (For the system-tests repository we launch the tests sequentially for each language.)
+    NOTE: if we are running the tests for a release, we are going to run agains all vms, this can exhausted
+          the resources (aws, gitlab). Let's add 5 minutes of delay for starting the next stage (for the next lang)
     """
+    # Check if we are generating a release
+    is_release = os.getenv("CI_COMMIT_TAG")
+    # Add a delay job for each stage
+    delayed = 3
+
+    # Extract all defined stages and count how many stages belong to LANG_STAGES
+    defined_stages = yaml_data.get("stages", [])
+    lang_stage_count = sum(1 for stage in defined_stages if stage in LANG_STAGES)
+
+    if lang_stage_count > 1 and is_release:
+        for stage in yaml_data["stages"]:
+            if stage in LANG_STAGES:
+                job_name = f"delayed_{stage}_trigger"
+                yaml_data[job_name] = {"extends": ".delayed_base_job", "stage": stage, "needs": ["compute_pipeline"]}
+                yaml_data[job_name]["start_in"] = f"{delayed} minutes"
+                delayed = delayed + 3
 
     for _job_name, job_data in yaml_data.items():  # noqa: PERF102
         if isinstance(job_data, dict) and "needs" in job_data:
             needs_list = job_data["needs"]
             # Check if 'compute_pipeline' is present and there is more than one value
             if isinstance(needs_list, list) and "compute_pipeline" in needs_list and len(needs_list) > 1:
-                job_data["needs"] = ["compute_pipeline"]  # Keep only 'compute_pipeline'
+                if lang_stage_count > 1 and is_release:
+                    job_data["needs"] = [
+                        "compute_pipeline",
+                        f"delayed_{job_data['stage']}_trigger",
+                    ]  # Delay the next stage
+                else:
+                    job_data["needs"] = ["compute_pipeline"]
 
 
 if __name__ == "__main__":
