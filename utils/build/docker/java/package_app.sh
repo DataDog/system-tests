2631c52f68efde47e8319e05b0d3fb1879920bd9
Manuel Álvarez Álvarez <manuel.alvarezalvarez@datadoghq.com>
2025-03-14 16:33:13 +0100

    Fix spell check

--- utils/build/docker/java/package_app.sh
+++ utils/build/docker/java/package_app.sh
@@ -8,6 +8,7 @@ if [ -f /dd-tracer/dd-trace-api.jar ]; then
   MAVEN_OPTS="-DcustomDdTraceApi=/dd-tracer/dd-trace-api.jar"
 fi
 
+# shellcheck disable=SC2068
 /usr/share/maven/bin/mvn "$MAVEN_OPTS" $@
 
 
