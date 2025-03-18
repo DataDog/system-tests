781deedb6fcea4faef894f673b12aa06bf4e1e84
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-03-18 16:37:04 +0100

    Lint shell script

--- utils/build/docker/cpp_httpd/install_ddtrace.sh
+++ utils/build/docker/cpp_httpd/install_ddtrace.sh
@@ -13,18 +13,18 @@ cd /binaries
 if [ -f "$FILENAME" ]; then
   echo "Install HTTPD plugin from binaries/$FILENAME"
   HTTPD_DATADOG_VERSION="v99.99.99"  # TODO: get version from the binary. Right now, use the "big-version" trick
-  cp $FILENAME $DEST_FOLDER/$FILENAME
+  cp $FILENAME "$DEST_FOLDER/$FILENAME"
 else
   HTTPD_DATADOG_VERSION="$(get_latest_release DataDog/httpd-datadog)"
   TARBALL="mod_datadog_artifact.zip"
   URL="https://github.com/DataDog/httpd-datadog/releases/download/${HTTPD_DATADOG_VERSION}/${TARBALL}"
   echo "Get APACHE plugin from $URL"
-  curl -Lf -o $TARBALL $URL
-  unzip $TARBALL -d $DEST_FOLDER
-  rm $TARBALL
+  curl -Lf -o "$TARBALL" "$URL"
+  unzip "$TARBALL" -d "$DEST_FOLDER"
+  rm "$TARBALL"
 fi
 
-echo '{"status": "ok", "library": {"language": "cpp_httpd", "version": "'$HTTPD_DATADOG_VERSION'"}}' > /app/healthcheck.json
-echo $HTTPD_DATADOG_VERSION > SYSTEM_TESTS_LIBRARY_VERSION
+echo '{"status": "ok", "library": {"language": "cpp_httpd", "version": "'"$HTTPD_DATADOG_VERSION"'"}}' > /app/healthcheck.json
+echo "$HTTPD_DATADOG_VERSION" > SYSTEM_TESTS_LIBRARY_VERSION
 cat /app/healthcheck.json
 
