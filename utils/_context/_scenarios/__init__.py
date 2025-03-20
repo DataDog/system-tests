3c2a435ecf3f34b07f7c00f69655017d201c2819
Matthew Li <matthew.li@datadoghq.com>
2025-03-20 11:55:18 -0400

    updating versions

--- utils/_context/_scenarios/__init__.py
+++ utils/_context/_scenarios/__init__.py
@@ -571,6 +571,7 @@ class _Scenarios:
             "DD_TRACE_CLIENT_IP_ENABLED": "true",
             "DD_TRACE_PROPAGATION_STYLE_EXTRACT": "datadog,tracecontext,b3multi,baggage",
             "DD_TRACE_PROPAGATION_BEHAVIOR_EXTRACT": "ignore",
+            "DD_TRACE_EXPERIMENTAL_FEATURES_ENABLED": "DD_LOGS_INJECTION",
         },
         include_kafka=True,
         include_postgres_db=True,
