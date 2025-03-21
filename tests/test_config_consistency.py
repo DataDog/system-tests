e0b8b757e34973a803f28bf931ad1d8538662018
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2025-03-21 11:03:36 +0100

    [java] Skip test failing for APMAPI-1251 (#4355)

--- tests/test_config_consistency.py
+++ tests/test_config_consistency.py
@@ -117,6 +117,7 @@ class Test_Config_ObfuscationQueryStringRegexp_Empty:
         self.r = weblog.get("/?application_key=value")
 
     @bug(context.library == "python", reason="APMAPI-772")
+    @bug(context.library >= "java@1.48.0" and context.weblog_variant == "spring-boot-3-native", reason="APMAPI-1251")
     def test_query_string_obfuscation_empty_server(self):
         spans = [s for _, _, s in interfaces.library.get_spans(request=self.r, full_trace=True)]
         server_span = _get_span_by_tags(spans, tags={"http.url": "http://localhost:7777/?application_key=value"})
@@ -298,6 +299,7 @@ class Test_Config_ClientIPHeader_Configured:
             "/make_distant_call", params={"url": "http://weblog:7777"}, headers={"custom-ip-header": "5.6.7.9"}
         )
 
+    @bug(context.library >= "java@1.48.0", reason="APMAPI-1251")
     def test_ip_headers_sent_in_one_request(self):
         # Ensures the header set in DD_TRACE_CLIENT_IP_HEADER takes precedence over all supported ip headers
         trace = [span for _, _, span in interfaces.library.get_spans(self.req, full_trace=True)]
@@ -377,11 +379,11 @@ def _get_span_by_tags(spans, tags):
     logger.info(f"Try to find span with metag tags {tags}")
 
     for span in spans:
+        meta = span["meta"]
+        logger.debug(f"Checking span {span['span_id']} meta:\n{'\n'.join(map(str,meta.items()))}")
         # Avoids retrieving the client span by the operation/resource name, this value varies between languages
         # Use the expected tags to identify the span
         for k, v in tags.items():
-            meta = span["meta"]
-
             if k not in meta:
                 logger.debug(f"Span {span['span_id']} does not have tag {k}")
                 break
