0ad15f08828e121c9cc517b72e250e428a8d55a1
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-03-25 15:29:20 +0100

    Fix authentication endpoint in Rails 4.2 weblog

--- utils/build/docker/ruby/rails42/app/models/user.rb
+++ utils/build/docker/ruby/rails42/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  self.primary_key = 'id'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
