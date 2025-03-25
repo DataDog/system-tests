b56aed3125e5ed49ad19f30691838159da1d0240
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-03-25 15:14:47 +0100

    Adapt to new cpp_nginx

--- tests/appsec/test_blocking_addresses.py
+++ tests/appsec/test_blocking_addresses.py
@@ -57,7 +57,7 @@ class Test_Blocking_client_ip:
     def setup_blocking_before(self):
         self.block_req2 = weblog.get("/tag_value/tainted_value_6512/200", headers={"X-Forwarded-For": "1.1.1.1"})
 
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # second request should block and must not set the tag in span
@@ -116,7 +116,7 @@ class Test_Blocking_request_method:
         context.scenario is scenarios.external_processing_blocking,
         reason="The endpoint /tag_value is not implemented in the weblog",
     )
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
@@ -177,7 +177,7 @@ class Test_Blocking_request_uri:
         context.scenario is scenarios.external_processing_blocking,
         reason="The endpoint /tag_value is not implemented in the weblog",
     )
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
@@ -238,7 +238,7 @@ class Test_Blocking_request_path_params:
         context.scenario is scenarios.external_processing_blocking,
         reason="The endpoint /param is not implemented in the weblog",
     )
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
@@ -294,7 +294,7 @@ class Test_Blocking_request_query:
         context.scenario is scenarios.external_processing_blocking,
         reason="The endpoint /tag_value is not implemented in the weblog",
     )
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
@@ -350,7 +350,7 @@ class Test_Blocking_request_headers:
         context.scenario is scenarios.external_processing_blocking,
         reason="The endpoint /tag_value is not implemented in the weblog",
     )
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
@@ -406,7 +406,7 @@ class Test_Blocking_request_cookies:
         context.scenario is scenarios.external_processing_blocking,
         reason="The endpoint /tag_value is not implemented in the weblog",
     )
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
@@ -473,7 +473,7 @@ class Test_Blocking_request_body:
         self.set_req1 = weblog.post("/tag_value/clean_value_3882/200", data={"good": "value"})
         self.block_req2 = weblog.post("/tag_value/tainted_value_body/200", data={"value5": "bsldhkuqwgervf"})
 
-    @irrelevant(context.weblog_variant == "nginx", reason="Tag adding happens before WAF run")
+    @irrelevant(context.library == "cpp_nginx", reason="Tag adding happens before WAF run")
     def test_blocking_before(self):
         """Test that blocked requests are blocked before being processed"""
         # first request should not block and must set the tag in span accordingly
