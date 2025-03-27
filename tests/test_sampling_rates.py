54d78746014dc40d5855a56af17a303dd395550f
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-03-27 10:28:01 +0100

    [nodejs] Add uds-express4 for APMAPI-1252

--- tests/test_sampling_rates.py
+++ tests/test_sampling_rates.py
@@ -101,7 +101,10 @@ class Test_SamplingRates:
             self.paths.append(p)
             weblog.get(p)
 
-    @flaky(context.library == "nodejs" and context.weblog_variant in ("express4", "express5"), reason="APMAPI-1252")
+    @flaky(
+        context.library == "nodejs" and context.weblog_variant in ("express4", "express5", "uds-express4"),
+        reason="APMAPI-1252",
+    )
     @missing_feature(library="cpp_httpd", reason="/sample_rate_route is not implemented")
     def test_sampling_rates(self):
         """Basic test"""
