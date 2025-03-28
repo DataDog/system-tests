f06e8f467b3538e71ea3c14bf0476a7dc6a3679c
Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
2025-03-29 00:21:25 +0100

    [java] Add Java endpoints for ATO SDK v2 tests (#4423)

--- utils/build/docker/java/install_ddtrace.sh
+++ utils/build/docker/java/install_ddtrace.sh
@@ -5,14 +5,15 @@ set -eu
 mkdir /dd-tracer
 
 # Look for custom dd-trace-api jar in custom binaries folder
-CUSTOM_DD_TRACE_API_COUNT=$(find /binaries/dd-trace-api*.jar 2>/dev/null | wc -l)
+CUSTOM_DD_TRACE_API_COUNT=$(ls /binaries/dd-trace-api*.jar | wc -l)
 if [ "$CUSTOM_DD_TRACE_API_COUNT" = 0 ]; then
     echo "Using default dd-trace-api"
 elif [ "$CUSTOM_DD_TRACE_API_COUNT" = 1 ]; then
     [[ "$#" -eq 0 ]] && MVN_OPTS= || MVN_OPTS="$1"
-    CUSTOM_DD_TRACE_API=$(find /binaries/dd-trace-api*.jar)
+    cp $(ls /binaries/dd-trace-api*.jar) /dd-tracer/dd-trace-api.jar
+    CUSTOM_DD_TRACE_API="/dd-tracer/dd-trace-api.jar"
     echo "Using custom dd-trace-api: ${CUSTOM_DD_TRACE_API}"
-    mvn -Dfile="$CUSTOM_DD_TRACE_API" -DgroupId=com.datadoghq -DartifactId=dd-trace-api -Dversion=9999 -Dpackaging=jar $MVN_OPTS install:install-file
+    /usr/share/maven/bin/mvn -Dfile="$CUSTOM_DD_TRACE_API" -DgroupId=com.datadoghq -DartifactId=dd-trace-api -Dversion=9999 -Dpackaging=jar $MVN_OPTS install:install-file
 else
     echo "Too many dd-trace-api within binaries folder"
     exit 1
