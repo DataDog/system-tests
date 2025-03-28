7463e9079939807f60f8607b7470b4b8a962a86e
Yury Lebedev <yury.lebedev@datadoghq.com>
2025-03-28 18:06:22 +0100

    [Ruby] Add SSRF endpoints to Rails Weblogs

--- utils/build/docker/ruby/rails50/config/routes.rb
+++ utils/build/docker/ruby/rails50/config/routes.rb
@@ -43,5 +43,9 @@ Rails.application.routes.draw do
   get '/rasp/sqli' => 'system_test#rasp_sqli'
   post '/rasp/sqli' => 'system_test#rasp_sqli'
 
+  namespace :rasp do
+    resource :ssrf, only: %i[show create]
+  end
+
   get '/sample_rate_route/:i' => 'system_test#sample_rate_route'
 end
