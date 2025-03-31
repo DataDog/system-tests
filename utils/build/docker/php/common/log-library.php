ba209ba2a29a3720c0efdd7bf7d62ccd56158b15
Munir Abdinur <munir_abdinur@hotmail.com>
2025-03-30 21:46:57 -0400

    first attempt at adding monolog

--- utils/build/docker/php/common/log-library.php
+++ utils/build/docker/php/common/log-library.php
@@ -1,3 +1,20 @@
 <?php
-error_log($_GET['msg'], 0);
+require __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// Create the logger
+$logger = new Logger('webapp');
+
+// Add handler to write to stderr (which is where error_log writes to by default)
+$logger->pushHandler(new StreamHandler('php://stderr', Level::Debug));
+
+// Log the message from GET parameter
+if (isset($_GET['msg'])) {
+    $logger->info('Received message', ['message' => $_GET['msg']]);
+} else {
+    $logger->warning('No message provided in GET parameters');
+}
 ?>
