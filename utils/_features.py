e37a4581e12c82fbc42012e764f3a8b73f2383f1
Rachel Yang <rachel.yang@datadoghq.com>
2025-04-04 11:03:00 -0400

    python manifest

--- utils/_features.py
+++ utils/_features.py
@@ -2329,6 +2329,10 @@ class _Features:
 
     @staticmethod
     def datadog_baggage_headers(test_object):
+        """Baggage support
+
+        https://feature-parity.us1.prod.dog/#/?feature=348
+        """
         pytest.mark.features(feature_id=348)(test_object)
         return test_object
 
