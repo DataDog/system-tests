0d766686bf5951d828380dee1d03a7a48dc9572a
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2025-04-07 17:30:10 +0200

    [nodejs@ugaitz/user-tracking-sdk-v2] Fix app and enable tests

--- utils/build/docker/nodejs/express/app.js
+++ utils/build/docker/nodejs/express/app.js
@@ -202,7 +202,7 @@ app.post('/user_login_success_event_v2', (req, res) => {
   const userId = req.body.user_id
   const metadata = req.body.metadata
 
-  tracer.appsec.v2?.trackUserLoginSuccess(login, userId, metadata)
+  tracer.appsec.eventTrackingV2?.trackUserLoginSuccess(login, userId, metadata)
 
   res.send('OK')
 })
@@ -212,7 +212,7 @@ app.post('/user_login_failure_event_v2', (req, res) => {
   const exists = req.body.exists?.trim() === 'true'
   const metadata = req.body.metadata
 
-  tracer.appsec.v2?.trackUserLoginFailure(login, exists, metadata)
+  tracer.appsec.eventTrackingV2?.trackUserLoginFailure(login, exists, metadata)
 
   res.send('OK')
 })
