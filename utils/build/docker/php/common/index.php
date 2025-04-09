1dad8ff1eb9a99db9cc8f170a6e05f2ebe208c58
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-04-09 11:49:25 +0200

    Add cookie managment to weblog

--- utils/build/docker/php/common/index.php
+++ utils/build/docker/php/common/index.php
@@ -1,5 +1,14 @@
-<?php phpinfo();
+<?php
+
+require_once 'cookie_manager.php';
 
 if (!isset($_SERVER['HTTP_X_DATADOG_ORIGIN'])) {
     \datadog\appsec\track_authenticated_user_event_automated($_GET['username'] ?? 'social-security-id');
 }
+
+$user = getLoggedInCookie();
+if (isset($user)) {
+    \datadog\appsec\track_authenticated_user_event_automated($user);
+}
+
+phpinfo();
