2260e425612568a3bd5ff5f83f4f674967419da1
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-04-17 15:55:09 +0200

    Simplify dockerfiles

--- utils/build/docker/php/php-fpm/build.sh
+++ utils/build/docker/php/php-fpm/build.sh
@@ -24,9 +24,7 @@ apt-get install -y
 find /var/www/html -mindepth 1 -delete
 
 mkdir -p /var/www/html/rasp
-cp -rf /tmp/php/common/*.php /var/www/html/
-cp -rf /tmp/php/common/*.json /var/www/html/
-cp -rf /tmp/php/common/rasp/*.php /var/www/html/rasp/
+cp -rf /tmp/php/common/* /var/www/html/
 cp /tmp/php/php-fpm/php-fpm.conf /etc/apache2/conf-available/php$PHP_VERSION-fpm.conf
 cp /tmp/php/common/php.ini /etc/php/$PHP_VERSION/fpm/php.ini
 cp /tmp/php/php-fpm/entrypoint.sh /
