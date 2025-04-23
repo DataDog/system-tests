52aab6d962d6a12361f6c3ea924998e7a1a65db9
Munir Abdinur <munir_abdinur@hotmail.com>
2025-04-23 11:36:09 -0400

    simplify test

--- tests/parametric/test_telemetry.py
+++ tests/parametric/test_telemetry.py
@@ -498,25 +498,21 @@ class Test_Stable_Configuration_Origin(StableConfigWriter):
 
     @missing_feature(context.library in ("java", "nodejs"), reason="Not implemented")
     @pytest.mark.parametrize(
-        ("local_cfg", "library_env", "fleet_cfg", "expected_origin", "fleet_config_id"),
+        ("local_cfg", "library_env", "fleet_cfg", "fleet_config_id"),
         [
             (
-                {"DD_LOGS_INJECTION": False, "DD_RUNTIME_METRICS_ENABLED": True, "DD_PROFILING_ENABLED": True},
+                {"DD_PROFILING_ENABLED": True},
                 {
                     "DD_TELEMETRY_HEARTBEAT_INTERVAL": "0.1",  # Decrease the heartbeat/poll intervals to speed up the tests
                     "DD_RUNTIME_METRICS_ENABLED": True,
                 },
                 {"DD_LOGS_INJECTION": True},
-                {
-                    "logs_injection_enabled": "fleet_stable_config",
-                    "profiling_enabled": "local_stable_config",
-                },
                 "1231231231231",
             )
         ],
     )
     def test_stable_configuration_config_id(
-        self, local_cfg, library_env, fleet_cfg, test_agent, test_library, expected_origin, fleet_config_id
+        self, local_cfg, library_env, fleet_cfg, test_agent, test_library, fleet_config_id
     ):
         with test_library:
             self.write_stable_config(
@@ -538,14 +534,16 @@ class Test_Stable_Configuration_Origin(StableConfigWriter):
             test_library.dd_start_span("test")
 
         configurations = test_agent.wait_for_telemetry_configurations()
-        for cfg_name in expected_origin:
-            apm_telemetry_name = _mapped_telemetry_name(context, cfg_name)
-            telemetry_item = configurations[apm_telemetry_name]
-            assert telemetry_item["origin"] == expected_origin[cfg_name], f"wrong origin for {telemetry_item}"
-            if telemetry_item["origin"] == "fleet_stable_config":
-                assert telemetry_item["config_id"] == fleet_config_id
-            else:
-                assert "config_id" not in telemetry_item
+        # Configuration set via fleet config should have the config_id set
+        apm_telemetry_name = _mapped_telemetry_name(context, "logs_injection_enabled")
+        telemetry_item = configurations[apm_telemetry_name]
+        assert telemetry_item["origin"] == "fleet_stable_config"
+        assert telemetry_item["config_id"] == fleet_config_id
+        # Configuration set via local config should not have the config_id set
+        apm_telemetry_name = _mapped_telemetry_name(context, "profiling_enabled")
+        telemetry_item = configurations[apm_telemetry_name]
+        assert telemetry_item["origin"] == "local_stable_config"
+        assert "config_id" not in telemetry_item
 
 
 DEFAULT_ENVVARS = {
