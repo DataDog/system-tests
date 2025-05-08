5e4e16d9088145d9b7683425bec0ed8b10f9e41e
Gustavo Lopes <mail@geleia.net>
2025-05-08 16:40:22 +0100

    Improve mechanism to active nginx debug logging

--- utils/build/docker/cpp_nginx/nginx/app.sh
+++ utils/build/docker/cpp_nginx/nginx/app.sh
@@ -2,8 +2,15 @@
 
 backend &
 
-if [[ "${DDPROF_ENABLE:-,,}" == "yes" ]]; then
-  ddprof -l notice nginx-debug -g 'daemon off;'
+if [[ $DD_TRACE_DEBUG = true ]]; then
+  sed -i 's/\(error_log [^ ]* \)info;/\1debug;/' /etc/nginx/nginx.conf
+  NGINX_BINARY=nginx-debug
 else
-  nginx-debug -g 'daemon off;'
+  NGINX_BINARY=nginx
+fi
+
+if [[ "${DDPROF_ENABLE:-,,}" == "yes" ]]; then
+  ddprof -l notice "$NGINX_BINARY" -g 'daemon off;'
+else
+  "$NGINX_BINARY" -g 'daemon off;'
 fi
