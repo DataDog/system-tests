605be581d163b86ebe855143c7615b1718b7ab6e
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2025-05-08 12:26:26 +0200

    Docker ssi: crashtracking dedicated scenario (#4611)

--- tests/test_the_test/test_group_rules.py
+++ tests/test_the_test/test_group_rules.py
@@ -34,6 +34,7 @@ def test_tracer_release():
         scenarios.container_auto_injection_install_script_profiling,
         scenarios.container_auto_injection_install_script,
         scenarios.docker_ssi,
+        scenarios.docker_ssi_crashtracking,
         scenarios.external_processing_blocking,  # need to declare a white list of library in get-workflow-parameters
         scenarios.external_processing,  # need to declare a white list of library in get-workflow-parameters
         scenarios.host_auto_injection_install_script_profiling,
