eb3ead2272c091de51652660ac02d7622ba80159
Bruce Bujon <bruce.bujon@datadoghq.com>
2025-05-14 11:06:56 +0200

    feat(java): Enable last parent id in tracestate test

--- tests/parametric/test_headers_tracecontext.py
+++ tests/parametric/test_headers_tracecontext.py
@@ -750,7 +750,7 @@ class Test_Headers_Tracecontext:
     @missing_feature(context.library < "dotnet@2.51.0", reason="Not implemented")
     @missing_feature(context.library < "php@0.99.0", reason="Not implemented")
     @missing_feature(context.library < "nodejs@5.6.0", reason="Not implemented")
-    @missing_feature(context.library == "java", reason="Not implemented")
+    @missing_feature(context.library < "java@1.50.0", reason="Not implemented")
     @missing_feature(context.library < "cpp@0.2.0", reason="Not implemented")
     @missing_feature(context.library < "ruby@2.0.0", reason="Not implemented")
     @missing_feature(context.library < "golang@1.64.0", reason="Not implemented")
