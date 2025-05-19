01574fe2b1741fca10064a03a3a2d9c4307f008f
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-05-19 17:28:10 +0200

    Add ato v2 test when Appsec is disabled

--- utils/_context/_scenarios/__init__.py
+++ utils/_context/_scenarios/__init__.py
@@ -970,6 +970,14 @@ class _Scenarios:
         scenario_groups=[scenario_groups.appsec],
     )
 
+    ato_sdk = EndToEndScenario(
+        "ATO_SDK",
+        weblog_env={"DD_APPSEC_ENABLED": "false"},
+        doc="Test ato when appsec is diabled",
+        github_workflow="endtoend",
+        scenario_groups=[scenario_groups.end_to_end],
+    )
+
     appsec_ato_sdk = EndToEndScenario(
         "APPSEC_ATO_SDK",
         weblog_env={"DD_APPSEC_ENABLED": "true", "DD_APPSEC_RULES": "/appsec_ato_sdk.json"},
