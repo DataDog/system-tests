50354553aafeb44661e6d0d88fc440cc814e2487
Igor Unanua <igor.unanua@datadoghq.com>
2025-05-22 16:13:15 +0200

    try to use GITHUB_TOKEN

--- utils/build/docker/rust/install_ddtrace.sh
+++ utils/build/docker/rust/install_ddtrace.sh
@@ -10,7 +10,7 @@ if [ -e /binaries/rust-load-from-git ]; then
     if [[ -n "$rev_or_branch" ]]; then
         git checkout "$rev_or_branch"
     fi
-
+        
     echo "cloned git@github.com:DataDog/dd-trace-rs.git && checkout $rev_or_branch"
 fi
 
@@ -19,13 +19,13 @@ cd /usr/app
 if [ -e /binaries/dd-trace-rs ]; then
     cargo add --path /binaries/dd-trace-rs/datadog-opentelemetry
     cargo add --path /binaries/dd-trace-rs/dd-trace
-
+    
     echo "install from /binaries/dd-trace-rs"
 else
     cargo add datadog-opentelemetry
     cargo add dd-trace
-
+    
     echo "install from crates.io"
-
+    
 fi
 
