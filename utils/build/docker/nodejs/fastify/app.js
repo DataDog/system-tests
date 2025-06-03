4f6cd23edcc2871c93ee87ea1a1a454ca752aca7
ishabi <ilyasshabi94@gmail.com>
2025-06-03 14:45:02 +0200

    add missing fastify tests on nodejs yml

--- utils/build/docker/nodejs/fastify/app.js
+++ utils/build/docker/nodejs/fastify/app.js
@@ -103,6 +103,28 @@ fastify.get('/headers', async (request, reply) => {
   return 'Hello, headers!'
 })
 
+fastify.get('/customResponseHeaders', (request, reply) => {
+  reply.headers({
+    'content-type': 'text/plain',
+    'content-language': 'en-US',
+    'x-test-header-1': 'value1',
+    'x-test-header-2': 'value2',
+    'x-test-header-3': 'value3',
+    'x-test-header-4': 'value4',
+    'x-test-header-5': 'value5'
+  })
+  return 'OK'
+})
+
+fastify.get('/exceedResponseHeaders', (request, reply) => {
+  reply.header('content-type', 'text/plain')
+  for (let i = 0; i < 50; i++) {
+    reply.header(`x-test-header-${i}`, `value${i}`)
+  }
+  reply.header('content-language', 'en-US')
+  return 'OK'
+})
+
 fastify.get('/identify', async (request, reply) => {
   tracer.setUser({
     id: 'usr.id',
