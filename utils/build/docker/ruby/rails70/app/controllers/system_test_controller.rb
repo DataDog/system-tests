461c0702ff1efe63722bebbcb44b207bb16ef351
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-06-06 16:24:38 +0200

    Enable API Security standalone tests

--- utils/build/docker/ruby/rails70/app/controllers/system_test_controller.rb
+++ utils/build/docker/ruby/rails70/app/controllers/system_test_controller.rb
@@ -260,4 +260,12 @@ class SystemTestController < ApplicationController
   def sample_rate_route
     render plain: 'OK'
   end
+
+  def api_security_sampling
+    render plain: 'Hello!'
+  end
+
+  def api_security_with_sampling
+    render plain: 'OK'
+  end
 end
