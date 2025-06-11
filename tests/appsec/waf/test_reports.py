7a28911caa4aca13ba943a26f07054871fa81e6d
Dario Castañé <dario.castane@datadoghq.com>
2025-06-11 09:12:34 +0200

    general: update dd-trace-go to v2 (#1759)
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>
    Co-authored-by: Kyle Nusbaum <kyle@datadog.com>
    Co-authored-by: Hannah Kim <hannahs.kim@datadoghq.com>
    Co-authored-by: Rodrigo Argüello <rarguellof91@gmail.com>
    Co-authored-by: Benjamin De Bernardi <debernardi.benjamin@gmail.com>
    Co-authored-by: Eliott Bouhana <47679741+eliottness@users.noreply.github.com>
    Co-authored-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>
    Co-authored-by: Mikayla Toffler <46911781+mtoffl01@users.noreply.github.com>
    Co-authored-by: Sergey Fedorov <sergey.fedorov@datadoghq.com>
    Co-authored-by: Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
    Co-authored-by: Santiago M. Mola <santiago.mola@datadoghq.com>
    Co-authored-by: Ida Liu <119438987+ida613@users.noreply.github.com>
    Co-authored-by: Ilyas Shabi <ilyas.shabi@datadoghq.com>
    Co-authored-by: Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
    Co-authored-by: Tyler Finethy <tylfin@gmail.com>
    Co-authored-by: Alexander Seleznyov <alexander.seleznyov@datadoghq.com>
    Co-authored-by: Charles de Beauchesne <charles.de.beauchesne@gmail.com>
    Co-authored-by: Munir Abdinur <munir_abdinur@hotmail.com>
    Co-authored-by: Brian Marks <bm1549@users.noreply.github.com>
    Co-authored-by: Marco Costa <marco.costa@datadoghq.com>
    Co-authored-by: Romain Marcadier <romain@datadoghq.com>

--- tests/appsec/waf/test_reports.py
+++ tests/appsec/waf/test_reports.py
@@ -4,7 +4,7 @@
 import re
 import json
 
-from utils import weblog, context, interfaces, irrelevant, scenarios, features
+from utils import weblog, context, interfaces, irrelevant, scenarios, features, bug
 
 
 @features.support_in_app_waf_metrics_report
@@ -188,6 +188,7 @@ class Test_Monitoring:
         self.r_errors = weblog.get("/waf/", params={"v": ".htaccess"})
 
     @scenarios.appsec_rules_monitoring_with_errors
+    @bug(library="golang", reason="LANGPLAT-584")
     def test_waf_monitoring_errors(self):
         """Some WAF monitoring span tags and metrics are expected to be sent at
         least once in a request span at some point
