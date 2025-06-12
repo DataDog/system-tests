11061eb8bf53686ae1ae97aaa6a98da1538d9b87
Zach Montoya <zach.montoya@datadoghq.com>
2025-06-12 12:34:50 -0700

    Update .NET weblog test app with OTel SDK sending metrics via OTLP and console

--- utils/build/docker/dotnet/weblog/OpenTelemetryInstrumentation.cs
+++ utils/build/docker/dotnet/weblog/OpenTelemetryInstrumentation.cs
@@ -1,10 +1,17 @@
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using OpenTelemetry.Context.Propagation;
 
 namespace weblog
 {
     public static class OpenTelemetryInstrumentation
     {
+        internal const string ServiceName = "ApmTestApi";
+        internal const string MeterName = "ApmTestApi";
+
+        private static readonly Meter _meter = new Meter(MeterName);
+        public static Counter<long> LongCounter => _meter.CreateCounter<long>("parametric.count.long");
+
         public static TextMapPropagator Propagator { get; } = Propagators.DefaultTextMapPropagator;
     }
 }
