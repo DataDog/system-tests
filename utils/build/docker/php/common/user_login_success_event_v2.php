33e7ce0f4e0a47e80270a4912b77a7e9dcb0dd6f
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-06-13 15:52:22 +0200

    Rename v2 ato sdk functions

--- utils/build/docker/php/common/user_login_success_event_v2.php
+++ utils/build/docker/php/common/user_login_success_event_v2.php
@@ -3,18 +3,18 @@ $body = file_get_contents("php://input");
 $decoded = json_decode($body, 1);
 
 if (isset($decoded['login']) && isset($decoded['user_id']) && isset($decoded['metadata'])) {
-    \DDTrace\ATO\V2\track_user_login_success(
+    \datadog\appsec\v2\track_user_login_success(
         $decoded['login'],
         $decoded['user_id'],
         $decoded['metadata']
     );
 } else if (isset($decoded['login']) && isset($decoded['user_id'])) {
-    \DDTrace\ATO\V2\track_user_login_success(
+    \datadog\appsec\v2\track_user_login_success(
         $decoded['login'],
         $decoded['user_id']
     );
 } else {
-    \DDTrace\ATO\V2\track_user_login_success(
+    \datadog\appsec\v2\track_user_login_success(
         $decoded['login']
     );
 }
