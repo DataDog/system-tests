1abefb3336b03f8fff58b72f450fcd793b473720
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-06-25 18:09:57 +0200

    disable gevent patch for uwsgi

--- utils/build/docker/python/flask/app.py
+++ utils/build/docker/python/flask/app.py
@@ -1,8 +1,12 @@
-import ddtrace.auto  # noqa: E402
-import gevent  # noqa: E402
-from gevent import monkey  # noqa: E402
+import sys
 
-monkey.patch_all(thread=True)  # noqa: E402
+if sys.environ.get("UWSGI_ENABLED", "false") == "false":
+    # Patch with gevent but not for uwsgi-poc
+    import ddtrace.auto  # noqa: E402
+    import gevent  # noqa: E402
+    from gevent import monkey  # noqa: E402
+
+    monkey.patch_all(thread=True)  # noqa: E402
 
 
 import base64
@@ -1702,7 +1706,7 @@ def create_extra_service():
 @app.route("/requestdownstream/", methods=["GET", "POST", "OPTIONS"])
 def request_downstream():
     # Propagate the received headers to the downstream service
-    http_poolmanager = urllib3.PoolManager(num_pools=4)
+    http_poolmanager = urllib3.PoolManager(num_pools=1)
     # Sending a GET request and getting back response as HTTPResponse object.
     response = http_poolmanager.request("GET", "http://localhost:7777/returnheaders")
     http_poolmanager.clear()
@@ -1723,7 +1727,7 @@ def return_headers(*args, **kwargs):
 def vulnerable_request_downstream():
     weak_hash()
     # Propagate the received headers to the downstream service
-    http_poolmanager = urllib3.PoolManager(num_pools=4)
+    http_poolmanager = urllib3.PoolManager(num_pools=1)
     # Sending a GET request and getting back response as HTTPResponse object.
     response = http_poolmanager.request("GET", "http://localhost:7777/returnheaders")
     http_poolmanager.clear()
