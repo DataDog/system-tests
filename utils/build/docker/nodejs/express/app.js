778503573e2b17d9b36fcf37401b9a7fc582ba6d
Munir Abdinur <munir_abdinur@hotmail.com>
2025-06-25 20:02:30 -0400

    update manifests and lint app.js

--- utils/build/docker/nodejs/express/app.js
+++ utils/build/docker/nodejs/express/app.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const http = require('http')
 const fs = require('fs')
 const crypto = require('crypto')
-const winston = require('winston');
+const winston = require('winston')
 const api = require('@opentelemetry/api')
 
 const iast = require('./iast')
@@ -43,20 +43,19 @@ const { sqsProduce, sqsConsume } = require('./integrations/messaging/aws/sqs')
 const { kafkaProduce, kafkaConsume } = require('./integrations/messaging/kafka/kafka')
 const { rabbitmqProduce, rabbitmqConsume } = require('./integrations/messaging/rabbitmq/rabbitmq')
 
-
 // Unstructured logging (plain text)
 const plainLogger = winston.createLogger({
   level: 'info',
   format: winston.format.simple(), // plain text
   transports: [new winston.transports.Console()]
-});
+})
 
 // Structured logging (JSON)
 const jsonLogger = winston.createLogger({
   level: 'info',
   format: winston.format.json(), // structured
   transports: [new winston.transports.Console()]
-});
+})
 
 iast.initData().catch(() => {})
 
@@ -337,7 +336,7 @@ app.get('/kafka/consume', (req, res) => {
 
 app.get('/log/library', (req, res) => {
   const msg = req.query.msg || 'msg'
-  const logger = (req.query.structured ?? true) ? jsonLogger : plainLogger;
+  const logger = (req.query.structured ?? true) ? jsonLogger : plainLogger
   switch (req.query.level) {
     case 'warn':
       logger.warn(msg)
