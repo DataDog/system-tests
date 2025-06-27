62f6fbc0c5dbf0e32ef11ef6f31b7b10922a9535
Sydney Tung <sydney.tung@datadoghq.com>
2025-06-27 16:17:11 -0400

    update for python bug

--- tests/docker_ssi/test_docker_ssi.py
+++ tests/docker_ssi/test_docker_ssi.py
@@ -157,8 +157,7 @@ class TestDockerSSIFeatures:
         self._setup_all()
 
     @features.ssi_injection_metadata
-    @missing_feature(context.library in ("nodejs", "dotnet", "java", "php", "ruby"), reason="Not implemented yet")
-    @missing_feature(context.library < "python@3.9.5.dev", reason="Not implemented")
+    @missing_feature(context.library in ("python", "nodejs", "dotnet", "java", "php", "ruby"), reason="Not implemented yet")
     def test_injection_metadata(self):
         logger.info("Testing injection result variables")
         events = interfaces.test_agent.get_injection_metadata_for_autoinject()
