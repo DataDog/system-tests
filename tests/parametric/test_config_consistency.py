30fae208efb0fd350b4e8f1321d087732879e4c4
Mikayla Toffler <46911781+mtoffl01@users.noreply.github.com>
2025-07-01 12:04:10 -0400

    Enable Stable Config Phase 1 tests (#4799)
    Co-authored-by: Manuel Álvarez Álvarez <manuel-alvarez-alvarez@users.noreply.github.com>
    Co-authored-by: Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
    Co-authored-by: Flavien Darche <11708575+e-n-0@users.noreply.github.com>
    Co-authored-by: Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>
    Co-authored-by: Charles de Beauchesne <charles.debeauchesne@datadoghq.com>

--- tests/parametric/test_config_consistency.py
+++ tests/parametric/test_config_consistency.py
@@ -386,13 +386,19 @@ class Test_Config_Dogstatsd:
 
 SDK_DEFAULT_STABLE_CONFIG = {
     "dd_runtime_metrics_enabled": "false" if context.library != "java" else "true",
-    "dd_profiling_enabled": "false"
-    if context.library != "php"
-    else "1",  # Profiling is enabled as "1" by default in PHP if loaded
+    "dd_profiling_enabled": "1"
+    if context.library == "php"
+    else "true"
+    if context.library == "golang"
+    else "false",  # Profiling is enabled as "1" by default in PHP if loaded. As for Go, the profiler must be started manually, so it is enabled by default when started
     "dd_data_streams_enabled": "false",
-    "dd_logs_injection": {"ruby": "true", "java": "true", "python": "structured", "nodejs": "structured"}.get(
-        context.library.name, "false"
-    ),  # Enabled by default in ruby, set to None in python
+    "dd_logs_injection": {
+        "ruby": "true",
+        "java": "true",
+        "golang": None,
+        "python": "structured",
+        "nodejs": "structured",
+    }.get(context.library.name, "false"),  # Enabled by default in ruby
 }
 
 
@@ -460,7 +466,11 @@ class Test_Stable_Config_Default(StableConfigWriter):
                 },
                 {
                     **SDK_DEFAULT_STABLE_CONFIG,
-                    "dd_logs_injection": "true" if context.library != "ruby" else "false",
+                    "dd_logs_injection": None
+                    if context.library == "golang"
+                    else "false"
+                    if context.library == "ruby"
+                    else "true",  # Logs injection is not supported in dd-trace-go and enabled by default in ruby
                 },
             ),
         ],
@@ -473,7 +483,9 @@ class Test_Stable_Config_Default(StableConfigWriter):
             "/etc/datadog-agent/application_monitoring.yaml",
         ],
     )
-    def test_default_config(self, test_library, path, library_env, name, apm_configuration_default, expected):
+    def test_default_config(
+        self, test_agent, test_library, path, library_env, name, apm_configuration_default, expected
+    ):
         with test_library:
             self.write_stable_config(
                 {
@@ -486,7 +498,7 @@ class Test_Stable_Config_Default(StableConfigWriter):
             config = test_library.config()
             assert expected.items() <= config.items()
 
-    @pytest.mark.parametrize("library_env", [{}])
+    # @pytest.mark.parametrize("library_env", [{}])
     @pytest.mark.parametrize(
         "test",
         [
@@ -497,7 +509,11 @@ class Test_Stable_Config_Default(StableConfigWriter):
                 },
                 "expected": {
                     **SDK_DEFAULT_STABLE_CONFIG,
-                    "dd_logs_injection": "true",
+                    "dd_logs_injection": None
+                    if context.library == "golang"
+                    else "false"
+                    if context.library == "ruby"
+                    else "true",  # Logs injection is not supported in dd-trace-go and enabled by default in ruby
                 },
             },
         ],
@@ -509,7 +525,7 @@ class Test_Stable_Config_Default(StableConfigWriter):
             "/etc/datadog-agent/application_monitoring.yaml",
         ],
     )
-    def test_unknown_key_skipped(self, test_library, path, library_env, test):
+    def test_unknown_key_skipped(self, test_agent, test_library, path, library_env, test):
         with test_library:
             self.write_stable_config(
                 {
@@ -530,7 +546,7 @@ class Test_Stable_Config_Default(StableConfigWriter):
             "/etc/datadog-agent/application_monitoring.yaml",
         ],
     )
-    def test_invalid_files(self, test_library, path, library_env):
+    def test_invalid_files(self, test_agent, test_library, path, library_env):
         with test_library:
             self.write_stable_config_content(
                 "🤖 👾; 🤖\t\n\n --- `💣",
