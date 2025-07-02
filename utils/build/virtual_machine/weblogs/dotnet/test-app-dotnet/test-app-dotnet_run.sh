da764f8a7c14af2d016c94105cdf9ff707e1add4
Robert Pickering <robert.pickering@datadoghq.com>
2025-07-02 09:31:21 +0200

    try and get a core dump from failing dotnet tests

--- utils/build/virtual_machine/weblogs/dotnet/test-app-dotnet/test-app-dotnet_run.sh
+++ utils/build/virtual_machine/weblogs/dotnet/test-app-dotnet/test-app-dotnet_run.sh
@@ -7,10 +7,14 @@ sudo chmod -R 755 *
 echo "START dotnet APP (debug active)"
 #If we are trying to inject the library on the "restore" or "build" command we should show the traces
 export DD_APM_INSTRUMENTATION_DEBUG=false
-export DOTNET_DbgEnableMiniDump=1
-export DOTNET_DbgMiniDumpType=4
-export DOTNET_CreateDumpDiagnostics=1
-export DOTNET_DbgMiniDumpName=/var/log/datadog/dotnet/coredump.txt
+export COMPlus_DbgEnableMiniDump=1
+export  DOTNET_DbgEnableMiniDump=1
+export COMPlus_DbgMiniDumpType=4
+export  DOTNET_DbgMiniDumpType=4
+export COMPlus_CreateDumpDiagnostics=1
+export  DOTNET_CreateDumpDiagnostics=1
+export DOTNET_DbgMiniDumpName=/var/log/datadog/dotnet/coredump.%t.%p
+export DOTNET_EnableCrashReport=1
 
 #We are running the app for dotnet 6.0
 sudo sed -i "s/net7.0/net6.0/g" MinimalWebApp.csproj 
@@ -27,6 +31,6 @@ sudo dotnet publish -c Release -o /home/datadog
 #Copy app service and start it
 export DD_APM_INSTRUMENTATION_DEBUG=true
 sudo chmod 755 create_and_run_app_service.sh
-./create_and_run_app_service.sh "dotnet MinimalWebApp.dll" "ASPNETCORE_URLS=http://+:5985 DOTNET_DbgEnableMiniDump=1 DOTNET_DbgMiniDumpType=4"
+./create_and_run_app_service.sh "dotnet MinimalWebApp.dll" "ASPNETCORE_URLS=http://+:5985"
 
 echo "RUN dotnet DONE"
