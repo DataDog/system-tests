cb6fc6a20930581bc56631b3c855ed7ecb966a7a
Florentin Labelle <florentin.labelle@datadoghq.com>
2025-07-04 14:05:33 +0200

    Fix build lambda layer issue

--- utils/build/build.sh
+++ utils/build/build.sh
@@ -269,7 +269,7 @@ build() {
                 --load \
                 --progress=plain \
                 -f utils/build/docker/lambda-proxy.Dockerfile \
-                -t datadog/system-tests:lambda-proxy \
+                -t system_tests/lambda-proxy \
                 $EXTRA_DOCKER_ARGS \
                 .
         else
