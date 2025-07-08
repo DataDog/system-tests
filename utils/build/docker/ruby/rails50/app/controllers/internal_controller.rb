aabcf0dfe818f30b5c58ac265530c895017b91cd
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-07-08 15:07:07 +0200

    Add flush endpoint and finish tests

--- utils/build/docker/ruby/rails50/app/controllers/internal_controller.rb
+++ utils/build/docker/ruby/rails50/app/controllers/internal_controller.rb
@@ -16,6 +16,8 @@ class InternalController < ApplicationController
   def flush
     # NOTE: If anything needs to be flushed here before the test suite ends,
     #       this is the place to do it.
+    #
+    # See https://github.com/DataDog/system-tests/blob/64539d1d19d14e0ab040d8e4a01562da1531b7d5/docs/internals/flushing.md
     Datadog.send(:components)&.telemetry&.flush
 
     render plain: 'OK'
