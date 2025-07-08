3266170d4da35acf2b2802f74c95c5f31cb1b3c1
Florentin Labelle <florentin.labelle@datadoghq.com>
2025-07-08 13:53:06 +0200

    Easy fixes after review

--- utils/_context/_scenarios/__init__.py
+++ utils/_context/_scenarios/__init__.py
@@ -1072,7 +1072,6 @@ class _Scenarios:
     appsec_lambda_default = LambdaScenario(
         "APPSEC_LAMBDA_DEFAULT",
         doc="Default Lambda scenario",
-        scenario_groups=[scenario_groups.appsec_lambda],
     )
 
 
