1245b2200c2c515eb7a3f4052a2969d2fd896e29
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-07-08 16:40:25 +0200

    rails80: patch controllers and routes for new endpoints

---
+++ utils/build/docker/ruby/rails80/app/controllers/internal_controller.rb
@@ -0,0 +1,25 @@
+class InternalController < ApplicationController
+  def healthcheck
+    gemspec = Gem.loaded_specs['datadog'] || Gem.loaded_specs['ddtrace']
+    version = gemspec.version.to_s
+    version = "#{version}-dev" unless gemspec.source.is_a?(Bundler::Source::Rubygems)
+
+    render json: {
+      status: 'ok',
+      library: {
+        name: 'ruby',
+        version: version
+      }
+    }
+  end
+
+  def flush
+    # NOTE: If anything needs to be flushed here before the test suite ends,
+    #       this is the place to do it.
+    #
+    # See https://github.com/DataDog/system-tests/blob/64539d1d19d14e0ab040d8e4a01562da1531b7d5/docs/internals/flushing.md
+    Datadog.send(:components)&.telemetry&.flush
+
+    render plain: 'OK'
+  end
+end 
