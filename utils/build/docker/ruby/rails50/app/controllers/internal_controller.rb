bb5f30c6b91db3a9e0a769a5b808f258302f9a2e
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2025-07-10 16:55:26 +0200

    Try in thread flushing

--- utils/build/docker/ruby/rails50/app/controllers/internal_controller.rb
+++ utils/build/docker/ruby/rails50/app/controllers/internal_controller.rb
@@ -1,8 +1,4 @@
-require 'timeout'
-
 class InternalController < ApplicationController
-  FLUSH_MAX_WAIT_S = 8
-
   def healthcheck
     gemspec = Gem.loaded_specs['datadog'] || Gem.loaded_specs['ddtrace']
     version = gemspec.version.to_s
@@ -17,12 +13,18 @@ class InternalController < ApplicationController
     #       See https://github.com/DataDog/system-tests/blob/64539d1d19d14e0ab040d8e4a01562da1531b7d5/docs/internals/flushing.md
     #
     # WARNING: To be able to respond on time to the system-tests we are flushing
-    #          and time-boxing us to time less than system-tests allows
+    #          in the thread and time-boxing us to time less than system-tests allows
     #          See: https://github.com/DataDog/system-tests/blob/7d555e474e6ce32825dd79c1b65ac64805ab09a8/utils/_context/_scenarios/endtoend.py#L547
-    begin
-      Timeout.timeout(FLUSH_MAX_WAIT_S) { Datadog.send(:components)&.telemetry&.flush }
-    rescue Timeout::Error
-      Rails.logger.warn("Unable to flush telemetry within `#{FLUSH_MAX_WAIT_S}` second(s)")
+    started_at = Time.now
+    max_wait_time_seconds = 5
+
+    thread = Thread.new { Datadog.send(:components)&.telemetry&.flush rescue nil }
+
+    loop do
+      break if Time.now - started_at >= max_wait_time_seconds
+      break unless thread.alive?
+
+      sleep(0.1)
     end
 
     render plain: 'OK'
