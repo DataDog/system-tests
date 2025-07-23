af1a3572950493bc4b9c3c053225afc08b212b2c
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-07-23 10:52:00 +0200

    WIP

--- tests/appsec/test_remote_config_rule_changes.py
+++ tests/appsec/test_remote_config_rule_changes.py
@@ -460,19 +460,27 @@ FOO_ACTION = (
 @features.changing_rules_using_rc
 class Test_Unknown_Action:
     def setup_unknown_action(self):
-        self.config_state_1 = rc.rc_state.set_config(*FOO_ACTION).apply()
+        self.config_state_1 = rc.rc_state.reset().set_config(*CONFIG_ENABLED).apply()
         self.response_1 = weblog.get("/waf/", headers={"User-Agent": "dd-test-scanner-log-block"})
 
-        self.config_state_2 = rc.rc_state.reset().apply()
+        self.config_state_2 = rc.rc_state.set_config(*FOO_ACTION).apply()
         self.response_2 = weblog.get("/waf/", headers={"User-Agent": "dd-test-scanner-log-block"})
+
+        self.config_state_3 = rc.rc_state.reset().apply()
+        self.response_3 = weblog.get("/waf/", headers={"User-Agent": "dd-test-scanner-log-block"})
 
     def test_unknown_action(self):
         assert self.config_state_1.state == rc.ApplyState.ACKNOWLEDGED
-        assert self.response_1.status_code == 200
+        interfaces.library.assert_waf_attack(self.response_1, rule="ua0-600-56x")
+        assert self.response_1.status_code == 403
 
         assert self.config_state_2.state == rc.ApplyState.ACKNOWLEDGED
-        assert self.response_2.status_code == 200
-        interfaces.library.assert_no_appsec_event(self.response_2)
+        interfaces.library.assert_waf_attack(self.response_2, rule="ua0-600-56x")
+        assert self.response_2.status_code == 403
+
+        assert self.config_state_3.state == rc.ApplyState.ACKNOWLEDGED
+        assert self.response_3.status_code == 200
+        interfaces.library.assert_no_appsec_event(self.response_3)
 
 
 # tests that the library can handle multiple actions in the same remote config.
