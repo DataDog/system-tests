081e0f7f08e570c2930fb60abc484a2e91068e3c
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-07-29 09:34:08 +0200

    WIP

--- tests/appsec/test_remote_config_rule_changes.py
+++ tests/appsec/test_remote_config_rule_changes.py
@@ -14,7 +14,6 @@ from utils import remote_config as rc
 from utils import rfc
 from utils import scenarios
 from utils import weblog
-from typing import Never
 
 CONFIG_ENABLED = (
     "datadog/2/ASM_FEATURES/asm_features_activation/config",
@@ -522,30 +521,30 @@ class Test_Multiple_Actions:
         interfaces.library.assert_no_appsec_event(self.response_3)
 
 
-EMPTY_FILE: tuple[str, dict[Never, Never]] = (
-    "datadog/2/ASM_DATA-base/ASM_DATA-base/config",
-    {},
-)
+EMPTY_CONFIG: tuple[str, dict] = ("datadog/2/ASM/actions/config", {})
 
 
 @scenarios.appsec_runtime_activation
-class Test_Empty_File:
-    def setup_empty_file(self):
+@features.changing_rules_using_rc
+class Test_Empty_Config:
+    def setup_empty_config(self):
         self.config_state_1 = rc.rc_state.reset().set_config(*CONFIG_ENABLED).apply()
         self.response_1 = weblog.get("/waf/", headers={"User-Agent": "dd-test-scanner-log-block"})
 
-        self.config_state_2 = rc.rc_state.set_config(*EMPTY_FILE).apply()
+        self.config_state_2 = rc.rc_state.set_config(*EMPTY_CONFIG).apply()
         self.response_2 = weblog.get("/waf/", headers={"User-Agent": "dd-test-scanner-log-block"})
 
         self.config_state_3 = rc.rc_state.reset().apply()
         self.response_3 = weblog.get("/waf/", headers={"User-Agent": "dd-test-scanner-log-block"})
 
-    def test_empty_file(self):
+    def test_empty_config(self):
         assert self.config_state_1.state == rc.ApplyState.ACKNOWLEDGED
         interfaces.library.assert_waf_attack(self.response_1, rule="ua0-600-56x")
         assert self.response_1.status_code == 403
 
-        assert self.config_state_2.state == rc.ApplyState.ACKNOWLEDGED  # empty file should not be sent to the WAF
+        assert (
+            self.config_state_2.configs["actions"]["apply_state"] != rc.ApplyState.ERROR
+        )  # empty config should not be sent to the WAF
         interfaces.library.assert_waf_attack(self.response_2, rule="ua0-600-56x")
         assert self.response_2.status_code == 403
 
