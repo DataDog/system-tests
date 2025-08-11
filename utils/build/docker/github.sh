de4daa238130b011d4bc3177f6424abc2528b8e6
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-08-11 17:07:41 +0200

    fix script

--- utils/build/docker/github.sh
+++ utils/build/docker/github.sh
@@ -7,24 +7,28 @@
 set -euo pipefail
 
 get_authentication_header() {
+
     local github_token_file="/run/secrets/github_token"
     local github_auth_header=()
 
     if [[ -f "$github_token_file" ]]; then
         echo "🔒 Using GitHub token for authentication" >&2
-        github_auth_header=(-H "Authorization: Bearer $(<"$github_token_file")")
+        echo "-H \"Authorization: Bearer $(<"$github_token_file")\""
+    else
+        echo "⚠️ Using unauthenticated call to github" >&2
+        echo ""
     fi
-
-    echo "${github_auth_header[@]}"
 }
 
 get_latest_release() {
     local repo="$1"
     local releases
-    local auth_header=()
-    IFS=' ' read -r -a auth_header <<< "$(get_authentication_header)"
+    local auth_header
+    auth_header=$(get_authentication_header)
 
-    if ! releases=$(curl --fail --retry 3 "${auth_header[@]}" "https://api.github.com/repos/${repo}/releases/latest"); then
+    local curl_cmd="curl --fail --retry 3 $auth_header https://api.github.com/repos/${repo}/releases/latest"
+
+    if ! releases=$(eval "$curl_cmd"); then
         echo "❌ Failed to get latest release for ${repo}" >&2
         exit 1
     fi
