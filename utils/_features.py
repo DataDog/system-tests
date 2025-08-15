9220ceeb427e8796684a888abf0c895d11cc4caf
Marco Costa <marco.costa@datadoghq.com>
2025-08-15 16:48:15 -0700

    [GraphQL] Test Error Tracking span events

--- utils/_features.py
+++ utils/_features.py
@@ -2151,12 +2151,20 @@ class _Features:
         return _mark_test_object(test_object, feature_id=343, owner=_Owner.asm)
 
     @staticmethod
-    def graphql_query_error_reporting(test_object):
-        """GraphQL query error reporting
+    def graphql_operation_error_reporting(test_object):
+        """GraphQL operation error reporting
 
         https://feature-parity.us1.prod.dog/#/?feature=354
         """
         return _mark_test_object(test_object, feature_id=354, owner=_Owner.idm)
+
+    @staticmethod
+    def graphql_operation_error_tracking(test_object):
+        """GraphQL operation error tracking
+
+        https://feature-parity.us1.prod.dog/#/?feature=485
+        """
+        return _mark_test_object(test_object, feature_id=485, owner=_Owner.idm)
 
     @staticmethod
     def envoy_external_processing(test_object):
