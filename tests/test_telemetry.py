9e2626cefa12e7d6067ef4a4a4d49e8ae9531013
Brett Langdon <brett.langdon@datadoghq.com>
2025-08-21 15:46:33 -0400

    Update test_telemetry.py

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -808,7 +808,7 @@ class Test_TelemetryV2:
         reason="Re-enable when this automatically updates the dd-go files.",
     )
     @irrelevant(
-        condition=True, reason="This test causes to many friction. It has been replaced by alerts on slack channels"
+        condition=context.library not in ("python", ), reason="This test causes to many friction. It has been replaced by alerts on slack channels"
     )
     def test_config_telemetry_completeness(self):
         """Assert that config telemetry is handled properly by telemetry intake
