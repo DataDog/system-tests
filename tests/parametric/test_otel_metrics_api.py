205a1ffa2a44a7444209903b3274334ba17751ae
Zach Montoya <zach.montoya@datadoghq.com>
2025-08-25 10:40:19 -0700

    Update tests/parametric/test_otel_metrics_api.py to use hypothesis to test the OTel Metrics Counter API against ranges of non-negative values and negative values

--- tests/parametric/test_otel_metrics_api.py
+++ tests/parametric/test_otel_metrics_api.py
@@ -4,6 +4,8 @@ import json
 import pytest
 import pprint
 
+from hypothesis import given, settings, HealthCheck, strategies as st
+
 from utils.parametric._library_client import Link
 from opentelemetry.trace import StatusCode
 from opentelemetry.trace import SpanKind
@@ -25,17 +27,20 @@ EXPECTED_TAGS = [("foo", "bar1"), ("baz", "qux1")]
 DEFAULT_ENVVARS = {
     "DD_TRACE_OTEL_ENABLED": "true",
     "DD_METRICS_OTEL_ENABLED": "true",
-    "OTEL_METRIC_EXPORT_INTERVAL": "1000", # Reduce the interval to speed up the tests
+    "OTEL_METRIC_EXPORT_INTERVAL": "100", # Reduce the interval to speed up the tests
     "CORECLR_ENABLE_PROFILING": "1",
 }
 
 @scenarios.parametric
 @features.otel_metrics_api
 class Test_Otel_Metrics_Api:
+    # This test takes upwards of 25 seconds to run
     @pytest.mark.parametrize("library_env", [{**DEFAULT_ENVVARS}])
-    def test_otel_counter_add_positive_value(self, test_agent, test_library):
+    @given(st.integers(min_value=0, max_value=2**32)) # Limit the range of integers to avoid int/float equality issues
+    @settings(suppress_health_check=[HealthCheck.function_scoped_fixture], deadline=None, max_examples=20) # Limit the number of examples to speed up the test
+    def test_otel_counter_add_non_negative_value(self, test_agent, test_library, n):
         meter_name = "parametric-api"
-        counter_name = "counter1"
+        counter_name = f"counter1-{n}"
         counter_unit = "triggers"
         counter_description = "test_description"
         expected_scope_attributes = {"scope.attr": "scope.value"}
@@ -45,10 +50,10 @@ class Test_Otel_Metrics_Api:
             t.disable_traces_flush()
             t.otel_get_meter(name=meter_name, version="1.0.0", schema_url="https://opentelemetry.io/schemas/1.27.0", attributes=expected_scope_attributes)
             t.otel_create_counter(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description)
-            t.otel_counter_add(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description, value=2, attributes=expected_add_attributes)
-            time.sleep(5)
+            t.otel_counter_add(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description, value=n, attributes=expected_add_attributes)
+            time.sleep(0.5)
 
-        metrics_data = test_agent.wait_for_num_otlp_metrics(num=1)
+        metrics_data = test_agent.wait_for_num_otlp_metrics(num=1, clear=True)
         pprint.pprint(metrics_data)
 
         # Assert that there is only one item in ResourceMetrics
@@ -67,7 +72,7 @@ class Test_Otel_Metrics_Api:
         assert scope_metrics[0]["schema_url"] == "https://opentelemetry.io/schemas/1.27.0"
 
         counter = scope_metrics[0]["metrics"][0]
-        assert counter["name"] == "counter1"
+        assert counter["name"] == counter_name
         assert counter["unit"] == "triggers"
         assert counter["description"] == "test_description"
 
@@ -75,14 +80,17 @@ class Test_Otel_Metrics_Api:
         assert counter["sum"]["is_monotonic"] == True
 
         sum_data_point = counter["sum"]["data_points"][0]
-        assert sum_data_point["as_double"] == 2.0
+        assert sum_data_point["as_double"] == n
         assert set(expected_add_attributes) == set({item['key']:item['value']['string_value'] for item in sum_data_point["attributes"]})
         assert "time_unix_nano" in sum_data_point
 
+    # This test takes upwards of 25 seconds to run
     @pytest.mark.parametrize("library_env", [{**DEFAULT_ENVVARS}])
-    def test_otel_counter_add_zero_value(self, test_agent, test_library):
+    @given(st.integers(min_value=-2**32, max_value=-1)) # Limit the range of integers to avoid int/float equality issues
+    @settings(suppress_health_check=[HealthCheck.function_scoped_fixture], deadline=None, max_examples=20) # Limit the number of examples to speed up the test
+    def test_otel_counter_add_negative_value(self, test_agent, test_library, n):
         meter_name = "parametric-api"
-        counter_name = "counter1"
+        counter_name = f"counter1-{n}"
         counter_unit = "triggers"
         counter_description = "test_description"
         expected_scope_attributes = {"scope.attr": "scope.value"}
@@ -92,64 +100,12 @@ class Test_Otel_Metrics_Api:
             t.disable_traces_flush()
             t.otel_get_meter(name=meter_name, version="1.0.0", schema_url="https://opentelemetry.io/schemas/1.27.0", attributes=expected_scope_attributes)
             t.otel_create_counter(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description)
-            t.otel_counter_add(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description, value=0, attributes=expected_add_attributes)
-            time.sleep(5)
+            t.otel_counter_add(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description, value=n, attributes=expected_add_attributes)
+            time.sleep(0.5)
 
-        metrics_data = test_agent.wait_for_num_otlp_metrics(num=1)
+        metrics_data = test_agent.wait_for_num_otlp_metrics(num=0, clear=True)
         pprint.pprint(metrics_data)
-
-        # Assert that there is only one item in ResourceMetrics
-        assert len(metrics_data) == 1
-
-        # Assert that the ResourceMetrics has the expected ScopeMetrics
-        resource_metrics = metrics_data[0]["resource_metrics"]
-        scope_metrics = resource_metrics[0]["scope_metrics"]
-        assert len(scope_metrics) == 1
-
-        # Assert that the ScopeMetrics has the correct Scope, SchemaUrl, and Metrics data
-        assert scope_metrics[0]["scope"]["name"] == "parametric-api"
-        assert scope_metrics[0]["scope"]["version"] == "1.0.0"
-        assert set(expected_scope_attributes) == set({item['key']:item['value']['string_value'] for item in scope_metrics[0]["scope"]["attributes"]})
-
-        assert scope_metrics[0]["schema_url"] == "https://opentelemetry.io/schemas/1.27.0"
-
-        counter = scope_metrics[0]["metrics"][0]
-        assert counter["name"] == "counter1"
-        assert counter["unit"] == "triggers"
-        assert counter["description"] == "test_description"
-
-        assert counter["sum"]["aggregation_temporality"].casefold() == "AGGREGATION_TEMPORALITY_DELTA".casefold()
-        assert counter["sum"]["is_monotonic"] == True
-
-        sum_data_point = counter["sum"]["data_points"][0]
-        assert sum_data_point["as_double"] == 0
-        assert set(expected_add_attributes) == set({item['key']:item['value']['string_value'] for item in sum_data_point["attributes"]})
-        assert "time_unix_nano" in sum_data_point
-
-    @pytest.mark.parametrize("library_env", [{**DEFAULT_ENVVARS}])
-    def test_otel_counter_add_negative_value(self, test_agent, test_library):
-        meter_name = "parametric-api"
-        counter_name = "counter1"
-        counter_unit = "triggers"
-        counter_description = "test_description"
-        expected_scope_attributes = {"scope.attr": "scope.value"}
-        expected_add_attributes = {"test_attr": "test_value"}
-
-        with test_library as t:
-            t.disable_traces_flush()
-            t.otel_get_meter(name=meter_name, version="1.0.0", schema_url="https://opentelemetry.io/schemas/1.27.0", attributes=expected_scope_attributes)
-            t.otel_create_counter(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description)
-            t.otel_counter_add(meter_name=meter_name, name=counter_name, unit=counter_unit, description=counter_description, value=-1, attributes=expected_add_attributes)
-            time.sleep(5)
-
-        try:
-            metrics_data = test_agent.wait_for_num_otlp_metrics(num=1)
-        except ValueError as e:
-            if "Number (1) of metrics not available from test agent" not in str(e):
-                raise e
-            
-            # In this scenario, it is expected that no metrics will be reported to the test agent
-            return
+        assert len(metrics_data) == 0
 
     @pytest.mark.parametrize(
         "library_env",
