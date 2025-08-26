221ad2471ea350b240bf7c56ccdd6d76d337bb79
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-08-26 11:03:20 -0400

    [python]: re-enable python crashtracker test for ssi

--- tests/docker_ssi/test_docker_ssi_crash.py
+++ tests/docker_ssi/test_docker_ssi_crash.py
@@ -42,7 +42,6 @@ class TestDockerSSICrash:
     )
     @irrelevant(context.library == "python" and context.installed_language_runtime < "3.7.0")
     @irrelevant(context.library == "nodejs" and context.installed_language_runtime < "17.0")
-    @bug(context.library >= "python@3.0.0.dev", reason="INPLAT-603")
     def test_crash(self):
         """Validate that a crash report is generated when the application crashes"""
         logger.info(f"Testing Docker SSI crash tracking: {context.library.name}")
