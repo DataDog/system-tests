844ceb35126d764ea301904d1cc0fbc363783ea3
Zach Montoya <zach.montoya@datadoghq.com>
2025-08-26 11:15:28 -0700

    Add tests for the OTLP Metrics Asynchronous Gauge API, using Hypothesis property based testing

--- utils/parametric/_library_client.py
+++ utils/parametric/_library_client.py
@@ -523,6 +523,12 @@ class APMLibraryClient:
             json={"meter_name": meter_name, "name": name, "unit": unit, "description": description, "value": value, "attributes": attributes},
         )
 
+    def otel_create_asynchronous_gauge(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None) -> None:
+        self._session.post(
+            self._url("/metrics/otel/create_asynchronous_gauge"),
+            json={"meter_name": meter_name, "name": name, "unit": unit, "description": description, "value": value, "attributes": attributes},
+        )
+
 
 class _TestSpan:
     def __init__(self, client: APMLibraryClient, span_id: int, trace_id: int):
@@ -771,6 +777,9 @@ class APMLibrary:
 
     def otel_create_asynchronous_updowncounter(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None) -> None:
         self._client.otel_create_asynchronous_updowncounter(meter_name, name, unit, description, value, attributes)
+
+    def otel_create_asynchronous_gauge(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None) -> None:
+        self._client.otel_create_asynchronous_gauge(meter_name, name, unit, description, value, attributes)
 
     def is_alive(self) -> bool:
         try:
