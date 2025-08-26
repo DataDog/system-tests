f94234677145f964cf0b1fa51c8173de6b0ad55d
Zach Montoya <zach.montoya@datadoghq.com>
2025-08-26 10:34:22 -0700

    Add tests for the OTLP Metrics Asynchronous Counter API, using Hypothesis property based testing

--- utils/parametric/_library_client.py
+++ utils/parametric/_library_client.py
@@ -511,6 +511,12 @@ class APMLibraryClient:
             json={"meter_name": meter_name, "name": name, "unit": unit, "description": description, "value": value, "attributes": attributes},
         )
 
+    def otel_create_asynchronous_counter(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None) -> None:
+        self._session.post(
+            self._url("/metrics/otel/create_asynchronous_counter"),
+            json={"meter_name": meter_name, "name": name, "unit": unit, "description": description, "value": value, "attributes": attributes},
+        )
+
 
 class _TestSpan:
     def __init__(self, client: APMLibraryClient, span_id: int, trace_id: int):
@@ -753,6 +759,9 @@ class APMLibrary:
 
     def otel_histogram_record(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None = None) -> None:
         self._client.otel_histogram_record(meter_name, name, unit, description, value, attributes)
+
+    def otel_create_asynchronous_counter(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None) -> None:
+        self._client.otel_create_asynchronous_counter(meter_name, name, unit, description, value, attributes)
 
     def is_alive(self) -> bool:
         try:
