fec2e94cd8f18f9eba54947a436ab6fb541ee86f
Zach Montoya <zach.montoya@datadoghq.com>
2025-08-26 09:57:21 -0700

    Add tests for the OTLP Metrics Histogram API, using Hypothesis property based testing

--- utils/parametric/_library_client.py
+++ utils/parametric/_library_client.py
@@ -444,6 +444,18 @@ class APMLibraryClient:
             json={"meter_name": meter_name, "name": name, "unit": unit, "description": description, "value": value, "attributes": attributes},
         )
 
+    def otel_create_histogram(self, meter_name: str, name: str, unit: str, description: str) -> None:
+        self._session.post(
+            self._url("/metrics/otel/create_histogram"),
+            json={"meter_name": meter_name, "name": name, "unit": unit, "description": description},
+        )
+
+    def otel_histogram_record(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None) -> None:
+        self._session.post(
+            self._url("/metrics/otel/histogram_record"),
+            json={"meter_name": meter_name, "name": name, "unit": unit, "description": description, "value": value, "attributes": attributes},
+        )
+
 
 class _TestSpan:
     def __init__(self, client: APMLibraryClient, span_id: int, trace_id: int):
@@ -674,6 +686,12 @@ class APMLibrary:
 
     def otel_gauge_record(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None = None) -> None:
         self._client.otel_gauge_record(meter_name, name, unit, description, value, attributes)
+
+    def otel_create_histogram(self, meter_name: str, name: str, unit: str, description: str) -> None:
+        self._client.otel_create_histogram(meter_name, name, unit, description)
+
+    def otel_histogram_record(self, meter_name: str, name: str, unit: str, description: str, value: float, attributes: dict | None = None) -> None:
+        self._client.otel_histogram_record(meter_name, name, unit, description, value, attributes)
 
     def is_alive(self) -> bool:
         try:
