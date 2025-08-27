856c109e79da63256bc2f027dcdad2fbd63e62a1
Florentin Labelle <florentin.labelle@datadoghq.com>
2025-08-27 09:24:28 +0200

    python_lambda: avoid rebuilding the lambda-proxy on each workflow run (#5103)

--- utils/build/build.sh
+++ utils/build/build.sh
@@ -270,7 +270,7 @@ build() {
                 --load \
                 --progress=plain \
                 -f utils/build/docker/lambda-proxy.Dockerfile \
-                -t system_tests/lambda-proxy \
+                -t datadog/system-tests:lambda-proxy \
                 $EXTRA_DOCKER_ARGS \
                 .
         else
