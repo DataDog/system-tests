2a191eb5c12b124dc333ac3148e0e2c17d59d600
Guilhem Charles <guilhem.charles@datadoghq.com>
2025-09-03 21:37:04 +0200

    ci: migrate to the new docker in docker runners

--- utils/build/build_python_base_images.sh
+++ utils/build/build_python_base_images.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
-# build and push python base images
-
-
-
-docker buildx build --load --progress=plain -f utils/build/docker/python/django-py3.13.base.Dockerfile -t datadog/system-tests:django-py3.13.base-v4 .
-docker buildx build --load --progress=plain -f utils/build/docker/python/fastapi.base.Dockerfile -t datadog/system-tests:fastapi.base-v6 .
-docker buildx build --load --progress=plain -f utils/build/docker/python/python3.12.base.Dockerfile -t datadog/system-tests:python3.12.base-v9 .
-docker buildx build --load --progress=plain -f utils/build/docker/python/django-poc.base.Dockerfile -t datadog/system-tests:django-poc.base-v7 .
-docker buildx build --load --progress=plain -f utils/build/docker/python/flask-poc.base.Dockerfile -t datadog/system-tests:flask-poc.base-v11 .
-docker buildx build --load --progress=plain -f utils/build/docker/python/uwsgi-poc.base.Dockerfile -t datadog/system-tests:uwsgi-poc.base-v7 .
-
 if [ "$1" = "--push" ]; then
-      docker push datadog/system-tests:django-py3.13.base-v4
-      docker push datadog/system-tests:fastapi.base-v6
-      docker push datadog/system-tests:python3.12.base-v9
-      docker push datadog/system-tests:django-poc.base-v7
-      docker push datadog/system-tests:flask-poc.base-v11
-      docker push datadog/system-tests:uwsgi-poc.base-v7
+    BUILD_ARGS="--push --progress=plain"
+else
+    BUILD_ARGS="--load --progress=plain"
 fi
+
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/django-py3.13.base.Dockerfile -t datadog/system-tests:django-py3.13.base-v4 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/fastapi.base.Dockerfile -t datadog/system-tests:fastapi.base-v6 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/python3.12.base.Dockerfile -t datadog/system-tests:python3.12.base-v9 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/django-poc.base.Dockerfile -t datadog/system-tests:django-poc.base-v7 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/flask-poc.base.Dockerfile -t datadog/system-tests:flask-poc.base-v11 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/uwsgi-poc.base.Dockerfile -t datadog/system-tests:uwsgi-poc.base-v7 .
 
--- utils/build/build_tracer_buddies.sh
+++ utils/build/build_tracer_buddies.sh
@@ -3,18 +3,15 @@
 # buddies are weblog app in another lang
 # they are used in the CROSSED_TRACING_LIBRARIES scenario, where we can tests data propagation between different languages
 
-docker buildx build --load --progress=plain -f utils/build/docker/python/flask-poc.Dockerfile -t datadog/system-tests:python_buddy-v1 .
-docker buildx build --load --progress=plain -f utils/build/docker/nodejs/express4.Dockerfile -t datadog/system-tests:nodejs_buddy-v1 .
-docker buildx build --load --progress=plain -f utils/build/docker/java/spring-boot.Dockerfile -t datadog/system-tests:java_buddy-v1 .
-docker buildx build --load --progress=plain -f utils/build/docker/ruby/rails70.Dockerfile -t datadog/system-tests:ruby_buddy-v1 .
-docker buildx build --load --progress=plain -f utils/build/docker/golang/net-http.Dockerfile -t datadog/system-tests:golang_buddy-v1 .
-
-
 if [ "$1" = "--push" ]; then
-      docker push datadog/system-tests:python_buddy-v1
-      docker push datadog/system-tests:nodejs_buddy-v1
-      docker push datadog/system-tests:java_buddy-v1
-      docker push datadog/system-tests:ruby_buddy-v1
-      docker push datadog/system-tests:golang_buddy-v1
+    BUILD_ARGS="--push --progress=plain"
+else
+    BUILD_ARGS="--load --progress=plain"
 fi
+
+docker buildx build $BUILD_ARGS -f utils/build/docker/python/flask-poc.Dockerfile -t datadog/system-tests:python_buddy-v1 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/nodejs/express4.Dockerfile -t datadog/system-tests:nodejs_buddy-v1 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/java/spring-boot.Dockerfile -t datadog/system-tests:java_buddy-v1 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/ruby/rails70.Dockerfile -t datadog/system-tests:ruby_buddy-v1 .
+docker buildx build $BUILD_ARGS -f utils/build/docker/golang/net-http.Dockerfile -t datadog/system-tests:golang_buddy-v1 .
 
