df7d4332378f3a593c332d42ec41812434fa3474
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2025-09-04 11:12:48 +0200

    Changing version number

--- utils/build/build_tracer_buddies.sh
+++ utils/build/build_tracer_buddies.sh
@@ -6,7 +6,7 @@
 docker buildx build --load --progress=plain -f utils/build/docker/python/flask-poc.Dockerfile -t datadog/system-tests:python_buddy-v1 .
 docker buildx build --load --progress=plain -f utils/build/docker/nodejs/express4.Dockerfile -t datadog/system-tests:nodejs_buddy-v1 .
 docker buildx build --load --progress=plain -f utils/build/docker/java/spring-boot.Dockerfile -t datadog/system-tests:java_buddy-v1 .
-docker buildx build --load --progress=plain -f utils/build/docker/ruby/rails72.Dockerfile -t datadog/system-tests:ruby_buddy-v1 .
+docker buildx build --load --progress=plain -f utils/build/docker/ruby/rails72.Dockerfile -t datadog/system-tests:ruby_buddy-v2 .
 docker buildx build --load --progress=plain -f utils/build/docker/golang/net-http.Dockerfile -t datadog/system-tests:golang_buddy-v1 .
 
 
@@ -14,7 +14,7 @@ if [ "$1" = "--push" ]; then
       docker push datadog/system-tests:python_buddy-v1
       docker push datadog/system-tests:nodejs_buddy-v1
       docker push datadog/system-tests:java_buddy-v1
-      docker push datadog/system-tests:ruby_buddy-v1
+      docker push datadog/system-tests:ruby_buddy-v2
       docker push datadog/system-tests:golang_buddy-v1
 fi
 
