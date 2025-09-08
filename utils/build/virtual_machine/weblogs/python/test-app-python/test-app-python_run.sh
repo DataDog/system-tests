6d6ce5684f6ccfc770ccc158f5b54e247ee2a79d
roberto montero <roberto.montero@datadoghq.com>
2025-09-08 07:50:34 +0200

    aws ssi install django 5.2.4

--- utils/build/virtual_machine/weblogs/python/test-app-python/test-app-python_run.sh
+++ utils/build/virtual_machine/weblogs/python/test-app-python/test-app-python_run.sh
@@ -9,6 +9,6 @@ eval "$(pyenv init -)"
 sudo chmod -R 755 * 
 
 sudo cp django_app.py /home/datadog/
-sudo pip3 install django || true
+sudo pip3 install django==5.2.4 || true
 ./create_and_run_app_service.sh "python -m django runserver 0.0.0.0:5985" "PYTHONUNBUFFERED=1 DJANGO_SETTINGS_MODULE=django_app"
 echo "RUN python DONE"
