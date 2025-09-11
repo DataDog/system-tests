d32f9389407487cd2246fa6c388485edf01b402e
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-09-11 21:04:57 +0200

    Fix few owners declarations

--- utils/_features.py
+++ utils/_features.py
@@ -7,14 +7,14 @@ class _Owner(StrEnum):
 
     apm_serverless = "@DataDog/apm-serverless"
     asm = "@DataDog/asm-libraries"  # application security monitoring
-    auto_instrumentation = "DataDog/teams/unified-instrumentation-setup"
+    auto_instrumentation = "@DataDog/unified-instrumentation-setup"
     data_pipeline = "@DataDog/libdatadog-apm"  # or agent-apm? TODO @ekump
     debugger = "@DataDog/debugger"
     idm = "@DataDog/apm-idm"
     language_platform = "@DataDog/apm-lang-platform"
     profiler = "@DataDog/profiling"  # it does not exists
     rp = "@DataDog/apm-reliability-and-performance"  # reliability & performance
-    sdk_capabilities = "DataDog/apm-sdk-api"
+    sdk_capabilities = "@DataDog/apm-sdk-api"
     tracer = "n/a"  # legacy client libraries from feature parity dashboard, need to be adressed to good teams
 
 
@@ -2089,7 +2089,7 @@ class _Features:
         https://feature-parity.us1.prod.dog/#/?feature=339
         """
         return _mark_test_object(
-            test_object, feature_id=339, owner=_Owner.tracer
+            test_object, feature_id=339, owner=_Owner.rp
         )  # tracing/otel/api, tracing/datadog/api
 
     @staticmethod
@@ -2398,7 +2398,7 @@ class _Features:
 
         https://feature-parity.us1.prod.dog/#/?feature=395
         """
-        return _mark_test_object(test_object, feature_id=395, owner=_Owner.tracer)
+        return _mark_test_object(test_object, feature_id=395, owner=_Owner.asm)
 
     @staticmethod
     def appsec_collect_all_headers(test_object):
@@ -2406,7 +2406,7 @@ class _Features:
 
         https://feature-parity.us1.prod.dog/#/?feature=390
         """
-        return _mark_test_object(test_object, feature_id=390, owner=_Owner.tracer)  # appsec/25q2
+        return _mark_test_object(test_object, feature_id=390, owner=_Owner.asm)  # appsec/25q2
 
     @staticmethod
     def appsec_collect_request_body(test_object):
@@ -2414,7 +2414,7 @@ class _Features:
 
         https://feature-parity.us1.prod.dog/#/?feature=391
         """
-        return _mark_test_object(test_object, feature_id=391, owner=_Owner.tracer)  # appsec/rasp, appsec/25q2
+        return _mark_test_object(test_object, feature_id=391, owner=_Owner.asm)  # appsec/rasp, appsec/25q2
 
     @staticmethod
     def referrer_hostname(test_object):
