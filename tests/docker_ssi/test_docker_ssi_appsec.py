f4027be84c02c34d907cb463982078779a30771e
Alberto Vara <alberto.vara@datadoghq.com>
2025-09-16 09:44:54 +0200

    add appsec ssi origin tests

--- tests/docker_ssi/test_docker_ssi_appsec.py
+++ tests/docker_ssi/test_docker_ssi_appsec.py
@@ -13,9 +13,20 @@ class TestDockerSSIAppsecFeatures:
         parsed_url = urlparse(scenarios.docker_ssi.weblog_url)
         self.r = weblog.request("GET", parsed_url.path, domain=parsed_url.hostname, port=parsed_url.port)
 
-    def test_telemetry_appsec(self):
-        # TODO Create a method on interfaces.test_agent to get the telemetry data for appsec
-        telemetry_autoinject_data = interfaces.test_agent.get_telemetry_for_autoinject()
-        assert len(telemetry_autoinject_data) >= 1
-        for data in telemetry_autoinject_data:
-            logger.info(f"Telemetry data: {data}")
+
+    def test_telemetry_source_ssi(self):
+        logger.info("Testing Docker SSI service tracking")
+        # There are traces related with the request
+        root_span = interfaces.test_agent.get_traces(request=self.r)
+        assert root_span, f"No traces found for request {self.r.get_rid()}"
+        assert "service" in root_span, f"No service name found in root_span: {root_span}"
+        # Get all captured telemetry configuration data
+        configurations = interfaces.test_agent.get_telemetry_configurations(
+            root_span["service"], root_span["meta"]["runtime-id"]
+        )
+
+        # Check that instrumentation source is ssi
+        injection_source = configurations.get("DD_APPSEC_ENABLED")
+        assert injection_source, f"instrumentation_source not found in configuration {configurations}"
+        assert injection_source["value"] ["value"] in ["1", 1, True], f"instrumentation_source value is not ssi {injection_source}"
+        assert injection_source["origin"] == "ssi", f"instrumentation_source value is not ssi {injection_source}"
