f8dd9d4f725f3a46c99a7f03b0ad927667713afd
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-09-17 19:20:52 +0200

    use xml for job summary

--- utils/scripts/get-summary.py
+++ utils/scripts/get-summary.py
@@ -2,8 +2,28 @@ import argparse
 from collections import defaultdict
 from enum import StrEnum
 import json
+import logging
 import os
 from pathlib import Path
+import sys
+import xml.etree.ElementTree as ET
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+REPORT_FILENAME = "reportJunit.xml"
+
+
+class PropertyName(StrEnum):
+    # testsuite
+    WEBLOG = "dd_tags[systest.suite.context.weblog_variant]"
+    SCENARIO = "dd_tags[systest.suite.context.scenario]"
+
+    # testcase
+    CODEOWNERS = "test.codeowners"
+    OUTCOME = "dd_tags[systest.case.outcome]"
 
 
 class Outcome(StrEnum):
@@ -16,20 +36,12 @@ class Outcome(StrEnum):
 
 
 class Test:
-    def __init__(self, context: dict, nodeid: str, outcome: str, metadata: dict, **kwargs: dict):
+    def __init__(self, nodeid: str, outcome: str, scenario: str, weblog: str, owners: list[str]):
         self.nodeid = nodeid
         self.outcome = Outcome(outcome)
-        self.metadata = metadata
-        self.extra = kwargs
-        self.context = context
-
-    @property
-    def scenario(self) -> str:
-        return self.context.get("scenario", "unknown")
-
-    @property
-    def weblog(self) -> str:
-        return self.context.get("weblog_variant", "unknown")
+        self.scenario = scenario
+        self.weblog = weblog
+        self.owners = owners
 
 
 class Summary:
@@ -43,7 +55,7 @@ class Summary:
         self.tests_by_outcome[test.outcome].append(test)
 
         if test.outcome == Outcome.failed:
-            for owner in test.metadata.get("owners", []):
+            for owner in test.owners:
                 if owner not in self.failures:
                     self.failures[owner] = []
                 self.failures[owner].append(test)
@@ -87,20 +99,42 @@ def crawl_folder(base_dir: Path, summary: Summary) -> None:
     for entry in os.listdir(base_dir):
         entry_path = Path(base_dir) / entry
 
-        if entry_path.is_file() and entry == "report.json":
+        if entry_path.is_file() and entry == REPORT_FILENAME:
             compute_file(entry_path, summary)
 
         elif entry_path.is_dir() and entry.startswith("logs"):
             crawl_folder(entry_path, summary)
 
 
-def compute_file(file_path: Path, summary: Summary) -> None:
-    with file_path.open("r") as f:
-        data = json.load(f)
+def get_properties(node: ET.Element) -> dict[str, str]:
+    result: dict[str, str] = {}
+    for prop in node.iterfind("./properties/property"):
+        result[prop.attrib["name"]] = prop.attrib["value"]
 
-        for test_kwargs in data["tests"]:
-            test = Test(context=data["context"], **test_kwargs)
-            summary.add_test(test)
+    return result
+
+
+def compute_file(file_path: Path, summary: Summary) -> None:
+    logging.info("Parse %s", file_path)
+
+    with file_path.open("r") as f:
+        root = ET.parse(f).getroot()  # noqa: S314
+
+        for testsuite in root.iterfind("testsuite"):
+            suite_properties = get_properties(testsuite)
+
+            for testcase in testsuite.iterfind("testcase"):
+                logging.debug("   Process %s", testcase.attrib["name"])
+
+                test_properties = get_properties(testcase)
+                test = Test(
+                    nodeid=testcase.attrib["name"],
+                    outcome=test_properties[PropertyName.OUTCOME],
+                    scenario=suite_properties.get(PropertyName.SCENARIO, "unknown"),
+                    weblog=suite_properties.get(PropertyName.WEBLOG, "unknown"),
+                    owners=json.loads(test_properties.get(PropertyName.CODEOWNERS, "[]")),
+                )
+                summary.add_test(test)
 
 
 def main() -> None:
@@ -109,7 +143,7 @@ def main() -> None:
         "--base-dir",
         type=str,
         default=".",
-        help="Base directory to start crawling for report.json files.",
+        help=f"Base directory to start crawling for {REPORT_FILENAME} files.",
     )
     parser.add_argument(
         "--output-file",
