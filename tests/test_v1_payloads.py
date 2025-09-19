7d25a643380ede690ebea1bd6fc9c7ea76e300c7
Andrew Glaude <andrew.glaude@datadoghq.com>
2025-09-19 16:26:37 -0400

    more fixes based on using real agent and dd-trace-go with proxy

--- tests/test_v1_payloads.py
+++ tests/test_v1_payloads.py
@@ -9,14 +9,20 @@ class Test_V1Payloads:
 
     def test_field_changes(self):
         traces = list(interfaces.library.get_traces_v1(self.r))
+        agent_chunks = list(interfaces.agent.get_chunks_v1(self.r))
         assert len(traces) == 1
         _, trace = traces[0]
         assert len(trace["spans"]) == 1
         assert len(trace["trace_id"]) == 34  # 32 bytes for ID and 2 for "0x"
-        assert trace["sampling_mechanism"] == 4
-        assert trace["priority"] == 1
+        assert trace["sampling_mechanism"] == 3  # TODO: Why is this 3 (local rule sampler) for go?
+        assert trace["priority"] == 2
         span = trace["spans"][0]
         assert span["error"], "Error field must be boolean"
         assert span["env"] == "system-tests"
         assert span["component"] == "net/http"
-        assert span["kind"] == 2
+        assert span["span_kind"] == 1  # server
+
+        assert len(agent_chunks) == 1
+        _, agent_chunk = agent_chunks[0]
+        assert len(agent_chunk["spans"]) == 1
+        assert agent_chunk["traceID"] == trace["trace_id"]
