5a466e0601b2f359e08e490d68c5c0ab1a704f2f
Baptiste Foy <baptiste.foy@datadoghq.com>
2025-10-02 10:10:51 +0200

    fix(incident-43718): Adjust for install script changes (#5390)

--- utils/build/ssi/base/install_script_ssi_installer.sh
+++ utils/build/ssi/base/install_script_ssi_installer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DD_INSTALL_ONLY=true DD_INSTALLER=true bash -c "$(curl -L https://dd-agent.s3.amazonaws.com/scripts/install_script_agent7.sh?versionId=wGX9PBJbKTepGT5lqqccic4kCg8DPzJ1)"
+DD_INSTALL_ONLY=true DD_INSTALLER=true bash -c "$(curl -L https://dd-agent.s3.amazonaws.com/scripts/install_script_agent7.sh)"
