16ae7a8284f52921108bddd22ff0960262cf3956
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2025-10-14 09:33:04 +0200

    [dotnet] Skip test for DEBUG-4582 (#5482)

--- tests/debugger/test_debugger_exception_replay.py
+++ tests/debugger/test_debugger_exception_replay.py
@@ -31,6 +31,7 @@ _timeout_next = 30
 @missing_feature(context.library == "ruby", reason="Not yet implemented", force_skip=True)
 @missing_feature(context.library == "nodejs", reason="Not yet implemented", force_skip=True)
 @missing_feature(context.library == "golang", reason="Not yet implemented", force_skip=True)
+@bug(context.library == "dotnet", reason="DEBUG-4582")
 class Test_Debugger_Exception_Replay(debugger.BaseDebuggerTest):
     snapshots: dict = {}
     spans: dict = {}
