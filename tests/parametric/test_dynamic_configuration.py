5b5645a516781e1c431b0cefcfa300b947f84f33
Ayan Khan <khanayan_123@hotmail.com>
2025-10-14 15:37:29 -0400

    skip test entirely reenable in a follow up PR

--- tests/parametric/test_dynamic_configuration.py
+++ tests/parametric/test_dynamic_configuration.py
@@ -256,7 +256,7 @@ class TestDynamicConfigTracingEnabled:
     @parametrize("library_env", [{**DEFAULT_ENVVARS}])
     @bug(context.library == "java", reason="APMAPI-1225")
     @missing_feature(context.library < "dotnet@3.29.0", reason="Added new capabilities", force_skip=True)
-    @missing_feature(context.library < "nodejs@5.72.0", reason="Added new FFE flag capabilities", force_skip=True)
+    @missing_feature(context.library == "nodejs", reason="Added new FFE flag capabilities, wait till it lands on master to reenable test", force_skip=True)
     def test_default_capability_completeness(self, library_env, test_agent, test_library):
         """Ensure the RC request contains the expected default capabilities per language, no more and no less."""
         if context.library is not None and context.library.name is not None:
