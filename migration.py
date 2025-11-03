5267a67d86ec48a9e48da573ef93aa08382aa9c8
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2025-11-03 14:11:44 +0100

    Adding support for references (nodejs)

--- migration.py
+++ migration.py
@@ -1,5 +1,6 @@
 import yaml
 import unittest
+from semantic_version import Version
 from collections import OrderedDict
 
 variants = {"agent": set(),
@@ -124,12 +125,19 @@ def count(node_id: str):
 
 
 
-def flatten(data, lib, root = "tests/", end = False, leaves = None):
+def flatten(data, lib, refs, root = "tests/", end = False, leaves = None):
     global output
     if not leaves: leaves = set()
     if isinstance(data, str):
         # print(f"{root}: {data}")
-        output += f"{root}: \"{data}\"\n"
+        if refs and data in refs:
+            data_str = f"*{refs[data]}"
+            if "||" in data:
+                output += f"{root}:\n    - library_version: {data_str}\n      declaration: missing_feature\n"
+            else:
+                output += f"{root}: {data_str}\n"
+        else:
+            output += f"{root}: \"{data}\"\n"
         pass
     elif end:
         root = f"{root}:"
@@ -152,7 +160,11 @@ def flatten(data, lib, root = "tests/", end = False, leaves = None):
             else:
                 leaf += f"variant: {var[0]}\n    "
             if var[1].startswith(("v", "<", ">")):
-                leaf += f"library_version: \"<{var[1][1:]}\""
+                if refs and var[1] in refs:
+                    data_str = f"*{refs[var[1]]}"
+                else:
+                    data_str = f"\"<{var[1][1:].strip('=')}\""
+                leaf += f"library_version: {data_str}"
                 leaf += f"\n    declaration: missing_feature"
             else:
                 leaf += f"declaration: {var[1]}"
@@ -165,7 +177,7 @@ def flatten(data, lib, root = "tests/", end = False, leaves = None):
             end = True
 
         for next in data.items():
-            res = flatten(next[1], lib, root + next[0], end)
+            res = flatten(next[1], lib, refs, root + next[0], end)
             if res:
                 branch_leaves[root+next[0]] = res
 
@@ -226,16 +238,66 @@ def add_refs(file_path, output_file):
             # print(line, end="")
             f.write(line)
 
+def update_refs(file_path):
+    if not "nodejs" in file_path:
+        return
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+
+    refs = {}
+    for iline, line in enumerate(lines):
+        if line.startswith("tests"):
+            break
+        if "refs:" in line or "---" in line:
+            continue
+        # print(line)
+        
+        semver_range = line[line.find("'")+1:line.rfind("'")]
+        ref = line[line.find("&")+1:line.find(" '")]
+        refs[semver_range] = ref
+
+        if "||" not in line:
+            continue
+
+        elements = semver_range.split(" || ")
+        if " - " in elements[0]:
+            mrel = elements[0].split(" - ")
+            main_range = f">{mrel[1]} || <{mrel[0]}"
+        else:
+            main_range = f"<{elements[0][2:]}"
+
+        new_ranges = []
+        for element in elements[1:]:
+            version = Version(element[1:])
+            new_ranges.append(f">={version.next_major()} || <{version}") 
+        new_line = line[:line.find("'")+1] + main_range
+        for new_range in new_ranges:
+            new_line += f" {new_range}"
+        new_line += line[line.rfind("'"):]
+        # print(new_line)
+        semver_range = new_line[new_line.find("'")+1:new_line.rfind("'")]
+        refs[semver_range] = ref
+        lines[iline] = new_line
+
+    with open(file_path, "w") as file:
+        file.writelines(lines)
+
+    return refs
+
 def main():
     global output
     for lib in variants:
         output = ""
         file_path =f"./manifests/{lib}.yml" 
         output_file =f"./new.manifests/{lib}.yml" 
+
+        refs = update_refs(file_path)
+
         add_refs(file_path, output_file)
         with open(file_path) as f:
             data = yaml.safe_load(f)
-            flatten(data["tests/"], lib)
+            # print(refs)
+            flatten(data["tests/"], lib, refs)
 
         yml_sort(output_file)
 
