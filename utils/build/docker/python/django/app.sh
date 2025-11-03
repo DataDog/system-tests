288b88542494fe1340efeca6afe9631de6d2675f
Federico Mon <federico.mon@datadoghq.com>
2025-11-03 20:16:30 +0100

    Revert "use ddtrace.auto instead of ddtrace-run"
    This reverts commit eac06a4ed9142451e38ee556e9c36edceb55ec67.

--- utils/build/docker/python/django/app.sh
+++ utils/build/docker/python/django/app.sh
@@ -4,9 +4,8 @@ echo "--- PIP FREEZE ---"
 python -m pip freeze
 echo "------------------"
 
-
 if [[ $(python -c 'import sys; print(sys.version_info >= (3,12))') == "True" ]]; then
-    gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
+    ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
 else
-    gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
+    ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
 fi
