eac06a4ed9142451e38ee556e9c36edceb55ec67
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-11-03 18:20:39 +0100

    use ddtrace.auto instead of ddtrace-run

--- utils/build/docker/python/django/app.sh
+++ utils/build/docker/python/django/app.sh
@@ -4,8 +4,9 @@ echo "--- PIP FREEZE ---"
 python -m pip freeze
 echo "------------------"
 
+
 if [[ $(python -c 'import sys; print(sys.version_info >= (3,12))') == "True" ]]; then
-    ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
+    gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
 else
-    ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
+    gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
 fi
--- utils/build/docker/python/django/django_app/wsgi.py
+++ utils/build/docker/python/django/django_app/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+import ddtrace.auto
 import os
 
 from django.core.wsgi import get_wsgi_application
