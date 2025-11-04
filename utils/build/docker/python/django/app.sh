4cecb7d3796342340331874b17856f824bb2b1d5
Christophe Papazian <christophe.papazian@datadoghq.com>
2025-11-04 13:11:28 +0100

    restore

--- utils/build/docker/python/django/app.sh
+++ utils/build/docker/python/django/app.sh
@@ -5,7 +5,7 @@ python -m pip freeze
 echo "------------------"
 
 if [[ $(python -c 'import sys; print(sys.version_info >= (3,12))') == "True" ]]; then
-    DD_UNLOAD_MODULES_FROM_SITECUSTOMIZE=0 ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
+    ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --header-map dangerous --access-logfile - django_app.wsgi -k gevent
 else
-    DD_UNLOAD_MODULES_FROM_SITECUSTOMIZE=0 ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
+    ddtrace-run gunicorn -w 1 -b 0.0.0.0:7777 --access-logfile - django_app.wsgi -k gevent
 fi
