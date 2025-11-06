fcc424ce8209b3382bec1cd3474f720d6c34aafc
Quinna Halim <quinna.halim@datadoghq.com>
2025-11-06 13:43:43 -0500

    add otel_collector_e2e scenario in CI (#5669)

--- utils/scripts/ci_orchestrators/workflow_data.py
+++ utils/scripts/ci_orchestrators/workflow_data.py
@@ -557,8 +557,8 @@ def _is_supported(library: str, weblog: str, scenario: str, _ci_environment: str
             return False
 
     # otel collector
-    if weblog == "otel_collector" or scenario == "OTEL_COLLECTOR":
-        return weblog == "otel_collector" and scenario == "OTEL_COLLECTOR"
+    if weblog == "otel_collector" or scenario in ("OTEL_COLLECTOR", "OTEL_COLLECTOR_E2E"):
+        return weblog == "otel_collector" and scenario in ("OTEL_COLLECTOR", "OTEL_COLLECTOR_E2E")
 
     if "@" in weblog or scenario == "INTEGRATION_FRAMEWORKS":
         return "@" in weblog and scenario == "INTEGRATION_FRAMEWORKS"
