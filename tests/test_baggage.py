7c1bb6b71ccce682ee9ba730b9adce457d281272
Zach Montoya <zach.montoya@datadoghq.com>
2025-11-07 14:35:09 -0800

    Add system-tests to establish the expected behavior for using the OpenTelemetry Baggage API to modify the automatically propagated W3C Baggage header

--- tests/test_baggage.py
+++ tests/test_baggage.py
@@ -162,3 +162,69 @@ class Test_Baggage_Headers_Max_Bytes:
         assert len(items) == 1
         header_size = len(header_str.encode("utf-8"))
         assert header_size <= self.max_bytes
+
+
+@scenarios.tracing_config_empty
+@features.datadog_baggage_headers
+class Test_Baggage_Headers_Api_OTel:
+    def setup_otel_api_update(self):
+        self.r = weblog.get(
+            "/otel_drop_in_baggage_api_otel",
+            params={"url": "http://weblog:7777", "baggage_set": "foo=overwrite_value,new_foo=new_value", "baggage_remove": "remove_me_key"},
+            headers={
+                "x-datadog-parent-id": "10",
+                "x-datadog-trace-id": "2",
+                "baggage": "foo=value_to_be_replaced,FOO=UNTOUCHED,remove_me_key=remove_me_value",
+            },
+        )
+
+    def test_otel_api_update(self):
+        interfaces.library.assert_trace_exists(self.r)
+        assert self.r.status_code == 200
+        data = json.loads(self.r.text)
+        baggage_header_value = extract_baggage_value(data["request_headers"])
+        assert baggage_header_value is not None
+        header_str = baggage_header_value[0] if isinstance(baggage_header_value, list) else baggage_header_value
+        items = header_str.split(",")
+
+        # Expect the following baggage items:
+        # - "foo=overwrite_value (new pair with conflicting case-sensitive key replaces old pair)
+        # - "FOO=UNTOUCHED (keys are case-sensitive, so it does not get replaced)
+        # - "new_foo=new_value (new pair added)
+        assert len(items) == 3
+        assert "foo=overwrite_value" in items
+        assert "FOO=UNTOUCHED" in items
+        assert "new_foo=new_value" in items
+
+
+@scenarios.tracing_config_empty
+@features.datadog_baggage_headers
+class Test_Baggage_Headers_Api_Datadog:
+    def setup_datadog_api_update(self):
+        self.r = weblog.get(
+            "/otel_drop_in_baggage_api_datadog",
+            params={"url": "http://weblog:7777", "baggage_set": "foo=overwrite_value,new_foo=new_value", "baggage_remove": "remove_me_key"},
+            headers={
+                "x-datadog-parent-id": "10",
+                "x-datadog-trace-id": "2",
+                "baggage": "foo=value_to_be_replaced,FOO=UNTOUCHED,remove_me_key=remove_me_value",
+            },
+        )
+
+    def test_datadog_api_update(self):
+        interfaces.library.assert_trace_exists(self.r)
+        assert self.r.status_code == 200
+        data = json.loads(self.r.text)
+        baggage_header_value = extract_baggage_value(data["request_headers"])
+        assert baggage_header_value is not None
+        header_str = baggage_header_value[0] if isinstance(baggage_header_value, list) else baggage_header_value
+        items = header_str.split(",")
+
+        # Expect the following baggage items:
+        # - "foo=overwrite_value (new pair with conflicting case-sensitive key replaces old pair)
+        # - "FOO=BAR (keys are case-sensitive, so it does not get replaced)
+        # - "new_foo=new_value (new pair added)
+        assert len(items) == 3
+        assert "foo=overwrite_value" in items
+        assert "FOO=UNTOUCHED" in items
+        assert "new_foo=new_value" in items
