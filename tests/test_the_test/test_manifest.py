504a286642465f5f16cabcfb439c8adb98e940bd
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2025-11-07 13:40:31 +0100

    Cleanup

--- tests/test_the_test/test_manifest.py
+++ tests/test_the_test/test_manifest.py
@@ -1,31 +1,5 @@
-import os
-from pathlib import Path
-
 from manifests.parser.core import validate_manifest_files
-
 from utils import scenarios
-
-
-def get_variants_map():
-    result = {}
-
-    for folder in os.listdir("utils/build/docker"):
-        folder_path = os.path.join("utils/build/docker/", folder)
-        if not Path(folder_path).is_dir():
-            continue
-
-        result[folder] = ["*"]
-        for file in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file)
-            if Path(file_path).is_dir():
-                continue
-            if not file.endswith(".Dockerfile"):
-                continue
-
-            variant = file[: -len(".Dockerfile")]
-            result[folder].append(variant)
-
-    return result
 
 
 @scenarios.test_the_test
