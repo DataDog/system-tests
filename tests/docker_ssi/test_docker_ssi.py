6d02d86d456377cca26e10adb80249d0ee5108fb
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2025-11-13 08:38:09 -0800

    unconditionally  skip python telemetry abort test (#5706)

--- tests/docker_ssi/test_docker_ssi.py
+++ tests/docker_ssi/test_docker_ssi.py
@@ -93,7 +93,7 @@ class TestDockerSSIFeatures:
     @features.ssi_guardrails
     @irrelevant(context.library == "java" and context.installed_language_runtime >= "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime >= "7.0")
-    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime < "3.9.0")
+    @irrelevant(context.library == "python")
     @bug(context.library == "nodejs" and context.installed_language_runtime < "12.17.0", reason="INPLAT-252")
     @bug(context.library == "java" and context.installed_language_runtime == "1.7.0-201", reason="INPLAT-427")
     @irrelevant(context.library == "nodejs" and context.installed_language_runtime >= "17.0")
