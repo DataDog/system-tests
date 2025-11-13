3df25b04f7b6e69ef252e27b59d942a847e4bfdf
Emmett Butler <723615+emmettbutler@users.noreply.github.com>
2025-11-13 06:02:50 -0800

    fix docker ssi python skip marker logic (#5702)

--- tests/docker_ssi/test_docker_ssi.py
+++ tests/docker_ssi/test_docker_ssi.py
@@ -32,8 +32,7 @@ class TestDockerSSIFeatures:
     @irrelevant(context.library == "java" and context.installed_language_runtime < "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime < "7.0")
     @irrelevant(context.library == "nodejs" and context.installed_language_runtime < "17.0")
-    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime > "3.9.0")
-    @irrelevant(context.library < "python@4.0.0rc1" and context.installed_language_runtime < "3.8.0")
+    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime < "3.9.0")
     def test_install_supported_runtime(self):
         logger.info(f"Testing Docker SSI installation on supported lang runtime: {context.library}")
         assert self.r.status_code == 200, f"Failed to get response from {scenarios.docker_ssi.weblog_url}"
@@ -67,8 +66,7 @@ class TestDockerSSIFeatures:
     @irrelevant(context.library == "java" and context.installed_language_runtime < "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime < "7.0")
     @irrelevant(context.library == "nodejs" and context.installed_language_runtime < "17.0")
-    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime > "3.9.0")
-    @irrelevant(context.library < "python@4.0.0rc1" and context.installed_language_runtime < "3.8.0")
+    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime < "3.9.0")
     @bug(context.library == "python@2.19.1", reason="INPLAT-448")
     @bug(context.library >= "python@3.0.0dev", reason="INPLAT-448")
     def test_telemetry(self):
@@ -95,12 +93,9 @@ class TestDockerSSIFeatures:
     @features.ssi_guardrails
     @irrelevant(context.library == "java" and context.installed_language_runtime >= "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime >= "7.0")
-    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime > "3.9.0")
-    @irrelevant(context.library < "python@4.0.0rc1" and context.installed_language_runtime > "3.8.0")
-    @irrelevant(context.library < "python@3.0.0.dev" and context.installed_language_runtime > "3.7.0")
+    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime < "3.9.0")
     @bug(context.library == "nodejs" and context.installed_language_runtime < "12.17.0", reason="INPLAT-252")
     @bug(context.library == "java" and context.installed_language_runtime == "1.7.0-201", reason="INPLAT-427")
-    @bug(context.library >= "python@4.0.0rc1" and context.installed_language_runtime < "3.9.0", reason="INPLAT-448")
     @irrelevant(context.library == "nodejs" and context.installed_language_runtime >= "17.0")
     @irrelevant(context.library == "dotnet" and context.installed_language_runtime >= "6.0.0")
     def test_telemetry_abort(self):
@@ -164,8 +159,7 @@ class TestDockerSSIFeatures:
     @irrelevant(context.library == "java" and context.installed_language_runtime < "1.8.0_0")
     @irrelevant(context.library == "php" and context.installed_language_runtime < "7.1")
     @irrelevant(context.library == "nodejs" and context.installed_language_runtime < "17.0")
-    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime > "3.9.0")
-    @irrelevant(context.library < "python@4.0.0rc1" and context.installed_language_runtime < "3.8.0")
+    @irrelevant(context.library >= "python@4.0.0rc1" and context.installed_language_runtime < "3.9.0")
     @missing_feature(context.library < "java@1.52.0", reason="Not implemented yet")
     @missing_feature(context.library < "python@3.11.0", reason="Not implemented yet")
     @missing_feature(context.library < "dotnet@3.22.0", reason="Not implemented yet")
