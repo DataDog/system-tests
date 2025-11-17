7c18c481f75a86742459d7334b9f1698642ffee4
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2025-11-17 10:46:36 +0100

    Scenario selection for manifest edits deactivation

--- utils/scripts/compute_libraries_and_scenarios.py
+++ utils/scripts/compute_libraries_and_scenarios.py
@@ -12,7 +12,8 @@ from typing import TYPE_CHECKING, Any
 
 import yaml
 
-from utils._context._scenarios import scenario_groups, scenarios
+from manifests.parser.core import load as load_manifests
+from utils._context._scenarios import scenario_groups as all_scenario_groups, scenarios, get_all_scenarios
 from utils._logger import logger
 
 if TYPE_CHECKING:
@@ -205,9 +206,8 @@ class ScenarioProcessor:
         self.scenarios_by_files: dict[str, set[str]] = defaultdict(set)
 
     def process_manifests(self, inputs: Inputs) -> None:
-        self.scenario_groups |= {scenario_groups.all.name}
+        self.scenario_groups |= {all_scenario_groups.all.name}
         return
-
         modified_nodeids = set()
 
         for nodeid in set(list(inputs.new_manifests.keys()) + list(inputs.old_manifests.keys())):
@@ -293,21 +293,21 @@ class Inputs:
         output: str | None = None,
         mapping_file: str = "utils/scripts/libraries_and_scenarios_rules.yml",
         scenario_map_file: str = "logs_mock_the_test/scenarios.json",
-        new_manifests: str = "manifests/",  # noqa: ARG002
-        old_manifests: str = "original/manifests/",  # noqa: ARG002
+        new_manifests: str = "manifests/",
+        old_manifests: str = "original/manifests/",
     ) -> None:
         self.is_gitlab = False
         self.load_git_info()
         self.output = output
         self.mapping_file = os.path.join(root_dir, mapping_file)
         self.scenario_map_file = os.path.join(root_dir, scenario_map_file)
-        # self.new_manifests = load_manifests(new_manifests)
-        # self.old_manifests = load_manifests(old_manifests)
+        self.new_manifests = load_manifests(new_manifests)
+        self.old_manifests = load_manifests(old_manifests)
 
-        # if not self.new_manifests:
-        #     raise FileNotFoundError(f"Manifest files not found: {new_manifests}")
-        # if not self.old_manifests:
-        #     raise FileNotFoundError(f"Manifest files not found: {old_manifests}")
+        if not self.new_manifests:
+            raise FileNotFoundError(f"Manifest files not found: {new_manifests}")
+        if not self.old_manifests:
+            raise FileNotFoundError(f"Manifest files not found: {old_manifests}")
 
         self.load_raw_impacts()
         self.load_scenario_mappings()
