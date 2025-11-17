b1e94158846adbe9560e5c4e8ac4eadeff24d07e
Igor Unanua <igor.unanua@datadoghq.com>
2025-11-17 10:44:50 +0100

    Update dd-trace-rs version

--- utils/build/docker/rust/install_ddtrace.sh
+++ utils/build/docker/rust/install_ddtrace.sh
@@ -5,7 +5,7 @@ set -eu
 cd /usr/app
 
 REPO_URL=https://github.com/DataDog/dd-trace-rs
-PROD_TAG=v0.0.1
+PROD_TAG=v0.0.2
 
 if [ -e /binaries/rust-load-from-git ]; then
     rev_or_branch=$(</binaries/rust-load-from-git)
@@ -16,13 +16,11 @@ fi
 
 if [ -e /binaries/dd-trace-rs ]; then
     cargo add --path /binaries/dd-trace-rs/datadog-opentelemetry
-    cargo add --path /binaries/dd-trace-rs/dd-trace
 
     echo "install from /binaries/dd-trace-rs"
 else
     # TODO: add lastest release from crates.io
     cargo add --git "$REPO_URL" --tag "$PROD_TAG" datadog-opentelemetry
-    cargo add --git "$REPO_URL" --tag "$PROD_TAG" dd-trace
 
     echo "install from --git $REPO_URL --tag $PROD_TAG"
 fi
