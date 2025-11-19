46e60b9fefd1f33ac1c3cc52e0c1c0c1fdabcd14
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2025-11-19 13:24:11 +0100

    Test

--- utils/scripts/compute_libraries_and_scenarios.py
+++ utils/scripts/compute_libraries_and_scenarios.py
@@ -303,6 +303,7 @@ class Inputs:
         self.mapping_file = os.path.join(root_dir, mapping_file)
         self.scenario_map_file = os.path.join(root_dir, scenario_map_file)
         if self.ref != "refs/pull/5575/merge":
+            raise Exception("Test (find me oiaueryoiua)", self.ref)
             self.new_manifests: dict[str, list[dict[str, Any]]] = Manifest.parse(new_manifests)
             self.old_manifests: dict[str, list[dict[str, Any]]] = Manifest.parse(old_manifests)
 
