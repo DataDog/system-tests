351121d5cc8d5cc9892d6902316e843bd334a59f
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-11-20 13:08:00 +0100

    Put java option before jar name

--- utils/build/docker/java/spring-boot/app.sh
+++ utils/build/docker/java/spring-boot/app.sh
@@ -6,7 +6,7 @@ fi
 
 java \
     -Xmx362m \
+    -XX:ErrorFile=/var/log/system-tests/hs_err_%p_%t_%u.log \
     -javaagent:/app/dd-java-agent.jar \
     -jar /app/myproject-0.0.1-SNAPSHOT.jar \
-    -XX:ErrorFile=/var/log/system-tests/hs_err_%p_%t_%u.log \
     --server.port=7777
