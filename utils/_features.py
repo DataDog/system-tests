b5bf1e897eb8e5c8ffdfa390a4bbe6e9ccd14eb6
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2025-12-08 22:00:19 +0100

    Use apm_serverless rather serverless team

--- utils/_features.py
+++ utils/_features.py
@@ -22,7 +22,6 @@ class _Owner(StrEnum):
     profiler = "@DataDog/profiling"  # it does not exists
     remote_config = "@DataDog/remote-config"
     rp = "@DataDog/apm-reliability-and-performance"  # reliability & performance
-    serverless = "@DataDog/serverless"
     sdk_capabilities = "@DataDog/apm-sdk-capabilities"
     feature_flag_exposure = "@DataDog/feature-flagging-and-experimentation-sdk"
 
@@ -2077,7 +2076,7 @@ class _Features:
 
         https://feature-parity.us1.prod.dog/#/?feature=328
         """
-        return _mark_test_object(test_object, feature_id=328, owner=_Owner.serverless)
+        return _mark_test_object(test_object, feature_id=328, owner=_Owner.apm_serverless)
 
     @staticmethod
     def aws_api_gateway_inferred_span_creation(test_object):
