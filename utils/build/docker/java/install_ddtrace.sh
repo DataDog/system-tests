35d369e1986120b089158a62b3f6a0f02016ff51
alejandro.gonzalez <alejandro.gonzalez@datadoghq.com>
2025-12-15 10:40:45 +0100

    hack to test

--- utils/build/docker/java/install_ddtrace.sh
+++ utils/build/docker/java/install_ddtrace.sh
@@ -34,7 +34,9 @@ install_custom_jar "dd-openfeature*.jar" "dd-openfeature" "$MVN_OPTS"
 
 # Look for custom dd-trace-java jar in custom binaries folder
 if [ $(ls /binaries/dd-java-agent*.jar | wc -l) = 0 ]; then
-    BUILD_URL="https://github.com/DataDog/dd-trace-java/releases/latest/download/dd-java-agent.jar"
+    # TEMPORARY: Force using the PR branch instead of latest release
+    TARGET_BRANCH="alejandro.gonzalez/test_api_sec_sampling"
+    BUILD_URL="https://s3.us-east-1.amazonaws.com/dd-trace-java-builds/${TARGET_BRANCH}/dd-java-agent.jar"
     echo "install from Github release: $BUILD_URL"
     curl  -Lf -o /dd-tracer/dd-java-agent.jar $BUILD_URL
 
