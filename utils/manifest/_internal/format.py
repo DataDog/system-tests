4066e5d0ce5c8377be81bfd42a33b68e16e63fcf
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2025-12-16 17:18:03 +0100

    Auto sort for manifest files

---
+++ utils/manifest/_internal/format.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import re
+
+
+def yml_sort(directory: Path = Path("manifests/")) -> None:
+    for file in directory.iterdir():
+        manifest_data: list[tuple[str, list[str]]] = []
+        header_data = []
+        if file.is_dir():
+            continue
+
+        in_manifest = False
+        for line in file.read_text().splitlines():
+            if line == "manifest:":
+                in_manifest = True
+                header_data.append(line)
+                continue
+            if not in_manifest:
+                header_data.append(line)
+                continue
+
+            if line.startswith("    ") or re.match(r" *\#", line):
+                manifest_data[-1][1].append(line)
+            else:
+                manifest_data.append((line, []))
+
+        manifest_data.sort(key=lambda x: x[0][: x[0].rfind(":")])
+
+        text = ""
+        for line in header_data:
+            text += f"{line}\n"
+
+        for rule in manifest_data:
+            text += f"{rule[0]}\n"
+            for line in rule[1]:
+                text += f"{line}\n"
+
+        file.write_text(text)
+
+
+if __name__ == "__main__":
+    yml_sort()
