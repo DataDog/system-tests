b0f20fc4e0b2ac8ba2307f3bcde1ade7929bfdf5
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2025-12-19 09:23:52 -0500

    Add FFE exposure event tests and Ruby /ffe/start endpoint
    - Add test_exposures.py with tests for FFE exposure event caching and deduplication
    - Add /ffe/start endpoint to Ruby rails72 weblog to initialize OpenFeature provider
    - Update routes from /ffe to /ffe/start and /ffe/evaluate
    - Fix typos in comments (FFL -> FFE, "is have" -> "has")
    - Update python manifest version for exposure tests

--- utils/build/docker/ruby/rails72/app/controllers/open_feature_controller.rb
+++ utils/build/docker/ruby/rails72/app/controllers/open_feature_controller.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
+require 'openfeature/sdk'
+require 'datadog/open_feature/provider'
+
 class OpenFeatureController < ApplicationController
   skip_before_action :verify_authenticity_token
+
+  def start
+    OpenFeature::SDK.set_provider(Datadog::OpenFeature::Provider.new)
+
+    # NOTE: There is no set_provider_and_wait in OpenFeature::SDK
+    loop do
+      break unless Datadog::OpenFeature.evaluator.ufc_json.nil?
+      sleep 0.1
+    end
+
+    render json: {}
+  end
 
   def evaluate
     client = OpenFeature::SDK.build_client
