423e03f25caf95d5c3323fa0330b890e854109aa
simon-id <simon.id@datadoghq.com>
2025-12-31 16:28:48 +0800

    better weblog endpoints

--- utils/build/docker/nodejs/express/stripe.js
+++ utils/build/docker/nodejs/express/stripe.js
@@ -11,25 +11,34 @@ const webhookSecret = 'whsec_FAKE'
 
 function init (app) {
   app.post('/stripe/create_checkout_session', async (req, res) => {
-    const result = await stripe.checkout.sessions.create(req.body)
-
-    res.json(result)
+    try {
+      const result = await stripe.checkout.sessions.create(req.body)
+      res.json(result)
+    } catch (error) {
+      res.status(500).json({ error })
+    }
   })
 
   app.post('/stripe/create_payment_intent', async (req, res) => {
-    const result = await stripe.paymentIntents.create(req.body)
-
-    res.json(result)
+    try {
+      const result = await stripe.paymentIntents.create(req.body)
+      res.json(result)
+    } catch (error) {
+      res.status(500).json({ error })
+    }
   })
 
   app.post('/stripe/webhook', async (req, res) => {
-    const event = stripe.webhooks.constructEvent(
-      req.rawBody,
-      req.headers['stripe-signature'],
-      webhookSecret
-    )
-
-    res.json(event)
+    try {
+      const event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        req.headers['stripe-signature'],
+        webhookSecret
+      )
+      res.json(event)
+    } catch (error) {
+      res.status(403).json({ error })
+    }
   })
 }
 
