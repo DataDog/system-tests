ed4cfd6d9b45dc7913a4811c03d015e7c4007e5d
Louis Tricot <louis.tricot@datadoghq.com>
2026-01-06 15:52:57 +0100

    enable process tags tests for python

--- tests/apm_tracing_e2e/test_process_tags.py
+++ tests/apm_tracing_e2e/test_process_tags.py
@@ -6,7 +6,7 @@ from utils.interfaces._library.miscs import validate_process_tags
 @scenarios.tracing_config_nondefault
 @features.process_tags
 @missing_feature(
-    condition=context.library.name not in ("java", "golang", "dotnet"),
+    condition=context.library.name not in ("java", "golang", "dotnet", "python"),
     reason="Not yet implemented",
 )
 class Test_Process_Tags:
