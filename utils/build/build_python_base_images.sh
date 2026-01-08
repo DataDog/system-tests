01d7f49e1a3116d969f2488d104466e66c48ae56
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2026-01-08 14:30:12 +0100

    Revert "Fix push load conflict (#5968)"
    This reverts commit a988e1b6cd5b684e7ed602146e9ae15cbf7c5c85.

--- utils/build/build_python_base_images.sh
+++ utils/build/build_python_base_images.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-set -e
-
-if [ "$1" = "--push" ]; then
-    exec docker buildx bake --progress=plain -f "utils/build/docker/python/docker-bake.hcl" "$@"
-else
-    exec docker buildx bake --progress=plain --load -f "utils/build/docker/python/docker-bake.hcl" "$@"
-fi
+exec docker buildx bake --progress=plain --load  -f "utils/build/docker/python/docker-bake.hcl" "$@"
