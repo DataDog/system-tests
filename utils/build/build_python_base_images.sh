f19f379f6058cab4195328f7fa7f10c93fbb340f
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2026-01-08 11:40:25 +0100

    Fix push load conflict

--- utils/build/build_python_base_images.sh
+++ utils/build/build_python_base_images.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-exec docker buildx bake --progress=plain --load  -f "utils/build/docker/python/docker-bake.hcl" "$@"
+set -e
+
+if [ "$1" = "--push" ]; then
+    exec docker buildx bake --progress=plain -f "utils/build/docker/python/docker-bake.hcl" "$@"
+else
+    exec docker buildx bake --progress=plain --load -f "utils/build/docker/python/docker-bake.hcl" "$@"
+fi
