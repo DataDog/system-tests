d918e626c38edee50747f42f67829940276d191b
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2026-01-09 12:31:34 +0100

    Add manual flushing to the telemetry metrics

--- utils/build/docker/ruby/rails72/app/controllers/internal_controller.rb
+++ utils/build/docker/ruby/rails72/app/controllers/internal_controller.rb
@@ -15,15 +15,19 @@ class InternalController < ApplicationController
     #       See https://github.com/DataDog/system-tests/blob/64539d1d19d14e0ab040d8e4a01562da1531b7d5/docs/internals/flushing.md
     if telemetry = Datadog.send(:components)&.telemetry
       worker = telemetry.instance_variable_get(:@worker)
-      metrics_manager = worker.instance_variable_get(:@metrics_manager)
 
-      # NOTE: In the current implementation there is no way to force the flushing.
-      #       Instead we are checking if there are any pending events and flushing them
-      #       manually.
-      if worker.send(:work_pending?)
-        metric_events = metrics_manager.flush!
-        worker.send(:flush_events, metric_events) if metric_events.any?
-      end
+      # NOTE: We are flushing telemetry metrics manually.
+      metric_events = worker.instance_variable_get(:@metrics_manager).flush!
+      worker.send(:flush_events, metric_events) if metric_events.any?
+
+      # HACK: In the current implementation there is no way to force the flushing.
+      #       Instead we are giving us a fraction of time after setting `loop_wait_time`
+      #       and just wait till all penging messages are flushed.
+      #
+      # NOTE: Be aware that system-tests doesn't like slow responses, so change that
+      #       value carefully.
+      telemetry.instance_variable_get(:@worker)&.loop_wait_time = 0
+      sleep 0.2
     end
 
     # NOTE: We don't expose directly flushing in the OpenFeature component as it
