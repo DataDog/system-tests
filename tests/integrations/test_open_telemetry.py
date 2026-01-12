6ba015572b805e35266f7720dac7ceb7d66cdb82
Jade Guiton <jade.guiton@datadoghq.com>
2026-01-12 14:59:47 +0100

    [OTELS-844] Check for exception span events instead of promoted attributes

--- tests/integrations/test_open_telemetry.py
+++ tests/integrations/test_open_telemetry.py
@@ -1,6 +1,7 @@
 from utils import context, bug, features, irrelevant, missing_feature, scenarios, logger
 from .utils import BaseDbIntegrationsTestClass
 
+import json
 
 class _BaseOtelDbIntegrationTestClass(BaseDbIntegrationsTestClass):
     """Verify basic DB operations over different databases.
@@ -93,14 +94,15 @@ class _BaseOtelDbIntegrationTestClass(BaseDbIntegrationsTestClass):
         assert len(span["meta"]["error.msg"].strip()) != 0
 
     @missing_feature(library="nodejs_otel", reason="Open telemetry with nodejs is not generating this information.")
-    def test_error_type_and_stack(self):
+    def test_error_exception_event(self):
         span = self.get_span_from_agent(self.requests[self.db_service]["select_error"])
-
-        # A string representing the type of the error
-        assert span["meta"]["error.type"].strip()
-
-        # A human readable version of the stack trace
-        assert span["meta"]["error.stack"].strip()
+        events = json.loads(span["meta"]["events"])
+        exception_events = [event for event in events if event["name"] == "exception"]
+        assert len(exception_events) > 0
+        for event in exception_events:
+            assert event["attributes"]["exception.type"].strip()
+            assert event["attributes"]["exception.message"].strip()
+            assert event["attributes"]["exception.stacktrace"].strip()
 
     @bug(library="python_otel", reason="OTEL-940")
     @bug(library="nodejs_otel", reason="OTEL-940")
