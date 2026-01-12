e8b6615d6dc99fe4f7b9a40366f1b84dcaebcdd8
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2026-01-12 14:18:32 +0100

    Refactor internal controller flush action

--- utils/build/docker/ruby/rails52/app/controllers/internal_controller.rb
+++ utils/build/docker/ruby/rails52/app/controllers/internal_controller.rb
@@ -14,19 +14,21 @@ class InternalController < ApplicationController
     #       this is the place to do it.
     #       See https://github.com/DataDog/system-tests/blob/64539d1d19d14e0ab040d8e4a01562da1531b7d5/docs/internals/flushing.md
     if telemetry = Datadog.send(:components)&.telemetry
-      worker = telemetry.instance_variable_get(:@worker)
+      if worker = telemetry.instance_variable_get(:@worker)
+        if metrics_manager = worker.instance_variable_get(:@metrics_manager)
+          metric_events = metrics_manager.flush!
+          worker.send(:flush_events, metric_events) if metric_events.any?
+        end
 
-      metric_events = worker.instance_variable_get(:@metrics_manager).flush!
-      worker.send(:flush_events, metric_events) if metric_events.any?
-
-      # HACK: In the current implementation there is no way to force the flushing.
-      #       Instead we are giving us a fraction of time after setting `loop_wait_time`
-      #       and just wait till all penging messages are flushed.
-      #
-      # NOTE: Be aware that system-tests doesn't like slow responses, so change that
-      #       value carefully.
-      telemetry.instance_variable_get(:@worker)&.loop_wait_time = 0
-      sleep 0.2
+        # HACK: In the current implementation there is no way to force the flushing.
+        #       Instead we are giving us a fraction of time after setting `loop_wait_time`
+        #       and just wait till all penging messages are flushed.
+        #
+        # NOTE: Be aware that system-tests doesn't like slow responses, so change that
+        #       value carefully.
+        worker.loop_wait_time = 0
+        sleep 0.2
+      end
     end
 
     if open_feature = Datadog.send(:components)&.open_feature
--- utils/build/docker/ruby/rails72/app/controllers/internal_controller.rb
+++ utils/build/docker/ruby/rails72/app/controllers/internal_controller.rb
@@ -14,20 +14,22 @@ class InternalController < ApplicationController
     #       this is the place to do it.
     #       See https://github.com/DataDog/system-tests/blob/64539d1d19d14e0ab040d8e4a01562da1531b7d5/docs/internals/flushing.md
     if telemetry = Datadog.send(:components)&.telemetry
-      worker = telemetry.instance_variable_get(:@worker)
+      if worker = telemetry.instance_variable_get(:@worker)
+        # NOTE: We are flushing telemetry metrics manually.
+        if metrics_manager = worker.instance_variable_get(:@metrics_manager)
+          metric_events = metrics_manager.flush!
+          worker.send(:flush_events, metric_events) if metric_events.any?
+        end
 
-      # NOTE: We are flushing telemetry metrics manually.
-      metric_events = worker.instance_variable_get(:@metrics_manager).flush!
-      worker.send(:flush_events, metric_events) if metric_events.any?
-
-      # HACK: In the current implementation there is no way to force the flushing.
-      #       Instead we are giving us a fraction of time after setting `loop_wait_time`
-      #       and just wait till all penging messages are flushed.
-      #
-      # NOTE: Be aware that system-tests doesn't like slow responses, so change that
-      #       value carefully.
-      telemetry.instance_variable_get(:@worker)&.loop_wait_time = 0
-      sleep 0.2
+        # HACK: In the current implementation there is no way to force the flushing.
+        #       Instead we are giving us a fraction of time after setting `loop_wait_time`
+        #       and just wait till all penging messages are flushed.
+        #
+        # NOTE: Be aware that system-tests doesn't like slow responses, so change that
+        #       value carefully.
+        worker.loop_wait_time = 0
+        sleep 0.2
+      end
     end
 
     # NOTE: We don't expose directly flushing in the OpenFeature component as it
