1693a7a151d47fb9c7e016b28c7bec987eb6887e
Flavien Darche <flavien.darche@datadoghq.com>
2026-01-20 16:05:35 +0100

    disable parametric for proxies

--- utils/scripts/compute-workflow-parameters.py
+++ utils/scripts/compute-workflow-parameters.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from pathlib import Path
 import secrets
 import sys
 
@@ -93,7 +94,7 @@ class CiData:
             "job_matrix": list(range(1, parametric_job_count + 1)),
             "enable": len(scenario_map["parametric"]) > 0
             and "otel" not in library
-            and library not in ("cpp_nginx", "cpp_httpd", "python_lambda"),
+            and library not in ("cpp_nginx", "cpp_httpd", "python_lambda", "envoy", "haproxy"),
         }
 
         self.data["libinjection_scenario_defs"] = get_k8s_matrix(
