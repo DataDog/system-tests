33c8370c66430455271f8dea8550db508ae1c303
Andrew Glaude <andrew.glaude@datadoghq.com>
2026-01-21 16:09:38 -0500

    fix flaky otel tests

--- tests/apm_tracing_e2e/test_otel.py
+++ tests/apm_tracing_e2e/test_otel.py
@@ -35,7 +35,8 @@ class Test_Otel_Span:
             assert parent.get("spanID") == "10000"
         assert parent_meta.get("attributes") == "values"
         assert parent_meta.get("error.message") == "testing_end_span_options"
-        assert parent["metrics"]["_dd.top_level"] == 1.0
+        parent_metrics = interfaces.agent.get_span_metrics(parent, parent_span_format)
+        assert parent_metrics["_dd.top_level"] == 1.0
         # Assert the child sent by the agent.
         # childName is no longer the operation name, rather the resource name
         # after remapping the OTel attributes to Datadog semantics
@@ -62,20 +63,21 @@ class Test_Otel_Span:
         assert len(spans) >= 3, "Agent did not submit the spans we want!"
 
         # Assert the parent span sent by the agent.
-        parent, _parent_span_format = _get_span_by_resource(spans, "root-otel-name.dd-resource")
-        assert parent["name"] == "internal"
+        parent, parent_span_format = _get_span_by_resource(spans, "root-otel-name.dd-resource")
+        assert interfaces.agent.get_span_name(parent, parent_span_format) == "internal"
         assert parent.get("parentID") is None
-        assert parent["metrics"]["_dd.top_level"] == 1.0
+        parent_metrics = interfaces.agent.get_span_metrics(parent, parent_span_format)
+        assert parent_metrics["_dd.top_level"] == 1.0
 
         # Assert the Roundtrip child span sent by the agent, this span is created by an external OTel contrib package
-        roundtrip_span, _roundtrip_span_format = _get_span_by_name(spans, "client.request")
-        assert roundtrip_span["name"] == "client.request"
-        assert roundtrip_span["resource"] == "HTTP GET"
+        roundtrip_span, roundtrip_span_format = _get_span_by_name(spans, "client.request")
+        assert interfaces.agent.get_span_name(roundtrip_span, roundtrip_span_format) == "client.request"
+        assert interfaces.agent.get_span_resource(roundtrip_span, roundtrip_span_format) == "HTTP GET"
         assert roundtrip_span.get("parentID") == parent.get("spanID")
 
         # Assert the Handler function child span sent by the agent.
-        handler_span, _handler_span_format = _get_span_by_name(spans, "server.request")
-        assert handler_span["resource"] == "testOperation"
+        handler_span, handler_span_format = _get_span_by_name(spans, "server.request")
+        assert interfaces.agent.get_span_resource(handler_span, handler_span_format) == "testOperation"
         assert handler_span.get("parentID") == roundtrip_span.get("spanID")
 
         # Assert the spans received from the backend!
@@ -87,7 +89,7 @@ def _get_span_by_name(
     spans: list[tuple[dict, TraceAgentPayloadFormat]], span_name: str
 ) -> tuple[dict, TraceAgentPayloadFormat]:
     for s, span_format in spans:
-        if s["name"] == span_name:
+        if interfaces.agent.get_span_name(s, span_format) == span_name:
             return s, span_format
     return {}, TraceAgentPayloadFormat.legacy
 
@@ -96,6 +98,6 @@ def _get_span_by_resource(
     spans: list[tuple[dict, TraceAgentPayloadFormat]], resource_name: str
 ) -> tuple[dict, TraceAgentPayloadFormat]:
     for s, span_format in spans:
-        if s["resource"] == resource_name:
+        if interfaces.agent.get_span_resource(s, span_format) == resource_name:
             return s, span_format
     return {}, TraceAgentPayloadFormat.legacy
