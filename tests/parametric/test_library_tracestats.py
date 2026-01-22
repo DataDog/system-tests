b5599dfc8cc38bc4cbf320c8060ed25e279c686b
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2026-01-22 17:31:57 +0100

    Decorator migration php (#6071)

--- tests/parametric/test_library_tracestats.py
+++ tests/parametric/test_library_tracestats.py
@@ -49,7 +49,6 @@ class Test_Library_Tracestats:
     @enable_agent_version()
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_metrics_msgpack_serialization_TS001(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When spans are finished
         Each trace has stats metrics computed for it serialized properly in msgpack format with required fields
@@ -102,7 +101,6 @@ class Test_Library_Tracestats:
     @enable_agent_version()
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_distinct_aggregationkeys_TS003(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When spans are created with a unique set of dimensions
         Each span has stats computed for it and is in its own bucket
@@ -187,7 +185,6 @@ class Test_Library_Tracestats:
     @enable_agent_version()
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_measured_spans_TS004(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When spans are marked as measured
         Each has stats computed for it
@@ -229,7 +226,6 @@ class Test_Library_Tracestats:
     @enable_agent_version()
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_top_level_TS005(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When top level (service entry) spans are created
         Each top level span has trace stats computed for it.
@@ -280,7 +276,6 @@ class Test_Library_Tracestats:
     @enable_agent_version()
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_successes_errors_recorded_separately_TS006(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When spans are marked as errors
         The errors count is incremented appropriately and the stats are aggregated into the ErrorSummary
@@ -335,7 +330,6 @@ class Test_Library_Tracestats:
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "java", reason="FIXME: Undefined behavior according the java tracer core team")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_sample_rate_0_TS007(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When the sample rate is 0 and trace stats is enabled
         non-P0 traces should be dropped
@@ -406,7 +400,6 @@ class Test_Library_Tracestats:
     @enable_agent_version()
     @missing_feature(context.library == "cpp", reason="cpp has not implemented stats computation yet")
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     def test_metrics_computed_after_span_finsh_TS009(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When trace stats are computed for traces
         Metrics must be computed after spans are finished, otherwise components of the aggregation key may change after
@@ -456,7 +449,6 @@ class Test_Library_Tracestats:
         assert stats[0]["Synthetics"] is True
 
     @missing_feature(context.library == "nodejs", reason="nodejs has not implemented stats computation yet")
-    @missing_feature(context.library == "php", reason="php has not implemented stats computation yet")
     @parametrize("library_env", [{"DD_TRACE_STATS_COMPUTATION_ENABLED": "0"}])
     def test_metrics_computed_after_span_finish_TS010(self, test_agent: TestAgentAPI, test_library: APMLibrary):
         """When DD_TRACE_STATS_COMPUTATION_ENABLED=False
--- tests/test_data_integrity.py
+++ tests/test_data_integrity.py
@@ -142,7 +142,6 @@ class Test_LibraryHeaders:
 
         interfaces.library.validate_all(validator, allow_no_data=True)
 
-    @missing_feature(library="php", reason="not implemented yet")
     @missing_feature(library="cpp_nginx", reason="not implemented yet")
     @missing_feature(library="cpp_httpd")
     @irrelevant(library="golang", reason="implemented but not testable")
@@ -181,7 +180,6 @@ class Test_LibraryHeaders:
 
     @missing_feature(library="cpp_nginx", reason="not implemented yet")
     @missing_feature(library="cpp_httpd", reason="not implemented yet")
-    @missing_feature(library="php", reason="not implemented yet")
     @missing_feature(context.library < "golang@1.73.0-dev", reason="Implemented in v1.72.0")
     def test_datadog_external_env(self):
         """Datadog-External-Env header if present is in the {prefix}-{value},... format"""
