6b5d7763294efafe33b943df9c1c50bfe177022a
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2026-01-31 10:16:56 -0500

    fix: add required endpoints and correct manifest version
    - Add /healthcheck endpoint returning library info JSON
    - Add /flush endpoint for trace flushing
    - Update manifest from 6.0.0-pre to >=5.75.0 (test passes now)

--- utils/build/docker/nodejs/express/app-no-openfeature.js
+++ utils/build/docker/nodejs/express/app-no-openfeature.js
@@ -10,6 +10,44 @@ app.get('/', (req, res) => {
   res.send('Hello\n')
 })
 
+app.get('/healthcheck', (req, res) => {
+  res.json({
+    status: 'ok',
+    library: {
+      name: 'nodejs',
+      version: require('dd-trace/package.json').version
+    }
+  })
+})
+
+app.get('/flush', (req, res) => {
+  tracer.dogstatsd?.flush?.()
+  const timeout = Number(req.query.timeout) || 5000
+  const start = Date.now()
+  let interval
+
+  function onFlush () {
+    clearInterval(interval)
+    res.json({ status: 'ok' })
+  }
+
+  function onTimeout () {
+    clearInterval(interval)
+    res.status(504).json({ error: 'Timed out waiting for traces to flush' })
+  }
+
+  interval = setInterval(() => {
+    const stats = tracer._tracer._exporter?._writer?._stats || {}
+    if (stats.count === stats.acked || Date.now() - start > timeout) {
+      if (stats.count === stats.acked) {
+        onFlush()
+      } else {
+        onTimeout()
+      }
+    }
+  }, 50)
+})
+
 app.listen(7777, '0.0.0.0', () => {
   tracer.trace('init.service', () => {})
   console.log('listening')
