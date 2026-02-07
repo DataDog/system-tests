eb76a471cf7bee1dd41839c2cfee3fcc6c1ef11d
Thomas Watson <thomas.watson@datadoghq.com>
2026-02-07 10:09:00 +0100

    Revert "more revert2"
    This reverts commit 1340fd960a360002cb07b4529c629ed88f6f130b.

--- tests/debugger/utils.py
+++ tests/debugger/utils.py
@@ -471,9 +471,10 @@ class BaseDebuggerTest:
         logger.debug(f"No capture reason span found: {self._no_capture_reason_span_found}")
         return self._no_capture_reason_span_found
 
-    def wait_for_code_origin_span(self, timeout: int = 5) -> bool:
+    def wait_for_code_origin_span(self, timeout: int = 5, threshold: int | None = None) -> bool:
         self._span_found = False
-        threshold = self._get_max_trace_file_number()
+        if threshold is None:
+            threshold = self._get_max_trace_file_number()
 
         interfaces.agent.wait_for(
             lambda data: self._wait_for_code_origin_span(data, threshold=threshold),
