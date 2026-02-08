47d07c8236d115b276d48b9485d71e9e2786d38c
Thomas Watson <thomas.watson@datadoghq.com>
2026-02-08 08:09:26 +0100

    fix5

--- tests/debugger/test_debugger_inproduct_enablement.py
+++ tests/debugger/test_debugger_inproduct_enablement.py
@@ -173,9 +173,8 @@ class Test_Debugger_InProduct_Enablement_Code_Origin(debugger.BaseDebuggerTest):
     ########### code origin ############
     def _check_code_origin(self):
         """Send a request and check if code origin spans are present."""
-        threshold = self._get_max_trace_file_number()
         self.send_weblog_request("/headers")
-        return self.wait_for_code_origin_span(TIMEOUT, threshold=threshold)
+        return self.wait_for_code_origin_span(TIMEOUT)
 
     def _set_code_origin_and_check(self, *, enabled: bool | None):
         """Set code origin via remote config and check if spans are present."""
