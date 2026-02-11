2aac7553d1801ba1da672ce164dcb2ec5c2ec769
Anais Raison <anais.raison@datadoghq.com>
2026-02-11 18:10:41 +0100

    fix(crashtracker): fix test

--- tests/parametric/test_crashtracking.py
+++ tests/parametric/test_crashtracking.py
@@ -19,15 +19,13 @@ class Test_Crashtracking:
             event = test_agent.wait_for_telemetry_event("logs", wait_loops=400)
             # Handling both v1 and v2 of the crashtracking payload so that we can
             # update to v2 without breaking the test.
-            if event is None or isinstance(event.get("payload"), list):
-                version = "v1"
+            if isinstance(event.get("payload"), list):
                 if "is_crash_ping:true" not in event["payload"][0]["tags"]:
+                    self.assert_crash_report_v1(test_library, event)
                     break
-            else:  # If payload is a dict
-                version = "v2"
-                if "is_crash_ping:true" not in event["payload"]["logs"][0]["tags"]:
-                    break
-        self.assert_crash_report(test_library, event, version)
+            elif "is_crash_ping:true" not in event["payload"]["logs"][0]["tags"]:
+                self.assert_crash_report_v2(test_library, event)
+                break
 
     @pytest.mark.parametrize("library_env", [{"DD_CRASHTRACKING_ENABLED": "false"}])
     def test_disable_crashtracking(self, test_agent: TestAgentAPI, test_library: APMLibrary):
@@ -40,11 +38,11 @@ class Test_Crashtracking:
 
             if event["request_type"] == "logs":
                 if isinstance(event.get("payload"), list):
-                    version = "v1"
+                    with pytest.raises(AssertionError):
+                        self.assert_crash_report_v1(test_library, event)
                 else:  # If payload is a dict
-                    version = "v2"
-                with pytest.raises(AssertionError):
-                    self.assert_crash_report(test_library, event, version)
+                    with pytest.raises(AssertionError):
+                        self.assert_crash_report_v2(test_library, event)
 
     @pytest.mark.parametrize("library_env", [{"DD_CRASHTRACKING_ENABLED": "true"}])
     def test_telemetry_timeout(self, test_agent: TestAgentAPI, test_library: APMLibrary):
@@ -58,24 +56,50 @@ class Test_Crashtracking:
         finally:
             test_agent.set_trace_delay(0)
 
-    def assert_crash_report(self, test_library: APMLibrary, event: dict, version: str):
+    def assert_crash_report_v1(self, test_library: APMLibrary, event: dict):
         logger.debug(f"event: {json.dumps(event, indent=2)}")
 
-        if version == "v1":
-            assert isinstance(event.get("payload"), list), event.get("payload")
-            assert event["payload"], event["payload"]
-            assert isinstance(event["payload"][0], dict), event["payload"][0]
-            assert "tags" in event["payload"][0]
-            tags = event["payload"][0]["tags"]
+        assert isinstance(event.get("payload"), list), event.get("payload")
+        assert event["payload"], event["payload"]
+        assert isinstance(event["payload"][0], dict), event["payload"][0]
+        assert "tags" in event["payload"][0]
 
-        else:  # If v2 payload
-            assert isinstance(event.get("payload"), dict), event.get("payload")
-            assert isinstance(event["payload"].get("logs"), list), event["payload"].get("logs")
-            assert event["payload"]["logs"], event["payload"]["logs"]
-            assert isinstance(event["payload"]["logs"][0], dict), event["payload"]["logs"][0]
-            assert "tags" in event["payload"]["logs"][0]
-            tags = event["payload"]["logs"][0]["tags"]
+        tags = event["payload"][0]["tags"]
+        tags_dict = dict(item.split(":") for item in tags.split(","))
+        logger.debug(f"tags_dict: {json.dumps(tags_dict, indent=2)}")
 
+        # Until the crash tracking RFC is out, there is no standard way to identify crash reports.
+        # Most client libraries are using libdatadog so tesing signum tag would work,
+        # but Java isn't so we end up with testing for severity tag.
+        if test_library.lang == "java":
+            assert "severity" in tags_dict, tags_dict
+            assert tags_dict["severity"] == "crash", tags_dict
+        else:
+            # those values are defined in python's module signal. But it's more clear to have this defined here
+            SIGABRT = 6  # noqa: N806
+            SIGSEGV = 11  # noqa: N806
+
+            # According to the RFC, si_signo should be set to 11 for SIGSEGV
+            # though, it's difficult for .NET to simulate a segfault, so SIGABRT is used instead
+            expected_signal_value = f"{SIGABRT}" if test_library.lang == "dotnet" else f"{SIGSEGV}"
+
+            if "signum" in tags_dict:
+                assert tags_dict["signum"] == expected_signal_value
+            elif "si_signo" in tags_dict:
+                assert tags_dict["si_signo"] == expected_signal_value
+            else:
+                raise AssertionError("signum/si_signo not found in tags_dict")
+
+    def assert_crash_report_v2(self, test_library: APMLibrary, event: dict):
+        logger.debug(f"event: {json.dumps(event, indent=2)}")
+
+        assert isinstance(event.get("payload"), dict), event.get("payload")
+        assert isinstance(event["payload"].get("logs"), list), event["payload"].get("logs")
+        assert event["payload"]["logs"], event["payload"]["logs"]
+        assert isinstance(event["payload"]["logs"][0], dict), event["payload"]["logs"][0]
+        assert "tags" in event["payload"]["logs"][0]
+
+        tags = event["payload"]["logs"][0]["tags"]
         tags_dict = dict(item.split(":") for item in tags.split(","))
         logger.debug(f"tags_dict: {json.dumps(tags_dict, indent=2)}")
 
