615fab1a690c51d865348b4ad0d831026b46d1a9
Christophe Papazian <christophe.papazian@datadoghq.com>
2026-02-13 13:55:25 +0100

    fix comments and factors

--- tests/appsec/test_span_tags_headers.py
+++ tests/appsec/test_span_tags_headers.py
@@ -31,7 +31,7 @@ class Test_Headers_No_Event:
         self.r = weblog.get("/", headers={"User-Agent": "Mozilla/5.0"})
 
     def test_content_type_no_event(self):
-        # Send a random attack on the identify endpoint - should not affect the usr.id tag
+        # Send a non-malicious request with no triggered rules - should have the content-type and content-length tags
 
         assert self.r.status_code == 200
 
@@ -47,7 +47,7 @@ class Test_Headers_Event_No_Blocking:
         self.r = weblog.get("/", headers={"User-Agent": "TraceTagging/v3"})
 
     def test_content_type_event(self):
-        # Send a random attack on the identify endpoint - should not affect the usr.id tag
+        # Send a request that triggers a security event but not blocking - should have the content-type and content-length tags
 
         assert self.r.status_code == 200
 
@@ -63,22 +63,8 @@ class Test_Headers_Event_Blocking:
         self.r = weblog.get("/", headers={"User-Agent": "Arachni/v1"})
 
     def test_content_type_event_blocking(self):
-        # Send a random attack on the identify endpoint - should not affect the usr.id tag
+        # Send a request that triggers a blocking security event - should have the content-type and content-length tags
 
         assert self.r.status_code == 403
-
-        def validate_headers_tags(span: dict):
-            assert (enabled := span["metrics"].get("_dd.appsec.enabled")) == 1.0, (
-                f"Expected _dd.appsec.enabled to be '1', got {enabled}"
-            )
-
-            assert (content_type := span["meta"].get("http.response.headers.content-type")), (
-                f"Expected content-type, got {content_type}"
-            )
-            assert content_type.startswith(("text/", "application/json")), (
-                f"Expected content-type to be 'text/html', 'text/plain' or 'application/json', got {content_type}"
-            )
-
-            return True
 
         interfaces.library.validate_one_span(self.r, validator=validate_headers_tags)
