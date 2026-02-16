9eda3cb59b8de4c1dfc8704ef64f575e9148b89f
Romain Marcadier <romain@datadoghq.com>
2026-02-16 12:08:40 +0100

    Remove trailing white space

--- utils/build/docker/php/common/index.php
+++ utils/build/docker/php/common/index.php
@@ -11,7 +11,7 @@ if (function_exists('\DDTrace\add_endpoint')) {
     \DDTrace\add_endpoint('/', 'http.request', 'GET /', 'GET');
     \DDTrace\add_endpoint('/another', 'http.request', 'GET /another', 'GET');
 }
-  
+
 header('Content-Type: text/plain');
 header('Content-Length: 13');
 echo "Hello world!\n";
