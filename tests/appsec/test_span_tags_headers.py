e46bbbb928b081c55acdb1bfd38922a70de38933
Christophe Papazian <114495376+christophe-papazian@users.noreply.github.com>
2026-02-17 11:03:34 +0100

    content-length optional on blocking response (#6317)

--- tests/appsec/test_span_tags_headers.py
+++ tests/appsec/test_span_tags_headers.py
@@ -1,25 +1,46 @@
+import logging
 from utils import weblog, interfaces, features, scenarios
+from utils._weblog import CaseInsensitiveDict
+
+logger = logging.getLogger(__name__)
 
 
-def validate_headers_tags(span: dict):
-    assert (enabled := span["metrics"].get("_dd.appsec.enabled")) == 1.0, (
-        f"Expected _dd.appsec.enabled to be '1.0', got {enabled}"
-    )
+def validate_builder(headers: CaseInsensitiveDict, *, mandatory: bool = True):
+    content_type = headers.get("Content-Type")
+    content_length = headers.get("Content-Length")
 
-    assert (content_type := span["meta"].get("http.response.headers.content-type")), (
-        f"Expected content-type, got {content_type}"
-    )
+    def validator(span: dict):
+        assert (enabled := span["metrics"].get("_dd.appsec.enabled")) == 1.0, (
+            f"Expected _dd.appsec.enabled to be '1.0', got {enabled}"
+        )
 
-    assert isinstance(content_type, str), f"Expected content-type to be a string, got {type(content_type)}"
-    assert content_type.startswith(("text/", "application/json")), (
-        f"Expected content-type to be 'text/html', 'text/plain' or 'application/json', got {content_type}"
-    )
+        assert (content_type_tag := span["meta"].get("http.response.headers.content-type")), (
+            f"Expected content-type, got {content_type_tag}"
+        )
 
-    assert (content_length := span["meta"].get("http.response.headers.content-length")), (
-        f"Expected content-length, got {content_length}"
-    )
+        assert isinstance(content_type_tag, str), f"Expected content-type to be a string, got {type(content_type_tag)}"
+        assert content_type_tag.startswith(("text/", "application/json")), (
+            f"Expected content-type to be 'text/html', 'text/plain' or 'application/json', got {content_type_tag}"
+        )
+        assert content_type == content_type_tag, (
+            f"Expected content-type header to be {content_type}, got {content_type_tag}"
+        )
 
-    return True
+        if mandatory:
+            assert content_length is not None, "Expected content-length header to be present"
+        else:
+            logger.info(
+                f"Content-length is optional for this test, skipping assertion. Got content-length: {content_length}"
+            )
+
+        if content_length is not None:
+            assert (content_length_tag := span["meta"].get("http.response.headers.content-length")) == content_length, (
+                f"Expected content-length to be {content_length}, got {content_length_tag}"
+            )
+
+        return True
+
+    return validator
 
 
 @features.appsec_request_blocking
@@ -34,7 +55,8 @@ class Test_Headers_No_Event:
         # Send a non-malicious request with no triggered rules - should have the content-type and content-length tags
         assert self.r.status_code == 200
         interfaces.library.assert_no_appsec_event(self.r)
-        interfaces.library.validate_one_span(self.r, validator=validate_headers_tags)
+        # content-length is mandatory on this endpoint
+        interfaces.library.validate_one_span(self.r, validator=validate_builder(self.r.headers))
 
 
 @features.appsec_request_blocking
@@ -49,7 +71,8 @@ class Test_Headers_Event_No_Blocking:
         # Send a request that triggers a security event but not blocking - should have the content-type and content-length tags
         assert self.r.status_code == 200
         interfaces.library.assert_waf_attack(self.r, rule="ttr-000-003")
-        interfaces.library.validate_one_span(self.r, validator=validate_headers_tags)
+        # content-length is mandatory on this endpoint
+        interfaces.library.validate_one_span(self.r, validator=validate_builder(self.r.headers))
 
 
 @features.appsec_request_blocking
@@ -64,4 +87,5 @@ class Test_Headers_Event_Blocking:
         # Send a request that triggers a blocking security event - should have the content-type and content-length tags
         assert self.r.status_code == 403
         interfaces.library.assert_waf_attack(self.r, rule="arachni_rule")
-        interfaces.library.validate_one_span(self.r, validator=validate_headers_tags)
+        # content-length is optional on blocking response
+        interfaces.library.validate_one_span(self.r, validator=validate_builder(self.r.headers, mandatory=False))
