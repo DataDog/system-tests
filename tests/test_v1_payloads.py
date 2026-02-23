78459ec43c354108177f09cc191595086eeba116
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2026-02-23 13:24:00 +0100

    Removes interfaces.library.get_traces_v1

--- tests/test_v1_payloads.py
+++ tests/test_v1_payloads.py
@@ -20,23 +20,24 @@ class Test_V1Payloads:
         self.r = weblog.get("/status?code=500")
 
     def test_field_changes(self):
-        traces = list(interfaces.library.get_traces_v1(self.r))
+        traces = list(interfaces.library.get_traces(self.r))
         agent_chunks = list(interfaces.agent.get_chunks_v1(self.r))
         assert len(traces) == 1
         _, trace = traces[0]
-        assert len(trace["spans"]) == 1
-        assert len(trace["trace_id"]) == 34  # 32 bytes for ID and 2 for "0x"
+        assert isinstance(trace.raw_trace, dict)
+        assert len(trace.raw_trace["spans"]) == 1
+        assert len(trace.raw_trace["trace_id"]) == 34  # 32 bytes for ID and 2 for "0x"
         assert (
-            trace["sampling_mechanism"] == SamplingMechanism.RULE_RATE
+            trace.raw_trace["sampling_mechanism"] == SamplingMechanism.RULE_RATE
         )  # TODO: Why is this local rule sampler for go?
-        assert trace["priority"] == SamplingPriority.USER_KEEP
-        span = trace["spans"][0]
-        assert span["error"], "Error field must be boolean"
-        assert span["env"] == "system-tests"
-        assert span["component"] == "net/http"
-        assert span["span_kind"] == SpanKind.SERVER
+        assert trace.raw_trace["priority"] == SamplingPriority.USER_KEEP
+        span = trace.spans[0]
+        assert span.raw_span["error"], "Error field must be boolean"
+        assert span.raw_span["env"] == "system-tests"
+        assert span.raw_span["component"] == "net/http"
+        assert span.raw_span["span_kind"] == SpanKind.SERVER
 
         assert len(agent_chunks) == 1
         _, agent_chunk = agent_chunks[0]
         assert len(agent_chunk["spans"]) == 1
-        assert agent_chunk["traceID"] == trace["trace_id"]
+        assert agent_chunk["traceID"] == trace.raw_trace["trace_id"]
