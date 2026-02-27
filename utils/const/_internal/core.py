82f878b0c8fa82e34c7764b583792a532eaa4e0d
Nicolas Catoni <nicolas.catoni@datadoghq.com>
2026-02-27 16:31:45 +0100

    Simplification

--- utils/const/_internal/core.py
+++ utils/const/_internal/core.py
@@ -9,20 +9,19 @@ class ConstGroup(set):
 
 
 class ConstGroups:
-    groups: dict[str, ConstGroup]
     all = ConstGroup()
 
     def __init__(self) -> None:
-        self.groups = {}
+        groups = {}
         for group_name in [att for att in dir(self) if isinstance(getattr(self, att), ConstGroup)]:
-            self.groups[group_name] = getattr(self, group_name)
-            self.groups[group_name].init(group_name)
+            groups[group_name] = getattr(self, group_name)
+            groups[group_name].init(group_name)
 
         items = self._items()
-        for item, groups in items.items():
-            for group in groups:
-                self.groups[group.name].add(item)
-            self.groups["all"].add(item)
+        for item, item_groups in items.items():
+            for group in item_groups:
+                groups[group.name].add(item)
+            groups["all"].add(item)
 
     def _items(self) -> dict[str, list[ConstGroup]]:
         return {}
