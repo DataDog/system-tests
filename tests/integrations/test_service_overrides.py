7758ca7e6239777e8240baf2dead97bdec7e0852
Andrea Marziali <andrea.marziali@datadoghq.com>
2026-03-05 10:20:49 +0100

    Add e2e test for sql service name override

---
+++ tests/integrations/test_service_overrides.py
@@ -0,0 +1,25 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2021 Datadog, Inc.
+
+from utils import weblog, interfaces, scenarios, features
+
+
+@features.service_override_source
+@scenarios.integrations
+class Test_SqlServiceNameSource:
+    """Verify that _dd.srv.src is set on sql spans when the integration overrides the service name"""
+
+    def setup_sql_srv_src(self):
+        self.r = weblog.get("/trace/sql")
+
+    def test_sql_srv_src(self):
+        assert self.r.status_code == 200
+
+        srv_src_found = False
+        for _, _, span in interfaces.library.get_spans(request=self.r, full_trace=True):
+            if span.get("meta", {}).get("_dd.srv.src"):
+                srv_src_found = True
+                break
+
+        assert srv_src_found, "Expected at least one sql span to have _dd.srv.src set"
