a0b2978264522b47ce5a724518c70011a36faf09
Ayan Khan <khanayan_123@hotmail.com>
2026-03-20 10:21:06 -0400

    enable nodejs system test and fix test app for node

--- utils/build/docker/nodejs/express/fork_child.js
+++ utils/build/docker/nodejs/express/fork_child.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 // Child process for spawn_child endpoint. Args: sleep (seconds), crash (true|false).
+require('dd-trace').init()
 const sleepSec = parseInt(process.argv[2] || '2', 10) * 1000
 const crash = process.argv[3] === 'true'
 setTimeout(() => {
