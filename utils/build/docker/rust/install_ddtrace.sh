9bb139d6a99b5c46f0ac9a4624eaf99db4a36c1f
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2026-03-24 10:32:19 +0100

    Fix rust CI

--- utils/build/docker/rust/install_ddtrace.sh
+++ utils/build/docker/rust/install_ddtrace.sh
@@ -10,18 +10,18 @@ PROD_TAG=datadog-opentelemetry-v0.2.1
 if [ -e /binaries/rust-load-from-git ]; then
     rev_or_branch=$(</binaries/rust-load-from-git)
 
+    echo "Clone $REPO_URL -b $rev_or_branch into /binaries/dd-trace-rs"
     git clone -b "$rev_or_branch" "$REPO_URL" /binaries/dd-trace-rs
-    echo "Cloned $REPO_URL -b $rev_or_branch into /binaries/dd-trace-rs"
 fi
 
 if [ -e /binaries/dd-trace-rs ]; then
+    echo "install from /binaries/datadog-opentelemetry with metrics-http and metrics-grpc features"
     cargo add --path /binaries/dd-trace-rs/datadog-opentelemetry --features metrics-http,metrics-grpc
 
-    echo "install from /binaries/datadog-opentelemetry with metrics-http and metrics-grpc features"
 else
     # TODO: add lastest release from crates.io
+    echo "install from --git $REPO_URL --tag $PROD_TAG with metrics-http and metrics-grpc features"
     cargo add --git "$REPO_URL" --tag "$PROD_TAG" datadog-opentelemetry --features metrics-http,metrics-grpc
 
-    echo "install from --git $REPO_URL --tag $PROD_TAG with metrics-http and metrics-grpc features"
 fi
 
