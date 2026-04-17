ba3218588cbeb903d96114d45cdf5bb833650264
iunanua <igor.unanua@datadoghq.com>
2026-04-17 10:24:38 +0200

    Remove previous datadog-opentelemetry dependency

--- utils/build/docker/rust/install_ddtrace.sh
+++ utils/build/docker/rust/install_ddtrace.sh
@@ -13,9 +13,6 @@ if [ -e /binaries/rust-load-from-git ]; then
     git clone -b "$rev_or_branch" "$REPO_URL" /binaries/dd-trace-rs
 fi
 
-# remove previous depedency on datadog-opentelemetry
-cargo remove datadog-opentelemetry
-
 if [ -e /binaries/dd-trace-rs ]; then
     echo "install from /binaries/datadog-opentelemetry with metrics-http and metrics-grpc features"
 
@@ -42,9 +39,12 @@ if [ -e /binaries/dd-trace-rs ]; then
     cargo release version -p datadog-opentelemetry "$dev_version" -x --no-confirm
 
     cd /usr/app
-    cargo add --path /binaries/dd-trace-rs/datadog-opentelemetry --features metrics-http,metrics-grpc,logs-http,logs-grpc
+    cargo add datadog-opentelemetry --path /binaries/dd-trace-rs/datadog-opentelemetry --features metrics-http,metrics-grpc,logs-http,logs-grpc
 else
     echo "install from crates.io with metrics-http and metrics-grpc features"
+
+    # remove previous depedency on datadog-opentelemetry and add the new one from crates.io
+    cargo remove datadog-opentelemetry || true
     cargo add datadog-opentelemetry --features metrics-http,metrics-grpc,logs-http,logs-grpc
 fi
 
