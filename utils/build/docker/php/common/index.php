e3bca54d5eb6b7788e40d07c7e1ae033952e26dd
Gustavo André dos Santos Lopes <gustavo.lopes@datadoghq.com>
2026-05-05 22:36:15 +0100

    php weblog: detect new _automated API via function_exists on internal namespace
    Replace the reflection-based version detection with a simpler
    function_exists check on the new \datadog\appsec\internal\ namespace.
    Update all callers to use the new namespace when available.

--- utils/build/docker/php/common/index.php
+++ utils/build/docker/php/common/index.php
@@ -1,10 +1,15 @@
 <?php
 
 require_once 'cookie_manager.php';
+require_once 'appsec_compat.php';
 
 $user = $_GET['username'] ?? getLoggedInCookie();
 if (isset($user)) {
-    \datadog\appsec\track_authenticated_user_event_automated($user);
+    if (_dd_appsec_new_api()) {
+        \datadog\appsec\internal\track_authenticated_user_event_automated('custom', $user);
+    } else {
+        \datadog\appsec\track_authenticated_user_event_automated($user);
+    }
 }
 
 if (function_exists('\DDTrace\add_endpoint')) {
