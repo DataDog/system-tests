7d1b677c7770ba350e9ac65dd6630eeab5f66ff1
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2026-06-25 17:12:14 +0200

    Make symfony app more symfony approach

--- utils/build/docker/php/weblogs/symfony7x/config/bundles.php
+++ utils/build/docker/php/weblogs/symfony7x/config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
