4d98282705bb49be33ac24843a78b985c0723126
Milan Garnier <milan.garnier@datadoghq.com>
2026-07-03 14:01:56 +0200

    prod check before checking library target branch (same behavior)

--- utils/scripts/load-binary.sh
+++ utils/scripts/load-binary.sh
@@ -166,7 +166,10 @@ elif [ "$TARGET" = "php" ]; then
     rm -rf *.tar.gz
     mkdir -p temp
 
-    if [ -n "${LIBRARY_TARGET_BRANCH:-}" ]; then
+    if [ "${VERSION:-}" = 'prod' ]; then
+        ../utils/scripts/docker_base_image.sh ghcr.io/datadog/dd-trace-php/dd-library-php:latest ./temp
+
+    elif [ -n "${LIBRARY_TARGET_BRANCH:-}" ]; then
         # Match GitLab's CI_COMMIT_REF_SLUG: lowercase, non-alphanumeric → '-', collapse, truncate to 63 bytes and trim
         NORMALIZED_BRANCH=$(echo "$LIBRARY_TARGET_BRANCH" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g;s/-\+/-/g;s/^-//;s/-$//' | cut -c1-63 | sed 's/-$//')
         ghcr_login_if_token_set
@@ -191,9 +194,6 @@ elif [ "$TARGET" = "php" ]; then
         echo "Downloading dd-library-php from: $URL"
         curl --fail --location --silent --show-error --output "./temp/dd-library-php-${VERSION_HASH}-$(arch)-linux-gnu.tar.gz" "$URL"
         echo "dd-library-php $(arch) downloaded"
-
-    elif [ "${VERSION:-}" = 'prod' ]; then
-        ../utils/scripts/docker_base_image.sh ghcr.io/datadog/dd-trace-php/dd-library-php:latest ./temp
 
     else
         echo "Don't know how to load version ${VERSION:-} for $TARGET"
