15791f27694d1a5d2a6eb8adbda7fc1ada8511ab
Charles de Beauchesne <charles.debeauchesne@datadoghq.com>
2026-07-07 16:20:17 +0200

    Fix error on graphql

--- utils/scripts/ci_orchestrators/workflow_data.py
+++ utils/scripts/ci_orchestrators/workflow_data.py
@@ -499,7 +499,6 @@ def _is_supported(weblog: Weblog, scenario: Scenario, _ci_environment: str) -> b
 
     library = weblog.library
     weblog_name = weblog.name
-    scenario_name = scenario.name
 
     # Only Allow Lambda scenarios for the lambda libraries
     is_lambda_library = library in (
@@ -508,7 +507,7 @@ def _is_supported(weblog: Weblog, scenario: Scenario, _ci_environment: str) -> b
         "nodejs_lambda",
         "ruby_lambda",
     )
-    is_lambda_scenario = scenario_name in (
+    is_lambda_scenario = scenario.name in (
         "APPSEC_LAMBDA_DEFAULT",
         "APPSEC_LAMBDA_BLOCKING",
         "APPSEC_LAMBDA_API_SECURITY",
@@ -519,7 +518,7 @@ def _is_supported(weblog: Weblog, scenario: Scenario, _ci_environment: str) -> b
         return False
 
     # open-telemetry-automatic
-    if scenario_name == "OTEL_INTEGRATIONS":
+    if scenario.name == "OTEL_INTEGRATIONS":
         possible_values: tuple = (
             ("java_otel", "spring-boot-otel"),
             ("nodejs_otel", "express4-otel"),
@@ -529,11 +528,11 @@ def _is_supported(weblog: Weblog, scenario: Scenario, _ci_environment: str) -> b
             return False
 
     # open-telemetry-manual
-    if scenario_name in ("OTEL_LOG_E2E", "OTEL_METRIC_E2E", "OTEL_TRACING_E2E"):
+    if scenario.name in ("OTEL_LOG_E2E", "OTEL_METRIC_E2E", "OTEL_TRACING_E2E"):
         if (library, weblog_name) != ("java_otel", "spring-boot-native"):
             return False
 
-    if scenario_name in ("GRAPHQL_APPSEC",):
+    if scenario.name in ("GRAPHQL_APPSEC", "GRAPHQL_ERROR_TRACKING"):
         possible_values: tuple = (
             ("golang", "gqlgen"),
             ("golang", "graph-gophers"),
@@ -547,34 +546,34 @@ def _is_supported(weblog: Weblog, scenario: Scenario, _ci_environment: str) -> b
         if (library, weblog_name) not in possible_values:
             return False
 
-    if scenario_name in ("PERFORMANCES",):
+    if scenario.name in ("PERFORMANCES",):
         return False
 
-    if scenario_name == "IPV6":
+    if scenario.name == "IPV6":
         if library == "ruby" or _is_uds_weblog(weblog_name):
             return False
 
-    if scenario_name in ("CROSSED_TRACING_LIBRARIES",):
+    if scenario.name in ("CROSSED_TRACING_LIBRARIES",):
         if weblog_name in ("python3.12", "django-py3.13", "spring-boot-payara"):
             # python 3.13 issue : APMAPI-1096
             return False
 
-    if scenario_name in ("APPSEC_MISSING_RULES", "APPSEC_CORRUPTED_RULES") and library in ("cpp_nginx", "cpp_httpd"):
+    if scenario.name in ("APPSEC_MISSING_RULES", "APPSEC_CORRUPTED_RULES") and library in ("cpp_nginx", "cpp_httpd"):
         # C++ 1.2.0 freeze when the rules file is missing
         return False
 
     if weblog_name in ["gqlgen", "graph-gophers", "graphql-go", "graphql23"]:
-        if scenario_name not in ("GRAPHQL_APPSEC",):
+        if scenario.name not in ("GRAPHQL_APPSEC",):
             return False
 
     # open-telemetry-manual
     if weblog_name == "spring-boot-native":
-        if scenario_name not in ("OTEL_LOG_E2E", "OTEL_METRIC_E2E", "OTEL_TRACING_E2E"):
+        if scenario.name not in ("OTEL_LOG_E2E", "OTEL_METRIC_E2E", "OTEL_TRACING_E2E"):
             return False
 
     # open-telemetry-automatic
     if weblog_name in ["express4-otel", "flask-poc-otel", "spring-boot-otel"]:
-        if scenario_name not in ("OTEL_INTEGRATIONS",):
+        if scenario.name not in ("OTEL_INTEGRATIONS",):
             return False
 
     # Go proxies
@@ -583,101 +582,42 @@ def _is_supported(weblog: Weblog, scenario: Scenario, _ci_environment: str) -> b
             return False
 
     # otel collector
-    if weblog_name == "otel_collector" or scenario_name in ("OTEL_COLLECTOR", "OTEL_COLLECTOR_E2E"):
-        return weblog_name == "otel_collector" and scenario_name in ("OTEL_COLLECTOR", "OTEL_COLLECTOR_E2E")
+    if weblog_name == "otel_collector" or scenario.name in ("OTEL_COLLECTOR", "OTEL_COLLECTOR_E2E"):
+        return weblog_name == "otel_collector" and scenario.name in ("OTEL_COLLECTOR", "OTEL_COLLECTOR_E2E")
 
-    if "@" in weblog_name or scenario_name == "INTEGRATION_FRAMEWORKS":
-        return "@" in weblog_name and scenario_name == "INTEGRATION_FRAMEWORKS"
+    if "@" in weblog_name or scenario.name == "INTEGRATION_FRAMEWORKS":
+        return "@" in weblog_name and scenario.name == "INTEGRATION_FRAMEWORKS"
 
     return True
 
 
 if __name__ == "__main__":
+    from utils._context._scenarios import scenarios
+
     m = {
         "endtoend": [
-            "AGENT_NOT_SUPPORTING_SPAN_EVENTS",
-            "APM_TRACING_E2E_OTEL",
-            "APM_TRACING_OTLP",
-            "APM_TRACING_E2E_SINGLE_SPAN",
-            "APPSEC_API_SECURITY",
-            "APPSEC_API_SECURITY_NO_RESPONSE_BODY",
-            "APPSEC_API_SECURITY_RC",
-            "APPSEC_API_SECURITY_WITH_SAMPLING",
-            "APPSEC_AUTO_EVENTS_EXTENDED",
-            "APPSEC_AUTO_EVENTS_RC",
-            "APPSEC_BLOCKING",
-            "APPSEC_BLOCKING_FULL_DENYLIST",
-            "APPSEC_CORRUPTED_RULES",
-            "APPSEC_CUSTOM_OBFUSCATION",
-            "APPSEC_CUSTOM_RULES",
-            "APPSEC_LOW_WAF_TIMEOUT",
-            "APPSEC_META_STRUCT_DISABLED",
-            "APPSEC_MISSING_RULES",
-            "APPSEC_RASP",
-            "APPSEC_RASP_NON_BLOCKING",
-            "APPSEC_RATE_LIMITER",
-            "APPSEC_RULES_MONITORING_WITH_ERRORS",
-            "APPSEC_RUNTIME_ACTIVATION",
-            "APPSEC_STANDALONE",
-            "APPSEC_STANDALONE_V2",
-            "APPSEC_WAF_TELEMETRY",
-            "CROSSED_TRACING_LIBRARIES",
-            "DEBUGGER_EXCEPTION_REPLAY",
-            "DEBUGGER_EXPRESSION_LANGUAGE",
-            "DEBUGGER_INPRODUCT_ENABLEMENT",
-            "DEBUGGER_PII_REDACTION",
-            "DEBUGGER_PROBES_SNAPSHOT",
-            "DEBUGGER_PROBES_STATUS",
-            "DEBUGGER_SYMDB",
-            "DEFAULT",
-            "EVERYTHING_DISABLED",
-            "IAST_DEDUPLICATION",
-            "IAST_STANDALONE",
-            "IAST_STANDALONE_V2",
-            "INTEGRATIONS",
-            "INTEGRATIONS_AWS",
-            "IPV6",
-            "LIBRARY_CONF_CUSTOM_HEADER_TAGS",
-            "LIBRARY_CONF_CUSTOM_HEADER_TAGS_INVALID",
-            "OTLP_RUNTIME_METRICS",
-            "PERFORMANCES",
-            "PROFILING",
-            "REMOTE_CONFIG_MOCKED_BACKEND_ASM_DD",
-            "REMOTE_CONFIG_MOCKED_BACKEND_ASM_FEATURES",
-            "REMOTE_CONFIG_MOCKED_BACKEND_ASM_FEATURES_NOCACHE",
-            "REMOTE_CONFIG_MOCKED_BACKEND_LIVE_DEBUGGING",
-            "RUNTIME_METRICS_ENABLED",
-            "SAMPLING",
-            "SAMPLING_RATE_CAPPING",
-            "SCA_STANDALONE",
-            "SCA_STANDALONE_V2",
-            "TELEMETRY_APP_STARTED_PRODUCTS_DISABLED",
-            "TELEMETRY_DEPENDENCY_LOADED_TEST_FOR_DEPENDENCY_COLLECTION_DISABLED",
-            "TELEMETRY_LOG_GENERATION_DISABLED",
-            "TELEMETRY_METRIC_GENERATION_DISABLED",
-            "TRACE_PROPAGATION_STYLE_W3C",
-            "TRACING_CONFIG_EMPTY",
-            "TRACING_CONFIG_NONDEFAULT",
-            "TRACING_CONFIG_NONDEFAULT_2",
-            "TRACING_CONFIG_NONDEFAULT_3",
+            scenarios.agent_not_supporting_span_events,
+            scenarios.apm_tracing_e2e_otel,
+            scenarios.apm_tracing_otlp,
+            scenarios.apm_tracing_e2e_single_span,
         ],
         "aws_ssi": [],
-        "dockerssi": ["DOCKER_SSI"],
-        "graphql": ["GRAPHQL_APPSEC"],
+        "dockerssi": [scenarios.docker_ssi],
+        "graphql": [scenarios.graphql_appsec],
         "libinjection": [
-            "K8S_LIB_INJECTION",
-            "K8S_LIB_INJECTION_NO_AC",
-            "K8S_LIB_INJECTION_NO_AC_UDS",
-            "K8S_LIB_INJECTION_OPERATOR",
-            "K8S_LIB_INJECTION_PROFILING_DISABLED",
-            "K8S_LIB_INJECTION_PROFILING_ENABLED",
-            "K8S_LIB_INJECTION_PROFILING_OVERRIDE",
-            "K8S_LIB_INJECTION_SPARK_DJM",
-            "K8S_LIB_INJECTION_UDS",
+            scenarios.k8s_lib_injection,
+            scenarios.k8s_lib_injection_no_ac,
+            scenarios.k8s_lib_injection_no_ac_uds,
+            scenarios.k8s_lib_injection_operator,
+            scenarios.k8s_lib_injection_profiling_disabled,
+            scenarios.k8s_lib_injection_profiling_enabled,
+            scenarios.k8s_lib_injection_profiling_override,
+            scenarios.k8s_lib_injection_spark_djm,
+            scenarios.k8s_lib_injection_uds,
         ],
         "testthetest": [],
-        "opentelemetry": ["OTEL_INTEGRATIONS", "OTEL_LOG_E2E", "OTEL_METRIC_E2E", "OTEL_TRACING_E2E"],
-        "parametric": ["PARAMETRIC"],
+        "opentelemetry": [scenarios.otel_integrations],
+        "parametric": [scenarios.parametric],
     }
 
     get_endtoend_definitions(
@@ -691,16 +631,16 @@ if __name__ == "__main__":
         unique_id="000",
     )
 
-# if __name__ == "__main__":
-#     from utils._context._scenarios import get_all_scenarios
+if __name__ == "__main__":
+    from utils._context._scenarios import get_all_scenarios
 
-#     library = "python"
-#     for weblog in Weblog.load(library):
-#         for scenario in get_all_scenarios():
-#             if scenario.github_workflow != "endtoend":
-#                 continue
-#             groups = [group.name for group in scenario.scenario_groups]
-#             legacy = _is_supported(weblog, scenario, "")
-#             new_value = weblog.support_scenario(scenario.name, groups)
-#             if legacy is not new_value:
-#                 print((legacy, new_value, weblog.name, scenario.name, groups))
+    library = "python"
+    for weblog in Weblog.load(library):
+        for scenario in get_all_scenarios():
+            if scenario.github_workflow != "endtoend":
+                continue
+            groups = [group.name for group in scenario.scenario_groups]
+            legacy = _is_supported(weblog, scenario, "")
+            new_value = weblog.support_scenario(scenario.name, groups)
+            if legacy is not new_value:
+                print((legacy, new_value, weblog.name, scenario.name, groups))
