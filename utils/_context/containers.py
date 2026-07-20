aeda4105cbc6154c5101ea6c4357cdbf073272c2
Roberto Montero <108007532+robertomonteromiguel@users.noreply.github.com>
2026-07-20 17:05:01 +0200

    Docker SSI: roolback apm test agent (#7343)

--- utils/_context/containers.py
+++ utils/_context/containers.py
@@ -1473,7 +1473,7 @@ class OpenTelemetryCollectorContainer(TestedContainer):
 class APMTestAgentContainer(TestedContainer):
     def __init__(self, agent_port: int = 8126) -> None:
         super().__init__(
-            image_name="ghcr.io/datadog/dd-apm-test-agent/ddapm-test-agent:v1.63.0",
+            image_name="ghcr.io/datadog/dd-apm-test-agent/ddapm-test-agent:v1.31.1",
             name="ddapm-test-agent",
             environment={
                 "SNAPSHOT_CI": "0",
