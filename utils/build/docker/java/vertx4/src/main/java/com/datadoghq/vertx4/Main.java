6a0dc9fba56ad576775309524cb9331d8c94ef9a
Benjamin De Bernardi <benjamin.debernardi@datadoghq.com>
2026-07-29 09:42:57 +0200

    feat(java): add /trace/manual_keep_drop endpoint to vertx4

--- utils/build/docker/java/vertx4/src/main/java/com/datadoghq/vertx4/Main.java
+++ utils/build/docker/java/vertx4/src/main/java/com/datadoghq/vertx4/Main.java
@@ -12,6 +12,7 @@ import com.datadoghq.vertx4.iast.routes.IastSamplingRouteProvider;
 import com.datadoghq.vertx4.rasp.RaspRouteProvider;
 import datadog.appsec.api.blocking.Blocking;
 import datadog.appsec.api.login.EventTrackerV2;
+import datadog.trace.api.DDTags;
 import datadog.trace.api.EventTracker;
 import datadog.trace.api.interceptor.MutableSpan;
 import io.opentracing.Span;
@@ -402,6 +403,21 @@ public class Main {
                     setRootSpanTag("service", serviceName);
                     ctx.response().end("ok");
                 });
+
+        router.get("/trace/manual_keep_drop").handler(ctx -> {
+            String decision = ctx.request().getParam("decision");
+            if (!"keep".equals(decision) && !"drop".equals(decision)) {
+                ctx.response().setStatusCode(400).end("decision must be keep or drop");
+                return;
+            }
+
+            final Span span = GlobalTracer.get().activeSpan();
+            if (span != null) {
+                span.setTag("keep".equals(decision) ? DDTags.MANUAL_KEEP : DDTags.MANUAL_DROP, true);
+            }
+
+            ctx.response().end("OK");
+        });
 
         router.get("/make_distant_call").handler(ctx -> {
             String url = ctx.request().getParam("url");
