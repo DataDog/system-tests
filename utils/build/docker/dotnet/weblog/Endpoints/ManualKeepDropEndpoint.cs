58563147d43d39bdbe02b72c0c28dfd2bf8cd64c
Benjamin De Bernardi <benjamin.debernardi@datadoghq.com>
2026-07-30 11:19:24 +0200

    feat(dotnet): call downstream after manual keep/drop

--- utils/build/docker/dotnet/weblog/Endpoints/ManualKeepDropEndpoint.cs
+++ utils/build/docker/dotnet/weblog/Endpoints/ManualKeepDropEndpoint.cs
@@ -1,11 +1,28 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
 using Datadog.Trace;
 
 namespace weblog
 {
     public class ManualKeepDropEndpoint : ISystemTestEndpoint
     {
+        private const string DownstreamUrl = "http://localhost:7777/";
+
+        private class EndpointResponse
+        {
+            [JsonPropertyName("url")]
+            public string? Url { get; set; }
+            [JsonPropertyName("status_code")]
+            public int StatusCode { get; set; }
+            [JsonPropertyName("request_headers")]
+            public Dictionary<string, string>? RequestHeaders { get; set; }
+            [JsonPropertyName("response_headers")]
+            public Dictionary<string, string>? ResponseHeaders { get; set; }
+        }
+
         public void Register(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder routeBuilder)
         {
             routeBuilder.MapGet("/trace/manual_keep_drop", async context =>
@@ -21,7 +38,17 @@ namespace weblog
                 var span = Tracer.Instance.ActiveScope?.Span;
                 span?.SetTag(decision == "keep" ? Tags.ManualKeep : Tags.ManualDrop, "true");
 
-                await context.Response.WriteAsync("OK\\n");
+                // Call downstream so that tests can assert on the sampling decision that gets propagated
+                var response = await HttpClientWrapper.LocalGetRequest(DownstreamUrl);
+                var endpointResponse = new EndpointResponse()
+                {
+                    Url = DownstreamUrl,
+                    StatusCode = (int)response.StatusCode,
+                    RequestHeaders = response.RequestMessage?.Headers.Select(kvp => new KeyValuePair<string, string>(kvp.Key, kvp.Value.First())).ToDictionary(),
+                    ResponseHeaders = response.Headers.Select(kvp => new KeyValuePair<string, string>(kvp.Key, kvp.Value.First())).ToDictionary(),
+                };
+
+                await context.Response.WriteAsJsonAsync(endpointResponse);
             });
         }
     }
