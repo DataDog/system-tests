eb72bdf55a97df93b2041828eef35b199417b6a3
Benjamin De Bernardi <benjamin.debernardi@datadoghq.com>
2026-07-30 11:18:24 +0200

    feat(nodejs): call downstream after manual keep/drop in nextjs

--- utils/build/docker/nodejs/nextjs/src/app/trace/manual_keep_drop/route.js
+++ utils/build/docker/nodejs/nextjs/src/app/trace/manual_keep_drop/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+import http from 'http'
 
 export const dynamic = 'force-dynamic'
 
@@ -12,5 +13,27 @@ export async function GET (request) {
   const tracer = global._ddtrace
   tracer?.scope().active()?.setTag(decision === 'keep' ? 'manual.keep' : 'manual.drop', true)
 
-  return new NextResponse('OK')
+  // Call downstream so that tests can assert on the sampling decision that gets propagated
+  const url = 'http://localhost:7777/'
+  return new Promise((resolve) => {
+    const downstream = http.request({ hostname: 'localhost', port: 7777, path: '/', method: 'GET' }, (response) => {
+      response.on('data', () => {})
+
+      response.on('end', () => {
+        resolve(NextResponse.json({
+          url,
+          status_code: response.statusCode,
+          request_headers: response.req.getHeaders(),
+          response_headers: response.headers
+        }))
+      })
+    })
+
+    downstream.on('error', (error) => {
+      console.log(error)
+      resolve(new NextResponse(error.message, { status: 500 }))
+    })
+
+    downstream.end()
+  })
 }
