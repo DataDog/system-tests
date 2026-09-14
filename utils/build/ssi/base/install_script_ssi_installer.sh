d012eebbfbe7f2eb5529b93e058f285aee45a821
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-09-14 13:30:36 +0000

    APMSP-3973 Make installer failure explicit
    Co-authored-by: nccatoni <222672590+nccatoni@users.noreply.github.com>

--- utils/build/ssi/base/install_script_ssi_installer.sh
+++ utils/build/ssi/base/install_script_ssi_installer.sh
@@ -5,11 +5,14 @@ source ./download_with_retry.sh
 
 download_with_retry https://dd-agent.s3.amazonaws.com/scripts/install_script_agent7.sh || exit 1
 
-run_with_retry \
+if ! run_with_retry \
     "Datadog Agent installer" \
     3 \
     5 \
     env \
     DD_INSTALL_ONLY=true \
     DD_INSTALLER=true \
-    bash ./install_script_agent7.sh || exit 1
+    bash ./install_script_agent7.sh; then
+    echo "[ERROR] aborting SSI install after Datadog Agent installer failure" >&2
+    exit 1
+fi
