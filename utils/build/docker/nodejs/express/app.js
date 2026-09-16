1de1d0f7f6928ad8d5bafbf2e35415e5ee7886d1
Roch Devost <roch.devost@datadoghq.com>
2026-09-15 20:03:02 -0400

    test(nodejs): avoid rebuilding express base images

--- utils/build/docker/nodejs/express/app.js
+++ utils/build/docker/nodejs/express/app.js
@@ -641,64 +641,6 @@ app.get('/otel_drop_in_default_propagator_inject', (req, res) => {
   res.json(result)
 })
 
-app.get('/otel_drop_in_baggage_api_datadog', (req, res) => {
-  const {
-    setBaggageItem,
-    removeBaggageItem
-  } = require('dd-trace/packages/dd-trace/src/baggage')
-
-  const url = req.query.url
-  const parsedUrl = new URL(url)
-  const options = {
-    hostname: parsedUrl.hostname,
-    port: parsedUrl.port || 80,
-    path: parsedUrl.pathname,
-    method: 'GET'
-  }
-
-  const baggageRemove = req.query.baggage_remove
-  const baggageSet = req.query.baggage_set
-  const baggageToRemove = baggageRemove ? baggageRemove.split(',') : []
-  const baggageToSet = baggageSet
-    ? baggageSet.split(',').map(item => item.split('='))
-    : []
-
-  for (const key of baggageToRemove) {
-    removeBaggageItem(key.trim())
-  }
-  for (const [key, value] of baggageToSet) {
-    setBaggageItem(key.trim(), value.trim())
-  }
-
-  const httpRequest = http.request(options, (response) => {
-    let responseBody = ''
-    response.on('data', (chunk) => {
-      responseBody += chunk
-    })
-    response.on('end', () => {
-      res.json({
-        url,
-        status_code: response.statusCode,
-        request_headers: response.req._headers,
-        response_headers: response.headers,
-        response_body: responseBody
-      })
-    })
-  })
-
-  httpRequest.on('error', (error) => {
-    console.log(error)
-    res.json({
-      url,
-      status_code: 500,
-      request_headers: null,
-      response_headers: null
-    })
-  })
-
-  httpRequest.end()
-})
-
 app.post('/shell_execution', (req, res) => {
   const options = { shell: !!req?.body?.options?.shell }
   const reqArgs = req?.body?.args
